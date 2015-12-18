0b65e7b6dff16e7265c86fd863c1ab2bfe5e6b01
2youyouo2 <501251991@qq.com>
2015-12-18 18:30:17 +0800

    use enabledInHierarchy

--- cocos2d/core/components/CCButton.js
+++ cocos2d/core/components/CCButton.js
@@ -413,7 +413,7 @@ var Button = cc.Class({
 
     // touch event handler
     _onTouchBegan: function (touch) {
-        if (!this.interactable || !this.node.active) return false;
+        if (!this.interactable || !this.enabledInHierarchy) return false;
 
         var hit = this._hitTest(touch.getLocation());
         if (hit) {
@@ -436,7 +436,7 @@ var Button = cc.Class({
     },
 
     _onMouseMove: function (event) {
-        if (this._pressed || !this.interactable || !this.node.active) return;
+        if (this._pressed || !this.interactable || !this.enabledInHierarchy) return;
 
         var hit = this._hitTest(event.getLocation());
         if (hit) {
