176925da48852022f3377823c62aff509b19681c
pandamicro <lphuabin@gmail.com>
2016-01-14 10:17:26 +0800

    Improve WebGL support detection

--- cocos2d/core/platform/CCSys.js
+++ cocos2d/core/platform/CCSys.js
@@ -641,14 +641,17 @@ else {
                 _supportWebGL = true;
             }
 
-            // Accept only Android 5+ and QQ Brwoser 6.2+
+            // Accept only Android 5+ default browser and QQ Brwoser 6.2+
             if (sys.os === sys.OS_ANDROID) {
-                if (!sys.osMainVersion || sys.osMainVersion < 5) {
-                    _supportWebGL = false;
-                }
+                _supportWebGL = false;
+                // QQ Brwoser 6.2+
                 var browserVer = parseFloat(sys.browserVersion);
-                if (sys.browserType !== sys.BROWSER_TYPE_MOBILE_QQ || browserVer < 6.2) {
-                    _supportWebGL = false;
+                if (sys.browserType === sys.BROWSER_TYPE_MOBILE_QQ && browserVer >= 6.2) {
+                    _supportWebGL = true;
+                }
+                // Android 5+ default browser
+                else if (sys.osMainVersion && sys.osMainVersion >= 5 && sys.browserType === sys.BROWSER_TYPE_ANDROID) {
+                    _supportWebGL = true;
                 }
             }
         }
