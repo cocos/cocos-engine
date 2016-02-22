a206e7f6baed548d593951c9cfa0ca9f2218cd46
Huabing.Xu <dabingnn@gmail.com>
2016-02-22 11:00:43 +0800

    add test cases for scale9 sprite

--- test/qunit/unit/test-spriteRenderer.js
+++ test/qunit/unit/test-spriteRenderer.js
@@ -38,7 +38,7 @@ asyncTest('simple spriteQuad trimmed', function () {
         s9Sprite.setRenderingType(cc.Scale9Sprite.RenderingType.SIMPLE);
         s9Sprite._rebuildQuads();
         var quads = s9Sprite._quads;
-        deepEqual(quads.length, 1, 'quads number success');
+        deepEqual(quads.length, 1, 'quads number[1] success');
         deepEqual(quads[0]._bl.vertices.x,0, 'vertex-bl-x test success');
         deepEqual(quads[0]._bl.vertices.y,0, 'vertex-bl-y test success');
 
@@ -80,7 +80,7 @@ asyncTest('simple spriteQuad no trimmed', function () {
         s9Sprite.setRenderingType(cc.Scale9Sprite.RenderingType.SIMPLE);
         s9Sprite._rebuildQuads();
         var quads = s9Sprite._quads;
-        deepEqual(quads.length, 1, 'quads number success');
+        deepEqual(quads.length, 1, 'quads number[1] success');
         deepEqual(quads[0]._bl.vertices.x, rect.x, 'vertex-bl-x test success');
         deepEqual(quads[0]._bl.vertices.y, contentSize.height - rect.y - rect.height, 'vertex-bl-y test success');
 
@@ -96,6 +96,75 @@ asyncTest('simple spriteQuad no trimmed', function () {
 
         deepEqual(Math.abs(u2) < EPSILON, true, 'texCoords-tr-x test success');
         deepEqual(Math.abs(v2) < EPSILON, true, 'texCoords-tr-y test success');
+
+        ok(true);
+        start();
+    },500);
+
+});
+
+asyncTest('scale9 spriteQuad trimmed', function () {
+    var url = assetDir + '/button.png';
+    var texture = cc.textureCache.addImage(url);
+    setTimeout(function() {
+        var spriteFrame = new cc.SpriteFrame();
+        var rect = cc.rect(10,10,40,60);
+        var offset = cc.v2(-20,10);
+        var originalSize = cc.size(texture.getPixelWidth(), texture.getPixelHeight());
+        spriteFrame.initWithTexture(texture, rect,false, offset, originalSize);
+        var contentSize = cc.size(originalSize);
+        var color = cc.color(255,128,0,255);
+        var isTrimmed = false;
+        var s9Sprite = new cc.Scale9Sprite(spriteFrame);
+        //set border
+        s9Sprite.setInsetLeft(5);
+        s9Sprite.setInsetRight(6);
+        s9Sprite.setInsetBottom(7);
+        s9Sprite.setInsetTop(8);
+
+        s9Sprite.setContentSize(contentSize);
+        s9Sprite.setColor(color);
+        s9Sprite.enableTrimmedContentSize(isTrimmed);
+        s9Sprite.setRenderingType(cc.Scale9Sprite.RenderingType.SLICED);
+        s9Sprite._rebuildQuads();
+        var quads = s9Sprite._quads;
+        var EPSILON = 0.00001;
+        deepEqual(quads.length, 9, 'quads number[9] success');
+        deepEqual(quads[0]._bl.vertices.x, 0, 'x0 test success');
+        deepEqual(quads[0]._bl.vertices.y, 0, 'y0 test success');
+
+        deepEqual(quads[4]._bl.vertices.x, 5, 'x1 test success');
+        deepEqual(quads[4]._bl.vertices.y, 7, 'y1 test success');
+
+        deepEqual(quads[8]._bl.vertices.x, contentSize.width - 6, 'x2 test success');
+        deepEqual(quads[8]._bl.vertices.y, contentSize.height - 8, 'y2 test success');
+
+        deepEqual(quads[8]._tr.vertices.x, contentSize.width, 'x3 test success');
+        deepEqual(quads[8]._tr.vertices.y, contentSize.height, 'y3 test success');
+
+        var u0 = Math.abs(quads[0]._bl.texCoords.u - 10/100);
+        var v0 = Math.abs(quads[0]._bl.texCoords.v - 70/100);
+
+        var u1 = Math.abs(quads[4]._bl.texCoords.u - 15/100);
+        var v1 = Math.abs(quads[4]._bl.texCoords.v - 63/100);
+
+        var u2 = Math.abs(quads[8]._bl.texCoords.u - 44/100);
+        var v2 = Math.abs(quads[8]._bl.texCoords.v - 18/100);
+
+        var u3 = Math.abs(quads[8]._tr.texCoords.u - 50/100);
+        var v3 = Math.abs(quads[8]._tr.texCoords.v - 10/100);
+
+        deepEqual(Math.abs(u0) < EPSILON, true, 'u0 test success');
+        deepEqual(Math.abs(v0) < EPSILON, true, 'v0 test success');
+
+        deepEqual(Math.abs(u1) < EPSILON, true, 'u1 test success');
+        deepEqual(Math.abs(v1) < EPSILON, true, 'v1 test success');
+
+        deepEqual(Math.abs(u2) < EPSILON, true, 'u2 test success');
+        deepEqual(Math.abs(v2) < EPSILON, true, 'v2 test success');
+
+        deepEqual(Math.abs(u3) < EPSILON, true, 'u3 test success');
+        deepEqual(Math.abs(v3) < EPSILON, true, 'v3 test success');
 
         ok(true);
         start();
