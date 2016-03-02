b6a54cb215bf2694a287a5841e1435a755671bee
Jare Guo <jare85@163.com>
2016-03-02 11:25:53 +0800

    remove angle brackets support from cc.find

--- cocos2d/core/utils/find.js
+++ cocos2d/core/utils/find.js
@@ -56,28 +56,11 @@ cc.find = module.exports = function (path, referenceNode) {
     // parse path
     for (var n = startIndex; n < nameList.length; n++) {
         var name = nameList[n];
-        var findByComp = name[0] === '<' && name[name.length - 1] === '>';
-        var Comp;
-        if (findByComp) {
-            var compName = name.slice(1, -1);
-            Comp = cc.js.getClassByName(compName);
-            if (!Comp) {
-                cc.warn('Failed to find component ' + compName);
-                return null;
-            }
-        }
-        // visit sub nodes
         var children = match._children;
         match = null;
         for (var t = 0, len = children.length; t < len; ++t) {
             var subChild = children[t];
-            if (findByComp) {
-                if (subChild.getComponent(Comp)) {
-                    match = subChild;
-                    break;
-                }
-            }
-            else if (subChild.name === name) {
+            if (subChild.name === name) {
                 match = subChild;
                 break;
             }
