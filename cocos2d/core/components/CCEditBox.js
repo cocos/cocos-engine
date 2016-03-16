963dad915281290e698aeac16ce841a62de91afb
zilongshanren <guanghui8827@126.com>
2016-03-16 17:04:11 +0800

    we should update editbox size first.

--- cocos2d/core/components/CCEditBox.js
+++ cocos2d/core/components/CCEditBox.js
@@ -276,6 +276,9 @@ var EditBox = cc.Class({
 
         this._createBackgroundSprite();
 
+        if (!this._useOriginalSize) {
+            sgNode.setContentSize(this.node.getContentSize());
+        }
 
         sgNode.inputMode = this.inputMode;
         sgNode.maxLength = this.maxLength;
@@ -290,9 +293,6 @@ var EditBox = cc.Class({
         sgNode.returnType = this.returnType;
         sgNode.setLineHeight(this.lineHeight);
 
-        if (!this._useOriginalSize) {
-            sgNode.setContentSize(this.node.getContentSize());
-        }
 
         sgNode.setDelegate(this);
     },
