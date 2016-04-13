37d1fb98d452a9f3aafaad96399200dd9932819a
Huabing.Xu <dabingnn@gmail.com>
2016-04-13 18:54:19 +0800

    add ellipse mask (#595)
    * add ellipse mask
    
    * clean up code and add comments
    
    * adjust comments

--- cocos2d/core/components/CCMask.js
+++ cocos2d/core/components/CCMask.js
@@ -27,6 +27,28 @@
  * @class Mask
  * @extends _RendererInSG
  */
+
+/**
+ * !#en the type for mask.
+ * !#zh 遮罩组件的类型
+ * @enum Mask.Type
+ */
+
+var MaskType = cc.Enum({
+    /**
+     * !#en Rect mask.
+     * !#zh 使用矩形作为遮罩
+     * @property {Number} RECT
+     */
+    RECT: 0,
+    /**
+     * !#en Ellipse Mask.
+     * !#zh 使用椭圆作为遮罩
+     * @property {Number} ELLIPSE
+     */
+    ELLIPSE: 1
+});
+
 var Mask = cc.Class({
     name: 'cc.Mask',
     extends: cc._RendererInSG,
@@ -41,6 +63,51 @@ var Mask = cc.Class({
             default: null,
             serializable: false,
         },
+
+        _type:0,
+        _segements: 64,
+
+        /**
+         * !#en The mask type.
+         * !#zh 遮罩类型
+         * @property type
+         * @type {MaskType}
+         * @example
+         * mask.type = MaskType.RECT;
+         */
+        type: {
+            get: function() {
+                return this._type;
+            },
+            set: function(value) {
+                this._type = value;
+                this._refreshStencil();
+            },
+            type: MaskType
+        },
+
+        /**
+         * !#en The segements for ellipse mask.
+         * !#zh 椭圆遮罩的曲线细分数
+         * @property segements
+         * @type {Number}
+         * @default 64
+         */
+        segements: {
+            get: function() {
+                return this._segements;
+            },
+            set: function(value) {
+                if(value < 3) value = 3;
+                this._segements = value;
+                this._refreshStencil();
+            },
+            type: cc.Integer
+        },
+    },
+
+    statics: {
+        Type: MaskType,
     },
 
     _createSgNode: function () {
@@ -80,6 +147,17 @@ var Mask = cc.Class({
         this._clippingStencil.release();
     },
 
+    _calculateCircle: function(center, radius, segements) {
+        var polies =[];
+        var anglePerStep = Math.PI * 2 / segements;
+        for(var step = 0; step < segements; ++ step) {
+            polies.push(cc.v2(radius.x * Math.cos(anglePerStep * step) + center.x,
+                radius.y * Math.sin(anglePerStep * step) + center.y));
+        }
+
+        return polies;
+    },
+
     _refreshStencil: function () {
         var contentSize = this.node.getContentSize();
         var anchorPoint = this.node.getAnchorPoint();
@@ -87,13 +165,20 @@ var Mask = cc.Class({
         var height = contentSize.height;
         var x = - width * anchorPoint.x;
         var y = - height * anchorPoint.y;
-        var rectangle = [ cc.v2(x, y),
-                          cc.v2(x + width, y),
-                          cc.v2(x + width, y + height),
-                          cc.v2(x, y + height) ];
         var color = cc.color(255, 255, 255, 0);
         this._clippingStencil.clear();
-        this._clippingStencil.drawPoly(rectangle, color, 0, color);
+        if(this._type === MaskType.RECT) {
+            var rectangle = [ cc.v2(x, y),
+                cc.v2(x + width, y),
+                cc.v2(x + width, y + height),
+                cc.v2(x, y + height) ];
+            this._clippingStencil.drawPoly(rectangle, color, 0, color);
+        } else {
+            var center = cc.v2(x + width /2, y+height/2);
+            var radius = {x: width/2, y: height/2};
+            var segements = this._segements;
+            this._clippingStencil.drawPoly(this._calculateCircle(center,radius, segements), color, 0, color);
+        }
     }
 });
 
