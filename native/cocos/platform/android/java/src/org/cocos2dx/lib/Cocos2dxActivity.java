19e9c085e043f64be8e57973ffe75def69fdb3a7
Wenhai Lin <wenhai.lin@chukong-inc.com>
2016-04-18 23:49:35 +0800

    Bug fix:1.Set music volume control as default 2.Fix calculation of base64Encode outLength

--- native/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ native/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
+import android.media.AudioManager;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
@@ -266,6 +267,7 @@ public abstract class Cocos2dxActivity extends Activity {
         Cocos2dxWebViewHelper.init();
         Window window = getWindow();
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
 
     //native method,call GLViewImpl::getGLContextAttrs() to get the OpenGL ES context attributions
