a819423530e37d0431b77daaebf93558892934f2
2youyouo2 <501251991@qq.com>
2016-05-05 15:23:17 +0800

    add link template

---
+++ native/templates/js-template-link/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -0,0 +1,75 @@
+#include "AppDelegate.h"
+
+#include "SimpleAudioEngine.h"
+
+#include "js_module_register.h"
+
+USING_NS_CC;
+using namespace CocosDenshion;
+
+AppDelegate::AppDelegate()
+{
+}
+
+AppDelegate::~AppDelegate()
+{
+    ScriptEngineManager::destroyInstance();
+}
+
+void AppDelegate::initGLContextAttrs()
+{
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+    
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+    // initialize director
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+#if(CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+        glview = cocos2d::GLViewImpl::create("HelloJavascript");
+#else
+        glview = cocos2d::GLViewImpl::createWithRect("HelloJavascript", Rect(0,0,900,640));
+#endif
+        director->setOpenGLView(glview);
+    }
+    
+    // set FPS. the default value is 1.0/60 if you don't call this
+    director->setAnimationInterval(1.0 / 60);
+    
+    js_module_register();
+    
+    ScriptingCore* sc = ScriptingCore::getInstance();
+    sc->start();
+    sc->runScript("script/jsb_boot.js");
+#if defined(COCOS2D_DEBUG) && (COCOS2D_DEBUG > 0)
+    sc->enableDebugger();
+#endif
+    ScriptEngineManager::getInstance()->setScriptEngine(sc);
+    ScriptingCore::getInstance()->runScript("main.js");
+    
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground()
+{
+    auto director = Director::getInstance();
+    director->stopAnimation();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_hide");
+    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+    SimpleAudioEngine::getInstance()->pauseAllEffects();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    auto director = Director::getInstance();
+    director->startAnimation();
+    director->getEventDispatcher()->dispatchCustomEvent("game_on_show");
+    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+    SimpleAudioEngine::getInstance()->resumeAllEffects();
+}
