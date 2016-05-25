04b4ba3d25f0010f36a704c578ddfee0b0d7410a
zilongshanren <guanghui8827@126.com>
2016-05-25 17:03:32 +0800

    Add scrollview event api (#729)
    * add scrollview event type
    
    * make scroll event working
    
    * add bounce event
    
    * improve code

--- cocos2d/core/components/CCScrollView.js
+++ cocos2d/core/components/CCScrollView.js
@@ -39,6 +39,74 @@ var getTimeInMilliseconds = function() {
 };
 
 /**
+ * !#en Enum for ScrollView event type.
+ * !#zh 滚动视图事件类型
+ * @enum ScrollView.EventType
+ */
+var EventType = cc.Enum({
+    /**
+     * !#en The event emmitted when ScrollView scroll to the top boundary of inner container
+     * !#zh 滚动视图滚动到顶部边界事件
+     * @property {Number} SCROLL_TO_TOP
+     */
+    SCROLL_TO_TOP : 0,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the bottom boundary of inner container
+     * !#zh 滚动视图滚动到底部边界事件
+     * @property {Number} SCROLL_TO_BOTTOM
+     */
+    SCROLL_TO_BOTTOM : 1,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the left boundary of inner container
+     * !#zh 滚动视图滚动到左边界事件
+     * @property {Number} SCROLL_TO_LEFT
+     */
+    SCROLL_TO_LEFT : 2,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the right boundary of inner container
+     * !#zh 滚动视图滚动到右边界事件
+     * @property {Number} SCROLL_TO_RIGHT
+     */
+    SCROLL_TO_RIGHT : 3,
+    /**
+     * !#en The event emmitted when ScrollView is scrolling
+     * !#zh 滚动视图正在滚动时发出的事件
+     * @property {Number} SCROLLING
+     */
+    SCROLLING : 4,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the top boundary of inner container and start bounce
+     * !#zh 滚动视图滚动到顶部边界并且开始回弹时发出的事件
+     * @property {Number} BOUNCE_TOP
+     */
+    BOUNCE_TOP : 5,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the bottom boundary of inner container and start bounce
+     * !#zh 滚动视图滚动到底部边界并且开始回弹时发出的事件
+     * @property {Number} BOUNCE_BOTTOM
+     */
+    BOUNCE_BOTTOM : 6,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the left boundary of inner container and start bounce
+     * !#zh 滚动视图滚动到左边界并且开始回弹时发出的事件
+     * @property {Number} BOUNCE_LEFT
+     */
+    BOUNCE_LEFT : 7,
+    /**
+     * !#en The event emmitted when ScrollView scroll to the right boundary of inner container and start bounce
+     * !#zh 滚动视图滚动到右边界并且开始回弹时发出的事件
+     * @property {Number} BOUNCE_RIGHT
+     */
+    BOUNCE_RIGHT : 8,
+    /**
+     * !#en The event emmitted when ScrollView scroll ended
+     * !#zh 滚动视图滚动滚动结束的时候发出的事件
+     * @property {Number} AUTOSCROLL_ENDED
+     */
+    AUTOSCROLL_ENDED : 9
+});
+
+/**
  * !#en
  * Layout container for a view hierarchy that can be scrolled by the user,
  * allowing it to be larger than the physical display.
@@ -199,7 +267,21 @@ var ScrollView = cc.Class({
                 }
             },
             animatable: false
+        },
+
+        /**
+         * !#en Scrollview events callback
+         * !#zh 滚动视图的事件回调函数
+         * @property {Component.EventHandler[]} scrollEvents
+         */
+        scrollEvents: {
+            default: [],
+            type: cc.Component.EventHandler
         }
+    },
+
+    statics: {
+        EventType: EventType,
     },
 
     /**
@@ -497,7 +579,14 @@ var ScrollView = cc.Class({
 
         this._outOfBoundaryAmountDirty = true;
 
-        //TODO: process bouncing and container move event
+        if(this.elastic)
+        {
+            var outOfBoundary = this._getHowMuchOutOfBoundary();
+            if (outOfBoundary.y > 0) this._dispatchEvent(EventType.BOUNCE_TOP);
+            if (outOfBoundary.y < 0) this._dispatchEvent(EventType.BOUNCE_BOTTOM);
+            if (outOfBoundary.x > 0) this._dispatchEvent(EventType.BOUNCE_RIGHT);
+            if (outOfBoundary.x < 0) this._dispatchEvent(EventType.BOUNCE_LEFT);
+        }
     },
 
     /**
@@ -694,7 +783,46 @@ var ScrollView = cc.Class({
             realMove = cc.pAdd(realMove, outOfBoundary);
         }
 
+        var scrollEventType = '';
+
+        if (realMove.y > 0) { //up
+            var icBottomPos = this.content.y - this.content.anchorY * this.content.height;
+
+            if (icBottomPos + realMove.y > this._bottomBoundary) {
+                scrollEventType = EventType.SCROLL_TO_BOTTOM;
+            }
+        }
+        else if (realMove.y < 0) { //down
+            var icTopPos = this.content.y - this.content.anchorY * this.content.height + this.content.height;
+
+            if(icTopPos + realMove.y <= this._topBoundary) {
+                scrollEventType = EventType.SCROLL_TO_TOP;
+            }
+        }
+        else if (realMove.x < 0) { //left
+            var icRightPos = this.content.x - this.content.anchorX * this.content.width + this.content.width;
+            if (icRightPos + realMove.x <= this._rightBoundary) {
+                scrollEventType = EventType.SCROLL_TO_RIGHT;
+            }
+        }
+        else if (realMove.x > 0) { //right
+            var icLeftPos = this.content.x - this.content.anchorX * this.content.width;
+            if (icLeftPos + realMove.x >= this._leftBoundary) {
+                scrollEventType = EventType.SCROLL_TO_LEFT;
+            }
+        }
+
         this._moveContent(realMove, false);
+
+        if(realMove.x !== 0 || realMove.y !== 0)
+        {
+            this._dispatchEvent(EventType.SCROLLING);
+        }
+
+        if (scrollEventType) {
+            this._dispatchEvent(scrollEventType);
+        }
+
     },
 
     _handlePressLogic: function() {
@@ -829,6 +957,7 @@ var ScrollView = cc.Class({
 
         if (reachedEnd) {
             this._autoScrolling = false;
+            this._dispatchEvent(EventType.AUTOSCROLL_ENDED);
         }
 
         var contentPos = cc.pSub(newPosition, this.getContentPosition());
@@ -1025,6 +1154,10 @@ var ScrollView = cc.Class({
         if (this.verticalScrollBar) {
             this.verticalScrollBar._onTouchEnded();
         }
+    },
+
+    _dispatchEvent: function(event) {
+        cc.Component.EventHandler.emitEvents(this.scrollEvents, this, event);
     },
 
     //component life cycle methods
