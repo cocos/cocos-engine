45f70f758c79b817647b70fb60bb187b6962fc39
Nan Wang <nantas@gmail.com>
2016-05-27 16:02:15 +0800

    update engine version

--- CCBoot.js
+++ CCBoot.js
@@ -58,7 +58,7 @@ _p = null;
  * @name cc.ENGINE_VERSION
  */
 var engineVersion;
-engineVersion = '1.1.0-beta.6';
+engineVersion = '1.1.0';
 window['CocosEngine'] = cc.ENGINE_VERSION = engineVersion;
 
 /**
