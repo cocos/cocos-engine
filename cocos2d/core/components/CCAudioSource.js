39ca441ca395f253b83de3be046920fdbe15ecbd
knoxHuang <821686174@qq.com>
2016-06-08 10:35:11 +0800

    add CCAudioSource isPlaying api

--- cocos2d/core/components/CCAudioSource.js
+++ cocos2d/core/components/CCAudioSource.js
@@ -57,8 +57,12 @@ var AudioSource = cc.Class({
         _loop: false,
 
         /**
-         * !#en Is the audio source playing (Read Only).
-         * !#zh 该音频剪辑是否正播放（只读）。
+         * !#en
+         * Is the audio source playing (Read Only). <br/>
+         * Note: isPlaying is not supported for Native platforms.
+         * !#zh
+         * 该音频剪辑是否正播放（只读）。<br/>
+         * 注意：Native 平台暂时不支持 isPlaying。
          * @property isPlaying
          * @type {Boolean}
          * @readOnly
