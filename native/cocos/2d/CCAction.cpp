aa266594db81c2a33863e92fe69d79931f10fd36
knoxHuang <821686174@qq.com>
2016-06-29 15:21:51 +0800

    fix Follow native error, for #fireball/issues/3409

--- native/cocos/2d/CCAction.cpp
+++ native/cocos/2d/CCAction.cpp
@@ -58,6 +58,16 @@ Action::~Action()
     CCLOGINFO("deallocing Action: %p - tag: %i", this, _tag);
 }
 
+void Action::sendUpdateEventToScript(float dt, Action *actionObject)
+{
+#if CC_ENABLE_SCRIPT_BINDING
+	if (_scriptType == kScriptTypeJavascript)
+	{
+		ScriptEngineManager::sendActionEventToJS(actionObject, kActionUpdate, (void *)&dt);
+	}
+#endif
+}
+
 std::string Action::description() const
 {
     return StringUtils::format("<Action | Tag = %d", _tag);
@@ -271,6 +281,12 @@ void Follow::step(float dt)
 {
     CC_UNUSED_PARAM(dt);
 
+    Vec2 targetWorldPos = _target->convertToWorldSpaceAR(Vec2::ZERO);
+    Vec2 followedWorldPos = _followedNode->convertToWorldSpaceAR(Vec2::ZERO);
+    // compute the offset between followed and target node
+    Vec2 delta = targetWorldPos - followedWorldPos;
+    Vec2 tempPos = _target->getParent()->convertToNodeSpaceAR(delta + _halfScreenSize);
+
     if(_boundarySet)
     {
         // whole map fits inside a single screen, no need to modify the position - unless map boundaries are increased
@@ -278,16 +294,15 @@ void Follow::step(float dt)
         {
             return;
         }
-
-        Vec2 tempPos = _halfScreenSize - _followedNode->getPosition();
-
         _target->setPosition(clampf(tempPos.x, _leftBoundary, _rightBoundary),
                                    clampf(tempPos.y, _bottomBoundary, _topBoundary));
     }
     else
     {
-        _target->setPosition(_halfScreenSize - _followedNode->getPosition());
+        _target->setPosition(tempPos);
     }
+
+    sendUpdateEventToScript(dt, this);
 }
 
 bool Follow::isDone() const
