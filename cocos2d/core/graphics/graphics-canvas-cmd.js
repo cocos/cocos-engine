f1051664d1598f857764f9a408c91f93e6803302
2youyouo2 <501251991@qq.com>
2016-06-30 11:43:18 +0800

    add license

--- cocos2d/core/graphics/graphics-canvas-cmd.js
+++ cocos2d/core/graphics/graphics-canvas-cmd.js
@@ -21,6 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
+ 
 var Js = cc.js;
 
 var LineCap     = require('./types').LineCap;
