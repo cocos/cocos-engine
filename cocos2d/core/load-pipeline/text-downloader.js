5dcff33b98fe560eb50d8687c5664b2dfe2d632e
Jare Guo <jare85@163.com>
2016-06-30 21:52:14 +0800

    fix pack loader on JSB

--- cocos2d/core/load-pipeline/text-downloader.js
+++ cocos2d/core/load-pipeline/text-downloader.js
@@ -1,43 +1,58 @@
-var Pipeline = require('./pipeline');
-var urlAppendTimestamp = require('./utils').urlAppendTimestamp;
+if (CC_JSB) {
+    module.exports = function (item, callback) {
+        var url = item.url;
 
-module.exports = function (item, callback) {
-    var url = item.url,
-        xhr = Pipeline.getXMLHttpRequest(),
-        errInfo = 'Load ' + url + ' failed!',
-        navigator = window.navigator;
-
-    url = urlAppendTimestamp(url);
-
-    xhr.open('GET', url, true);
-    if (/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent)) {
-        // IE-specific logic here
-        xhr.setRequestHeader('Accept-Charset', 'utf-8');
-        xhr.onreadystatechange = function () {
-            if(xhr.readyState === 4) {
-                if (xhr.status === 200 || xhr.status === 0) {
-                    callback(null, xhr.responseText);
-                }
-                else {
-                    callback({status:xhr.status, errorMessage:errInfo});
-                }
-            }
-        };
-    } else {
-        if (xhr.overrideMimeType) xhr.overrideMimeType('text\/plain; charset=utf-8');
-        xhr.onload = function () {
-            if(xhr.readyState === 4) {
-                if (xhr.status === 200 || xhr.status === 0) {
-                    callback(null, xhr.responseText);
-                }
-                else {
-                    callback({status:xhr.status, errorMessage:errInfo});
-                }
-            }
-        };
-        xhr.onerror = function(){
-            callback({status:xhr.status, errorMessage:errInfo});
-        };
+        var result = jsb.fileUtils.getStringFromFile(url);
+        if (typeof result === 'string' && result) {
+            callback(null, result);
+        }
+        else {
+            callback(new Error('Download text failed: ' + url));
+        }
     }
-    xhr.send(null);
-};
+}
+else {
+    var Pipeline = require('./pipeline');
+    var urlAppendTimestamp = require('./utils').urlAppendTimestamp;
+
+    module.exports = function (item, callback) {
+        var url = item.url,
+            xhr = Pipeline.getXMLHttpRequest(),
+            errInfo = 'Load ' + url + ' failed!',
+            navigator = window.navigator;
+
+        url = urlAppendTimestamp(url);
+
+        xhr.open('GET', url, true);
+        if (/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent)) {
+            // IE-specific logic here
+            xhr.setRequestHeader('Accept-Charset', 'utf-8');
+            xhr.onreadystatechange = function () {
+                if(xhr.readyState === 4) {
+                    if (xhr.status === 200 || xhr.status === 0) {
+                        callback(null, xhr.responseText);
+                    }
+                    else {
+                        callback({status:xhr.status, errorMessage:errInfo});
+                    }
+                }
+            };
+        } else {
+            if (xhr.overrideMimeType) xhr.overrideMimeType('text\/plain; charset=utf-8');
+            xhr.onload = function () {
+                if(xhr.readyState === 4) {
+                    if (xhr.status === 200 || xhr.status === 0) {
+                        callback(null, xhr.responseText);
+                    }
+                    else {
+                        callback({status:xhr.status, errorMessage:errInfo});
+                    }
+                }
+            };
+            xhr.onerror = function(){
+                callback({status:xhr.status, errorMessage:errInfo});
+            };
+        }
+        xhr.send(null);
+    };
+}
