3c525de2a95de1e30be042c2440e2e05a542a721
2youyou2 <501251991@qq.com>
2016-07-14 15:45:37 +0800

    update binary windows template

--- native/templates/js-template-binary/frameworks/runtime-src/proj.win32/main.h
+++ native/templates/js-template-binary/frameworks/runtime-src/proj.win32/main.h
@@ -4,7 +4,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 // Windows Header Files:
-#include <windows.h>
 #include <tchar.h>
+
+// C RunTime Header Files
+#include "CCStdC.h"
 
 #endif    // __WINMAIN_H__
