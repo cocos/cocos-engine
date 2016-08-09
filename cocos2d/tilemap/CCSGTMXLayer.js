af69c2ae3ca9932e4669a84731c7dee8818c3e41
zhangbin <zhangbin@cocos2d-x.org>
2016-08-09 15:56:52 +0800

    Solve the logic error in _ccsg.TMXLayer. It's caused by code merging.

--- cocos2d/tilemap/CCSGTMXLayer.js
+++ cocos2d/tilemap/CCSGTMXLayer.js
@@ -439,7 +439,7 @@ _ccsg.TMXLayer = _ccsg.Node.extend(/** @lends _ccsg.TMXLayer# */{
             return tile;
         }
 
-        var z = Math.floor(pos.x) + Math.floor(pos.y) * this._layerSize.width;
+        var z = Math.floor(x) + Math.floor(y) * this._layerSize.width;
         tile = this._spriteTiles[z];
         // tile not created yet. create it
         if (!tile) {
@@ -482,7 +482,7 @@ _ccsg.TMXLayer = _ccsg.Node.extend(/** @lends _ccsg.TMXLayer# */{
             return null;
         }
 
-        var idx = Math.floor(pos.x) + Math.floor(pos.y) * this._layerSize.width;
+        var idx = Math.floor(x) + Math.floor(y) * this._layerSize.width;
         // Bits on the far end of the 32-bit global tile ID are used for tile flags
         var tile = this.tiles[idx];
 
