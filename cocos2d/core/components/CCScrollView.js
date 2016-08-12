ecf8f8ce655a3037b9bca12a55f5efc305721f8b
pandamicro <lphuabin@gmail.com>
2016-08-12 12:46:45 +0800

    Add cancelInnerEvents property in ScrollView

--- cocos2d/core/components/CCScrollView.js
+++ cocos2d/core/components/CCScrollView.js
@@ -160,7 +160,7 @@ var ScrollView = cc.Class({
          * @property {Node} content
          */
         content: {
-            default: null,
+            default: undefined,
             type: cc.Node,
             tooltip: 'i18n:COMPONENT.scrollview.content',
         },
@@ -240,7 +240,7 @@ var ScrollView = cc.Class({
          * @property {Scrollbar} horizontalScrollBar
          */
         horizontalScrollBar: {
-            default: null,
+            default: undefined,
             type: cc.Scrollbar,
             tooltip: 'i18n:COMPONENT.scrollview.horizontal_bar',
             notify: function() {
@@ -258,7 +258,7 @@ var ScrollView = cc.Class({
          * @property {Scrollbar} verticalScrollBar
          */
         verticalScrollBar: {
-            default: null,
+            default: undefined,
             type: cc.Scrollbar,
             tooltip: 'i18n:COMPONENT.scrollview.vertical_bar',
             notify: function() {
@@ -272,12 +272,24 @@ var ScrollView = cc.Class({
 
         /**
          * !#en Scrollview events callback
-         * !#zh 滚动视图的事件回调函数
+         * !#zh 滚动过程中的事件回调函数
          * @property {Component.EventHandler[]} scrollEvents
          */
         scrollEvents: {
             default: [],
             type: cc.Component.EventHandler
+        },
+
+        /**
+         * !#en If cancel inner events is set to true, the scroll behavior will cancel touch events on inner content nodes of the scroll view
+         * It's set to true by default.
+         * !#zh 如果这个属性被设置为 true，那么滚动行为会取消 ScrollView 的子节点上注册的触摸事件，默认被设置为 true。
+         * 注意，子节点上的 touchstart 事件仍然会触发，触点移动距离非常短的情况下 touchmove 和 touchend 也不会受影响。
+         * @property {Boolean} cancelInnerEvents
+         */
+        cancelInnerEvents: {
+            default: true,
+            animatable: false,
         }
     },
 
@@ -846,6 +858,11 @@ var ScrollView = cc.Class({
         if (this.content) {
             this._handleMoveLogic(touch);
         }
+        // Do not prevent touch events in inner nodes
+        if (!this.cancelInnerEvents) {
+            return;
+        }
+
         var deltaMove = cc.pSub(touch.getLocation(), touch.getStartLocation());
         //FIXME: touch move delta should be calculated by DPI.
         if (cc.pLength(deltaMove) > 7) {
