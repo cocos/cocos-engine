b5f0abfcadae4c31a53a52297385e4bbf8a5ed8b
VisualSJ <itharbors@outlook.com>
2016-11-25 11:25:03 +0800

    Loader support url param

--- cocos2d/core/load-pipeline/loading-items.js
+++ cocos2d/core/load-pipeline/loading-items.js
@@ -39,20 +39,44 @@ var ItemState = {
 };
 
 var _queueDeps = {};
+var _parseUrl = function (url) {
+    var result = {
+        url: '',
+        param: {}
+    };
+
+
+    var split = url.split('?');
+    if (!split || !split[0]) {
+        return result;
+    }
+    result.url = split[0];
+    if (!split[1]) {
+        return result;
+    }
+    split = split[1].split('&');
+    split.forEach(function (item) {
+        var itemSplit = item.split('=');
+        result.param[itemSplit[0]] = itemSplit[1];
+    });
+    return result;
+};
 
 function isIdValid (id) {
     var realId = id.id || id;
     return (typeof realId === 'string');
 }
 function createItem (id, queueId) {
-    var result;
+    var result, urlItem;
     if (typeof id === 'object' && id.id) {
         if (!id.type) {
             id.type = Path.extname(id.id).toLowerCase().substr(1);
         }
+        urlItem = _parseUrl(id.url || id.id);
         result = {
             queueId: queueId,
-            url: id.url || id.id,
+            url: urlItem.url,
+            param: urlItem.param,
             error: null,
             content: null,
             complete: false,
@@ -62,10 +86,12 @@ function createItem (id, queueId) {
         JS.mixin(result, id);
     }
     else if (typeof id === 'string') {
+        urlItem = _parseUrl(id);
         result = {
             queueId: queueId,
             id: id,
-            url: id,
+            url: urlItem.url,
+            param: urlItem.param,
             type: Path.extname(id).toLowerCase().substr(1),
             error: null,
             content: null,
