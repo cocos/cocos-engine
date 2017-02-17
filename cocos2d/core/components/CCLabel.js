ae50aeff1a3b91f92233153ccad9cbf343ae2a59
zhangbin <zhangbin@cocos2d-x.org>
2017-02-17 11:06:15 +0800

    Improve the initialization logic of Label with BitmapFont.

--- cocos2d/core/components/CCLabel.js
+++ cocos2d/core/components/CCLabel.js
@@ -375,7 +375,12 @@ var Label = cc.Class({
                     var isAsset = value instanceof cc.Font;
 
                     if (this.font instanceof cc.BitmapFont) {
-                        this._sgNode.setFontFileOrFamily(this.font.fntDataStr, this.font.spriteFrame);
+                        if (this.font.spriteFrame) {
+                            this._sgNode.setFontFileOrFamily(this.font.fntDataStr, this.font.spriteFrame);
+                        } else {
+                            cc.warnID(4011, this.font.name);
+                            this._sgNode.setFontFileOrFamily('');
+                        }
                     } else {
                         var ttfName = isAsset ? value.rawUrl : '';
                         this._sgNode.setFontFileOrFamily(ttfName);
