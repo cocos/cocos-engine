bc1e86300d0b8f44cd4e15043fbe9a5d0a7bbff5
2youyou2 <501251991@qq.com>
2017-04-01 17:26:06 +0800

    update windows physics project

--- native/cocos/editor-support/creator/physics/CCPhysicsManifoldWrapper.h
+++ native/cocos/editor-support/creator/physics/CCPhysicsManifoldWrapper.h
@@ -29,7 +29,7 @@
 #include "cocos2d.h"
 
 namespace creator {
-    class PhysicsManifoldWrapper : public cocos2d::Ref
+    class CC_DLL PhysicsManifoldWrapper : public cocos2d::Ref
     {
     public:
         PhysicsManifoldWrapper();
