17298a447111de653fe69dbb9475c1d32ecce8c1
zilongshanren <guanghui8827@126.com>
2017-04-17 17:55:35 +0800

    optimize bmfont error log info

--- cocos2d/core/label/CCSGLabel.js
+++ cocos2d/core/label/CCSGLabel.js
@@ -922,7 +922,7 @@ cc.BMFontHelper = {
                 letterDef = this._fontAtlas.getLetterDefinitionForChar(character);
                 if (!letterDef) {
                     this._recordPlaceholderInfo(letterIndex, character);
-                    console.log("Can't find letter definition in font file for letter:" + character);
+                    console.log("Can't find letter definition in texture atlas " + this._config.atlasName + " for letter:" + character);
                     continue;
                 }
 
