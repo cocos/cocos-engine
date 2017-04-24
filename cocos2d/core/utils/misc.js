f378a670c7131f02ce00821cbb9bdb9f33cd7f83
pandamicro <lphuabin@gmail.com>
2017-04-24 14:18:34 +0800

    Use fixed size array in misc.imagePool

--- cocos2d/core/utils/misc.js
+++ cocos2d/core/utils/misc.js
@@ -125,17 +125,28 @@ if (CC_EDITOR) {
 }
 
 m.imagePool = {
-    _pool: [],
+    _pool: new Array(10),
     _MAX: 10,
     _smallImg: "data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=",
+
+    count: 0,
     get: function () {
-        return this._pool.pop() || new Image();
+        if (this.count > 0) {
+            this.count--;
+            var result = this._pool[this.count];
+            this._pool[this.count] = null;
+            return result;
+        }
+        else {
+            return new Image();
+        }
     },
     put: function (img) {
         var pool = this._pool;
-        if (img instanceof HTMLImageElement && pool.length < this._MAX) {
+        if (img instanceof HTMLImageElement && this.count < this._MAX) {
             img.src = this._smallImg;
-            pool.push(img);
+            pool[this.count] = img;
+            this.count++;
         }
     }
 }
