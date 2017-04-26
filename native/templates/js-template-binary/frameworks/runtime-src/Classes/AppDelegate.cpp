5c25d799d36e5b43b29b7c9994c31ad4999341cf
zilongshanren <guanghui8827@126.com>
2017-04-26 17:24:44 +0800

    update binary template

--- native/templates/js-template-binary/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ native/templates/js-template-binary/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -59,9 +59,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if(CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-        glview = GLViewImpl::create("raphael");
+        glview = GLViewImpl::create("HelloJavascript");
 #else
-        glview = GLViewImpl::createWithRect("raphael", Rect(0,0,900,640));
+        glview = GLViewImpl::createWithRect("HelloJavascript", Rect(0,0,900,640));
 #endif
         director->setOpenGLView(glview);
     }
