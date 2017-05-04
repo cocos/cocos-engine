17af1cac16f66a4364ee9a7c26d1bf20e0c7bc8b
pandamicro <lphuabin@gmail.com>
2017-05-04 14:14:25 +0800

    Force release assets in cc.loader.release(do not use AutoReleaseUtils)

--- cocos2d/core/load-pipeline/CCLoader.js
+++ cocos2d/core/load-pipeline/CCLoader.js
@@ -726,7 +726,10 @@ JS.mixin(CCLoader.prototype, {
      */
     release: function (asset) {
         if (Array.isArray(asset)) {
-            AutoReleaseUtils.autoRelease(this, asset);
+            for (var i = 0; i < asset.length; i++) {
+                var key = asset[i];
+                this.release(key);
+            }
         }
         else if (asset) {
             var id = this._getReferenceKey(asset);
