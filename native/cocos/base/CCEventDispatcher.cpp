f87f8c6854e39aa1cc7a0d87135b81a222b05d3a
James Chen <jianhua.chen@cocos2d-x.org>
2017-05-19 10:29:52 +0800

    fix pauseAllEventListenersForTarget couldn't work while dispatching touch event.

--- native/cocos/base/CCEventDispatcher.cpp
+++ native/cocos/base/CCEventDispatcher.cpp
@@ -477,9 +477,9 @@ void EventDispatcher::forceAddEventListener(EventListener* listener)
 
         associateNodeAndEventListener(node, listener);
 
-        if (node->isRunning())
+        if (!node->isRunning() && listener->isPaused())
         {
-            resumeEventListenersForTarget(node);
+            listener->setPaused(true);
         }
     }
     else
