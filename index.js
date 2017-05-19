1df57cb0315a40ceb2780567e750dd06523148e7
Jare Guo <jare85@163.com>
2017-05-19 15:56:26 +0800

    add macro’s api doc

--- index.js
+++ index.js
@@ -25,18 +25,55 @@
 
 // MACROS
 
-// if "global_defs" not preprocessed by uglify, just declare them globally,
-// this may happened in release version's preview page.
+/**
+ * !#zh
+ * 这里是一些用来判断执行环境的宏，这些宏都是全局变量，直接访问即可。<br>
+ * 在项目构建时，这些宏将会被预处理并根据构建的平台剔除不需要的代码，例如
+ *
+ *     if (CC_DEBUG) {
+ *         cc.log('debug');
+ *     }
+ *     else {
+ *         cc.log('release');
+ *     }
+ *
+ * 在构建后会只剩下
+ *
+ *     cc.log('release');
+ *
+ * 好棒棒
+ *
+ * !#en
+ *
+ * TODO
+ *
+ * @module GLOBAL MACROS
+ */
+/**
+ * @property {Boolean} CC_EDITOR - Running in the editor.
+ */
+/**
+ * @property {Boolean} CC_DEV - Running in the editor or preview.
+ */
+/**
+ * @property {Boolean} CC_JSB - Running in native platform.
+ */
+/**
+ * @property {Boolean} CC_TEST - Running in the engine's unit test.
+ */
 Function(
-    /* use evaled code to prevent the uglify from renaming symbols */
-    'if(typeof CC_TEST=="undefined")' +
-        'CC_TEST=typeof tap=="object"||typeof QUnit=="object";' +
-    'if(typeof CC_EDITOR=="undefined")' +
-        'CC_EDITOR=typeof Editor=="object"&&typeof process=="object"&&"electron" in process.versions;' +
-    'if(typeof CC_DEV=="undefined")' +
+    // if "global_defs" not preprocessed by uglify, just declare them globally,
+    // this may happened in release version's preview page.
+    // (use evaled code to prevent mangle by uglify)
+    'var u="undefined",o="object";' +
+    'if(typeof CC_TEST==u)' +
+        'CC_TEST=typeof tap==o||typeof QUnit==o;' +
+    'if(typeof CC_EDITOR==u)' +
+        'CC_EDITOR=typeof Editor==o&&typeof process==o&&"electron" in process.versions;' +
+    'if(typeof CC_DEV==u)' +
         'CC_DEV=CC_EDITOR||CC_TEST;' + /* CC_DEV contains CC_TEST and CC_EDITOR */
-    'if(typeof CC_JSB=="undefined")' +
-        'CC_JSB=false;'
+    'if(typeof CC_JSB==u)' +
+        'CC_JSB=!1;'
 )();
 
 // PREDEFINE
@@ -45,12 +82,11 @@ require('./predefine');
 
 // load Cocos2d engine code
 
-var isMainProcess = CC_EDITOR && Editor.isMainProcess;
-if (!isMainProcess) {
-    require('./cocos2d/index.js');
+if (CC_EDITOR && Editor.isMainProcess) {
+    cc._initDebugSetting(1);    // DEBUG_MODE_INFO
 }
 else {
-    cc._initDebugSetting(1);    // DEBUG_MODE_INFO
+    require('./cocos2d/index.js');
 }
 
 // LOAD EXTENDS FOR CREATOR
@@ -61,18 +97,10 @@ if (CC_EDITOR) {
     // In editor, in addition to the modules defined in cc scope, you can also access to the internal modules by using _require.
     // var isDomNode = cc._require('./cocos2d/core/platform/utils').isDomNode;
     cc._require = require;
-    /*
-     * Checks if the extension is loaded.
-     * This method is used to make editor code more elegant, so only available in editor.
-     * @param {string} moduleName - such as 'sp', 'dragonBones', 'TiledMap'... etc.
-     */
-    cc.hasExtension = function (moduleName) {
-        return moduleName in global || moduleName in cc;
-    };
-}
 
-if (isMainProcess) {
-    Editor.versions['cocos2d'] = require('./package.json').version;
+    if (Editor.isMainProcess) {
+        Editor.versions['cocos2d'] = require('./package.json').version;
+    }
 }
 
 module.exports = cc;
