f3d365dc422a17a6d8662abf75709000f210696f
Jare Guo <jare85@163.com>
2017-05-23 11:47:44 +0800

    add macro’s en api doc

--- index.js
+++ index.js
@@ -56,8 +56,32 @@
  *     }
  *
  * !#en
+ * Here are some of the macro used to determine the execution environment, these macros are global variables, can be accessed directly.<br>
+ * When the project is built, these macros will be preprocessed and discard unreachable code based on the built platform, for example:
  *
- * TODO
+ *     if (CC_DEBUG) {
+ *         cc.log('debug');
+ *     }
+ *     else {
+ *         cc.log('release');
+ *     }
+ *
+ * After build will become:
+ *
+ *     cc.log('release');
+ *
+ * <br>
+ * To determine whether the script is running on the specified platform, you can use the following expression:
+ *
+ *     {
+ *         "editor":  CC_EDITOR,
+ *         "editor or preview":  CC_DEV,
+ *         "editor or preview or build in debug mode":  CC_DEBUG,
+ *         "web preview":  CC_PREVIEW && !CC_JSB,
+ *         "simulator preview":  CC_PREVIEW && CC_JSB,
+ *         "build in debug mode":  CC_BUILD && CC_DEBUG,
+ *         "build in release mode":  CC_BUILD && !CC_DEBUG,
+ *     }
  *
  * @module GLOBAL MACROS
  */
@@ -71,7 +95,7 @@
  * @property {Boolean} CC_DEV - Running in the editor or preview.
  */
 /**
- * @property {Boolean} CC_DEBUG - Running in the editor or preview, or built as debug mode.
+ * @property {Boolean} CC_DEBUG - Running in the editor or preview, or build in debug mode.
  */
 /**
  * @property {Boolean} CC_BUILD - Running in published project.
