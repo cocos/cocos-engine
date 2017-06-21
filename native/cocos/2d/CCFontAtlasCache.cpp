e600022c4cfe59e7c3930b56b5b79ebc70d682b1
zilongshanren <guanghui8827@126.com>
2017-06-21 11:35:08 +0800

    fix multiple bmfont packer rendering issue

--- native/cocos/2d/CCFontAtlasCache.cpp
+++ native/cocos/2d/CCFontAtlasCache.cpp
@@ -97,7 +97,7 @@ FontAtlas* FontAtlasCache::getFontAtlasFNT(const std::string& fntDataString,
     auto realFontFilename = spriteFrame->getTexture()->getPath();
 
     char tmp[ATLAS_MAP_KEY_BUFFER];
-    snprintf(tmp, ATLAS_MAP_KEY_BUFFER, "%.2f %.2f %s", imageOffset.x, imageOffset.y, realFontFilename.c_str());
+    snprintf(tmp, ATLAS_MAP_KEY_BUFFER, "%.2f %.2f %s %p", imageOffset.x, imageOffset.y, realFontFilename.c_str(), spriteFrame);
     std::string atlasName = tmp;
 
     auto it = _atlasMap.find(atlasName);
