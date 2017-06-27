b6d4e91421ee4a17c5b32b245a980e88b205f070
zilongshanren <guanghui8827@126.com>
2017-06-27 10:20:38 +0800

    use pre-parsed bmfont assets data

--- cocos2d/core/label/CCSGLabel.js
+++ cocos2d/core/label/CCSGLabel.js
@@ -26,95 +26,6 @@
 require('./CCTextUtils.js');
 var EventTarget = require("../event/event-target");
 
-var FntLoader = {
-    INFO_EXP: /info [^\n]*(\n|$)/gi,
-    COMMON_EXP: /common [^\n]*(\n|$)/gi,
-    PAGE_EXP: /page [^\n]*(\n|$)/gi,
-    CHAR_EXP: /char [^\n]*(\n|$)/gi,
-    KERNING_EXP: /kerning [^\n]*(\n|$)/gi,
-    ITEM_EXP: /\w+=[^ \r\n]+/gi,
-    INT_EXP: /^[\-]?\d+$/,
-
-    _parseStrToObj: function (str) {
-        var arr = str.match(this.ITEM_EXP);
-        var obj = {};
-        if (arr) {
-            for (var i = 0, li = arr.length; i < li; i++) {
-                var tempStr = arr[i];
-                var index = tempStr.indexOf("=");
-                var key = tempStr.substring(0, index);
-                var value = tempStr.substring(index + 1);
-                if (value.match(this.INT_EXP)) value = parseInt(value);
-                else if (value[0] === '"') value = value.substring(1, value.length - 1);
-                obj[key] = value;
-            }
-        }
-        return obj;
-    },
-
-    /**
-     * Parse Fnt string.
-     * @param fntStr
-     * @returns {{}}
-     */
-    parseFnt: function (fntStr) {
-        var self = this, fnt = {};
-        //padding
-        var infoResult = fntStr.match(self.INFO_EXP);
-        if (!infoResult) return fnt;
-
-        var infoObj = self._parseStrToObj(infoResult[0]);
-        var paddingArr = infoObj["padding"].split(",");
-        var padding = {
-            left: parseInt(paddingArr[0]),
-            top: parseInt(paddingArr[1]),
-            right: parseInt(paddingArr[2]),
-            bottom: parseInt(paddingArr[3])
-        };
-
-        //common
-        var commonObj = self._parseStrToObj(fntStr.match(self.COMMON_EXP)[0]);
-        fnt.commonHeight = commonObj["lineHeight"];
-        fnt.fontSize = infoObj["size"];
-
-        if (cc._renderType === cc.game.RENDER_TYPE_WEBGL) {
-            var texSize = cc.configuration.getMaxTextureSize();
-            if (commonObj["scaleW"] > texSize.width || commonObj["scaleH"] > texSize.height)
-                cc.log("cc.LabelBMFont._parseCommonArguments(): page can't be larger than supported");
-        }
-        if (commonObj["pages"] !== 1) cc.log("cc.LabelBMFont._parseCommonArguments(): only supports 1 page");
-
-        //page
-        var pageObj = self._parseStrToObj(fntStr.match(self.PAGE_EXP)[0]);
-        if (pageObj["id"] !== 0) cc.log("cc.LabelBMFont._parseImageFileName() : file could not be found");
-        fnt.atlasName = pageObj["file"];
-
-        //char
-        var charLines = fntStr.match(self.CHAR_EXP);
-        var fontDefDictionary = fnt.fontDefDictionary = {};
-        for (var i = 0, li = charLines.length; i < li; i++) {
-            var charObj = self._parseStrToObj(charLines[i]);
-            var charId = charObj["id"];
-            fontDefDictionary[charId] = {
-                rect: {x: charObj["x"], y: charObj["y"], width: charObj["width"], height: charObj["height"]},
-                xOffset: charObj["xoffset"],
-                yOffset: charObj["yoffset"],
-                xAdvance: charObj["xadvance"]
-            };
-        }
-
-        //kerning
-        var kerningDict = fnt.kerningDict = {};
-        var kerningLines = fntStr.match(self.KERNING_EXP);
-        if (kerningLines) {
-            for (i = 0, li = kerningLines.length; i < li; i++) {
-                var kerningObj = self._parseStrToObj(kerningLines[i]);
-                kerningDict[(kerningObj["first"] << 16) | (kerningObj["second"] & 0xffff)] = kerningObj["amount"];
-            }
-        }
-        return fnt;
-    }
-};
 
 var FontLetterDefinition = function() {
     this._u = 0;
@@ -1303,8 +1214,7 @@ _ccsg.Label = _ccsg.Node.extend({
                 if (fntConfig) {
                     self._config = fntConfig;
                 } else {
-                    self._config = FntLoader.parseFnt(fntDataStr);
-                    this._fontAsset._fntConfig = self._config;
+                    cc.warn('Invalid BMFont Assets!');
                 }
                 self._createFontChars();
                 self._spriteFrame = spriteFrame;
