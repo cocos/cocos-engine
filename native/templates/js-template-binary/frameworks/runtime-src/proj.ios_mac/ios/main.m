070c1aabb73ee130e1930996d6a66e1f575fb082
James Chen <jianhua.chen@cocos2d-x.org>
2017-07-14 17:36:26 +0800

    Fixes binary template compilation fairlure. https://github.com/cocos-creator/fireball/issues/6028

--- native/templates/js-template-binary/frameworks/runtime-src/proj.ios_mac/ios/main.m
+++ native/templates/js-template-binary/frameworks/runtime-src/proj.ios_mac/ios/main.m
@@ -7,6 +7,17 @@
 
 #import <UIKit/UIKit.h>
 
+// Hack for binary template.
+// It's because cocos2d libraries may be compiled with iOS SDK (>=10.0),
+// and there is code of sdk version check in SocketRocket which will trigger 'undefined reference of
+// NSStreamNetworkServiceTypeCallSignaling' error while linking. Define a same variable in binrary
+// template if SDK version is lower than macOS10.12, iOS10, tvOS10, watchOS3 will be a workaround.
+// Please note that, this issue will not exist in 'default' & 'link' template.
+#if (__MAC_OS_X_VERSION_MAX_ALLOWED >= 101200 || __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000 || __TV_OS_VERSION_MAX_ALLOWED >= 100000 || __WATCH_OS_VERSION_MAX_ALLOWED >= 30000)
+#else
+NSString* NSStreamNetworkServiceTypeCallSignaling = @"kCFStreamNetworkServiceTypeCallSignaling";
+#endif
+
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
