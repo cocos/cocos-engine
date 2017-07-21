d9ec5dae6cb9de0dd204e47237ca42b60fd4fa49
zilongshanren <guanghui8827@126.com>
2017-07-21 13:56:01 +0800

    fix custom ttf md5 cache

--- cocos2d/core/label/CCTextUtils.js
+++ cocos2d/core/label/CCTextUtils.js
@@ -55,15 +55,21 @@ var CustomFontLoader = {
     _allFontsLoaded: false,
     _intervalId: 0,
     loadTTF: function (url, callback) {
+        var fontFamilyName = this._getFontFamily(url);
+
+        var md5Pipe = cc.loader.md5Pipe;
+        if (md5Pipe) {
+            url = md5Pipe.transformURL(url);
+        }
         //these platforms support window.FontFace, but it sucks sometimes.
         var useFontFace = (cc.sys.browserType !== cc.sys.BROWSER_TYPE_BAIDU
                            && cc.sys.browserType !== cc.sys.BROWSER_TYPE_BAIDU_APP
                            && cc.sys.browserType !== cc.sys.BROWSER_TYPE_MOBILE_QQ);
 
         if (window.FontFace && useFontFace) {
-            this._loadWithFontFace(url, callback);
+            this._loadWithFontFace(fontFamilyName, url, callback);
         } else {
-            this._loadWithCSS(url, callback);
+            this._loadWithCSS(fontFamilyName, url, callback);
         }
 
         if (this._intervalId === 0) {
@@ -95,8 +101,7 @@ var CustomFontLoader = {
         }
     },
 
-    _loadWithFontFace: function(url, callback) {
-        var fontFamilyName = this._getFontFamily(url);
+    _loadWithFontFace: function(fontFamilyName, url, callback) {
 
         var fontDescriptor = this._fontCache[fontFamilyName];
         if(!fontDescriptor) {
@@ -107,7 +112,7 @@ var CustomFontLoader = {
             fontDescriptor.addHandler(callback);
             this._fontCache[fontFamilyName] = fontDescriptor;
 
-            fontFace.loaded.then(function() {
+            fontFace.load().then(function() {
                 fontDescriptor.onLoaded();
             });
         } else {
@@ -117,9 +122,7 @@ var CustomFontLoader = {
         }
     },
 
-    _loadWithCSS: function(url, callback) {
-        var fontFamilyName = this._getFontFamily(url);
-
+    _loadWithCSS: function(fontFamilyName, url, callback) {
         var fontDescriptor = this._fontCache[fontFamilyName];
         if(!fontDescriptor) {
             //fall back implementations
