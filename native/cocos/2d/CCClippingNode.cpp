469f9a9ab8e59f4c8db9ef12664cb6565cbcaff0
2youyouo2 <501251991@qq.com>
2017-08-03 14:39:43 +0800

    fixed mask in camera

--- native/cocos/2d/CCClippingNode.cpp
+++ native/cocos/2d/CCClippingNode.cpp
@@ -194,6 +194,10 @@ void ClippingNode::visit(Renderer *renderer, const Mat4 &parentTransform, uint32
     if (!_visible || !hasContent())
         return;
 
+    if (_beforeVisitCallback) {
+        _beforeVisitCallback(renderer);
+    }
+    
     uint32_t flags = processParentFlags(parentTransform, parentFlags);
 
     // IMPORTANT:
@@ -272,6 +276,10 @@ void ClippingNode::visit(Renderer *renderer, const Mat4 &parentTransform, uint32
     renderer->popGroup();
 
     _director->popMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+    
+    if (_afterVisitCallback) {
+        _afterVisitCallback(renderer);
+    }
 }
 
 Node* ClippingNode::getStencil() const
