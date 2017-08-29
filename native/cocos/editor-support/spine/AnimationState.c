e4250dd01bcc0247e12b9b32d20026a8d1f10f77
James Chen <jianhua.chen@cocos2d-x.org>
2017-08-29 17:54:27 +0800

    Adds a method spTrackEntry_setDisposeCallback for notifying spTrackEntry is being disposed.

--- native/cocos/editor-support/spine/AnimationState.c
+++ native/cocos/editor-support/spine/AnimationState.c
@@ -33,6 +33,9 @@
 #include <limits.h>
 
 static spAnimation* SP_EMPTY_ANIMATION = 0;
+
+static TrackEntryDisposeCallback _trackEntryDisposeCallback = 0;
+
 void spAnimationState_disposeStatics () {
 	if (SP_EMPTY_ANIMATION) spAnimation_dispose(SP_EMPTY_ANIMATION);
 	SP_EMPTY_ANIMATION = 0;
@@ -175,6 +178,9 @@ void _spEventQueue_drain (_spEventQueue* self) {
 }
 
 void _spAnimationState_disposeTrackEntry (spTrackEntry* entry) {
+    if (_trackEntryDisposeCallback)
+        _trackEntryDisposeCallback(entry);
+
 	FREE(entry->timelinesFirst);
 	FREE(entry->timelinesRotation);
 	FREE(entry);
@@ -192,6 +198,11 @@ void _spAnimationState_disposeTrackEntries (spAnimationState* state, spTrackEntr
 		_spAnimationState_disposeTrackEntry(entry);
 		entry = next;
 	}
+}
+
+void spTrackEntry_setDisposeCallback(TrackEntryDisposeCallback cb)
+{
+    _trackEntryDisposeCallback = cb;
 }
 
 spAnimationState* spAnimationState_create (spAnimationStateData* data) {
