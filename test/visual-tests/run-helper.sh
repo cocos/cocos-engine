bd1cd1a9d3d2c65080374cd0d849033276041c0c
pandamicro <huabin.ling@cocos2d-x.org>
2017-10-11 13:51:05 +0800

    Remove visual tests

--- test/visual-tests/run-helper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sleep 1
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://127.0.0.1:8512/test/visual-tests/index.html
--- test/visual-tests/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Run unit tests in Chrome \(sh test/visual-tests/run.sh\)
-echo \(You need to run \"gulp build-test\" before testing.\)
-echo
-sh test/visual-tests/run-helper.sh &
-node test/visual-tests/server.js
