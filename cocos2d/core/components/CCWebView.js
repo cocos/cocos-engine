1bc94e3dafab66c1a32d09384e89ee363dc21ebc
knoxHuang <821686174@qq.com>
2017-10-24 15:02:39 +0800

    refine code

--- cocos2d/core/components/CCWebView.js
+++ cocos2d/core/components/CCWebView.js
@@ -177,8 +177,8 @@ var WebView = cc.Class({
     /**
      * !#en
      * Set javascript interface scheme (see also setOnJSCallback). <br/>
-     * Note: Only support Android and iOS, Web end usage, please go to the official document view.<br/>
-     * Please refer to the official document for more details
+     * Note: Supports only on the Android and iOS. For HTML5, please refer to the official documentation.<br/>
+     * Please refer to the official documentation for more details.
      * !#zh
      * 设置 JavaScript 接口方案（与 'setOnJSCallback' 配套使用）。<br/>
      * 注意：只支持 Android 和 iOS ，Web 端用法请前往官方文档查看。<br/>
@@ -196,8 +196,8 @@ var WebView = cc.Class({
      * !#en
      * This callback called when load URL that start with javascript
      * interface scheme (see also setJavascriptInterfaceScheme). <br/>
-     * Note: Only support Android and iOS, Web end usage, please go to the official document view.<br/>
-     * Please refer to the official document for more details
+     * Note: Supports only on the Android and iOS. For HTML5, please refer to the official documentation.<br/>
+     * Please refer to the official documentation for more details.
      * !#zh
      * 当加载 URL 以 JavaScript 接口方案开始时调用这个回调函数。<br/>
      * 注意：只支持 Android 和 iOS ，Web 端用法请前往官方文档查看。
@@ -213,9 +213,9 @@ var WebView = cc.Class({
 
     /**
      * !#en
-     * Execution of WebView internal page script <br/>
+     * Evaluates JavaScript in the context of the currently displayed page. <br/>
      * Please refer to the official document for more details <br/>
-     * Note: Cross domain issues need to be addressed by themselves <br/>
+     * Note: Cross domain issues need need to be resolved by yourself <br/>
      * !#zh
      * 执行 WebView 内部页面脚本（详情请参阅官方文档） <br/>
      * 注意：需要自行解决跨域问题
