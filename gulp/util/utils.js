1143c84522cc948566cdee7e09d7d08779dff7ce
Jare Guo <jare85@163.com>
2018-01-03 17:15:03 +0800

    support compile with CC_WECHATGAME macro

--- gulp/util/utils.js
+++ gulp/util/utils.js
@@ -1,29 +1,50 @@
 
 const PLATFORM_MACROS = ['CC_EDITOR', 'CC_PREVIEW', 'CC_BUILD', 'CC_TEST'];
+const FLAGS = ['jsb', 'wechatgame', 'debug'];
 
 // generate macros for uglify's global_defs
-// platform: editor | preview | build | test
-exports.getMacros = function (platform, isJSB, isDebugBuild) {
+// available platforms: 'editor' | 'preview' | 'build' | 'test'
+// available keys of flags: 'jsb' | 'wechatgame' | 'debug'
+exports.getMacros = function (platform, flags) {
+    // platform macros
     var platformMacro = 'CC_' + platform.toUpperCase();
     if (PLATFORM_MACROS.indexOf(platformMacro) === -1) {
         throw new Error('Unknown platform: ' + platform);
     }
     var res = {};
-    for (var i = 0; i < PLATFORM_MACROS.length; i++) {
-        var macro = PLATFORM_MACROS[i];
+    for (let i = 0; i < PLATFORM_MACROS.length; i++) {
+        let macro = PLATFORM_MACROS[i];
         res[macro] = (macro === platformMacro);
     }
+
+    // flag macros
+    if (flags) {
+        for (let flag in flags) {
+            if (flags.hasOwnProperty(flag) && flags[flag]) {
+                if (FLAGS.indexOf(flag) === -1) {
+                    throw new Error('Unknown flag: ' + flag);
+                }
+            }
+        }
+    }
+    for (let i = 0; i < FLAGS.length; i++) {
+        let flag = FLAGS[i];
+        let macro = 'CC_' + flag.toUpperCase();
+        res[macro] = !!(flags && flags[flag]);
+    }
+
+    // debug macros
     res['CC_DEV'] = res['CC_EDITOR'] || res['CC_PREVIEW'] || res['CC_TEST'];
-    res['CC_DEBUG'] = res['CC_DEV'] || !!isDebugBuild;
-    res['CC_JSB'] = !!isJSB;
+    res['CC_DEBUG'] = res['CC_DEBUG'] || res['CC_DEV'];
+    console.log(res);
     return res;
 };
 
-exports.getUglifyOptions = function (platform, isJSB, isDebugBuild) {
-    var global_defs = exports.getMacros(platform, isJSB, isDebugBuild);
+exports.getUglifyOptions = function (platform, flags) {
+    var global_defs = exports.getMacros(platform, flags);
     var releaseMode = !global_defs['CC_DEBUG'];
 
-    var optimizeForJSC = releaseMode && isJSB;
+    var optimizeForJSC = releaseMode && global_defs['CC_JSB'];
     if (optimizeForJSC) {
         return {
             output: {
