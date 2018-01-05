a39257030e17f4ac7d23ab27e617a8a29110ab45
knoxHuang <821686174@qq.com>
2018-01-05 13:14:20 +0800

    add Volume and Mute to CCVideoPlayer for fireball/issues/6811

--- cocos2d/core/components/CCVideoPlayer.js
+++ cocos2d/core/components/CCVideoPlayer.js
@@ -112,7 +112,7 @@ var VideoPlayer = cc.Class({
         resourceType: {
             tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.resourceType',
             type: ResourceType,
-            set: function ( value ) {
+            set: function (value) {
                 this._resourceType = value;
                 this._updateVideoSource();
             },
@@ -130,7 +130,7 @@ var VideoPlayer = cc.Class({
         remoteURL: {
             tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.url',
             type: cc.String,
-            set: function ( url ) {
+            set: function (url) {
                 this._remoteURL = url;
                 this._updateVideoSource();
             },
@@ -153,7 +153,7 @@ var VideoPlayer = cc.Class({
             get: function () {
                 return this._clip;
             },
-            set: function ( value ) {
+            set: function (value) {
                 if (typeof value !== 'string')
                     value = '';
                 this._clip = value;
@@ -170,17 +170,58 @@ var VideoPlayer = cc.Class({
         currentTime: {
             tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.currentTime',
             type: cc.Float,
-            set: function ( time ) {
-                if(this._sgNode) {
+            set: function (time) {
+                if (this._sgNode) {
                     this._sgNode.seekTo(time);
                 }
             },
             get: function () {
-                if(this._sgNode) {
+                if (this._sgNode) {
                     return this._sgNode.currentTime();
                 }
                 return -1;
             }
+        },
+
+        _volume: 1,
+        /**
+         * !#en The volume of the video player.
+         * !#zh 视频的音量（0.0 ~ 1.0）。
+         * @property volume
+         * @type {Number}
+         * @default 1
+         */
+        volume: {
+            get: function () {
+                return this._volume;
+            },
+            set: function (value) {
+                this._volume = value;
+                if (this.isPlaying() && !this._mute) {
+                    this._syncVolume();
+                }
+            },
+            type: cc.Float,
+            tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.volume'
+        },
+
+        _mute: false,
+        /**
+         * !#en Is the video player mute?
+         * !#zh 是否静音视频。Mute 是设置音量为 0，取消静音是恢复原来的音量。
+         * @property mute
+         * @type {Boolean}
+         * @default false
+         */
+        mute: {
+            get: function () {
+                return this._mute;
+            },
+            set: function (value) {
+                this._mute = value;
+                this._syncVolume();
+            },
+            tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.mute',
         },
 
         /**
@@ -206,7 +247,7 @@ var VideoPlayer = cc.Class({
             tooltip: CC_DEV && 'i18n:COMPONENT.videoplayer.isFullscreen',
             default: false,
             type: cc.Boolean,
-            notify: function() {
+            notify: function () {
                 this._sgNode.setFullScreenEnabled(this.isFullscreen);
             }
         },
@@ -227,16 +268,24 @@ var VideoPlayer = cc.Class({
         ResourceType: ResourceType
     },
 
-    onLoad: function() {
-        if(CC_JSB) {
+    onLoad: function () {
+        if (CC_JSB) {
             if (cc.sys.os === cc.sys.OS_OSX || cc.sys.os === cc.sys.OS_WINDOWS) {
                 this.enabled = false;
             }
         }
     },
 
+    _syncVolume: function () {
+        var sgNode = this._sgNode;
+        if (sgNode) {
+            var volume = this._mute ? 0 : this._volume;
+            sgNode.setVolume(volume);
+        }
+    },
+
     _createSgNode: function () {
-        if(CC_JSB) {
+        if (CC_JSB) {
             if (cc.sys.os === cc.sys.OS_OSX || cc.sys.os === cc.sys.OS_WINDOWS) {
                 console.log('VideoPlayer is not supported on Mac and Windows!');
                 return null;
@@ -248,16 +297,20 @@ var VideoPlayer = cc.Class({
     _updateVideoSource: function () {
         var sgNode = this._sgNode;
         if (this.resourceType === ResourceType.REMOTE) {
+            if (cc.loader.md5Pipe) {
+                this.remoteURL = cc.loader.md5Pipe.transformURL(this.remoteURL);
+            }
             sgNode.setURL(this.remoteURL);
-        } else {
+        }
+        else {
             sgNode.setURL(this._clip || '');
         }
     },
 
     _initSgNode: function () {
         var sgNode = this._sgNode;
-        if(sgNode) {
-            if(!CC_JSB) {
+        if (sgNode) {
+            if (!CC_JSB) {
                 sgNode.createDomElementIfNeeded();
             }
             this._updateVideoSource();
@@ -295,22 +348,22 @@ var VideoPlayer = cc.Class({
         this.node.emit('clicked', this);
     },
 
-    onPlaying: function(){
+    onPlaying: function () {
         cc.Component.EventHandler.emitEvents(this.videoPlayerEvent, this, EventType.PLAYING);
         this.node.emit('playing', this);
     },
 
-    onPasued: function() {
+    onPasued: function () {
         cc.Component.EventHandler.emitEvents(this.videoPlayerEvent, this, EventType.PAUSED);
         this.node.emit('paused', this);
     },
 
-    onStopped: function() {
+    onStopped: function () {
         cc.Component.EventHandler.emitEvents(this.videoPlayerEvent, this, EventType.STOPPED);
         this.node.emit('stopped', this);
     },
 
-    onCompleted: function() {
+    onCompleted: function () {
         cc.Component.EventHandler.emitEvents(this.videoPlayerEvent, this, EventType.COMPLETED);
         this.node.emit('completed', this);
     },
@@ -321,7 +374,8 @@ var VideoPlayer = cc.Class({
      * @method play
      */
     play: function () {
-        if(this._sgNode) {
+        if (this._sgNode) {
+            this._syncVolume();
             this._sgNode.play();
         }
     },
@@ -331,8 +385,9 @@ var VideoPlayer = cc.Class({
      * !#zh 如果一个视频播放被暂停播放了，调用这个接口可以继续播放。
      * @method resume
      */
-    resume: function() {
+    resume: function () {
         if (this._sgNode) {
+            this._syncVolume();
             this._sgNode.resume();
         }
     },
@@ -343,7 +398,7 @@ var VideoPlayer = cc.Class({
      * @method pause
      */
     pause: function () {
-        if(this._sgNode) {
+        if (this._sgNode) {
             this._sgNode.pause();
         }
     },
@@ -353,8 +408,8 @@ var VideoPlayer = cc.Class({
      * !#zh 如果一个视频正在播放，调用这个接口可以立马停止播放。
      * @method stop
      */
-    stop: function() {
-        if(this._sgNode) {
+    stop: function () {
+        if (this._sgNode) {
             this._sgNode.stop();
         }
     },
@@ -365,8 +420,8 @@ var VideoPlayer = cc.Class({
      * @method getDuration
      * @returns {Number}
      */
-    getDuration: function() {
-        if(this._sgNode) {
+    getDuration: function () {
+        if (this._sgNode) {
             return this._sgNode.duration();
         }
         return -1;
@@ -378,8 +433,8 @@ var VideoPlayer = cc.Class({
      * @method isPlaying
      * @returns {Boolean}
      */
-    isPlaying: function() {
-        if(this._sgNode) {
+    isPlaying: function () {
+        if (this._sgNode) {
             return this._sgNode.isPlaying();
         }
         return false;
