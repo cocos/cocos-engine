6c134875d7dbbb7f88da4edb72669194a02d5ead
CocosRobot <redmine@cocos2d-x.org>
2018-02-11 14:47:15 +0800

    [ci skip][AUTO]: updating jsbinding automatically (#1109)

--- native/cocos/scripting/js-bindings/auto/jsb_cocos2dx_auto.cpp
+++ native/cocos/scripting/js-bindings/auto/jsb_cocos2dx_auto.cpp
@@ -1197,23 +1197,38 @@ bool js_register_cocos2dx_Touch(se::Object* obj)
 se::Object* __jsb_cocos2d_Event_proto = nullptr;
 se::Class* __jsb_cocos2d_Event_class = nullptr;
 
-static bool js_cocos2dx_Event_isStopped(se::State& s)
+static bool js_cocos2dx_Event_reset(se::State& s)
 {
     cocos2d::Event* cobj = (cocos2d::Event*)s.nativeThisObject();
-    SE_PRECONDITION2(cobj, false, "js_cocos2dx_Event_isStopped : Invalid Native Object");
+    SE_PRECONDITION2(cobj, false, "js_cocos2dx_Event_reset : Invalid Native Object");
     const auto& args = s.args();
     size_t argc = args.size();
-    CC_UNUSED bool ok = true;
     if (argc == 0) {
-        bool result = cobj->isStopped();
-        ok &= boolean_to_seval(result, &s.rval());
-        SE_PRECONDITION2(ok, false, "js_cocos2dx_Event_isStopped : Error processing arguments");
+        cobj->reset();
         return true;
     }
     SE_REPORT_ERROR("wrong number of arguments: %d, was expecting %d", (int)argc, 0);
     return false;
 }
-SE_BIND_FUNC(js_cocos2dx_Event_isStopped)
+SE_BIND_FUNC(js_cocos2dx_Event_reset)
+
+static bool js_cocos2dx_Event_getCurrentTarget(se::State& s)
+{
+    cocos2d::Event* cobj = (cocos2d::Event*)s.nativeThisObject();
+    SE_PRECONDITION2(cobj, false, "js_cocos2dx_Event_getCurrentTarget : Invalid Native Object");
+    const auto& args = s.args();
+    size_t argc = args.size();
+    CC_UNUSED bool ok = true;
+    if (argc == 0) {
+        cocos2d::Node* result = cobj->getCurrentTarget();
+        ok &= native_ptr_to_seval<cocos2d::Node>((cocos2d::Node*)result, &s.rval());
+        SE_PRECONDITION2(ok, false, "js_cocos2dx_Event_getCurrentTarget : Error processing arguments");
+        return true;
+    }
+    SE_REPORT_ERROR("wrong number of arguments: %d, was expecting %d", (int)argc, 0);
+    return false;
+}
+SE_BIND_FUNC(js_cocos2dx_Event_getCurrentTarget)
 
 static bool js_cocos2dx_Event_getType(se::State& s)
 {
@@ -1233,23 +1248,23 @@ static bool js_cocos2dx_Event_getType(se::State& s)
 }
 SE_BIND_FUNC(js_cocos2dx_Event_getType)
 
-static bool js_cocos2dx_Event_getCurrentTarget(se::State& s)
+static bool js_cocos2dx_Event_isStopped(se::State& s)
 {
     cocos2d::Event* cobj = (cocos2d::Event*)s.nativeThisObject();
-    SE_PRECONDITION2(cobj, false, "js_cocos2dx_Event_getCurrentTarget : Invalid Native Object");
+    SE_PRECONDITION2(cobj, false, "js_cocos2dx_Event_isStopped : Invalid Native Object");
     const auto& args = s.args();
     size_t argc = args.size();
     CC_UNUSED bool ok = true;
     if (argc == 0) {
-        cocos2d::Node* result = cobj->getCurrentTarget();
-        ok &= native_ptr_to_seval<cocos2d::Node>((cocos2d::Node*)result, &s.rval());
-        SE_PRECONDITION2(ok, false, "js_cocos2dx_Event_getCurrentTarget : Error processing arguments");
+        bool result = cobj->isStopped();
+        ok &= boolean_to_seval(result, &s.rval());
+        SE_PRECONDITION2(ok, false, "js_cocos2dx_Event_isStopped : Error processing arguments");
         return true;
     }
     SE_REPORT_ERROR("wrong number of arguments: %d, was expecting %d", (int)argc, 0);
     return false;
 }
-SE_BIND_FUNC(js_cocos2dx_Event_getCurrentTarget)
+SE_BIND_FUNC(js_cocos2dx_Event_isStopped)
 
 static bool js_cocos2dx_Event_stopPropagation(se::State& s)
 {
@@ -1300,9 +1315,10 @@ bool js_register_cocos2dx_Event(se::Object* obj)
 {
     auto cls = se::Class::create("Event", obj, nullptr, _SE(js_cocos2dx_Event_constructor));
 
-    cls->defineFunction("isStopped", _SE(js_cocos2dx_Event_isStopped));
-    cls->defineFunction("getType", _SE(js_cocos2dx_Event_getType));
+    cls->defineFunction("reset", _SE(js_cocos2dx_Event_reset));
     cls->defineFunction("getCurrentTarget", _SE(js_cocos2dx_Event_getCurrentTarget));
+    cls->defineFunction("getType", _SE(js_cocos2dx_Event_getType));
+    cls->defineFunction("isStopped", _SE(js_cocos2dx_Event_isStopped));
     cls->defineFunction("stopPropagation", _SE(js_cocos2dx_Event_stopPropagation));
     cls->defineFinalizeFunction(_SE(js_cocos2d_Event_finalize));
     cls->install();
