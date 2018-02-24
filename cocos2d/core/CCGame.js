8dee57c7e309e5c0227740a579aeb07f8aa31a95
pandamicro <huabin.ling@cocos2d-x.org>
2018-02-24 17:45:02 +0800

    Exclusivly set webgl antialias option to false

--- cocos2d/core/CCGame.js
+++ cocos2d/core/CCGame.js
@@ -708,6 +708,7 @@ var game = {
         if (cc._renderType === game.RENDER_TYPE_WEBGL) {
             var opts = {
                 'stencil': true,
+                'antialias': false,
                 'alpha': cc.macro.ENABLE_TRANSPARENT_CANVAS
             };
             if (isWeChatGame) {
