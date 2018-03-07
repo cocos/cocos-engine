c826dd25488ed77d9a59195733deb61428562931
James Chen <jianhua.chen@cocos2d-x.org>
2018-03-07 10:02:35 +0800

    WebGL macro.

--- native/cocos/platform/CCGL.h
+++ native/cocos/platform/CCGL.h
@@ -30,6 +30,55 @@ THE SOFTWARE.
 
 #include "platform/CCPlatformConfig.h"
 
+#ifndef GL_UNPACK_FLIP_Y_WEBGL
+#define GL_UNPACK_FLIP_Y_WEBGL 0x9240
+#endif
+
+#ifndef GL_UNPACK_PREMULTIPLY_ALPHA_WEBGL
+#define GL_UNPACK_PREMULTIPLY_ALPHA_WEBGL 0x9241
+#endif
+
+#ifndef GL_CONTEXT_LOST_WEBGL
+#define GL_CONTEXT_LOST_WEBGL 0x9242
+#endif
+
+#ifndef GL_UNPACK_COLORSPACE_CONVERSION_WEBGL
+#define GL_UNPACK_COLORSPACE_CONVERSION_WEBGL 0x9243
+#endif
+
+#ifndef GL_BROWSER_DEFAULT_WEBGL
+#define GL_BROWSER_DEFAULT_WEBGL 0x9244
+#endif
+
+#ifndef GL_DEPTH_STENCIL_ATTACHMENT
+#define GL_DEPTH_STENCIL_ATTACHMENT 0x821A
+#endif
+
+/* Shader Precision-Specified Types */
+#ifndef GL_LOW_FLOAT
+#define GL_LOW_FLOAT                                     0x8DF0
+#endif
+
+#ifndef GL_MEDIUM_FLOAT
+#define GL_MEDIUM_FLOAT                                  0x8DF1
+#endif
+
+#ifndef GL_HIGH_FLOAT
+#define GL_HIGH_FLOAT                                    0x8DF2
+#endif
+
+#ifndef GL_LOW_INT
+#define GL_LOW_INT                                       0x8DF3
+#endif
+
+#ifndef GL_MEDIUM_INT
+#define GL_MEDIUM_INT                                    0x8DF4
+#endif
+
+#ifndef GL_HIGH_INT
+#define GL_HIGH_INT                                      0x8DF5
+#endif
+
 #if CC_TARGET_PLATFORM == CC_PLATFORM_MAC
 #include "platform/mac/CCGL-mac.h"
 #elif CC_TARGET_PLATFORM == CC_PLATFORM_IOS
