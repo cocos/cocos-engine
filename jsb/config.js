8f5f927a9517573ab7456cfe6823c57ad4ccaf00
minggo <ming@cocos2d-x.org>
2018-04-18 17:01:46 +0800

    optimize performance

---
+++ jsb/config.js
@@ -0,0 +1,39 @@
+let _stageOffset = 0;
+let _name2stageID = {};
+
+var config = {
+  addStage: function (name) {
+    // already added
+    if (_name2stageID[name] !== undefined) {
+      return;
+    }
+
+    let stageID = 1 << _stageOffset;
+    _name2stageID[name] = stageID;
+
+    _stageOffset += 1;
+
+    renderer.addStage(name);
+  },
+
+  stageID: function (name) {
+    let id = _name2stageID[name];
+    if (id === undefined) {
+      return -1;
+    }
+    return id;
+  },
+
+  stageIDs: function (nameList) {
+    let key = 0;
+    for (let i = 0; i < nameList.length; ++i) {
+      let id = _name2stageID[nameList[i]];
+      if (id !== undefined) {
+        key |= id;
+      }
+    }
+    return key;
+  }
+};
+
+module.exports = config;
--- jsb/renderer.js
+++ jsb/renderer.js
@@ -58,19 +58,45 @@ renderer.PARAM_TEXTURE_CUBE = 14;
 renderer.CLEAR_COLOR = 1;
 renderer.CLEAR_DEPTH = 2;
 renderer.CLEAR_STENCIL = 4;
-
-//renderer.addStage = renderer.Config.addStage;
-
-renderer.config = {
-	addStage: renderer.addStage,
-	// stageIDs: renderer.stageIDs,
-	stageIDs: renderer.stageIDs,
-	stageID: renderer.stageID
-};
+renderer.InputAssembler = require('./input-assembler');
+renderer.config = require('./config');
 renderer.Effect = require('./Effect');
 renderer.Technique = require('./Technique');
 renderer.Pass = require('./Pass');
 renderer.Model = require('./Model');
+
+var models = [];
+var sizeOfModel = 23;
+// length + 500 modles(8 for each model)
+var modlesData = new Float64Array(1 + 500*sizeOfModel);
+var fillModelData = function() {
+  if (models.length > 500)
+    modlesData = new FloatArray64(1 + models.length*sizeOfModel);
+
+  modlesData[0] = models.length;
+  var index = 1;
+  var model;
+  var worldMatrix;
+  var ia;
+  for (var i = 0, len = models.length; i < len; ++i) {
+    model = models[i];
+    modlesData[index++] = model._dynamicIA;
+    modlesData[index++] = model._viewID;
+    worldMatrix = model._node._getWorldRT4Native();
+    modlesData.set(worldMatrix, index);
+    index += 16;
+
+    // ia
+    ia = model._inputAssemblers[0];
+    modlesData[index++] = ia._vertexBuffer._nativePtr;
+    modlesData[index++] = ia._indexBuffer._nativePtr;
+    modlesData[index++] = ia._start;
+    modlesData[index++] = ia._count;
+
+    // effect
+    modlesData[index++] = model._effects[0]._nativePtr;
+  }
+}
 
 // ForwardRenderer adapter
 var _p;
@@ -79,45 +105,17 @@ _p = renderer.ForwardRenderer.prototype;
 _p._ctor = function(device, builtin) {
     this.init(device, builtin.programTemplates, builtin.defaultTexture, canvas.width, canvas.height);
 };
+_p.render = function(scene) {
+  fillModelData();
+  this.renderNative(scene, modlesData);
 
-// InputAssembler adapter
-_p = renderer.InputAssembler.prototype;
-_p._ctor = function(vb, ib, pt = gfx.PT_TRIANGLES) {
-    this.init(vb, ib, pt);
-};
-cc.defineGetterSetter(_p, "_vertexBuffer", _p.getVertexBuffer, _p.setVertexBuffer);
-cc.defineGetterSetter(_p, "_indexBuffer", _p.getIndexBuffer, _p.setIndexBuffer);
-cc.defineGetterSetter(_p, "_primitiveType", _p.getPrimitiveType, _p.setPrimitiveType);
-cc.defineGetterSetter(_p, "_start", _p.getStart, _p.setStart);
-cc.defineGetterSetter(_p, "_count", _p.getCount, _p.setCount);
+  models.length = 0;
+}
 
-// Scene
+// Scene 
 _p = renderer.Scene.prototype;
 _p.addModel = function(model) {
-  // var args = [];
-  // args.push(model._dynamicIA);
-  // args.push(model._viewID);
-  // args.push(model._node);
-
-  // var inputAssemberls = model._inputAssemblers;
-  // args.push(inputAssemberls.length);
-  // for (var i = 0, len = inputAssemberls.length; i < len; ++i) {
-  //   args.push(inputAssemberls[i])
-  // }
-
-  // var effects = model._effects;
-  // args.push(effects.length);
-  // for (var i = 0, len = effects.length; i < len; ++i) {
-  //   args.push(effects[i]._nativeObj);
-  // }
-
-  // FIXME: should handle the situation that, the length input assemblers or effects is greater than 1
-  // this.addModelNative.apply(this, args);
-  this.addModelNative(model._dynamicIA,
-                      model._viewID,
-                      model._node,
-                      model._inputAssemblers[0],
-                      model._effects[0]._nativeObj);
+  models.push(model); 
 }
 _p.removeModel = function() {}
 
