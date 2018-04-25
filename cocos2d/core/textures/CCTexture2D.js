e8b7b00a3c5fa1ec13628e965efa184901223b26
Knox <knox.h@qq.com>
2018-04-25 11:03:53 +0800

    fix wx cc.RenderTexture.create for engine/issues/2549 (#2590)

--- cocos2d/core/textures/CCTexture2D.js
+++ cocos2d/core/textures/CCTexture2D.js
@@ -803,7 +803,7 @@ game.once(game.EVENT_RENDERER_INITED, function () {
             var gl = this._gl;
             gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, premultiplyAlpha);
             if (
-                sys.platform === sys.WECHAT_GAME ||
+                (sys.platform === sys.WECHAT_GAME && !(img instanceof Uint8Array)) ||
                 sys.platform === sys.QQ_PLAY ||
                 img instanceof HTMLCanvasElement ||
                 img instanceof HTMLImageElement ||
