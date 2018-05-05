cab60f49848f03b7c67801d063491ccf9ad513f2
lionqu <guanghui8827@126.com>
2018-05-05 09:57:11 +0800

    improve runtime performance

--- cocos2d/core/label/CCSGLabel.js
+++ cocos2d/core/label/CCSGLabel.js
@@ -1259,9 +1259,6 @@ _ccsg.Label.pool = new JS.Pool(function (label) {
     label._fontHandle = "";
     label._labelType = 0;
     label._resetBMFont();
-    label._renderCmd._labelCanvas.width = 1;
-    label._renderCmd._labelCanvas.height = 1;
-    label._updateLabel();
     label._renderCmd._texture.releaseTexture();
     if (CC_DEV) {
         cc.assert(!label._parent, 'Recycling label\'s parent should be null!');
