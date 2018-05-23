45cf7dc1e1cc9cbf0491bc4c1096f40755879ceb
pandamicro <huabin.ling@cocos2d-x.org>
2018-05-23 09:56:36 +0800

    Fix issues of animation state and private node (#2717)
    * Fix private node position update issue
    
    * Fix animation state event API issues
    
    * Remove useless constant
    
    * Improve PrivateNode doc and fix its zIndex

--- cocos2d/core/CCPrivateNode.js
+++ cocos2d/core/CCPrivateNode.js
@@ -26,10 +26,12 @@
 'use strict';
 
 const Node = require('./CCNode');
+const RenderFlow = require('./renderer/render-flow');
 const renderEngine = require('./renderer/render-engine');
 const math = renderEngine.math;
 
-const ONE_DEGREE = Math.PI / 180;
+const LocalDirtyFlag = Node._LocalDirtyFlag;
+const POSITION_ON = 1 << 0;
 
 let _vec3_temp = math.vec3.create();
 
@@ -37,11 +39,21 @@ let _vec3_temp = math.vec3.create();
  * !#en
  * Class of private entities in Cocos Creator scenes.<br/>
  * The PrivateNode is hidden in editor, and completely transparent to users.<br/>
- * It has the minimum zIndex and the position is affected by parent's anchor point.
+ * It's normally used as Node's private content created by components in parent node.<br/>
+ * So in theory private nodes are not children, they are part of the parent node.<br/>
+ * Private node have two important characteristics:<br/>
+ * 1. It has the minimum z index and cannot be modified, because they can't be displayed over real children.<br/>
+ * 2. The positioning of private nodes is also special, they will consider the left bottom corner of the parent node's bounding box as the origin of local coordinates.<br/>
+ *    In this way, they can be easily kept inside the bounding box.<br/>
+ * Currently, it's used by RichText component and TileMap component.
  * !#zh
  * Cocos Creator 场景中的私有节点类。<br/>
  * 私有节点在编辑器中不可见，对用户透明。<br/>
- * 它有着最小的渲染排序的 Z 轴深度，同时位置受父节点的锚点影响。
+ * 通常私有节点是被一些特殊的组件创建出来作为父节点的一部分而存在的，理论上来说，它们不是子节点，而是父节点的组成部分。<br/>
+ * 私有节点有两个非常重要的特性：<br/>
+ * 1. 它有着最小的渲染排序的 Z 轴深度，并且无法被更改，因为它们不能被显示在其他正常子节点之上。<br/>
+ * 2. 它的定位也是特殊的，对于私有节点来说，父节点包围盒的左下角是它的局部坐标系原点，这个原点相当于父节点的位置减去它锚点的偏移。这样私有节点可以比较容易被控制在包围盒之中。<br/>
+ * 目前在引擎中，RichText 和 TileMap 都有可能生成私有节点。
  * @class PrivateNode
  * @constructor
  * @param {String} name
@@ -51,45 +63,88 @@ let PrivateNode = cc.Class({
     name: 'cc.PrivateNode',
     extends: Node,
 
+    properties: {
+        x: {
+            get () {
+                return this._originPos.x;
+            },
+            set (value) {
+                var localPosition = this._originPos;
+                if (value !== localPosition.x) {
+                    localPosition.x = value;
+                    this._posDirty(true);
+                }
+            },
+            override: true
+        },
+        y: {
+            get () {
+                return this._originPos.y;
+            },
+            set (value) {
+                var localPosition = this._originPos;
+                if (value !== localPosition.y) {
+                    localPosition.y = value;
+                    this._posDirty(true);
+                }
+            },
+            override: true
+        },
+        zIndex: {
+            get () {
+                return cc.macro.MIN_ZINDEX;
+            },
+            set () {
+                cc.warnID(1638);
+            },
+            override: true
+        },
+    },
+
     /**
      * @method constructor
      * @param {String} [name]
      */
     ctor (name) {
-        this.zIndex = cc.macro.MIN_ZINDEX;
+        this._localZOrder = cc.macro.MIN_ZINDEX << 16;
+        this._originPos = cc.v2();
     },
 
-    _onAnchorChangedHandler () {
-        this._localMatDirty = true;
+    _posDirty (sendEvent) {
+        this.setLocalDirty(LocalDirtyFlag.POSITION);
+        this._renderFlag |= RenderFlow.FLAG_LOCAL_TRANSFORM;
+        if (sendEvent === true && (this._eventMask & POSITION_ON)) {
+            this.emit(EventType.POSITION_CHANGED);
+        }
     },
 
     _updateLocalMatrix() {
-        if (this._localMatDirty) {
-            // Update transform
-            let t = this._matrix;
-            _vec3_temp.x = this._position.x - (this.parent._anchorPoint.x - 0.5) * this.parent._contentSize.width;
-            _vec3_temp.y = this._position.y - (this.parent._anchorPoint.y - 0.5) * this.parent._contentSize.height;
-            _vec3_temp.z = this._position.z;    
-            math.mat4.fromRTS(t, this._quat, _vec3_temp, this._scale);
+        if (!this._localMatDirty) return;
 
-            // skew
-            if (this._skewX || this._skewY) {
-                let a = t.m00, b = t.m01, c = t.m04, d = t.m05;
-                let skx = Math.tan(this._skewX * ONE_DEGREE);
-                let sky = Math.tan(this._skewY * ONE_DEGREE);
-                if (skx === Infinity)
-                    skx = 99999999;
-                if (sky === Infinity)
-                    sky = 99999999;
-                t.m00 = a + c * sky;
-                t.m01 = b + d * sky;
-                t.m04 = c + a * skx;
-                t.m05 = d + b * skx;
-            }
-            this._localMatDirty = false;
-            // Register dirty status of world matrix so that it can be recalculated
-            this._worldMatDirty = true;
+        // Position correction for transform calculation
+        this._position.x = this._originPos.x - (this.parent._anchorPoint.x - 0.5) * this.parent._contentSize.width;
+        this._position.y = this._originPos.y - (this.parent._anchorPoint.y - 0.5) * this.parent._contentSize.height;
+
+        this._super();
+    },
+
+    getPosition () {
+        return new cc.Vec2(this._originPos);
+    },
+
+    setPosition (x, y) {
+        if (y === undefined) {
+            x = x.x;
+            y = x.y;
         }
+
+        var pos = this._originPos;
+        if (pos.x === x && pos.y === y) {
+            return;
+        }
+        pos.x = x;
+        pos.y = y;
+        this._posDirty(true);
     },
 
     setParent(value) {
@@ -97,21 +152,19 @@ let PrivateNode = cc.Class({
         this._super(value);
         if (oldParent !== value) {
             if (oldParent) {
-                oldParent.off(Node.EventType.ANCHOR_CHANGED, this._onAnchorChangedHandler, this);
+                oldParent.off(Node.EventType.ANCHOR_CHANGED, this._posDirty, this);
             }
             if (value) {
-                value.on(Node.EventType.ANCHOR_CHANGED, this._onAnchorChangedHandler, this);
+                value.on(Node.EventType.ANCHOR_CHANGED, this._posDirty, this);
             }
         }
     },
 
-    _updateOrderOfArrival() {
-        let arrivalOrder = 0;
-        this._localZOrder = (this._localZOrder & 0xffff0000) | arrivalOrder;
-    },
-
+    // do not update order of arrival
+    _updateOrderOfArrival() {},
 });
 
 cc.js.getset(PrivateNode.prototype, "parent", PrivateNode.prototype.getParent, PrivateNode.prototype.setParent);
+cc.js.getset(PrivateNode.prototype, "position", PrivateNode.prototype.getPosition, PrivateNode.prototype.setPosition);
 
 cc.PrivateNode = module.exports = PrivateNode;
