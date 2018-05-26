6ae3ab2edd355155c45efcc7e63852e7a7a2252d
pandamicro <huabin.ling@cocos2d-x.org>
2018-05-26 18:09:21 +0800

    Update to 2.0.0 preview

--- CCBoot.js
+++ CCBoot.js
@@ -31,7 +31,7 @@
  * @property {String} ENGINE_VERSION
  */
 var engineVersion;
-engineVersion = '2.0.0 alpha5';
+engineVersion = '2.0.0 preview';
 window['CocosEngine'] = cc.ENGINE_VERSION = engineVersion;
 
 require('./cocos2d/core/utils');
