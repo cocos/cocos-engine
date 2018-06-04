160cbe8af3d4628556ea2e8c6b805d21ada28709
minggo <xiaoming.zhang@chukong-inc.com>
2018-06-04 17:50:30 +0800

    template adpat (#1284)

--- native/cocos/scripting/js-bindings/manual/jsb_module_register.cpp
+++ native/cocos/scripting/js-bindings/manual/jsb_module_register.cpp
@@ -23,23 +23,7 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-//
-//  js_module_register.cpp
-//  cocos2d_js_bindings
-//
-//  Created by James Chen on 7/11/17.
-//
-//
-
-#define USE_VIDEO 1
-#define USE_WEBVIEW 1
-#define USE_EDIT_BOX 1
-#define USE_CREATOR_PHYSICS 1
-#define USE_CREATOR_CAMERA 1
-#define USE_CREATOR_GRAPHICS 1
 #define USE_AUDIO 1
-#define USE_SPINE 1
-#define USE_DRAGON_BONES 1
 #define USE_NET_WORK 1
 
 
@@ -54,27 +38,16 @@
 #include "cocos/scripting/js-bindings/manual/jsb_global.h"
 #include "cocos/scripting/js-bindings/manual/jsb_node.hpp"
 #include "cocos/scripting/js-bindings/manual/jsb_conversions.hpp"
+
+#if USE_NET_WORK
 #include "cocos/scripting/js-bindings/manual/jsb_xmlhttprequest.hpp"
 #include "cocos/scripting/js-bindings/manual/jsb_websocket.hpp"
 #include "cocos/scripting/js-bindings/manual/jsb_socketio.hpp"
-
-#if USE_VIDEO
-#include "cocos/scripting/js-bindings/auto/jsb_cocos2dx_experimental_video_auto.hpp"
-#endif
-
-#if USE_WEBVIEW
-#include "cocos/scripting/js-bindings/auto/jsb_cocos2dx_experimental_webView_auto.hpp"
-#endif
-
-#if USE_CREATOR_PHYSICS
-#include "cocos/scripting/js-bindings/manual/jsb_box2d_manual.hpp"
-#include "cocos/scripting/js-bindings/auto/jsb_box2d_auto.hpp"
-#endif
+#endif // USE_NET_WORK
 
 #if USE_AUDIO
 #include "cocos/scripting/js-bindings/auto/jsb_cocos2dx_audioengine_auto.hpp"
 #endif
-
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
 #include "cocos/scripting/js-bindings/manual/JavaScriptObjCBridge.h"
@@ -84,21 +57,10 @@
 #include "cocos/scripting/js-bindings/manual/JavaScriptJavaBridge.h"
 #endif
 
+
 #include "cocos2d.h"
 
 using namespace cocos2d;
-
-static bool run_prepare_script(se::Object* global)
-{
-    jsb_run_script("script/jsb_prepare.js");
-    return true;
-}
-
-static bool run_boot_script(se::Object* global)
-{
-    jsb_run_script("script/jsb_boot.js");
-    return true;
-}
 
 bool jsb_register_all_modules()
 {
@@ -117,71 +79,25 @@ bool jsb_register_all_modules()
 
     se->addRegisterCallback(jsb_register_global_variables);
 
-    se->addRegisterCallback(run_prepare_script);
-
-    // se->addRegisterCallback(register_all_cocos2dx);
-    // se->addRegisterCallback(jsb_register_Node_manual);
-    // se->addRegisterCallback(register_all_cocos2dx_manual);
-    // se->addRegisterCallback(JSB_register_opengl);
-
-#if USE_AUDIO
-    // se->addRegisterCallback(register_all_cocos2dx_audioengine);
-#endif
-
-#if USE_VIDEO || USE_WEBVIEW || USE_EDIT_BOX
-    // se->addRegisterCallback(register_all_cocos2dx_ui);
-    // se->addRegisterCallback(register_ui_manual);
-#endif
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && !defined(CC_TARGET_OS_TVOS)
-#if USE_VIDEO
-    // se->addRegisterCallback(register_all_cocos2dx_experimental_video);
-#endif
-    
-#if USE_WEBVIEW
-    // se->addRegisterCallback(register_all_cocos2dx_experimental_webView);
-#endif
-#endif
-
-#if USE_SPINE
-    // se->addRegisterCallback(register_all_cocos2dx_spine);
-    // se->addRegisterCallback(register_all_spine_manual);
-#endif
-    
-#if USE_DRAGON_BONES
-    // se->addRegisterCallback(register_all_cocos2dx_dragonbones);
-    // se->addRegisterCallback(register_all_dragonbones_manual);
-#endif
-
-#if USE_CREATOR_PHYSICS
-    // se->addRegisterCallback(register_all_box2dclasses);
-    // se->addRegisterCallback(register_all_box2d_manual);
-    
-    // se->addRegisterCallback(register_all_creator_physics);
-    // se->addRegisterCallback(register_all_creator_physics_manual);
-#endif
-
-#if USE_NET_WORK
-    // se->addRegisterCallback(register_all_cocos2dx_network);
-    // se->addRegisterCallback(register_all_xmlhttprequest);
-    // se->addRegisterCallback(register_all_websocket);
-    // se->addRegisterCallback(register_all_socketio);
-#endif
-    
-
-    // se->addRegisterCallback(register_all_cocos2dx_extension);
-    // se->addRegisterCallback(register_all_cocos2dx_extension_manual);
-
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-    // se->addRegisterCallback(register_javascript_objc_bridge);
+    se->addRegisterCallback(register_javascript_objc_bridge);
 #endif
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-    // se->addRegisterCallback(register_javascript_java_bridge);
+    se->addRegisterCallback(register_javascript_java_bridge);
 #endif
 
-    // run_boot_script has to be at last.
-    se->addRegisterCallback(run_boot_script);
+#if USE_AUDIO
+    se->addRegisterCallback(register_all_cocos2dx_audioengine);
+#endif
+
+
+#if USE_NET_WORK
+    se->addRegisterCallback(register_all_cocos2dx_network);
+    se->addRegisterCallback(register_all_xmlhttprequest);
+    se->addRegisterCallback(register_all_websocket);
+    se->addRegisterCallback(register_all_socketio);
+#endif
 
     se->addAfterCleanupHook([](){
         PoolManager::getInstance()->getCurrentPool()->clear();
--- native/templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ native/templates/js-template-default/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -32,13 +32,6 @@
 #include "cocos/scripting/js-bindings/manual/jsb_global.h"
 #include "cocos/scripting/js-bindings/jswrapper/SeApi.h"
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && PACKAGE_AS
-#include "SDKManager.h"
-#include "jsb_anysdk_protocols_auto.hpp"
-#include "manualanysdkbindings.hpp"
-using namespace anysdk::framework;
-#endif
-
 USING_NS_CC;
 
 AppDelegate::AppDelegate()
@@ -48,41 +41,10 @@ AppDelegate::AppDelegate()
 AppDelegate::~AppDelegate()
 {
     ScriptEngineManager::destroyInstance();
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && PACKAGE_AS
-    SDKManager::getInstance()->purge();
-#endif
-}
-
-void AppDelegate::initGLContextAttrs()
-{
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
-    
-    GLView::setGLContextAttrs(glContextAttrs);
 }
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
-#if CC_TARGET_PLATFORM == CC_PLATFORM_IOS && PACKAGE_AS
-    SDKManager::getInstance()->loadAllPlugins();
-#endif
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-#if(CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-        glview = GLViewImpl::create("HelloJavascript");
-#else
-        glview = GLViewImpl::createWithRect("HelloJavascript", cocos2d::Rect(0,0,900,640));
-#endif
-        director->setOpenGLView(glview);
-    }
-    
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
-
-    ScriptingCore* sc = ScriptingCore::getInstance();
-    ScriptEngineManager::getInstance()->setScriptEngine(sc);
-
     se::ScriptEngine* se = se::ScriptEngine::getInstance();
 
     jsb_set_xxtea_key("");
@@ -100,14 +62,15 @@ bool AppDelegate::applicationDidFinishLaunching()
 
     jsb_register_all_modules();
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && PACKAGE_AS
-    se->addRegisterCallback(register_all_anysdk_framework);
-    se->addRegisterCallback(register_all_anysdk_manual);
-#endif
-
     se->start();
 
+    se::AutoHandleScope hs;
+    jsb_run_script("builtin/dist/jsb.js");
     jsb_run_script("main.js");
+
+    se->addAfterCleanupHook([](){
+        JSBClassType::destroy();
+    });
 
     return true;
 }
