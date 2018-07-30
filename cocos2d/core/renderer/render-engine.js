11c53907347b030a472d15400682b72c6a5cf377
pandamicro <huabin.ling@cocos2d-x.org>
2018-07-30 20:14:34 +0800

    Use mediump by default

--- cocos2d/core/renderer/render-engine.js
+++ cocos2d/core/renderer/render-engine.js
@@ -12835,7 +12835,7 @@ var ProgramLib = function ProgramLib(device, templates, chunks) {
   if ( chunks === void 0 ) chunks = {};
 
   this._device = device;
-  this._precision = "precision highp float;\n";
+  this._precision = "precision mediump float;\n";
 
   // register templates
   this._templates = {};
