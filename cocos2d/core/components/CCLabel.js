d1453efabca682bbd02a842efecae879cf73a58e
pandamicro <huabin.ling@cocos2d-x.org>
2018-07-31 19:41:33 +0800

    Fix Label not responding to node size change issue

--- cocos2d/core/components/CCLabel.js
+++ cocos2d/core/components/CCLabel.js
@@ -458,9 +458,17 @@ let Label = cc.Class({
             this.useSystemFont = true;
         }
 
+        // Keep track of Node size
+        this.node.on(cc.Node.EventType.SIZE_CHANGED, this._updateRenderData, this);
+
         this._checkStringEmpty();
         this._updateAssembler();
         this._activateMaterial();
+    },
+
+    onDisable () {
+        this._super();
+        this.node.off(cc.Node.EventType.SIZE_CHANGED, this._updateRenderData, this);
     },
 
     onDestroy () {
