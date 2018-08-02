93101a7a0e835479d36bf3e726cb129e6bdec545
W-MTZing <39689887+W-MTZing@users.noreply.github.com>
2018-08-02 17:28:49 +0800

    add loadImage error callback (#1406) (#1436)
    * add loadImage error callback
    
    * fix format

--- native/cocos/scripting/js-bindings/manual/jsb_global.cpp
+++ native/cocos/scripting/js-bindings/manual/jsb_global.cpp
@@ -731,13 +731,22 @@ bool jsb_global_load_image(const std::string& path, const se::Value& callbackVal
                 free(imageData);
             }
             else
+            {
                 loadSucceed = img->initWithImageFile(fullPath);
-            
-            auto imgInfo = createImageInfo(img);
+            }
+
+            struct ImageInfo* imgInfo = nullptr;
+            if(loadSucceed)
+            {
+                imgInfo = createImageInfo(img);
+            }
+
             Application::getInstance()->getScheduler()->performFunctionInCocosThread([=](){
+                se::AutoHandleScope hs;
+                se::ValueArray seArgs;
+
                 if (loadSucceed)
                 {
-                    se::AutoHandleScope hs;
                     se::HandleObject retObj(se::Object::createPlainObject());
                     Data data;
                     data.copy(imgInfo->data, imgInfo->length);
@@ -768,18 +777,18 @@ bool jsb_global_load_image(const std::string& path, const se::Value& callbackVal
                     retObj->setProperty("glFormat", se::Value(imgInfo->glFormat));
                     retObj->setProperty("glInternalFormat", se::Value(imgInfo->glInternalFormat));
                     retObj->setProperty("glType", se::Value(imgInfo->type));
-                    se::ValueArray seArgs;
+
                     seArgs.push_back(se::Value(retObj));
-                    callbackVal.toObject()->call(seArgs, nullptr);
+
+                    delete imgInfo;
                 }
                 else
                 {
                     SE_REPORT_ERROR("initWithImageFile: %s failed!", path.c_str());
-                    assert(false);
                 }
 
+                callbackVal.toObject()->call(seArgs, nullptr);
                 img->release();
-                delete imgInfo;
             });
 
         });
