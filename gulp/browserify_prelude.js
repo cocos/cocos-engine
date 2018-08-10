487ee65cd74d314a9200b409d724e5f7da30670d
2youyouo2 <501251991@qq.com>
2018-08-10 17:15:52 +0800

    remove previousRequire from engine prelude

--- gulp/browserify_prelude.js
+++ gulp/browserify_prelude.js
@@ -10,9 +10,6 @@
 // orig method which is the requireuire for previous bundles
 
 (function outer (modules, cache, entry) {
-    // Save the require from previous bundle to this closure if any
-    var previousRequire = typeof require == "function" && require;
-
     function newRequire(name, jumped){
         var module = cache[name];
         if(!module) {
@@ -23,12 +20,6 @@
                 // that was added to the page.
                 var currentRequire = typeof require == "function" && require;
                 if (!jumped && currentRequire) return currentRequire(name, true);
-
-                // If there are other bundles on this page the require from the
-                // previous one is saved to 'previousRequire'. Repeat this as
-                // many times as there are bundles until the module is found or
-                // we exhaust the require chain.
-                if (previousRequire) return previousRequire(name, true);
 
                 // this module is excluded from engine
                 return undefined;
