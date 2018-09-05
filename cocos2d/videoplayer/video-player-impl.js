af39059638439662a579a215bddec05f0d0866c1
Knox <knox.h@qq.com>
2018-09-05 14:08:01 +0800

    fix ios browser video player for fireball/issues/8343 (#3196)
    * fix ios browser video player for fireball/issues/8343
    
    * add tips

--- cocos2d/videoplayer/video-player-impl.js
+++ cocos2d/videoplayer/video-player-impl.js
@@ -30,6 +30,18 @@ const math = renderEngine.math;
 
 let _mat4_temp = math.mat4.create();
 
+// ios brower need to wait until the video's loadedmetadata event
+// has been fired before calling webkitEnterFullScreen()
+function triggerFullScene (video, enable) {
+    if (!video) return;
+    if (enable) {
+        video.webkitEnterFullscreen && video.webkitEnterFullscreen();
+    }
+    else {
+        video.webkitExitFullscreen && video.webkitExitFullscreen();
+    }
+}
+
 let VideoPlayerImpl = cc.Class({
     name: 'VideoPlayerImpl',
 
@@ -40,6 +52,9 @@ let VideoPlayerImpl = cc.Class({
         this._video = null;
         this._url = '';
 
+        this._fullScreenEnabled = false;
+
+        this._loadedmeta = false;
         this._loaded = false;
         this._visible = false;
         // 有一些浏览器第一次播放视频需要特殊处理，这个标记用来标识是否播放过
@@ -66,6 +81,10 @@ let VideoPlayerImpl = cc.Class({
         //binding event
         let cbs = this.__eventListeners;
         cbs.loadedmetadata = function () {
+            this._loadedmeta = true;
+            if (sys.os === sys.OS_IOS && sys.isBrowser) {
+                triggerFullScene(video, self._fullScreenEnabled);
+            }
             self._dispatchEvent(VideoPlayerImpl.EventType.META_LOADED);
         };
         cbs.ended = function () {
@@ -205,6 +224,7 @@ let VideoPlayerImpl = cc.Class({
         this._loaded = false;
         this._played = false;
         this._playing = false;
+        this._loadedmeta = false;
 
         source = document.createElement("source");
         source.src = path;
@@ -344,15 +364,14 @@ let VideoPlayerImpl = cc.Class({
     },
 
     setFullScreenEnabled: function (enable) {
-        let video = this._video;
-        if (!video) return;
-
+        let video =  this._video;
+        if (!video) {
+            return;
+        }
         if (sys.os === sys.OS_IOS && sys.isBrowser) {
-            if (enable) {
-                video.webkitEnterFullscreen && video.webkitEnterFullscreen();
-            }
-            else {
-                video.webkitExitFullscreen && video.webkitExitFullscreen();
+            this._fullScreenEnabled = enable;
+            if (this._loadedmeta) {
+                triggerFullScene(video, enable);
             }
         }
         else {
@@ -363,11 +382,10 @@ let VideoPlayerImpl = cc.Class({
                 cc.screen.exitFullScreen(video);
             }
         }
-
     },
 
     isFullScreenEnabled: function () {
-        cc.logID(7701);
+        return this._fullScreenEnabled;
     },
 
     setEventListener: function (event, callback) {
