3bdc22687ed136919a3063eecf41cc8b882897e7
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2018-09-12 14:54:55 +0800

    ES6 private node

--- cocos/scene-graph/index.js
+++ cocos/scene-graph/index.js
@@ -27,5 +27,6 @@
 export { default as BaseNode } from './base-node';
 export { default as Node } from './node';
 export { default as NodeActivator } from './node-activator';
+export { default as PrivateNode } from './private-node';
+export { default as Scene } from './scene';
 export { default as find } from './find';
-export { default as scene } from './scene';
