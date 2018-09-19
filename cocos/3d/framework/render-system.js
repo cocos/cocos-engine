8823a9f95452f6d1dea686fb19025d0220d6dbbf
Huabing.Xu <dabingnn@gmail.com>
2018-09-19 10:52:05 +0800

    add renderable and rendersystem

---
+++ cocos/3d/framework/render-system.js
@@ -0,0 +1,27 @@
+import render from '../../renderer';
+export default class RenderSystem {
+    constructor() {
+        this._scene = new render.Scene();
+    }
+
+    init() {
+
+    }
+
+    add(comp) {
+
+    }
+
+    remove(comp) {
+
+    }
+
+    update(dt) {
+        cc.render.render(this._scene);
+        this._scene.reset();
+    }
+
+    get scene() {
+        return this._scene;
+    }
+}
