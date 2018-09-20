06c91d68124d22f7583862e65cb2ae75f36d46e0
JoneLau <32630749+JoneLau@users.noreply.github.com>
2018-09-20 16:29:22 +0800

    decorator component (#3277) (#3277)

--- cocos/3d/framework/model-component.js
+++ cocos/3d/framework/model-component.js
@@ -1,173 +1,272 @@
-import { Component } from '../ecs';
-import renderer from '../renderer';
+/****************************************************************************
+ Copyright (c) 2013-2016 Chukong Technologies Inc.
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
 
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+// @ts-check
+import Component from '../../components/CCComponent';
+import renderer from '../../renderer/index';
+import { _decorator } from '../../core/data/index';
+import Material from '../assets/material';
+import { Enum } from '../../core/value-types/index';
+const { ccclass, property } = _decorator;
+/**
+ * @typedef {import ('../assets/mesh').default} Mesh
+ */
+
+/**
+ * !#en The Model Component
+ *
+ * !#ch 模型组件
+ * @class ModelComponent
+ * @extends Component
+ */
+@ccclass('cc.ModelComponent')
 export default class ModelComponent extends Component {
-  onInit() {
-    this._models = [];
+    @property
+    _materials = [];
+
+    @property
+    _mesh = null;
+
+    @property
+    _shadowCastingMode = ModelComponent.ShadowCastingMode.Off;
+
+    @property
+    _receiveShadows = false;
 
     /**
-     * **@schema** The material of the model
-     * @type {Material}
+     * !#en The material of the model
+     *
+     * !#ch 模型材质
+     * @type {Material[]}
      */
-    this.materials = this._materials;
+    @property
+    get materials() {
+        return this._materials;
+    }
+
+    set materials(val) {
+        this._materials = val;
+        this._updateModelParams();
+    }
+
     /**
-     * **@schema** The mesh of the model
+     * !#en The mesh of the model
+     *
+     * !#ch 模型网格
      * @type {Mesh}
      */
-    this.mesh = this._mesh;
+    @property
+    get mesh() {
+        return this._mesh;
+    }
+
+    set mesh(val) {
+        this._mesh = val;
+        this._updateModels();
+    }
+
     /**
-     * **@schema** The shadow casting mode
-     * @type {string}
+     * !#en The shadow casting mode
+     *
+     * !#ch 投射阴影方式
+     * @type {String}
      */
-    this.shadowCastingMode = this._shadowCastingMode;
+    @property
+    get shadowCastingMode() {
+        return this._shadowCastingMode;
+    }
+
+    set shadowCastingMode(val) {
+        this._shadowCastingMode = val;
+        this._updateCastShadow();
+    }
+
     /**
-     * **@schema** Does this model receive shadows?
-     * @type {boolean}
+     * !#en Does this model receive shadows?
+     *
+     * !#ch 是否接受阴影?
+     * @type {Boolean}
      */
-    this.receiveShadows = this._receiveShadows;
-  }
-
-  onEnable() {
-    for (let i = 0; i < this._models.length; ++i) {
-      this._app.scene.addModel(this._models[i]);
-    }
-  }
-
-  onDisable() {
-    for (let i = 0; i < this._models.length; ++i) {
-      this._app.scene.removeModel(this._models[i]);
-    }
-  }
-
-  getMaterial(idx) {
-    if (this._materials.length === 0) {
-      return null;
+    @property
+    get receiveShadows() {
+        return this._receiveShadows;
     }
 
-    if (idx < this._materials.length) {
-      return this._materials[idx];
+    set receiveShadows(val) {
+        this._receiveShadows = val;
+        this._updateReceiveShadow();
     }
 
-    return this._materials[this._materials.length - 1];
-  }
+    /**
+     * !#en Shadow projection mode
+     *
+     * !#ch 阴影投射方式
+     * @static
+     * @enum ModelComponent.ShadowCastingMode
+     */
+    static ShadowCastingMode = Enum({
+        /**
+         * !#en
+         *
+         * !#ch 关闭阴影投射
+         * @property Off
+         * @readonly
+         * @type {String}
+         */
+        Off: 'off',
+        /**
+         * !#en
+         *
+         * !#ch 开启阴影投射，当阴影光产生的时候
+         * @property On
+         * @readonly
+         * @type {String}
+         */
+        On: 'on',
+        /**
+         * !#en
+         *
+         * !#ch 可以从网格的任意一遍投射出阴影
+         * @property TwoSided
+         * @readonly
+         * @type {String}
+         */
+        TwoSided: 'twoSided',
+        /**
+         * !#en
+         *
+         * !#ch 只显示阴影
+         * @property ShadowsOnly
+         * @readonly
+         * @type {String}
+         */
+        ShadowsOnly: 'shadowsOnly',
+    });
 
-  get material() {
-    return this.getMaterial(0);
-  }
-  set material(val) {
-    if (this._materials.length === 1 && this._materials[0] === val) {
-      return;
+    onEnable() {
+        // for (let i = 0; i < this._models.length; ++i) {
+        //     this._app.scene.addModel(this._models[i]);
+        // }
     }
 
-    this._materials[0] = val;
-
-    if (this._models.length > 0) {
-      this._models[0].setEffect(val.effectInst);
-    }
-  }
-
-  _updateModels() {
-    let meshCount = this._mesh ? this._mesh.subMeshCount : 0;
-    let oldModels = this._models;
-
-    this._models = new Array(meshCount);
-    for (let i = 0; i < meshCount; ++i) {
-      let model = new renderer.Model();
-      model.createBoundingShape(this._mesh._minPos, this._mesh._maxPos);
-      this._models[i] = model;
+    onDisable() {
+        // for (let i = 0; i < this._models.length; ++i) {
+        //     this._app.scene.removeModel(this._models[i]);
+        // }
     }
 
-    this._updateModelParams();
+    /**
+     * !#en Returns the material corresponding to the sequence number
+     *
+     * !#ch 返回相对应序号的材质
+     * @param {Number} idx - Look for the material list number
+     */
+    getMaterial(idx) {
+        if (this._materials.length === 0) {
+            return null;
+        }
 
-    if (this.enabled) {
-      for (let i = 0; i < oldModels.length; ++i) {
-        this._app.scene.removeModel(oldModels[i]);
-      }
-      for (let i = 0; i < this._models.length; ++i) {
-        this._app.scene.addModel(this._models[i]);
-      }
+        if (idx < this._materials.length) {
+            return this._materials[idx];
+        }
+
+        return this._materials[this._materials.length - 1];
     }
-  }
 
-  _updateModelParams() {
-    for (let i = 0; i < this._models.length; ++i) {
-      let model = this._models[i];
-      let material = this.getMaterial(i);
-      let inputAssembler = this._mesh.getSubMesh(i);
-
-      model.setInputAssembler(inputAssembler);
-      model.setEffect(material ? material.effectInst : null);
-      model.setNode(this._entity);
+    get material() {
+        return this.getMaterial(0);
     }
-  }
 
-  _updateCastShadow() {
-    if (this._shadowCastingMode === 'off') {
-      for (let i = 0; i < this._models.length; ++i) {
-        let model = this._models[i];
-        model._castShadow = false;
-      }
-    } else if (this._shadowCastingMode === 'on') {
-      for (let i = 0; i < this._models.length; ++i) {
-        let model = this._models[i];
-        model._castShadow = true;
-      }
-    } else {
-      console.warn(`ShadowCastingMode ${this._shadowCastingMode} is not supported.`);
-    }
-  }
+    set material(val) {
+        if (this._materials.length === 1 && this._materials[0] === val) {
+            return;
+        }
 
-  _updateReceiveShadow() {
-    for (let i = 0; i < this._models.length; ++i) {
-      let model = this._models[i];
-      if (model._defines['USE_SHADOW_MAP'] != undefined) {
-        model._effect.define('USE_SHADOW_MAP', this._receiveShadows);
-      }
+        this._materials[0] = val;
+
+        if (this._models.length > 0) {
+            this._models[0].setEffect(val.effectInst);
+        }
     }
-  }
+
+    _updateModels() {
+        let meshCount = this._mesh ? this._mesh.subMeshCount : 0;
+        let oldModels = this._models;
+
+        this._models = new Array(meshCount);
+        for (let i = 0; i < meshCount; ++i) {
+            let model = new renderer.Model();
+            model.createBoundingShape(this._mesh._minPos, this._mesh._maxPos);
+            this._models[i] = model;
+        }
+
+        this._updateModelParams();
+
+        // if (this.enabled) {
+        //     for (let i = 0; i < oldModels.length; ++i) {
+        //         this._app.scene.removeModel(oldModels[i]);
+        //     }
+        //     for (let i = 0; i < this._models.length; ++i) {
+        //         this._app.scene.addModel(this._models[i]);
+        //     }
+        // }
+    }
+
+    _updateModelParams() {
+        for (let i = 0; i < this._models.length; ++i) {
+            let model = this._models[i];
+            let material = this.getMaterial(i);
+            let inputAssembler = this._mesh.getSubMesh(i);
+
+            model.setInputAssembler(inputAssembler);
+            model.setEffect(material ? material.effectInst : null);
+            model.setNode(this.node);
+        }
+    }
+
+    _updateCastShadow() {
+        if (this._shadowCastingMode === 'off') {
+            for (let i = 0; i < this._models.length; ++i) {
+                let model = this._models[i];
+                model._castShadow = false;
+            }
+        } else if (this._shadowCastingMode === 'on') {
+            for (let i = 0; i < this._models.length; ++i) {
+                let model = this._models[i];
+                model._castShadow = true;
+            }
+        } else {
+            console.warn(`ShadowCastingMode ${this._shadowCastingMode} is not supported.`);
+        }
+    }
+
+    _updateReceiveShadow() {
+        for (let i = 0; i < this._models.length; ++i) {
+            let model = this._models[i];
+            if (model._defines['USE_SHADOW_MAP'] != undefined) {
+                model._effect.define('USE_SHADOW_MAP', this._receiveShadows);
+            }
+        }
+    }
 }
-
-ModelComponent.schema = {
-  materials: {
-    type: 'asset',
-    default: [],
-    array: true,
-    set(val) {
-      this._materials = val;
-      this._updateModelParams();
-    }
-  },
-
-  mesh: {
-    type: 'asset',
-    default: null,
-    set(val) {
-      this._mesh = val;
-      this._updateModels();
-    }
-  },
-
-  shadowCastingMode: {
-    type: 'enums',
-    default: 'off',
-    options: [
-      'off',
-      'on',
-      'twoSided',
-      'shadowsOnly'
-    ],
-    set(val) {
-      this._shadowCastingMode = val;
-      this._updateCastShadow();
-    }
-  },
-
-  receiveShadows: {
-    type: 'boolean',
-    default: false,
-    set(val) {
-      this._receiveShadows = val;
-      this._updateReceiveShadow();
-    }
-  }
-};
--- cocos/3d/framework/skybox-component.js
+++ cocos/3d/framework/skybox-component.js
@@ -1,51 +1,112 @@
-import { Component } from '../ecs';
-import renderer from '../renderer';
-import { box } from '../primitives';
-import Material from '../assets/material';
-import enums from '../renderer';
+/****************************************************************************
+ Copyright (c) 2013-2016 Chukong Technologies Inc.
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
 
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+// @ts-check
+import Component from '../../components/CCComponent';
+import renderer from '../../renderer/index';
+// import { box } from '../primitive/index';
+import Material from '../assets/material';
+import { _decorator } from '../../core/data/index';
+const { ccclass, property } = _decorator;
+
+/**
+ * !#en The Skybox Component
+ *
+ * !#ch 天空盒组件
+ * @class SkyboxComponent
+ * @extends Component
+ */
+@ccclass('cc.SkyboxComponent')
 export default class SkyboxComponent extends Component {
+  @property
+  _material = null;
+
+  @property
+  _cubeMap = null;
+
+  /**
+   * !#en The material of the model
+   *
+   * !#ch 模型普通材质
+   * @type {Material}
+   */
+  get material() {
+    return this._material;
+  }
+
+  set material(val) {
+    this._material = val;
+    if (!this._material) return;
+    this._updateMaterialParams();
+    this._model.setEffect(val.effectInst);
+  }
+
+  /**
+   * !#en The material of the model
+   *
+   * !#ch 模型 cubeMap 材质
+   * @type {Material}
+   */
+  get cubeMap() {
+    return this._cubeMap;
+  }
+
+  set cubeMap(val) {
+    this._cubeMap = val;
+    this._updateMaterialParams();
+  }
+
   constructor() {
     super();
 
     this._model = new renderer.Model();
     this._attachedCamera = null;
-
-    /**
-     * **@schema** The material of the model
-     * @type {Material}
-     */
-    this.material = this._material;
-    /**
-     * **@schema** The material of the model
-     * @type {TextureCube}
-     */
-    this.cubeMap = this._cubeMap;
   }
-  
-  onInit() {
-    this._model.setNode(this._entity);
 
-    let ia = renderer.createIA(this._app.device, box(2, 2, 2, {
-      widthSegments: 1,
-      heightSegments: 1,
-      lengthSegments: 1,
-    }));
-    this._model.setInputAssembler(ia);
+  onLoad() {
+    // this._model.setNode(this.node);
 
-    if (this._material === null) {
-      this._material = new Material();
-      this._material.effect = this._app.assets.get('builtin-effect-skybox');
-    }
+    // let ia = renderer.createIA(this._app.device, box(2, 2, 2, {
+    //   widthSegments: 1,
+    //   heightSegments: 1,
+    //   lengthSegments: 1,
+    // }));
+    // this._model.setInputAssembler(ia);
 
-    this._updateMaterialParams();
-    this._model.setEffect(this._material.effectInst);
+    // if (this._material === null) {
+    //   this._material = new Material();
+    //   this._material.effect = this._app.assets.get('builtin-effect-skybox');
+    // }
+
+    // this._updateMaterialParams();
+    // this._model.setEffect(this._material.effectInst);
   }
 
   onEnable() {
-    if (this._entity != null) {
-      let cameraComponent = this._entity.getComp('Camera');
-      if (cameraComponent != null && (cameraComponent._clearFlags & enums.CLEAR_SKYBOX)) {
+    if (this.node != null) {
+        let cameraComponent = this.getComponent('Camera');
+      if (cameraComponent != null && (cameraComponent._clearFlags & renderer.CLEAR_SKYBOX)) {
         this._attachedCamera = cameraComponent._camera;
         this._attachedCamera._clearModel = this._model;
       }
@@ -68,25 +129,3 @@ export default class SkyboxComponent extends Component {
     }
   }
 }
-
-SkyboxComponent.schema = {
-  material: {
-    type: 'asset',
-    default: null,
-    set(val) {
-      this._material = val;
-      if (!this._material) return;
-      this._updateMaterialParams();
-      this._model.setEffect(val.effectInst);
-    }
-  },
-
-  cubeMap: {
-    type: 'asset',
-    default: null,
-    set(val) {
-      this._cubeMap = val;
-      this._updateMaterialParams();
-    }
-  }
-};
