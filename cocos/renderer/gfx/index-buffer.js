24e099885ff8f5efee4ee3b235cf3947d99198ab
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2018-09-28 18:46:58 +0800

    proper IA data extraction (#3323)

--- cocos/renderer/gfx/index-buffer.js
+++ cocos/renderer/gfx/index-buffer.js
@@ -1,6 +1,6 @@
 import { enums } from './enums';
 
-export default class IndexBuffer {
+class IndexBuffer {
   /**
    * @constructor
    * @param {Device} device
@@ -29,7 +29,6 @@ export default class IndexBuffer {
     // update
     this._glID = device._gl.createBuffer();
     this.update(0, data);
-    if (CC_EDITOR) this._data = data || 0;
 
     // stats
     device._stats.ib += this._bytes;
@@ -92,3 +91,44 @@ export default class IndexBuffer {
     return this._numIndices;
   }
 }
+
+if (CC_EDITOR) {
+  let _type2fn = {
+    [enums.ATTR_TYPE_INT8]: 'getInt8',
+    [enums.ATTR_TYPE_UINT8]: 'getUint8',
+    [enums.ATTR_TYPE_INT16]: 'getInt16',
+    [enums.ATTR_TYPE_UINT16]: 'getUint16',
+    [enums.ATTR_TYPE_INT32]: 'getInt32',
+    [enums.ATTR_TYPE_UINT32]: 'getUint32',
+    [enums.ATTR_TYPE_FLOAT32]: 'getFloat32'
+  };
+  let littleEndian = (function() {
+    let buffer = new ArrayBuffer(2);
+    new DataView(buffer).setInt16(0, 256, true);
+    // Int16Array uses the platform's endianness.
+    return new Int16Array(buffer)[0] === 256;
+  })();
+  let createCache = function(ib, data) {
+    if (!data) return [];
+    let icount = data.byteLength / ib._bytesPerIndex;
+    let result = new Array(icount);
+    for (let i = 0; i < icount; i++) {
+      let fname = _type2fn[ib._format];
+      result[i] = data[fname](i * ib._bytesPerIndex, littleEndian);
+    }
+    return result;
+  };
+  IndexBuffer.prototype.do_update = IndexBuffer.prototype.update;
+  IndexBuffer.prototype.update = function(offset, data) {
+    this.do_update(offset, data);
+    // don't need to create for IA pools
+    if (data && this._bytes !== data.byteLength) return;
+    if (data instanceof ArrayBuffer)
+      data = new DataView(data);
+    else if (ArrayBuffer.isView(data) && !(data instanceof DataView))
+      data = new DataView(data.buffer, data.byteOffset, data.byteLength);
+    this._data = createCache(this, data);
+  };
+}
+
+export default IndexBuffer;
--- cocos/renderer/gfx/vertex-buffer.js
+++ cocos/renderer/gfx/vertex-buffer.js
@@ -1,4 +1,6 @@
-export default class VertexBuffer {
+import { enums } from './enums';
+
+class VertexBuffer {
   /**
    * @constructor
    * @param {Device} device
@@ -19,7 +21,6 @@ export default class VertexBuffer {
     // update
     this._glID = device._gl.createBuffer();
     this.update(0, data);
-    if (CC_EDITOR) this._data = data || 0;
 
     // stats
     device._stats.vb += this._bytes;
@@ -81,3 +82,56 @@ export default class VertexBuffer {
     return this._numVertices;
   }
 }
+
+if (CC_EDITOR) {
+  let _type2fn = {
+    [enums.ATTR_TYPE_INT8]: 'getInt8',
+    [enums.ATTR_TYPE_UINT8]: 'getUint8',
+    [enums.ATTR_TYPE_INT16]: 'getInt16',
+    [enums.ATTR_TYPE_UINT16]: 'getUint16',
+    [enums.ATTR_TYPE_INT32]: 'getInt32',
+    [enums.ATTR_TYPE_UINT32]: 'getUint32',
+    [enums.ATTR_TYPE_FLOAT32]: 'getFloat32'
+  };
+  let littleEndian = (function() {
+    let buffer = new ArrayBuffer(2);
+    new DataView(buffer).setInt16(0, 256, true);
+    // Int16Array uses the platform's endianness.
+    return new Int16Array(buffer)[0] === 256;
+  })();
+  let getAttrAt = function(data, idx, fmt, out = []) {
+    let offset = idx * fmt.stride + fmt.offset;
+    let bytes = fmt.bytes / fmt.num;
+    let fname = _type2fn[fmt.type];
+    for (let i = 0; i < fmt.num; i++) {
+      out[i] = data[fname](offset + i * bytes, littleEndian);
+    }
+    return out;
+  };
+  let createAttrCache = function(vb, data, attr) {
+    if (!data) return [];
+    let fmt = vb._format.element(attr);
+    let vcount = data.byteLength / fmt.stride;
+    let result = new Array(vcount * fmt.num);
+    let t = new Array(fmt.num);
+    for (let i = 0; i < vcount; i++) {
+      getAttrAt(data, i, fmt, t);
+      for (let j = 0; j < fmt.num; j++)
+        result[i * fmt.num + j] = t[j];
+    }
+    return result;
+  };
+  VertexBuffer.prototype.do_update = VertexBuffer.prototype.update;
+  VertexBuffer.prototype.update = function(offset, data) {
+    this.do_update(offset, data);
+    // don't need to create for IA pools
+    if (data && this._bytes !== data.byteLength) return;
+    if (data instanceof ArrayBuffer)
+      data = new DataView(data);
+    else if (ArrayBuffer.isView(data) && !(data instanceof DataView))
+      data = new DataView(data.buffer, data.byteOffset, data.byteLength);
+    this[enums.ATTR_POSITION] = createAttrCache(this, data, enums.ATTR_POSITION);
+  };
+}
+
+export default VertexBuffer;
