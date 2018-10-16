d2fe49c331e0845e47029d6b449a0e25f583e974
2youyouo2 <501251991@qq.com>
2018-10-16 16:03:27 +0800

    fixed sync rotaionX/rotationY

--- cocos2d/core/CCNode.js
+++ cocos2d/core/CCNode.js
@@ -1297,11 +1297,10 @@ let NodeDefines = {
             else {
                 math.quat.fromEuler(quat, this._rotationX, this._rotationY, 0);
             }
+            this._rotationX = this._rotationY = undefined;
         }
-        // Update rotation from quaternion
-        else {
-            this._quat.getEulerAngles(this._eulerAngles);
-        }
+        
+        this._quat.getEulerAngles(this._eulerAngles);
 
         // Upgrade from 2.0.0 preview 4 & earlier versions
         // TODO: Remove after final version
