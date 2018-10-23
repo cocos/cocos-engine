254b7c1eb90612a10c3bee71e96d0e80c42956b3
cary <39078800+caryliu1999@users.noreply.github.com>
2018-10-23 14:32:31 +0800

    Fix rendering errors for pre-multiplied images using dynamicAtlas. (#3411)
    * Fix rendering errors for pre-multiplied images using dynamicAtlas.
    
    * Fix setting filter in RenderTexture is invalid.
    
    * Modified FilterIndex to private variable.

--- cocos2d/core/assets/CCRenderTexture.js
+++ cocos2d/core/assets/CCRenderTexture.js
@@ -37,6 +37,9 @@ let RenderTexture = cc.Class({
         opts.images = undefined;
         opts.wrapS = this._wrapS;
         opts.wrapT = this._wrapT;
+        opts.premultiplyAlpha = this._premultiplyAlpha;
+        opts.minFilter = Texture2D._FilterIndex[this._minFilter];
+        opts.magFilter = Texture2D._FilterIndex[this._magFilter];
 
         if (!this._texture) {
             this._texture = new renderer.Texture2D(renderer.device, opts);
@@ -86,7 +89,8 @@ let RenderTexture = cc.Class({
             width: texture.width,
             height: texture.height,
             level: 0,
-            flipY: false
+            flipY: false,
+            premultiplyAlpha: texture._premultiplyAlpha
         })
     },
 
