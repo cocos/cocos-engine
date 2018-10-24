cb0605a5b39aea57c0dd7a3c0e5c133471dd068c
Coco <crazypad@qq.com>
2018-10-24 19:07:42 +0800

    Update the url links

--- cocos2d/core/graphics/helper.js
+++ cocos2d/core/graphics/helper.js
@@ -1,7 +1,7 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
 
- http://www.cocos.com
+ https://www.cocos.com/
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated engine source code (the "Software"), a limited,
