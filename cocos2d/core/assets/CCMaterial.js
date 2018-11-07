2fe1b0a98c7e039200b32b9f8453c4443346acb2
2youyouo2 <501251991@qq.com>
2018-11-07 18:19:31 +0800

    add material and effect

---
+++ cocos2d/core/assets/CCMaterial.js
@@ -0,0 +1,242 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+  worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+  not use Cocos Creator software for developing other software or tools that's
+  used for developing games. You are not granted to publish, distribute,
+  sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+// @ts-check
+
+const Asset = require('./CCAsset');
+const Texture = require('./CCTexture2D');
+const effects = require('../../renderer/effects');
+const Color = require('../value-types/color');
+import Effect from '../../renderer/effect';
+
+let Material = cc.Class({
+    name: 'cc.Material',
+    extends: Asset,
+
+    ctor (effectName) {
+        if (effectName) this.effectName = effectName;
+    },
+
+    properties: {
+        _effect: null,
+        _effectName: '',
+        _defines: {
+            default: {},
+            type: Object
+        },
+        _props: {
+            default: {},
+            type: Object
+        },
+
+        effectName: {
+            get () {
+                return this._effectName;
+            },
+            set (val) {
+                if (this._effectName !== val) {
+                    this._effectName = val;
+                    const effectAsset = effects[val];
+                    if (!effectAsset) {
+                        cc.warn(`no effect named '${val}' found`);
+                        return;
+                    }
+                    this._effect = Effect.parseEffect(effectAsset);
+                }
+            }
+        },
+
+        effect: {
+            get () {
+                return this._effect;
+            }
+        }
+    },
+
+    statics: {
+        getInstantiatedMaterial(mat, rndCom) {
+            if (mat._owner === rndCom) {
+                return mat;
+            }
+            else {
+                let instance = new Material();
+                instance.copy(mat);
+                instance._native = mat._native + ' (Instance)';
+                instance._owner = rndCom;
+                return instance;
+            }
+        }
+    },
+
+    /**
+     *
+     * @param {Material} mat
+     */
+    copy(mat) {
+        this.effectName = mat.effectName;
+
+        for (let name in mat._defines) {
+            this.define(name, mat._defines[name]);
+        }
+
+        for (let name in mat._props) {
+            this.setProperty(name, mat._props[name]);
+        }
+    },
+
+    /**
+     *
+     * @param {string} name
+     * @param {*} val
+     */
+    setProperty(name, val) {
+        this._props[name] = val;
+
+        if (this._effect) {
+            if (val instanceof Texture) {
+                this._effect.setProperty(name, val._texture);
+            } 
+            else if (val instanceof Color) {
+                let color = this._effect.getProperty(name);
+                if (!color) {
+                    color = cc.color();
+                }
+                color.r = val.r / 255;
+                color.g = val.g / 255;
+                color.b = val.b / 255;
+                color.a = val.a / 255;
+                this._effect.setProperty(name, color);
+            }
+            else {
+                this._effect.setProperty(name, val);
+            }
+        }
+    },
+
+    getProperty(name) {
+        return this._props[name];
+    },
+
+    /**
+     *
+     * @param {string} name
+     * @param {*} val
+     */
+    define(name, val) {
+        this._defines[name] = val;
+        if (this._effect) {
+            this._effect.define(name, val);
+        }
+    },
+
+    getDefine(name) {
+        return this._defines[name];
+    },
+
+    destroy() {
+        // TODO: what should we do here ???
+        return super.destroy();
+    },
+
+    updateHash (val) {
+        if (val) {
+            this._hash = val;
+            return;
+        }
+
+        let str = this._effectName;
+        let props = this._props;
+        let defines = this._defines;
+        for (let name in props) {
+            let v = props[name];
+            if (typeof v === 'function') continue;
+            if (v) {
+                if (v instanceof cc.Texture2D) {
+                    v = v._id;
+                }
+                else {
+                    v = v.toString();
+                }
+            }
+            else {
+                v = '';
+            }
+            
+            str += ';' + name + ':' + v;
+        }
+        for (let name in defines) {
+            let v = defines[name];
+            if (typeof v === 'function') continue;
+            str += ';' + name + ':' + (v ? v.toString() : '');
+        }
+        this._hash = str;
+    },
+
+    _serialize () {
+        return {	
+            effectName: this._effectName,
+            props: Editor.serialize(this._props),
+            defines: Editor.serialize(this._defines),
+        };
+    },
+
+    _deserialize: (function () {
+        function loadAsset (handle, target, name, uuid) {
+            let depend = {
+                set asset (asset) {
+                    target.setProperty(name, asset);
+                }
+            };
+
+            handle.result.push(depend, 'asset', uuid);
+        }
+
+        return function (data, handle) {
+            this.effectName = data.effectName;
+    
+            let props = cc.deserialize(data.props);
+            let propNames = Object.keys(props);
+            for (let i = 0; i < propNames.length; i++) {
+                let name = propNames[i];
+                let prop = props[name];
+                
+                if (prop.uuid) {
+                    // load prop as an asset
+                    loadAsset(handle, this, name, prop.uuid);
+                    continue;
+                }
+                this.setProperty(name, props[name]);
+            }
+    
+            let defines = cc.deserialize(data.defines);
+            let defineNames = Object.keys(defines);
+            for (let i = 0; i < defineNames.length; i++) {
+                let name = defineNames[i];
+                this.define(name, defines[name]);
+            }
+        };
+    })()
+});
+
+module.exports = cc.Material = Material;
---
+++ cocos2d/renderer/effect.js
@@ -0,0 +1,270 @@
+// Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+import { Vec2, Vec3, Vec4, Color, Mat4 } from '../core/value-types';
+import { config, renderer, gfx } from '../core/renderer/render-engine';
+const { Technique, Pass } = renderer;
+const enums = renderer;
+
+const { Texture2D, TextureCube } = gfx;
+const CCObject = cc.Object;
+
+let _typeMap = {
+    [enums.PARAM_INT]: Number,
+    [enums.PARAM_INT2]: Vec2,
+    [enums.PARAM_INT3]: Vec3,
+    [enums.PARAM_INT4]: Vec4,
+    [enums.PARAM_FLOAT]: Number,
+    [enums.PARAM_FLOAT2]: Vec2,
+    [enums.PARAM_FLOAT3]: Vec3,
+    [enums.PARAM_FLOAT4]: Vec4,
+    [enums.PARAM_COLOR3]: Color,
+    [enums.PARAM_COLOR4]: Color,
+    [enums.PARAM_MAT4]: Mat4,
+    [enums.PARAM_TEXTURE_2D]: Texture2D,
+    [enums.PARAM_TEXTURE_CUBE]: TextureCube,
+    number: Number,
+    boolean: Boolean,
+    default: CCObject
+};
+let getInstanceType = function(t) { return _typeMap[t] || _typeMap.default; };
+let typeTest = function(value, type) {
+    let instanceType = getInstanceType(type);
+    switch (typeof value) {
+        case 'object': return (value === null) || (value instanceof instanceType);
+        case 'number': return instanceType === Number;
+        default: return false;
+    }
+};
+
+class Effect {
+    /**
+     * @param {Array} techniques
+     */
+    constructor(techniques, properties = {}, defines = [], dependencies = []) {
+        this._techniques = techniques;
+        this._properties = properties;
+        this._defines = defines;
+        this._dependencies = dependencies;
+
+        // TODO: check if params is valid for current technique???
+    }
+
+    clear() {
+        this._techniques.length = 0;
+        this._properties = null;
+        this._defines.length = 0;
+    }
+
+    getTechnique(stage) {
+        let stageID = config.stageID(stage);
+        if (stageID === -1) {
+            return null;
+        }
+
+        for (let i = 0; i < this._techniques.length; ++i) {
+            let tech = this._techniques[i];
+            if (tech.stageIDs & stageID) {
+                return tech;
+            }
+        }
+
+        return null;
+    }
+
+    getProperty(name) {
+        if (!this._properties[name]) {
+            console.warn(`Failed to get property ${name}, property not found.`);
+            return null;
+        }
+        return this._properties[name].value;
+    }
+
+    setProperty(name, value) {
+        let prop = this._properties[name];
+        if (!prop) {
+            console.warn(`Failed to set property ${name}, property not found.`);
+            return;
+        } else if (!typeTest(value, prop.type)) {
+            console.warn(`Failed to set property ${name}, property type mismatch.`);
+            return;
+        }
+        this._properties[name].value = value;
+    }
+
+    getDefine(name) {
+        for (let i = 0; i < this._defines.length; ++i) {
+            let def = this._defines[i];
+            if (def.name === name) {
+                return def.value;
+            }
+        }
+
+        console.warn(`Failed to get define ${name}, define not found.`);
+        return null;
+    }
+
+    define(name, value) {
+        for (let i = 0; i < this._defines.length; ++i) {
+            let def = this._defines[i];
+            if (def.name === name) {
+                def.value = value;
+                return;
+            }
+        }
+
+        console.warn(`Failed to set define ${name}, define not found.`);
+    }
+
+    extractDefines(out = {}) {
+        for (let i = 0; i < this._defines.length; ++i) {
+            let def = this._defines[i];
+            out[def.name] = def.value;
+        }
+
+        return out;
+    }
+
+    extractDependencies(out = {}) {
+        for (let i = 0; i < this._dependencies.length; ++i) {
+            let dep = this._dependencies[i];
+            out[dep.define] = dep.extension;
+        }
+
+        return out;
+    }
+}
+
+let cloneObjArray = function(val) { return val.map(obj => Object.assign({}, obj)); };
+
+let _ctorMap = {
+    [Number]: v => v || 0,
+    [Boolean]: v => v || false,
+    [Vec2]: v => v ? cc.v2(v[0], v[1]) : cc.v2(),
+    [Vec3]: v => v ? cc.v3(v[0], v[1], v[2]) : cc.v3(),
+    [Vec4]: v => v ? cc.v4(v[0], v[1], v[2], v[3]) : cc.v4(),
+    [Color]: v => v ? cc.color(v[0] * 255, v[1] * 255, v[2] * 255,
+        (v[3] || 1) * 255) : cc.color(),
+    [Mat4]: v => v ? cc.mat4(
+            v[0],  v[1],  v[2],  v[3],
+            v[4],  v[5],  v[6],  v[7],
+            v[8],  v[9],  v[10], v[11],
+            v[12], v[13], v[14], v[15],
+        ) : cc.mat4(),
+    [Texture2D]: () => null,
+    [TextureCube]: () => null,
+    [CCObject]: () => null
+};
+let getInstanceCtor = function(t) { return _ctorMap[getInstanceType(t)]; };
+
+let getInvolvedPrograms = function(json) {
+    let programs = [], lib = cc.renderer._forward._programLib;
+    json.techniques.forEach(tech => {
+        tech.passes.forEach(pass => {
+            programs.push(lib.getTemplate(pass.program));
+        });
+    });
+    return programs;
+};
+let parseProperties = function(json, programs) {
+    let props = {};
+    for (let prop in json.properties) {
+        let info = json.properties[prop], type;
+        // always try getting the type from shaders first
+        if (info.tech !== undefined && info.pass !== undefined) {
+            let pname = json.techniques[info.tech].passes[info.pass].program;
+            let program = programs.find(p => p.name === pname);
+            type = program.uniforms.find(u => u.name === prop);
+        } else {
+            for (let i = 0; i < programs.length; i++) {
+                type = programs[i].uniforms.find(u => u.name === prop);
+                if (type) break;
+            }
+        }
+        // the property is not defined in all the shaders used in techs
+        if (!type) {
+            console.warn(`illegal property: ${prop}`);
+            continue;
+        }
+        // TODO: different param with same name for different passes
+        // TODO: property alias (display name)
+        props[prop] = getInstanceCtor(info.type || type)(info.value);
+    }
+    return props;
+};
+
+Effect.getPropertyClassName = function(json) {
+    return `cc.Effect.${json._uuid}.Props`;
+};
+Effect.getDefineClassName = function(json) {
+    return `cc.Effect.${json._uuid}.Defines`;
+};
+
+Effect.parseType = function(json) {
+    let lib = cc.renderer._forward._programLib, 
+        propTypes = {},
+        defineTypes = {};
+
+    let props = json.properties;
+    // load default type from involved shaders
+    json.techniques.forEach(tech => {
+        tech.passes.forEach(pass => {
+            let program = lib.getTemplate(pass.program);
+            program.defines.forEach(define => {
+                defineTypes[define.name] = getInstanceType(define.type);
+            });
+            program.uniforms.forEach(uniform => {
+                let name = uniform.name, prop = props[name];
+                if (!prop) return; // don't add if not in the property list
+                propTypes[name] = getInstanceType(prop.type || uniform.type);
+            });
+        });
+    });
+
+    return {
+        props: propTypes,
+        defines: defineTypes
+    };
+};
+
+Effect.parseEffect = function(json) {
+    let programs = getInvolvedPrograms(json);
+    // techniques
+    let techNum = json.techniques.length;
+    let techniques = new Array(techNum);
+    for (let j = 0; j < techNum; ++j) {
+        let tech = json.techniques[j];
+        let passNum = tech.passes.length;
+        let passes = new Array(passNum);
+        for (let k = 0; k < passNum; ++k) {
+            let pass = tech.passes[k];
+            passes[k] = new Pass(pass.program);
+            passes[k].setDepth(pass.depthTest, pass.depthWrite);
+            passes[k].setCullMode(pass.cullMode);
+            if (pass.blend) passes[k].setBlend(pass.blendEq, pass.blendSrc,
+                pass.blendDst, pass.blendAlphaEq, pass.blendSrcAlpha, pass.blendDstAlpha);
+        }
+        techniques[j] = new Technique(tech.stages, passes, tech.layer);
+    }
+    // uniforms
+    let props = parseProperties(json, programs), uniforms = {};
+    programs.forEach(p => {
+        p.uniforms.forEach(u => {
+            let name = u.name, uniform = uniforms[name] = Object.assign({}, u);
+            // user defined type override
+            if (props[name]) uniform.type = json.properties[name].type;
+            uniform.value = props[name] || getInstanceCtor(u.type)();
+        });
+    });
+    // defines
+    let defines = programs.reduce((acc, cur) => acc = acc.concat(cur.defines), []);
+    defines = cloneObjArray(defines);
+    defines.forEach(d => d.value = getInstanceCtor(d.type)());
+    // extensions
+    let extensions = programs.reduce((acc, cur) => acc = acc.concat(cur.extensions), []);
+    extensions = cloneObjArray(extensions);
+
+    return new Effect(techniques, uniforms, defines, extensions);
+};
+
+export default Effect;
+cc.Effect = Effect;
