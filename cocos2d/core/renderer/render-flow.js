0059c0ccd8d1f8f93ede75a1fe6f9e757180df03
Coco <crazypad@qq.com>
2018-11-08 17:23:50 +0800

    add the transform render update when the node opacity change from 0

--- cocos2d/core/renderer/render-flow.js
+++ cocos2d/core/renderer/render-flow.js
@@ -97,9 +97,9 @@ _proto._children = function (node) {
     let children = node._children;
     for (let i = 0, l = children.length; i < l; i++) {
         let c = children[i];
+        c._renderFlag |= worldTransformFlag | worldOpacityFlag;
         if (!c._activeInHierarchy || c._opacity === 0) continue;
         _cullingMask = c._cullingMask = c.groupIndex === 0 ? cullingMask : 1 << c.groupIndex;
-        c._renderFlag |= worldTransformFlag | worldOpacityFlag;
 
         // TODO: Maybe has better way to implement cascade opacity
         c._color.a = c._opacity * _walker.parentOpacity;
