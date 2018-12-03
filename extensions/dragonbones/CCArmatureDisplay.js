51b782875e6135beeeafa6471308a3507dcb0e70
mac <1053210246@qq.com>
2018-12-03 19:52:07 +0800

    Change copyright time

--- extensions/dragonbones/CCArmatureDisplay.js
+++ extensions/dragonbones/CCArmatureDisplay.js
@@ -1,6 +1,5 @@
 /****************************************************************************
- Copyright (c) 2016 Chukong Technologies Inc.
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2018 Xiamen Yaji Software Co., Ltd.
 
  https://www.cocos.com/
 
