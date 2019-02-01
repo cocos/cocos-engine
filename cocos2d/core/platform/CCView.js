942bbe216d4e4272379b33c1462aa9c22268931e
caryliu <caryliu1999@gmail.com>
2019-02-01 17:58:29 +0800

    Cancel the maximum limit of pixelRatio.

--- cocos2d/core/platform/CCView.js
+++ cocos2d/core/platform/CCView.js
@@ -1081,7 +1081,7 @@ cc.ContainerStrategy = cc.Class({
         // Setup pixel ratio for retina display
         var devicePixelRatio = view._devicePixelRatio = 1;
         if (view.isRetinaEnabled())
-            devicePixelRatio = view._devicePixelRatio = Math.min(2, window.devicePixelRatio || 1);
+            devicePixelRatio = view._devicePixelRatio = window.devicePixelRatio || 1;
         // Setup canvas
         locCanvas.width = w * devicePixelRatio;
         locCanvas.height = h * devicePixelRatio;
