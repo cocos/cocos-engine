618c2bdfa412b73ffff1f8388fd0fcae7481866e
Coco <crazypad@qq.com>
2019-02-15 17:45:52 +0800

    remove the error in the WeChat sub project

--- cocos2d/core/platform/CCSys.js
+++ cocos2d/core/platform/CCSys.js
@@ -712,7 +712,7 @@ function initSys () {
         var _supportWebGL = _supportWebp = false;
         try {
             var _canvas = document.createElement("canvas");
-            _supportWebGL = _canvas.getContext("webgl");
+            _supportWebGL = CC_WECHATGAMESUB ? null : _canvas.getContext("webgl");
             _supportWebp = _canvas.toDataURL('image/webp').startsWith('data:image/webp');
         }
         catch (err) { }
