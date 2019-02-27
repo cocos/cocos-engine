547a1346fdd3b9b75a6e3c88351ce9388302feb6
Leslie <leslieleigh@outlook.com>
2019-02-27 15:18:03 +0800

    fix animation

---
+++ cocos/pipeline/cc-binding.ts
@@ -0,0 +1,7 @@
+
+const MAX_BINDING = 24;
+
+export enum UBOBinding {
+    CC_SKINNING = MAX_BINDING - 6,
+    CC_JOINTS_TEXTURE = MAX_BINDING - 7,
+}
--- cocos/renderer/models/model-uniforms.ts
+++ cocos/renderer/models/model-uniforms.ts
@@ -1,5 +1,6 @@
 import { GFXType } from '../../gfx/define';
 import { GFXUniformBlock, GFXUniformSampler } from '../../gfx/shader';
+import { UBOBinding as UBOBindings } from '../../pipeline/cc-binding';
 
 export class SkinningUBO {
     public static MAT_JOINT_OFFSET: number = 0;
@@ -8,13 +9,13 @@ export class SkinningUBO {
     public static SIZE: number = SkinningUBO.COUNT * 4;
 
     public static BLOCK: GFXUniformBlock = {
-        binding: 28, name: 'CCSkinning', members: [
+        binding: UBOBindings.CC_SKINNING, name: 'CCSkinning', members: [
             { name: 'cc_matJoint', type: GFXType.MAT4, count: 128 },
             { name: 'cc_jointsTextureSize', type: GFXType.FLOAT4, count: 1 },
         ],
     };
 
     public static JOINT_TEXTURE: GFXUniformSampler = {
-        binding: 27, name: 'cc_jointsTexture', type: GFXType.SAMPLER2D, count: 1,
+        binding: UBOBindings.CC_JOINTS_TEXTURE, name: 'cc_jointsTexture', type: GFXType.SAMPLER2D, count: 1,
     };
 }
