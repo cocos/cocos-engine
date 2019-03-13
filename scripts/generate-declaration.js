9c0b09e8f2746943faa538cf31ff79b913cd2889
Leslie <leslieleigh@outlook.com>
2019-03-13 11:49:40 +0800

    Support generate .d.ts

---
+++ scripts/generate-declaration.js
@@ -0,0 +1,23 @@
+const { spawn } = require('child_process');
+const { join } = require('path');
+
+const tscExecutableName = process.platform === 'win32' ? 'tsc.cmd' : 'tsc';
+const tscExecutablePath = join(__dirname, '..', 'node_modules', '.bin', tscExecutableName);
+const tscConfigPath = join(__dirname, '..', 'tsconfig-gendecls.json');
+
+const tsc = spawn(tscExecutablePath, [
+    '-p',
+    tscConfigPath,
+]);
+
+tsc.stdout.on('data', (data) => {
+    console.log(data.toString());
+});
+
+tsc.stderr.on('data', (data) => {
+    console.log(data.toString());
+});
+
+tsc.on('exit', (code) => {
+    console.log(`Exited with code ${code}`);
+});
