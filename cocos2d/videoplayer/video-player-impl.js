5a8feb0a6af3145da44b71be242e004284dc500f
knox <knox.h@qq.com>
2019-03-15 15:18:33 +0800

    fix video player currentTime invalid bug for engine/issues/4025

--- cocos2d/videoplayer/video-player-impl.js
+++ cocos2d/videoplayer/video-player-impl.js
@@ -101,10 +101,7 @@ let VideoPlayerImpl = cc.Class({
                 return;
             }
             self._playing = false;
-            if (self._ignorePause) {
-                return;
-            }
-            else {
+            if (!self._ignorePause) {
                 self._dispatchEvent(VideoPlayerImpl.EventType.PAUSED);
             }
         };
@@ -119,10 +116,11 @@ let VideoPlayerImpl = cc.Class({
         video.addEventListener("click", cbs.click);
 
         function onCanPlay () {
-            if (self._loaded || self._loadedmeta || self._playing)
+            if (self._loaded || self._playing)
                 return;
             let video = self._video;
-            if (video.readyState === READY_STATE.HAVE_ENOUGH_DATA) {
+            if (video.readyState === READY_STATE.HAVE_ENOUGH_DATA ||
+                video.readyState === READY_STATE.HAVE_METADATA) {
                 video.currentTime = 0;
                 self._loaded = true;
                 self._dispatchEvent(VideoPlayerImpl.EventType.READY_TO_PLAY);
