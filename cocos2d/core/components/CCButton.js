88644ae5d93fe70f109422678874ef3412f1f905
knox <knox.h@qq.com>
2019-04-03 11:33:43 +0800

    revert #3659, fixed 2d-tasks/issues/1265

--- cocos2d/core/components/CCButton.js
+++ cocos2d/core/components/CCButton.js
@@ -697,7 +697,7 @@ let Button = cc.Class({
         if (!this.interactable || !this.enabledInHierarchy) return;
 
         if (this._pressed) {
-            cc.Component.EventHandler.emitEvents(this.clickEvents, event, this);
+            cc.Component.EventHandler.emitEvents(this.clickEvents, event);
             this.node.emit('click', this);
         }
         this._pressed = false;
