48e50a25321459012039499240b671ca45d89d97
Leslie Leigh <leslieleigh@outlook.com>
2019-04-15 13:45:48 +0800

    Generate declarations(for temp) (#4218)

--- scripts/embedded-cocos-3d.d.ts
+++
@@ -1,6 +0,0 @@
-
-import _ = require("index");
-
-declare global {
-    const cc: typeof _;
-}
--- scripts/generate-declaration-cli.js
+++
@@ -1,8 +0,0 @@
-const { generate } = require('./generate-declaration');
-const { existsSync } = require('fs');
-const outputFiles = generate();
-if (!outputFiles || outputFiles.some((outputFile) => !existsSync(outputFile))) {
-    console.error(`Failed to generate declaration files.`);
-} else {
-    console.log(`Generate successed. Files:\n${outputFiles.join('\n')}\n`);
-}
