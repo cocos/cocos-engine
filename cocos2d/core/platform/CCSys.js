3a9e81f48795e8f7e668aaf12223a4dabd9f7695
PP <chinese1236@qq.com>
2019-04-18 10:33:01 +0800

    add XiaoMi platform

--- cocos2d/core/platform/CCSys.js
+++ cocos2d/core/platform/CCSys.js
@@ -31,6 +31,7 @@ let settingPlatform;
 const isBaiduGame = (settingPlatform === 'baidugame' || settingPlatform === 'baidugame-subcontext');
 const isVivoGame = (settingPlatform === 'qgame');
 const isOppoGame = (settingPlatform === 'quickgame');
+const isXiaomiGame = (settingPlatform === 'xiaomigame');
  
 function initSys () {
     /**
@@ -380,6 +381,12 @@ function initSys () {
      */
     sys.OPPO_GAME = 109;
     /**
+     * @property {Number} XIAOMI_GAME
+     * @readOnly
+     * @default 110
+     */
+    sys.XIAOMI_GAME = 110;
+    /**
      * BROWSER_TYPE_WECHAT
      * @property {String} BROWSER_TYPE_WECHAT
      * @readOnly
@@ -414,6 +421,13 @@ function initSys () {
      * @default "baidugamesub"
      */
     sys.BROWSER_TYPE_BAIDU_GAME_SUB = "baidugamesub";
+    /**
+     * BROWSER_TYPE_XIAOMI_GAME
+     * @property {String} BROWSER_TYPE_XIAOMI_GAME
+     * @readOnly
+     * @default "xiaomigame"
+     */
+    sys.BROWSER_TYPE_XIAOMI_GAME = "xiaomigame";
     /**
      * BROWSER_TYPE_QQ_PLAY
      * @property {String} BROWSER_TYPE_QQ_PLAY
@@ -573,7 +587,7 @@ function initSys () {
      * Is web browser ?
      * @property {Boolean} isBrowser
      */
-    sys.isBrowser = typeof window === 'object' && typeof document === 'object' && !CC_WECHATGAME && !CC_QQPLAY && !CC_JSB && !CC_RUNTIME && !isBaiduGame;
+    sys.isBrowser = typeof window === 'object' && typeof document === 'object' && !CC_WECHATGAME && !CC_QQPLAY && !CC_JSB && !CC_RUNTIME && !isBaiduGame && !isXiaomiGame;
     
     if (CC_EDITOR && Editor.isMainProcess) {
         sys.isMobile = false;
@@ -787,6 +801,69 @@ function initSys () {
         sys.localStorage = env.localStorage;
         sys.capabilities = env.capabilities;
         sys.__audioSupport = env.audioSupport;
+    }
+    else if (isXiaomiGame) {
+        var env = qg.getSystemInfoSync();
+        sys.isMobile = true;
+        sys.platform = sys.XIAOMI_GAME;
+        sys.language = env.language.substr(0, 2);
+        sys.languageCode = env.language.toLowerCase();
+        var system = env.system.toLowerCase();
+        if (env.platform === "android") {
+            sys.os = sys.OS_ANDROID;
+        }
+        else if (env.platform === "ios") {
+            sys.os = sys.OS_IOS;
+        }
+        else if (env.platform === 'devtools') {
+            sys.isMobile = false;
+            if (system.indexOf('android') > -1) {
+                sys.os = sys.OS_ANDROID;
+            }
+            else if (system.indexOf('ios') > -1) {
+                sys.os = sys.OS_IOS;
+            }
+        }
+        // Adaptation to Android P
+        if (system === 'android p') {
+            system = 'android p 9.0';
+        }
+
+        var version = /[\d\.]+/.exec(system);
+        sys.osVersion = version ? version[0] : system;
+        sys.osMainVersion = parseInt(sys.osVersion);
+        sys.browserType = sys.BROWSER_TYPE_XIAOMI_GAME;
+        sys.browserVersion = env.version;
+
+        var w = env.windowWidth;
+        var h = env.windowHeight;
+        var ratio = env.pixelRatio || 1;
+        sys.windowPixelResolution = {
+            width: ratio * w,
+            height: ratio * h
+        };
+
+        sys.localStorage = window.localStorage;
+
+        var _supportWebGL = _supportWebp = false;
+        try {
+            var _canvas = document.createElement("canvas");
+            _supportWebGL = _canvas.getContext("webgl");
+            _supportWebp = _canvas.toDataURL('image/webp').startsWith('data:image/webp');
+        }
+        catch (err) { }
+
+        sys.capabilities = {
+            "canvas": true,
+            "opengl": !!_supportWebGL,
+            "webp": _supportWebp
+        };
+        sys.__audioSupport = {
+            ONLY_ONE: false,
+            WEB_AUDIO: false,
+            DELAY_CREATE_CTX: false,
+            format: ['.mp3']
+        };
     }
     else {
         // browser or runtime
