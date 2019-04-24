ee806e9938e0c678fda8be7faeee14f67ace2136
JoneLau <32630749+JoneLau@users.noreply.github.com>
2019-04-24 17:45:31 +0800

    fix render-twice (#4294)

--- cocos/3d/ui/components/debug-canvas-component.ts
+++ cocos/3d/ui/components/debug-canvas-component.ts
@@ -1,4 +1,6 @@
 import { Vec3 } from '../../../core/value-types';
+import { GFXClearFlag } from '../../../gfx/define';
+import { CameraComponent } from '../../framework';
 import { CanvasComponent } from './canvas-component';
 
 const _worldPos = new Vec3();
@@ -6,6 +8,34 @@ export class DebugCanvasComponent extends CanvasComponent{
     constructor (){
         super();
         this._thisOnResized = this.alignWithScreen.bind(this);
+    }
+    public __preload (){
+        const cameraNode = new cc.Node('UICamera');
+        cameraNode.setPosition(0, 0, 1000);
+
+        this._camera = cc.director.root.ui.renderScene.createCamera({
+            name: 'ui',
+            node: cameraNode,
+            projection: CameraComponent.ProjectionType.ORTHO,
+            targetDisplay: 0,
+            priority: this._priority,
+            isUI: true,
+            flows: ['UIFlow'],
+        });
+
+        this._camera!.fov = 45;
+        this._camera!.clearFlag = GFXClearFlag.COLOR | GFXClearFlag.DEPTH | GFXClearFlag.STENCIL;
+
+        const device = cc.director.root.device;
+        this._camera!.resize(device.width, device.height);
+
+        cc.view.on('design-resolution-changed', this._thisOnResized);
+
+        this.alignWithScreen();
+
+        if (cc.director.root && cc.director.root.ui) {
+            cc.director.root.ui.debugScreen = this;
+        }
     }
 
     // don't remove
@@ -16,12 +46,15 @@ export class DebugCanvasComponent extends CanvasComponent{
     public onDisable (){
     }
 
-    public onDestroy (){
-        if (CC_EDITOR) {
-            cc.director.off(cc.Director.EVENT_AFTER_UPDATE, this.alignWithScreen, this);
+    public onDestroy () {
+        if (this._camera) {
+            this._getRenderScene().destroyCamera(this._camera);
         }
 
         cc.view.off('design-resolution-changed', this._thisOnResized);
+        if (cc.director.root && cc.director.root.ui) {
+            cc.director.root.ui.debugScreen = null;
+        }
     }
 
     public alignWithScreen (){
