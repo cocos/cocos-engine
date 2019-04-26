90c6164b51f23b903be9106e1e1ba5482bdb08c5
JayceLai <goodlaijiaxin@qq.com>
2019-04-26 18:57:58 +0800

    转换物理引擎为 built-in

--- cocos/3d/physics/impl-selector.ts
+++ cocos/3d/physics/impl-selector.ts
@@ -1,10 +1,10 @@
 
-export {
-    CannonBoxShape as BoxShape,
-    CannonRigidBody as RigidBody,
-    CannonSphereShape as SphereShape,
-    CannonWorld as PhysicsWorld,
-} from './cannon-impl';
+// export {
+//     CannonBoxShape as BoxShape,
+//     CannonRigidBody as RigidBody,
+//     CannonSphereShape as SphereShape,
+//     CannonWorld as PhysicsWorld,
+// } from './cannon-impl';
 
 // export {
 //     AmmoBoxShape as BoxShape,
@@ -15,7 +15,7 @@ export {
 // } from './ammo-impl';
 
 // 增加built-in物理模块
-// export { BuiltInBody as RigidBody } from './cocos/built-in-body';
-// export { BuiltInBoxShape as BoxShape } from './cocos/built-in-shape';
-// export { BuiltInSphereShape as SphereShape } from './cocos/built-in-shape';
-// export { BuiltInWorld as PhysicsWorld } from './cocos/built-in-world';
+export { BuiltInBody as RigidBody } from './cocos/built-in-body';
+export { BuiltInBoxShape as BoxShape } from './cocos/built-in-shape';
+export { BuiltInSphereShape as SphereShape } from './cocos/built-in-shape';
+export { BuiltInWorld as PhysicsWorld } from './cocos/built-in-world';
