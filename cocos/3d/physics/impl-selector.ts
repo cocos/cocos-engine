87e573d9564aa3c197fed31f6e9137d3609acf8c
jiaxin.lai <goodlaijiaxin@qq.com>
2019-04-30 15:05:07 +0800

    Implementing the physical selector function through conditional branching (#4352)

--- cocos/3d/physics/impl-selector.ts
+++ cocos/3d/physics/impl-selector.ts
@@ -1,21 +1,50 @@
 
-// export {
-//     CannonBoxShape as BoxShape,
-//     CannonRigidBody as RigidBody,
-//     CannonSphereShape as SphereShape,
-//     CannonWorld as PhysicsWorld,
-// } from './cannon-impl';
+declare const CC_PHYISCS_CANNON: boolean;
+declare const CC_PHYISCS_AMMO: boolean;
+declare const CC_PHYISCS_BUILT_IN: boolean;
+declare const global: any;
+const _global = typeof window === 'undefined' ? global : window;
 
-// export {
-//     AmmoBoxShape as BoxShape,
-//     AmmoRaycastResult as RaycastResult,
-//     AmmoRigidBody as RigidBody,
-//     AmmoSphereShape as SphereShape,
-//     AmmoWorld as PhysicsWorld,
-// } from './ammo-impl';
+// tslint:disable: no-string-literal
+if (typeof _global['CC_PHYISCS_CANNON'] === 'undefined') {
+    _global['CC_PHYISCS_CANNON'] = false;
+}
 
-// 增加built-in物理模块
-export { BuiltInBody as RigidBody } from './cocos/built-in-body';
-export { BuiltInBoxShape as BoxShape } from './cocos/built-in-shape';
-export { BuiltInSphereShape as SphereShape } from './cocos/built-in-shape';
-export { BuiltInWorld as PhysicsWorld } from './cocos/built-in-world';
+if (typeof _global['CC_PHYISCS_AMMO'] === 'undefined') {
+    _global['CC_PHYISCS_AMMO'] = false;
+}
+
+if (typeof _global['CC_PHYISCS_BUILT_IN'] === 'undefined') {
+    _global['CC_PHYISCS_BUILT_IN'] = true;
+}
+
+// Cannon
+import { CannonBoxShape, CannonRigidBody, CannonSphereShape, CannonWorld } from './cannon-impl';
+
+// Ammo
+// import { AmmoBoxShape, AmmoRaycastResult, AmmoRigidBody, AmmoSphereShape, AmmoWorld, } from './ammo-impl';
+
+// built-in
+import { BuiltInBody } from './cocos/built-in-body';
+import { BuiltInBoxShape } from './cocos/built-in-shape';
+import { BuiltInSphereShape } from './cocos/built-in-shape';
+import { BuiltInWorld } from './cocos/built-in-world';
+
+export let BoxShape: typeof CannonBoxShape | typeof BuiltInBoxShape;
+export let SphereShape: typeof CannonSphereShape | typeof BuiltInSphereShape;
+export let RigidBody: typeof CannonRigidBody | typeof BuiltInBody;
+export let PhysicsWorld: typeof CannonWorld | typeof BuiltInWorld;
+
+if (CC_PHYISCS_CANNON) {
+    BoxShape = CannonBoxShape;
+    SphereShape = CannonSphereShape;
+    RigidBody = CannonRigidBody;
+    PhysicsWorld = CannonWorld;
+} else if (CC_PHYISCS_AMMO) {
+    // TODO : 选择AMMO
+} else if (CC_PHYISCS_BUILT_IN) {
+    BoxShape = BuiltInBoxShape;
+    SphereShape = BuiltInSphereShape;
+    RigidBody = BuiltInBody;
+    PhysicsWorld = BuiltInWorld;
+}
