fddcd5b570e8a72b7285ef61a3e5f77bb132c7c2
cary <39078800+caryliu1999@users.noreply.github.com>
2019-05-06 17:14:09 +0800

    Optimize event manager. (#4209)
    * optimize array remove.
    
    * optimize _dirtyNodes.
    
    * Optimize the sorting of events.
    
    * delete globalZOrderNodeMap
    
    * move rendeQueue to constructor.
    
    * remove renderQueue from CCNode

--- cocos2d/core/renderer/render-flow.js
+++ cocos2d/core/renderer/render-flow.js
@@ -14,6 +14,8 @@ const FINAL = 1 << 10;
 
 let _batcher;
 let _cullingMask = 0;
+let _renderQueueIndex = 0;
+let _evm = cc.eventManager;
 
 function RenderFlow () {
     this._func = init;
@@ -119,6 +121,8 @@ _proto._children = function (node) {
     let children = node._children;
     for (let i = 0, l = children.length; i < l; i++) {
         let c = children[i];
+        _evm._updateRenderOrder(c, ++_renderQueueIndex);
+
         // Advance the modification of the flag to avoid node attribute modification is invalid when opacity === 0.
         c._renderFlag |= worldDirtyFlag;
         if (!c._activeInHierarchy || c._opacity === 0) continue;
@@ -222,6 +226,7 @@ RenderFlow.visit = function (scene) {
     _batcher.walking = true;
 
     _cullingMask = 1 << scene.groupIndex;
+    _renderQueueIndex = 0;
 
     if (scene._renderFlag & WORLD_TRANSFORM) {
         _batcher.worldMatDirty ++;
