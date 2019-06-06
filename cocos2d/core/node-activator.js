0553d0c87bfe2134b61f2a2e04b4c70c38688866
caryliu <caryliu1999@gmail.com>
2019-06-06 14:20:29 +0800

    Modify the reset of localZOrder.

--- cocos2d/core/node-activator.js
+++ cocos2d/core/node-activator.js
@@ -168,7 +168,7 @@ var NodeActivator = cc.Class({
         // activate children recursively
         for (let i = 0, len = node._children.length; i < len; ++i) {
             let child = node._children[i];
-            child._localZOrder === 0 && (child._localZOrder = i);
+            child._localZOrder = (child._localZOrder & 0xffff0000) | (i + 1);
             if (child._active) {
                 this._activateNodeRecursively(child, preloadInvoker, onLoadInvoker, onEnableInvoker);
             }
