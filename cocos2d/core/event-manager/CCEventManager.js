3d53a9894c90e3a6b7b4e266d24f1ad3fbdbe51e
cary <39078800+caryliu1999@users.noreply.github.com>
2019-06-12 11:40:51 +0800

    Modify the calculation of renderOrder in EventManager. (#4522)
    * Modify the calculation of renderOrder in EventManager.
    
    * Remove the calculation of renderOrder in EventManager.
    
    * modify format
    
    * fix bug
    
    * delete renderQueueIndex in render-flow.
    
    * Fix the sorting of the same localZorder.
    
    * fix the sorting of same localZOrder.
    
    * Fix the event bug caused by node activation.
    
    * Fix the event bug caused by node activation.
    
    * Modify the reset of localZOrder.
    
    * add reset childArrivalOrder

--- cocos2d/core/event-manager/CCEventManager.js
+++ cocos2d/core/event-manager/CCEventManager.js
@@ -120,7 +120,6 @@ var eventManager = {
     _dirtyListeners: {},
     _inDispatch: 0,
     _isEnabled: false,
-    _renderOrderMap: {},
 
     _internalCustomListenerIDs:[],
 
@@ -226,10 +225,11 @@ var eventManager = {
     },
 
     _updateDirtyFlagForSceneGraph: function () {
-        var locDirtyListeners = this._dirtyListeners
+        let locDirtyListeners = this._dirtyListeners
         for (var selKey in locDirtyListeners) {
             this._setDirty(selKey, this.DIRTY_SCENE_GRAPH_PRIORITY);
         }
+
         this._dirtyListeners = {};
     },
 
@@ -281,7 +281,7 @@ var eventManager = {
         var dirtyFlag = this.DIRTY_NONE, locFlagMap = this._priorityDirtyFlagMap;
         if (locFlagMap[listenerID])
             dirtyFlag = locFlagMap[listenerID];
-
+        
         if (dirtyFlag !== this.DIRTY_NONE) {
             // Clear the dirty flag first, if `rootNode` is null, then set its dirty flag of scene graph priority
             locFlagMap[listenerID] = this.DIRTY_NONE;
@@ -292,12 +292,12 @@ var eventManager = {
             if (dirtyFlag & this.DIRTY_SCENE_GRAPH_PRIORITY){
                 var rootEntity = cc.director.getScene();
                 if(rootEntity)
-                    this._sortListenersOfSceneGraphPriority(listenerID, rootEntity);
+                    this._sortListenersOfSceneGraphPriority(listenerID);
             }
         }
     },
 
-    _sortListenersOfSceneGraphPriority: function (listenerID, rootNode) {
+    _sortListenersOfSceneGraphPriority: function (listenerID) {
         var listeners = this._getListeners(listenerID);
         if (!listeners)
             return;
@@ -311,18 +311,28 @@ var eventManager = {
     },
 
     _sortEventListenersOfSceneGraphPriorityDes: function (l1, l2) {
-        var orders = eventManager._renderOrderMap;
-        var node1 = l1._getSceneGraphPriority(),
-            node2 = l2._getSceneGraphPriority(),
-            order1 = orders[node1._id],
-            order2 = orders[node2._id];
+        let node1 = l1._getSceneGraphPriority(),
+            node2 = l2._getSceneGraphPriority();
 
-        if (!l2 || !node2 || !order2)
+        if (!l2 || !node2 || node2.parent === null)
             return -1;
-        else if (!l1 || !node1 || !order1)
+        else if (!l1 || !node1 || node1.parent === null)
             return 1;
+        
+        let p1 = node1, p2 = node2, ex = false;
+        while (p1.parent._id !== p2.parent._id) {
+            p1 = p1.parent.parent === null ? (ex = true) && node2 : p1.parent;
+            p2 = p2.parent.parent === null ? (ex = true) && node1 : p2.parent;
+        }
 
-        return order2 - order1;
+        if (p1._id === p2._id) {
+            if (p1._id === node2._id) 
+                return -1;
+            if (p1._id === node1._id)
+                return 1;
+        }
+
+        return ex ? p1._localZOrder - p2._localZOrder : p2._localZOrder - p1._localZOrder;
     },
 
     _sortListenersOfFixedPriority: function (listenerID) {
@@ -590,7 +600,6 @@ var eventManager = {
             cc.js.array.remove(listeners, listener);
             if (listeners.length === 0)
                 delete this._nodeListenersMap[node._id];
-                delete this._renderOrderMap[node._id];
         }
     },
 
@@ -643,13 +652,6 @@ var eventManager = {
 
     _sortNumberAsc: function (a, b) {
         return a - b;
-    },
-
-    _updateRenderOrder: function (node, order) {
-        let selListeners = this._nodeListenersMap[node._id];
-        if (selListeners !== undefined) {
-            this._renderOrderMap[node._id] = order;
-        }   
     },
 
     /**
@@ -871,7 +873,6 @@ var eventManager = {
                 for (i = 0; i < listenersCopy.length; i++)
                     _t.removeListener(listenersCopy[i]);
                 delete _t._nodeListenersMap[listenerType._id];
-                delete _t._renderOrderMap[listenerType._id];
             }
 
             // Bug fix: ensure there are no references to the node in the list of listeners to be added.
