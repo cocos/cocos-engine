3c8c4e1ed673adf93a1c5471da16b56a8251f9a8
cary <39078800+caryliu1999@users.noreply.github.com>
2019-06-12 11:40:51 +0800

    Modify the calculation of renderOrder in EventManager. (#4522)
    * Modify the calculation of renderOrder in EventManager.
    
    * Remove the calculation of renderOrder in EventManager.
    
    * modify format
    
    * fix bug
    
    * delete renderQueueIndex in render-flow.
    
    * Fix the sorting of the same localZorder.
    
    * fix the sorting of same localZOrder.
    
    * Fix the event bug caused by node activation.
    
    * Fix the event bug caused by node activation.
    
    * Modify the reset of localZOrder.
    
    * add reset childArrivalOrder

--- cocos2d/core/renderer/render-flow.js
+++ cocos2d/core/renderer/render-flow.js
@@ -13,8 +13,6 @@ const FINAL = 1 << 9;
 
 let _batcher;
 let _cullingMask = 0;
-let _renderQueueIndex = 0;
-const EventManager = require("../event-manager/CCEventManager");
 
 function RenderFlow () {
     this._func = init;
@@ -92,7 +90,6 @@ _proto._children = function (node) {
     let children = node._children;
     for (let i = 0, l = children.length; i < l; i++) {
         let c = children[i];
-        EventManager._updateRenderOrder(c, ++_renderQueueIndex);
 
         // Advance the modification of the flag to avoid node attribute modification is invalid when opacity === 0.
         c._renderFlag |= worldDirtyFlag;
@@ -194,7 +191,6 @@ RenderFlow.visit = function (scene) {
     _batcher.walking = true;
 
     _cullingMask = 1 << scene.groupIndex;
-    _renderQueueIndex = 0;
 
     if (scene._renderFlag & WORLD_TRANSFORM) {
         _batcher.worldMatDirty ++;
