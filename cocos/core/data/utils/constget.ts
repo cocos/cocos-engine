d75fc250270a426d8af2911eb56058ee20367bc1
Leslie <leslieleigh@outlook.com>
2019-06-14 15:18:09 +0800

    doc

--- cocos/core/data/utils/constget.ts
+++ cocos/core/data/utils/constget.ts
@@ -1,7 +1,8 @@
 
 /**
- * 将属性的 Get 访问器标记为不可变的。
+ * 在调试模式下，将属性的 Get 访问器标记为不可变的。
  * 属性必须为 Javascript 原生类型或继承自 ValueType。
+ * 非调试模式下，此装饰器没有任何效果。
  */
 export function constget (target: Object, propertyKey: string | symbol) {
     if (!CC_DEBUG) {
