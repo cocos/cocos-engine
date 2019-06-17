44f1669fa6ce65f05b17c136f11573f3575137ff
Leslie Leigh <leslieleigh@outlook.com>
2019-06-17 12:00:06 +0800

    add readOnly decorator (#4626)
    * update api document for ILerpable
    
    * add readOnly decorator
    
    * rename readOnly to constget
    
    * doc

---
+++ cocos/core/data/utils/constget.ts
@@ -0,0 +1,22 @@
+
+/**
+ * 在调试模式下，将属性的 Get 访问器标记为不可变的。
+ * 属性必须为 Javascript 原生类型或继承自 ValueType。
+ * 非调试模式下，此装饰器没有任何效果。
+ */
+export function constget (target: Object, propertyKey: string | symbol) {
+    if (!CC_DEBUG) {
+        return;
+    }
+
+    const propertyDescriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+    if (propertyDescriptor && propertyDescriptor.get) {
+        const rawGet = propertyDescriptor.get;
+        function constGet (this: any) {
+            const value = rawGet.call(this);
+            return value ? Object.freeze(value.clone()) : value;
+        }
+        propertyDescriptor.get = constGet;
+    }
+    return propertyDescriptor;
+}
