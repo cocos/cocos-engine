4971fb97504c663384817494771f2f311041fe72
JoneLau <32630749+JoneLau@users.noreply.github.com>
2019-06-20 14:29:12 +0800

    fix circle (#4658)

--- cocos/3d/ui/index.ts
+++ cocos/3d/ui/index.ts
@@ -1,12 +1,14 @@
 
 import * as Assembler from './assembler';
 import { MeshBuffer } from './mesh-buffer';
+import * as UIVertexFormat from './ui-vertex-format';
 
 export * from './components';
 
 export {
     Assembler,
     MeshBuffer,
+    UIVertexFormat,
 };
 
 cc.UI = { Assembler, MeshBuffer};
