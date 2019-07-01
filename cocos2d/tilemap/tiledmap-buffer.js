2a7925442e427e0f6679f953bd432755c233125d
owen <1053210246@qq.com>
2019-07-01 16:42:36 +0800

    fix Tiledmap scale clip bug Dissociate Spine and DragonBones common logic

--- cocos2d/tilemap/tiledmap-buffer.js
+++ cocos2d/tilemap/tiledmap-buffer.js
@@ -26,27 +26,7 @@ let TiledMapBuffer = cc.Class({
     name: 'cc.TiledMapBuffer',
     extends: require('../core/renderer/webgl/quad-buffer'),
 
-    requestStatic (vertexCount, indiceCount) {
-        
-        this.checkAndSwitchBuffer(vertexCount);
-
-        let byteOffset = this.byteOffset + vertexCount * this._vertexBytes;
-        let indiceOffset = this.indiceOffset + indiceCount;
-
-        let byteLength = this._vData.byteLength;
-        let indiceLength = this._iData.length;
-        if (byteOffset > byteLength || indiceOffset > indiceLength) {
-            while (byteLength < byteOffset || indiceLength < indiceOffset) {
-                this._initVDataCount *= 2;
-                this._initIDataCount *= 2;
-
-                byteLength = this._initVDataCount * 4;
-                indiceLength = this._initIDataCount;
-            }
-
-            this._reallocBuffer();
-        }
-
+    _updateOffset () {
         let offsetInfo = this._offsetInfo;
         offsetInfo.vertexOffset = this.vertexOffset;
         offsetInfo.indiceOffset = this.indiceOffset;
@@ -62,4 +42,4 @@ let TiledMapBuffer = cc.Class({
     }
 });
 
-module.exports = TiledMapBuffer;
+cc.TiledMapBuffer = module.exports = TiledMapBuffer;
