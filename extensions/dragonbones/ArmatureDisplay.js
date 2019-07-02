1b9e8e05917f5f16c079ee6250492728d3e1d910
owen <1053210246@qq.com>
2019-07-02 14:43:39 +0800

    Dragonbones and Spine Material bug

--- extensions/dragonbones/ArmatureDisplay.js
+++ extensions/dragonbones/ArmatureDisplay.js
@@ -411,7 +411,7 @@ let ArmatureDisplay = cc.Class({
     },
 
     _updateBatch () {
-        let baseMaterial = this.sharedMaterials[0];
+        let baseMaterial = this.getMaterial(0);
         if (baseMaterial) {
             baseMaterial.define('CC_USE_MODEL', !this.enableBatch);
         }
