85ac9692fd9b0bcf72aa0fb99fb15093edac30f5
JoneLau <32630749+JoneLau@users.noreply.github.com>
2019-07-17 16:26:07 +0800

    revert depth test in model (#4898)

--- cocos/3d/ui/components/ui-model-component.ts
+++ cocos/3d/ui/components/ui-model-component.ts
@@ -101,25 +101,11 @@ export class UIModelComponent extends UIComponent {
             const passNum = passes.length;
             let needReconstruct = false;
             for (let j = 0; j < passNum; j++) {
-                let override = false;
                 if (!passes[j].blendState.targets[0].blend) {
                     needReconstruct = true;
                     const bs = passes[j].blendState.targets[0];
                     bs.blend = true;
-                    override = true;
-                }
-
-                if (passes[j].depthStencilState.depthTest) {
-                    needReconstruct = true;
-                    passes[j].depthStencilState.depthTest = false;
-                    override = true;
-                }
-
-                if (override) {
-                    passes[j].overridePipelineStates(ea.techniques[techIdx].passes[j], {
-                        blendState: passes[j].blendState,
-                        depthStencilState: passes[j].depthStencilState,
-                    });
+                    passes[j].overridePipelineStates(ea.techniques[techIdx].passes[j], { blendState: passes[j].blendState });
                 }
             }
             if (needReconstruct) {
