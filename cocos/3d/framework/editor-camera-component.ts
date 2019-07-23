ce7c3e6f1fa0d3fb85540e93774e1cc9180fc6bc
ChiaNing <sun.cloud.mo@gmail.com>
2019-07-23 11:15:36 +0800

    value types bug fixed (#4940)

--- cocos/3d/framework/editor-camera-component.ts
+++ cocos/3d/framework/editor-camera-component.ts
@@ -50,8 +50,7 @@ export class EditorCameraComponent extends CameraComponent {
     set color (val) {
         super.color = val;
         if (this._uiEditorCamera) {
-            this._uiEditorCamera.clearColor =
-                Color.create(val.r / 255, val.g / 255, val.b / 255, val.a / 255);
+            this._uiEditorCamera.clearColor = val;
         }
     }
 
@@ -138,7 +137,11 @@ export class EditorCameraComponent extends CameraComponent {
             this._uiEditorCamera!.fov = this._camera.fov;
             this._uiEditorCamera!.nearClip = this._camera.nearClip;
             this._uiEditorCamera!.farClip = this._camera.farClip;
-            this._uiEditorCamera!.clearColor = this._camera.clearColor;
+            const r = this._camera.clearColor.r / 255;
+            const g = this._camera.clearColor.g / 255;
+            const b = this._camera.clearColor.b / 255;
+            const a = this._camera.clearColor.a / 255;
+            this._uiEditorCamera!.clearColor = {r, g, b, a};
             this._uiEditorCamera!.clearDepth = this._camera.clearDepth;
             this._uiEditorCamera!.clearStencil = this._camera.clearStencil;
             this._uiEditorCamera!.clearFlag = this._camera.clearFlag;
