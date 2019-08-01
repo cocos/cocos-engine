c364feafad69599d7cc28ea2715bf63a2d397b59
Marssssssss <3503715604@qq.com>
2019-08-01 10:42:57 +0800

    refine: fix IE11's not supporting to Array.includes

--- polyfill/array.js
+++ polyfill/array.js
@@ -20,7 +20,7 @@ if (!Array.prototype.find) {
 
 // for ie 11
 if (!Array.prototype.includes) {
-   Array.prototype.includes = function (value) {
-       return this.indexOf(value) !== -1;
-   };
+    Array.prototype.includes = function (value) {
+        return this.indexOf(value) !== -1;
+    };
 }
