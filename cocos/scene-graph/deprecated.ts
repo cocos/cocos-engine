235334ac22bb98daef9968011a4a91d0711d188d
pandamicro <huabin.ling@cocos2d-x.org>
2019-08-08 21:27:20 +0800

    Fix  ci

--- cocos/scene-graph/deprecated.ts
+++ cocos/scene-graph/deprecated.ts
@@ -5,6 +5,7 @@ replaceProperty(BaseNode.prototype, 'BaseNode', [
     {
         name: 'childrenCount',
         custom: function () {
+            // @ts-ignore
             return this.children.length;
         }
     }
