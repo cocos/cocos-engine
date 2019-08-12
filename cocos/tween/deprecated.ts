3d6df119a4310a8e4d7408156775d79c9ff35360
JayceLai <goodlaijiaxin@qq.com>
2019-08-12 13:51:24 +0800

    using deprecated framework

--- cocos/tween/deprecated.ts
+++ cocos/tween/deprecated.ts
@@ -5,12 +5,13 @@
 // deprecated
 import { Tween, tweenUtil } from './tween';
 import { warn } from '../core/platform/CCDebug';
+import { replaceProperty } from '../deprecated';
 
 /**
  * @zh
  * 增加一个 tween 缓动，与 creator 2D 中的 cc.tween 功能类似
  * @deprecated
- * tween 已经被废弃，请使用 tweenUtil
+ * 下个版本中将会移除 tween 方法，请及时调整为 tweenUtil
  * @param target 缓动目标
  *
  * 注：请勿对 node 矩阵相关数据直接进行缓动，例如传入 this.node.position
@@ -30,7 +31,13 @@ import { warn } from '../core/platform/CCDebug';
  */
 export function tween (target: Object): Tween {
     warn("'%s' is deprecated, please use '%s' instead.", `tween`, `tweenUtil`);
-    warn("下个版本中将会移除 tween 方法，请及时调整为 tweenUtil");
     return tweenUtil(target);
 }
 cc.tween = tween;
+
+replaceProperty(cc, 'cc', [
+    {
+        name: 'tween',
+        newName: 'tweenUtil'
+    }
+]);
