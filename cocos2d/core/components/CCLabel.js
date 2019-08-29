cocos/cocos-engine

commit 1a6cb960e4319de5b9df8d470e59352b68d19f37

Fixed a display error when Label's string was empty. (#5266)
* Fixed a display error when Label's string was empty.

* modify the decision of the image size.

cocos2d/core/components/CCLabel.js
@@ -536,7 +536,7 @@ let Label = cc.Class({
         this.node.on(cc.Node.EventType.COLOR_CHANGED, this._updateColor, this);
 
         this._checkStringEmpty();
-        this._updateRenderData(true);
+        this._updateRenderData(!!this.string);
     },
 
     onDisable () {

cocos2d/core/assets/CCTexture2D.js
@@ -598,7 +598,7 @@ var Texture2D = cc.Class({
      * @param {Boolean} [premultiplied]
      */
     handleLoadedTexture () {
-        if (!this._image || !this._image.width || !this._image.height)
+        if (!this._image || this._image.width == null || this._image.height == null)
             return;
         
         this.width = this._image.width;

commit 2de6cc5f26ae6c197fc8a09e0b5a37c8f4750970

Fix an error caused by #5266.

cocos2d/core/components/CCLabel.js
@@ -535,8 +535,8 @@ let Label = cc.Class({
         this.node.on(cc.Node.EventType.ANCHOR_CHANGED, this._updateRenderData, this);
         this.node.on(cc.Node.EventType.COLOR_CHANGED, this._updateColor, this);
 
+        this._updateRenderData(true);
         this._checkStringEmpty();
-        this._updateRenderData(!!this.string);
     },
 
     onDisable () {