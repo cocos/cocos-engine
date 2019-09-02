d4ed3103f7453294986466736630389112811eda
jiaxin.lai <goodlaijiaxin@qq.com>
2019-09-02 14:36:58 +0800

    add deprecated for physics framework api (#5304)

---
+++ cocos/physics/components/deprecated.ts
@@ -0,0 +1,10 @@
+
+import { PhysicsSystem } from "./physics-system";
+import { replaceProperty } from "../../core";
+
+replaceProperty(PhysicsSystem, 'PhysicsSystem', [
+    {
+        "name": "ins",
+        "newName": "instance"
+    }
+]);
