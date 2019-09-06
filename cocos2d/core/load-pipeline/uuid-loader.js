9d32ee0e7084ea39814592f768435d137070ea3a
Santy-Wang <wh19930829@qq.com>
2019-09-06 18:53:10 +0800

    catch the error about initializing (#5331)
    * catch the error about initializing
    
    * replace console.error with cc._throw

--- cocos2d/core/load-pipeline/uuid-loader.js
+++ cocos2d/core/load-pipeline/uuid-loader.js
@@ -180,7 +180,14 @@ function loadDepends (pipeline, item, asset, depends, callback) {
             callback(null, asset);
         }
         else {
-            if (!errors && asset.onLoad) asset.onLoad();
+            if (!errors && asset.onLoad) {
+                try {
+                    asset.onLoad();
+                }
+                catch (e) {
+                    cc._throw(e);
+                } 
+            }
             callback(errors, asset);
         }
     });
