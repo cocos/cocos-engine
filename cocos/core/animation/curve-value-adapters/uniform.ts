18b4c6c5e7a9c82d7ad91b6226876091fee9890c
Lelie Leight (李的序) <leslieleigh@outlook.com>
2019-09-17 11:01:49 +0800

    support texture animation (#5395)
    * support texture animation
    
    * improves

--- cocos/core/animation/curve-value-adapters/uniform.ts
+++ cocos/core/animation/curve-value-adapters/uniform.ts
@@ -5,6 +5,11 @@
 import { CurveValueAdapter } from '../animation-curve';
 import { Material } from '../../assets/material';
 import { property, ccclass } from '../../data/class-decorator';
+import { Pass, samplerLib } from '../../renderer';
+import { GFXBindingType } from '../../gfx';
+import { GFXTextureView } from '../../gfx/texture-view';
+import { TextureBase } from '../../assets/texture-base';
+import { SpriteFrame } from '../../assets';
 
 @ccclass('cc.UniformCurveValueAdapter')
 export class UniformCurveValueAdapter extends CurveValueAdapter {
@@ -17,19 +22,62 @@ export class UniformCurveValueAdapter extends CurveValueAdapter {
     constructor() {
         super();
     }
-
+    
     public forTarget(target: Material) {
         const pass = target.passes[this.passIndex];
-        const uniformHandle = pass.getHandle(this.uniformName);
-        if (uniformHandle === undefined) {
+        const handle = pass.getHandle(this.uniformName);
+        if (handle === undefined) {
             throw new Error(`Material "${target.name}" has no uniform "${this.uniformName}"`);
         }
-        return {
-            set: (value: any) => {
-                pass.setUniform(uniformHandle, value);
-            },
-        };
+        const bindingType = Pass.getBindingTypeFromHandle(handle);
+        if (bindingType === GFXBindingType.UNIFORM_BUFFER) {
+            if (isUniformArray(pass, this.uniformName)) {
+                return {
+                    set: (value: any) => {
+                        pass.setUniformArray(handle, value);
+                    },
+                };
+            } else {
+                return {
+                    set: (value: any) => {
+                        pass.setUniform(handle, value);
+                    },
+                };
+            }
+        } else if (bindingType === GFXBindingType.SAMPLER) {
+            const binding = Pass.getBindingFromHandle(handle);
+            return {
+                set: (value: any) => {
+                    if (value instanceof GFXTextureView) {
+                        pass.bindTextureView(binding, value);
+                    } else if (value instanceof TextureBase || value instanceof SpriteFrame) {
+                        const textureView: GFXTextureView | null = value.getGFXTextureView();
+                        if (!textureView || !textureView.texture.width || !textureView.texture.height) {
+                            // console.warn(`material '${this._uuid}' received incomplete texture asset '${val._uuid}'`);
+                            return;
+                        }
+                        pass.bindTextureView(binding, textureView);
+                        if (value instanceof TextureBase) {
+                            pass.bindSampler(binding, samplerLib.getSampler(cc.game._gfxDevice, value.getSamplerHash()));
+                        }
+                    }
+                },
+            };
+        } else {
+            throw new Error(`Animations are not avaiable for uniforms with binding type ${bindingType}.`);
+        }
     }
+}
+
+function isUniformArray (pass: Pass, name: string) {
+    for (const block of pass.shaderInfo.blocks) {
+        for (const uniform of block.members) {
+            if (uniform.name === name) {
+                return uniform.count > 1;
+            }
+        }
+    }
+    return false;
 }
 
 cc.UniformCurveValueAdapter = UniformCurveValueAdapter;
