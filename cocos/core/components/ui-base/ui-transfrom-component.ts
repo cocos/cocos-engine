e2c6c515c2a98a348247b83e90cfe4f07377d44d
JayceLai <goodlaijiaxin@qq.com>
2019-09-20 17:20:06 +0800

    Remove set UI_2D on preload

--- cocos/core/components/ui-base/ui-transfrom-component.ts
+++ cocos/core/components/ui-base/ui-transfrom-component.ts
@@ -187,7 +187,6 @@ export class UITransformComponent extends Component {
 
     public __preload () {
         this.node.uiTransfromComp = this;
-        this.node.layer = Layers.Enum.UI_2D;
     }
 
     public onDestroy () {
