d1b2dbf113fb0a857cb5d5a499a47aec383d76bf
Unknown <leslieleigh@outlook.com>
2019-09-23 19:25:59 +0800

    update

--- cocos/core/data/utils/extends-enum.ts
+++ cocos/core/data/utils/extends-enum.ts
@@ -1,37 +1,38 @@
 
 /**
  * @zh
- * 用任意多个枚举扩展指定的枚举。
- * 此函数的行为等价于返回了一个新的枚举，其成员囊括了所有扩展枚举的成员。
- * 这些扩展枚举的成员必须各不相同（包括成员名和值），否则行为是未定义的。
+ * 组合任意多个枚举。
+ * 此函数的行为等价于返回了一个新的枚举，其成员囊括了所有源枚举的成员。
+ * 这些枚举的成员必须各不相同（包括成员名和值），否则行为是未定义的。
  * @en
- * Extends an enumeration with arbitray number of enumerations.
- * It behaves like an enumeration having members that is a combination of members of the extended enumerations
+ * Combine arbitray number of enumerations.
+ * It behaves like an enumeration having members that is a combination of members of the source enumerations
  * is returned.
  * These enumerations shall have non-overlaped member names or member values.
  * If not, the behavior is undefined.
- * @param e 被扩展的枚举。
  * @example
  * ```ts
  * enum Apple { apple = 'apple', }
  * enum Pen { pen = 'pen' }
  * // As if `enum ApplePen { apple = 'apple'; pen = 'pen'; }`
- * const ApplePen = extendsEnum(ApplePen);
+ * const ApplePen = extendsEnum(Apple, Pen);
  * ```
  */
-export function extendsEnum<E> (e: E): E;
+export function extendsEnum (): {};
 
-export function extendsEnum<E, E1> (e: E, e1: E1): E & E1;
+export function extendsEnum<E0> (e0: E0): E0;
 
-export function extendsEnum<E, E1, E2> (e: E, e1: E1, e2: E2): E & E1 & E2;
+export function extendsEnum<E0, E1> (e0: E0, e1: E1): E0 & E1;
 
-export function extendsEnum<E, E1, E2, E3> (e: E, e1: E1, e2: E2, e3: E3): E & E1 & E2 & E3;
+export function extendsEnum<E0, E1, E2> (e0: E0, e1: E1, e2: E2): E0 & E1 & E2;
 
-export function extendsEnum<E> (e: E, ...enums: any[]): any {
+export function extendsEnum<E0, E1, E2, E3> (e0: E0, e1: E1, e2: E2, e3: E3): E0 & E1 & E2 & E3;
+
+export function extendsEnum (...enums: any[]): any {
     if (CC_DEV) {
         const kvs: PropertyKey[] = [];
-        for (const emm of [e, ...enums]) {
-            for (const kv of Object.keys(emm)) {
+        for (const e of enums) {
+            for (const kv of Object.keys(e)) {
                 if (kvs.indexOf(kv) >= 0) {
                     throw new Error(`Violation error: extending enumerations shall have non-overlaped member names or member values`);
                 } else {
@@ -41,5 +42,5 @@ export function extendsEnum<E> (e: E, ...enums: any[]): any {
         }
         
     }
-    return Object.assign({}, e, ...enums);
+    return Object.assign({}, ...enums);
 }
