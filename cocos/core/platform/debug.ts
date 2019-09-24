01cef26d55e6ee8e72e1c1464bcbcddf0d21dc86
ChiaNing <sun.cloud.mo@gmail.com>
2019-09-24 18:40:55 +0800

    debug info bug fix (#5461)

--- cocos/core/platform/debug.ts
+++ cocos/core/platform/debug.ts
@@ -24,7 +24,7 @@
 */
 
  // @ts-ignore
-import * as debugInfos from '../../../DebugInfos';
+import debugInfos from '../../../DebugInfos';
 const ERROR_MAP_URL = 'https://github.com/cocos-creator/engine/blob/3d-v1.0.0/EngineErrorMap.md';
 
 // The html element displays log in web page (DebugMode.INFO_FOR_WEB_PAGE)
