552fdffa7b83e21c6543d49e7628b27d8f1733a8
cary <39078800+caryliu1999@users.noreply.github.com>
2019-09-27 18:44:32 +0800

    Add the API for dynamicAtlas and LabelAtlas. (#5435)

--- cocos2d/core/components/CCLabel.js
+++ cocos2d/core/components/CCLabel.js
@@ -512,6 +512,19 @@ let Label = cc.Class({
         VerticalAlign: VerticalAlign,
         Overflow: Overflow,
         CacheMode: CacheMode,
+
+        _shareAtlas: null,
+        /**
+         * !#zh 需要保证当前场景中没有使用CHAR缓存的Label才可以清除，否则已渲染的文字没有重新绘制会不显示
+         * !#en It can be cleared that need to ensure there is not use the CHAR cache in the current scene. Otherwise, the rendered text will not be displayed without repainting.
+         * @method clearCharCache
+         * @static
+         */
+        clearCharCache () {
+            if (Label._shareAtlas) {
+                Label._shareAtlas.clearAllCache();
+            }
+        }
     },
 
     onLoad () {
