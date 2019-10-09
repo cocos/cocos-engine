10efa7c671c9c9eef2ef25b25cb4ea86b462c120
caryliu <caryliu1999@gmail.com>
2019-10-09 14:41:59 +0800

    revert change

--- cocos2d/core/components/CCSprite.js
+++ cocos2d/core/components/CCSprite.js
@@ -437,7 +437,6 @@ var Sprite = cc.Class({
             }
         }
         else {
-            this.setVertsDirty();
             this._activateMaterial();
         }
     },
@@ -447,6 +446,7 @@ var Sprite = cc.Class({
     },
 
     _activateMaterialCanvas () {
+        this.setVertsDirty();
         this.markForUpdateRenderData(true);
         this.markForRender(true);
     },
@@ -470,6 +470,7 @@ var Sprite = cc.Class({
         
         material.setProperty('texture', spriteFrame.getTexture());
 
+        this.setVertsDirty();
         this.setMaterial(0, material);
         this.markForRender(true);
     },
