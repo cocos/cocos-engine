2c7b28f674bcc4d479273c55d1d324c25778c677
SijieWang <itharbors@outlook.com>
2019-10-09 17:47:45 +0800

    remove rc

--- predefine.ts
+++ predefine.ts
@@ -213,7 +213,7 @@ if (CC_DEV) {
  * If you post a bug to forum, please attach this flag.
  * @property {String} ENGINE_VERSION
  */
-const engineVersion = '1.0.0 rc';
+const engineVersion = '1.0.0';
 _global['CocosEngine'] = cc.ENGINE_VERSION = engineVersion;
 
 export default cc;
