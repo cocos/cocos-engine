ca5e44c3ad81d7180bfdf98f9c26f5a93eba2fb6
YuanJie <42939682+Marssssssss@users.noreply.github.com>
2019-10-15 17:20:41 +0800

    fix layout conflict between scrollView and widget, actually need to let just-activated nodes start in advance (#5528)
    * Add new unit test to test if node can go through the right life cycle when it is active.
    
    * add life-cycle states to cc.director for fix conflict between scrollView and widget
    
    * fix condition of the unit test
    
    * fix state fetch problem
    
    * try fixing by controlling widget adding time
    
    * refine: try fixing by controlling widget adding time
    
    * refine: try fixing by controlling widget adding time
    
    * rewrite unit test module
    
    * refine: rewrite unit test module
    
    * debug unit test code
    
    * refine: debug unit test code
    
    * debug unit test code, it can work now
    
    * edit engine code and now can execute node's start in async after node being activatived
    
    * refine: edit engine code and now can execute node's start in async after node being activatived
    
    * refine: edit engine code and now can execute node's start in async after node being activatived
    
    * simplified fixing code and add a new unit test
    
    * improve code comments and unit test code
    
    * delete unnecessary judge

--- cocos2d/core/CCDirector.js
+++ cocos2d/core/CCDirector.js
@@ -941,17 +941,26 @@ cc.Director.prototype = {
 
             // Update
             if (!this._paused) {
+                // before update
                 this.emit(cc.Director.EVENT_BEFORE_UPDATE);
+
                 // Call start for new added components
                 this._compScheduler.startPhase();
+
                 // Update for components
                 this._compScheduler.updatePhase(this._deltaTime);
                 // Engine update with scheduler
                 this._scheduler.update(this._deltaTime);
+
                 // Late update for components
                 this._compScheduler.lateUpdatePhase(this._deltaTime);
+
+                // After life-cycle executed
+                this._compScheduler.clearup();
+
                 // User can use this event to do things after update
                 this.emit(cc.Director.EVENT_AFTER_UPDATE);
+                
                 // Destroy entities that have been removed recently
                 Obj._deferredDestroy();
             }
