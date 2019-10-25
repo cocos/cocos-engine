a973b82f237826abb609a5f7e03c557bb31123d7
zhefeng.zhang <35944775+Jno1995@users.noreply.github.com>
2019-10-25 10:51:53 +0800

    optimize code

--- cocos2d/core/CCGame.js
+++ cocos2d/core/CCGame.js
@@ -136,7 +136,7 @@ var game = {
 
     _lastTime: null,
     _frameTime: null,
-    _maxFrameTime: null,
+    _maxDeltaTime: null,
     
     // Scenes list
     _sceneInfos: [],
@@ -577,7 +577,7 @@ var game = {
         this._lastTime = performance.now();
         var frameRate = game.config.frameRate;
         this._frameTime = 1000 / frameRate;
-        this._maxFrameTime = this._frameTime / 1000 * 1.5;
+        this._maxDeltaTime = this._frameTime / 1000 * 1.5;
         if (CC_JSB || CC_RUNTIME) {
             jsb.setPreferredFramesPerSecond(frameRate);
             window.requestAnimFrame = window.requestAnimationFrame;
