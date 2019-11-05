19de306e44344fb7df7ab37553e9ae21eaa01223
2youyou2 <501251991@qq.com>
2019-11-05 16:37:56 +0800

    revert change

--- cocos2d/core/assets/material/material-pool.js
+++ cocos2d/core/assets/material/material-pool.js
@@ -25,8 +25,8 @@ class MaterialPool extends Pool {
     
         if (!instance) {
             instance = new cc.Material();
-            instance._name = exampleMat._name + ' (Instance)';
             instance.copy(exampleMat);
+            instance._name = exampleMat._name + ' (Instance)';
             instance._uuid = exampleMat._uuid;
         }
         else {
