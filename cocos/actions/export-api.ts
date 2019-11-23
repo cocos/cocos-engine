4810041a56b6746be0da5189ce37d0ed5e813ba2
JayceLai <goodlaijiaxin@qq.com>
2019-11-23 18:23:14 +0800

    add typing

---
+++ cocos/actions/export-api.ts
@@ -0,0 +1,18 @@
+
+export type TweenEasing =
+'linear'    | 'smooth'     | 'fade'         |
+'quadIn'    | 'quadOut'    | 'quadInOut'    | 'quadOutIn'    |
+'cubicIn'   | 'cubicOut'   | 'cubicInOut'   | 'cubicOutIn'   |
+'quartIn'   | 'quartOut'   | 'quartInOut'   | 'quartOutIn'   |
+'quintIn'   | 'quintOut'   | 'quintInOut'   | 'quintOutIn'   |
+'sineIn'    | 'sineOut'    | 'sineInOut'    | 'sineOutIn'    |
+'expoIn'    | 'expoOut'    | 'expoInOut'    | 'expoOutIn'    |
+'circIn'    | 'circOut'    | 'circInOut'    | 'circOutIn'    |
+'elasticIn' | 'elasticOut' | 'elasticInOut' | 'elasticOutIn' |
+'backIn'    | 'backOut'    | 'backInOut'    | 'backOutIn'    |
+'bounceIn'  | 'bounceOut'  | 'bounceInOut'  | 'bounceOutIn'  ;
+
+export interface ITweenOption {
+    easing?: TweenEasing | ((k: number) => number);
+    progress?: (start: number, end: number, current: number, ratio: number) => number;
+}
