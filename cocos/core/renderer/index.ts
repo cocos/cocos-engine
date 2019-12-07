bbdce1b684872d33bdb3f16f2bc39557957b87d6
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2019-12-07 15:08:24 +0800

    fallback to renderbuffer if depth texture not supported (#5862)

--- cocos/core/renderer/index.ts
+++ cocos/core/renderer/index.ts
@@ -4,20 +4,29 @@ import config from './config';
 const addStage = config.addStage;
 export { addStage };
 
+export * from './core/constants';
+export * from './core/pass-utils';
+export * from './core/pass';
+export * from './core/program-lib';
+export * from './core/sampler-lib';
 export * from './core/texture-buffer-pool';
-export { RenderQueue, PassStage } from './core/constants';
-export { Pass } from './core/pass';
-export { programLib } from './core/program-lib';
-import { samplerLib } from './core/sampler-lib';
-cc.samplerLib = samplerLib;
-export { samplerLib };
 
-export { Light } from './scene/light';
-export { Camera } from './scene/camera';
-export { Model } from './scene/model';
-
-export { SkinningModel } from './models/skinning-model';
 export * from './models/skeletal-animation-utils';
+export * from './models/skinning-model';
+
+export * from './scene/ambient';
+export * from './scene/camera';
+export * from './scene/customization-manager';
+export * from './scene/deprecated';
+export * from './scene/directional-light';
+export * from './scene/light';
+export * from './scene/model';
+export * from './scene/planar-shadows';
+export * from './scene/render-scene';
+export * from './scene/skybox';
+export * from './scene/sphere-light';
+export * from './scene/spot-light';
+export * from './scene/submodel';
 
 import './scene/deprecated';
 import './ui/render-data';
