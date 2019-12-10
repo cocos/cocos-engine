d01896f9ee8500fc8b279ae20a28991cbf6d6d3f
JayceLai <goodlaijiaxin@qq.com>
2019-12-10 18:42:26 +0800

    remove some unneeded code

--- cocos/core/scene-graph/deprecated.ts
+++ cocos/core/scene-graph/deprecated.ts
@@ -7,12 +7,6 @@ import { replaceProperty, removeProperty } from '../utils/deprecated';
 import { Layers } from './layers';
 import { Node } from './node';
 
-// js.get(BaseNode.prototype, 'childrenCount', function () {
-//     cc.warn("`childrenCount` is deprecated, please use `node.children.length` instead.");
-//     // @ts-ignore
-//     return this.children.length;
-// });
-
 replaceProperty(BaseNode.prototype, 'BaseNode', [
     {
         'name': 'childrenCount',
