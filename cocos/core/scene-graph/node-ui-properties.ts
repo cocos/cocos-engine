8879b49484af5f61fd62bcb746f52a87aa36e57d
Jone <350791117@qq.com>
2019-12-23 19:21:04 +0800

    add file of additional properties and transparency downward

---
+++ cocos/core/scene-graph/node-ui-properties.ts
@@ -0,0 +1,50 @@
+/*
+ Copyright (c) 2017-2019 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+  worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+  not use Cocos Creator software for developing other software or tools that's
+  used for developing games. You are not granted to publish, distribute,
+  sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+import { UIComponent } from '../components/ui-base/ui-component';
+import { UITransformComponent } from '../components/ui-base/ui-transform-component';
+
+export class NodeUIProperties {
+    get uiTransformComp () {
+        if (!this._uiTransformComp) {
+            this._uiTransformComp = this._node.getComponent(UITransformComponent);
+        }
+
+        return this._uiTransformComp;
+    }
+
+    set uiTransformComp (value) {
+        this._uiTransformComp = value;
+    }
+
+    public uiComp: UIComponent | null = null;
+    public opacity = 1;
+    protected _uiTransformComp: UITransformComponent | null = null;
+    private _node: any;
+
+    constructor (node: any) {
+        this._node = node;
+    }
+}
