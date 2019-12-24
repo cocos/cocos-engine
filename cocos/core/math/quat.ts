108592596ff4f6a1abfcddcf863b9e70e8ca3107
Unknown <leslieleigh@outlook.com>
2019-12-24 19:11:34 +0800

    Fix that `Quat` constructor misbehaves when argument `w` is 0

--- cocos/core/math/quat.ts
+++ cocos/core/math/quat.ts
@@ -648,7 +648,7 @@ export class Quat extends ValueType {
             this.x = x || 0;
             this.y = y || 0;
             this.z = z || 0;
-            this.w = w || 1;
+            this.w = w ?? 1;
         }
     }
 
