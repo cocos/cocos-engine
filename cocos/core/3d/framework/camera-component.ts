ea700afb50eef1eb8dc6da157d107d38c076db04
JoneLau <32630749+JoneLau@users.noreply.github.com>
2020-01-04 11:22:14 +0800

    fix coordinate replace function (#6025)
    save

--- cocos/core/3d/framework/camera-component.ts
+++ cocos/core/3d/framework/camera-component.ts
@@ -431,6 +431,19 @@ export class CameraComponent extends Component {
         return out;
     }
 
+    /**
+     * @zh 3D 节点转 UI 本地节点坐标。
+     * 注意：千万不要设置负责做转换的 uiNode 和最终设置位置的 uiNode 是同一个 node，否则可能出现跳动现象。
+     * @param wpos 3D 节点事件坐标
+     * @param uiNode UI 节点
+     * @param out 返回在当前传入的 UI 节点下的偏移量
+     *
+     * @example
+     * ```typescript
+     * this.convertToUINode(target.worldPosition, uiNode.parent, out);
+     * uiNode.position = out;
+     * ```
+     */
     public convertToUINode(wpos: Vec3, uiNode: Node, out?: Vec3){
         if (!out) {
             out = new Vec3();
