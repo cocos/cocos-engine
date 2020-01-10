7747e60051a244481ea8fa2987364c77d51dc225
jho1995 <337031709@qq.com>
2020-01-10 17:39:26 +0800

    fixed call node.getChildren() error

--- cocos2d/core/event-manager/CCEventManager.js
+++ cocos2d/core/event-manager/CCEventManager.js
@@ -128,17 +128,16 @@ var eventManager = {
         // Mark the node dirty only when there is an event listener associated with it.
         let selListeners = this._nodeListenersMap[node._id];
         if (selListeners !== undefined) {
-            for (var j = 0, len = selListeners.length; j < len; j++) {
+            for (let j = 0, len = selListeners.length; j < len; j++) {
                 let selListener = selListeners[j];
                 let listenerID = selListener._getListenerID();
                 if (this._dirtyListeners[listenerID] == null)
                     this._dirtyListeners[listenerID] = true;
             }
         }
-        if (node.getChildren) {
-            var _children = node.getChildren();
-            for(var i = 0, len = _children ? _children.length : 0; i < len; i++)
-                this._setDirtyForNode(_children[i]);
+        if (node.childrenCount > 0) {
+            for(let i = 0, len = node.childrenCount; i < len; i++)
+                this._setDirtyForNode(node.children[i]);
         }
     },
 
@@ -160,7 +159,7 @@ var eventManager = {
                 listeners[i]._setPaused(true);
         }
         if (recursive === true) {
-            var locChildren = node.getChildren();
+            var locChildren = node.children;
             for (i = 0, len = locChildren ? locChildren.length : 0; i < len; i++)
                 this.pauseTarget(locChildren[i], true);
         }
@@ -185,7 +184,7 @@ var eventManager = {
         }
         this._setDirtyForNode(node);
         if (recursive === true && node.getChildren) {
-            var locChildren = node.getChildren();
+            var locChildren = node.children;
             for (i = 0, len = locChildren ? locChildren.length : 0; i < len; i++)
                 this.resumeTarget(locChildren[i], true);
         }
@@ -906,7 +905,7 @@ var eventManager = {
             }
 
             if (recursive === true) {
-                var locChildren = listenerType.getChildren(), len;
+                var locChildren = listenerType.children, len;
                 for (i = 0, len = locChildren.length; i< len; i++)
                     _t.removeListeners(locChildren[i], true);
             }
--- test/qunit/unit-es5/test-serialize-node.js
+++ test/qunit/unit-es5/test-serialize-node.js
@@ -90,11 +90,10 @@ if (TestEditorExtends) {
             equal(node1.key, node2.key, '"' + key + '" should be equal between two nodes.');
         }
 
-        equal(node1.getChildrenCount(), node2.getChildrenCount(), 'The children count should be equal between two nodes.');
-        if (node1.getChildrenCount() > 0) {
-            var children = node1.getChildren();
+        equal(node1.childrenCount, node2.childrenCount, 'The children count should be equal between two nodes.');
+        if (node1.childrenCount > 0) {
             for (var j = 0; j < children.length; ++j) {
-                compare2Nodes(children[j], node2.getChildren()[j], 'The children content should be equal between two nodes.');
+                compare2Nodes(node1.children[j], node2.children)[j], 'The children content should be equal between two nodes.');
             }
         }
     }
