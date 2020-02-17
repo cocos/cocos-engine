eb047aa5137887097a057232c99fb821696e3b37
jho1995 <337031709@qq.com>
2020-02-17 16:14:09 +0800

    correction action warn code

--- cocos2d/core/CCNode.js
+++ cocos2d/core/CCNode.js
@@ -2490,7 +2490,7 @@ let NodeDefines = {
         if (!this.active)
             return;
         cc.assertID(action, 1618);
-        cc.warnID(1639);
+        if (!cc.director.getActionManager().haveWarn) cc.director.getActionManager().haveWarn = new cc.warnID(1639);
         cc.director.getActionManager().addAction(action, this, false);
         return action;
     } : emptyFunc,
