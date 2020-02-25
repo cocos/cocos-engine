dc9676a51fa520511e9d6ff1d379b0d7b79cfdc4
minggo <nudtzxm@gmail.com>
2020-02-25 16:44:38 +0800

    image remove tiff support (#2181)

--- native/cocos/platform/CCImage.h
+++ native/cocos/platform/CCImage.h
@@ -75,8 +75,6 @@ public:
         JPG,
         //! PNG
         PNG,
-        //! TIFF
-        TIFF,
         //! WebP
         WEBP,
         //! PVR
@@ -238,7 +236,6 @@ public:
 protected:
     bool initWithJpgData(const unsigned char *  data, ssize_t dataLen);
     bool initWithPngData(const unsigned char * data, ssize_t dataLen);
-    bool initWithTiffData(const unsigned char * data, ssize_t dataLen);
     bool initWithWebpData(const unsigned char * data, ssize_t dataLen);
     bool initWithPVRData(const unsigned char * data, ssize_t dataLen);
     bool initWithPVRv2Data(const unsigned char * data, ssize_t dataLen);
@@ -295,7 +292,6 @@ protected:
     Format detectFormat(const unsigned char * data, ssize_t dataLen);
     bool isPng(const unsigned char * data, ssize_t dataLen);
     bool isJpg(const unsigned char * data, ssize_t dataLen);
-    bool isTiff(const unsigned char * data, ssize_t dataLen);
     bool isWebp(const unsigned char * data, ssize_t dataLen);
     bool isPvr(const unsigned char * data, ssize_t dataLen);
     bool isEtc(const unsigned char * data, ssize_t dataLen);
