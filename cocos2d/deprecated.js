e5ba6831cc0c06c32282dccbe5e081046859fb17
yinkaile <501251991@qq.com>
2020-03-03 01:22:23 +0800

    support pcf soft shadow (#6230)
    * refine shadow and light
    
    * support soft shadow
    
    * refine code
    
    * refine code
    
    * refine code

--- cocos2d/deprecated.js
+++ cocos2d/deprecated.js
@@ -514,6 +514,11 @@ if (CC_DEBUG) {
         'minFrameSize'
     ], 'cc.dynamicAtlasManager')
 
+    // light component
+    markAsRemovedInObject(cc.Light.prototype, [
+        'shadowDepthScale',
+    ], 'cc.Light.prototype');
+
     // Value types
     provideClearError(cc, {
         // AffineTransform
