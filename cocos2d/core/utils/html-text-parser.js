1330697f069441f2a5d2e5d9fe338de2d273af73
jho1995 <337031709@qq.com>
2020-03-11 17:57:27 +0800

    optimize the  richText-parse for '<' and '>'

--- cocos2d/core/utils/html-text-parser.js
+++ cocos2d/core/utils/html-text-parser.js
@@ -49,25 +49,37 @@ HtmlTextParser.prototype = {
         var startIndex = 0;
         var length = htmlString.length;
         while (startIndex < length) {
-            var tagBeginIndex = htmlString.indexOf('<', startIndex);
+            var tagEndIndex = htmlString.indexOf('>', startIndex);
+            var tagBeginIndex = -1;
+            if (tagEndIndex >= 0) {
+                tagBeginIndex = htmlString.lastIndexOf('<', tagEndIndex);
+                var noTagBegin = tagBeginIndex < (startIndex - 1);
+
+                if (noTagBegin) {
+                    tagBeginIndex = htmlString.indexOf('<', tagEndIndex + 1);
+                    tagEndIndex = htmlString.indexOf('>', tagBeginIndex + 1);
+                }
+            }
+
             if (tagBeginIndex < 0) {
                 this._stack.pop();
                 this._processResult(htmlString.substring(startIndex));
                 startIndex = length;
             } else {
-                this._processResult(htmlString.substring(startIndex, tagBeginIndex));
-
-                var tagEndIndex = htmlString.indexOf('>', startIndex);
+                var newStr = htmlString.substring(startIndex, tagBeginIndex);
+                var tagStr = htmlString.substring(tagBeginIndex + 1, tagEndIndex);
+                if (tagStr === "") newStr = htmlString.substring(startIndex, tagEndIndex + 1);
+                this._processResult(newStr);
                 if (tagEndIndex === -1) {
                     // cc.error('The HTML tag is invalid!');
                     tagEndIndex = tagBeginIndex;
                 } else if (htmlString.charAt(tagBeginIndex + 1) === '\/'){
                     this._stack.pop();
                 } else {
-                    this._addToStack(htmlString.substring(tagBeginIndex + 1, tagEndIndex));
+                    this._addToStack(tagStr);
                 }
                 startIndex = tagEndIndex + 1;
-
+                
             }
         }
 
