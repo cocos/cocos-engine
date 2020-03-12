77b7c4d7706962874fe1abcf731429513c5a0331
JayceLai <goodlaijiaxin@qq.com>
2020-03-12 11:44:34 +0800

    tweaks

--- cocos/physics/ammo/shapes/ammo-bvh-triangle-mesh-shape.ts
+++ cocos/physics/ammo/shapes/ammo-bvh-triangle-mesh-shape.ts
@@ -26,55 +26,59 @@ export class AmmoBvhTriangleMeshShape extends AmmoShape implements ITrimeshShape
         } else {
 
             const mesh = v;
-            if (mesh) {
-                this._btTriangleMesh = new Ammo.btTriangleMesh();
-                const primitiveMode = mesh.renderingMesh.subMeshes[0].primitiveMode;
+            if (mesh && mesh.renderingMesh && mesh.renderingMesh.subMeshes.length > 0) {
                 const geoInfo = mesh.renderingMesh.subMeshes[0].geometricInfo!;
-                const vb = geoInfo.positions;
-                const ib = geoInfo.indices as any;
-                if (primitiveMode == GFXPrimitiveMode.TRIANGLE_LIST) {
-                    const cnt = ib.length;
-                    for (let j = 0; j < cnt; j += 3) {
-                        var i0 = ib[j] * 3;
-                        var i1 = ib[j + 1] * 3;
-                        var i2 = ib[j + 2] * 3;
-                        const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
-                        const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
-                        const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
-                        this._btTriangleMesh.addTriangle(v0, v1, v2);
-                    }
-                } else if (primitiveMode == GFXPrimitiveMode.TRIANGLE_STRIP) {
-                    const cnt = ib.length - 2;
-                    let rev = 0;
-                    for (let j = 0; j < cnt; j += 1) {
-                        const i0 = ib[j - rev] * 3;
-                        const i1 = ib[j + rev + 1] * 3;
-                        const i2 = ib[j + 2] * 3;
-                        const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
-                        const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
-                        const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
-                        this._btTriangleMesh.addTriangle(v0, v1, v2);
-                    }
+                if (geoInfo) {
+                    this._btTriangleMesh = new Ammo.btTriangleMesh();
+                    const primitiveMode = mesh.renderingMesh.subMeshes[0].primitiveMode;
+                    const vb = geoInfo.positions;
+                    const ib = geoInfo.indices as any;
+                    if (primitiveMode == GFXPrimitiveMode.TRIANGLE_LIST) {
+                        const cnt = ib.length;
+                        for (let j = 0; j < cnt; j += 3) {
+                            var i0 = ib[j] * 3;
+                            var i1 = ib[j + 1] * 3;
+                            var i2 = ib[j + 2] * 3;
+                            const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
+                            const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
+                            const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
+                            this._btTriangleMesh.addTriangle(v0, v1, v2);
+                        }
+                    } else if (primitiveMode == GFXPrimitiveMode.TRIANGLE_STRIP) {
+                        const cnt = ib.length - 2;
+                        let rev = 0;
+                        for (let j = 0; j < cnt; j += 1) {
+                            const i0 = ib[j - rev] * 3;
+                            const i1 = ib[j + rev + 1] * 3;
+                            const i2 = ib[j + 2] * 3;
+                            const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
+                            const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
+                            const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
+                            this._btTriangleMesh.addTriangle(v0, v1, v2);
+                        }
 
-                } else if (primitiveMode == GFXPrimitiveMode.TRIANGLE_FAN) {
-                    const cnt = ib.length - 1;
-                    const i0 = ib[0] * 3;
-                    const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
-                    for (let j = 1; j < cnt; j += 1) {
-                        const i1 = ib[j] * 3;
-                        const i2 = ib[j + 1] * 3;
-                        const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
-                        const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
-                        this._btTriangleMesh.addTriangle(v0, v1, v2);
-                    }
+                    } else if (primitiveMode == GFXPrimitiveMode.TRIANGLE_FAN) {
+                        const cnt = ib.length - 1;
+                        const i0 = ib[0] * 3;
+                        const v0 = new Ammo.btVector3(vb[i0], vb[i0 + 1], vb[i0 + 2]);
+                        for (let j = 1; j < cnt; j += 1) {
+                            const i1 = ib[j] * 3;
+                            const i2 = ib[j + 1] * 3;
+                            const v1 = new Ammo.btVector3(vb[i1], vb[i1 + 1], vb[i1 + 2]);
+                            const v2 = new Ammo.btVector3(vb[i2], vb[i2 + 1], vb[i2 + 2]);
+                            this._btTriangleMesh.addTriangle(v0, v1, v2);
+                        }
 
+                    }
+                    this._btShape = new Ammo.btBvhTriangleMeshShape(this._btTriangleMesh, true, true);
+                    cocos2AmmoVec3(this.scale, this._collider.node.worldScale);
+                    this._btShape.setLocalScaling(this.scale);
+
+                    this.setWrapper();
+                    this.setCompound(this._btCompound);
+                }else{
+                    // TODO: 
                 }
-                this._btShape = new Ammo.btBvhTriangleMeshShape(this._btTriangleMesh, true, true);
-                cocos2AmmoVec3(this.scale, this._collider.node.worldScale);
-                this._btShape.setLocalScaling(this.scale);
-
-                this.setWrapper();
-                this.setCompound(this._btCompound);
             } else {
                 this._btShape = AmmoConstant.instance.emptyShape;
             }
