0d26ee4c93cb99e1b10d2e364558e91e247d2a1a
Unknown <leslieleigh@outlook.com>
2020-03-16 14:08:36 +0800

    Provide .d.ts file for illustration of DebugInfos.json

---
+++ DebugInfos.d.ts
@@ -0,0 +1,15 @@
+
+/**
+ * This is a declaration file for 'DebugInfos.json'
+ * which is generated from 'EngineErrorMap.md'.
+ * You may run cli command
+ * `gulp build-debug-infos` or `npx gulp build-debug-infos` to generate the json file.
+ */
+
+/**
+ * Engine error maps.
+ * The keys are error codes and their values are format strings.
+ */
+declare const $: Record<number, string>;
+
+export default $;
