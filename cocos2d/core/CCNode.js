2f8a8db60939daf8fd5eb7597e5557469dadb553
Santy-Wang <wh19930829@qq.com>
2020-03-16 18:10:23 +0800

    refine d.ts about node.position (#6341)

--- cocos2d/core/CCNode.js
+++ cocos2d/core/CCNode.js
@@ -925,7 +925,7 @@ let NodeDefines = {
         /**
          * !#en The position (x, y) of the node in its parent's coordinates.
          * !#zh 节点在父节点坐标系中的位置（x, y）。
-         * @property {Vec3} position
+         * @property {Vec2|Vec3} position
          * @example
          * cc.log("Node Position: " + node.position);
          */
