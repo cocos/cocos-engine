90400c0bebe87ff0f298380999ff1b6149e49930
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-03-17 18:38:29 +0800

    [3d-gfx] GFXObject add virtual destructor (#2285)
    * add virtual destructor
    
    * remove empty row

--- native/cocos/renderer/core/gfx/GFXObject.h
+++ native/cocos/renderer/core/gfx/GFXObject.h
@@ -7,10 +7,11 @@ NS_CC_BEGIN
 class GFXObject : public Object {
 public:
     GFXObject(GFXObjectType gfxType);
+    virtual ~GFXObject() = default;
     
     CC_INLINE GFXObjectType getType() const { return _gfxType; }
     CC_INLINE GFXStatus getStatus() const { return _status; }
-    
+
 protected:
     GFXObjectType _gfxType = GFXObjectType::UNKNOWN;
     GFXStatus _status = GFXStatus::UNREADY;
