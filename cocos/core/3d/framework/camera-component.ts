b06204f093db883d9f79db2b251d2080f42d3357
JayceLai <goodlaijiaxin@qq.com>
2020-03-20 15:51:00 +0800

    fix set of color & clear color

--- cocos/core/3d/framework/camera-component.ts
+++ cocos/core/3d/framework/camera-component.ts
@@ -150,7 +150,7 @@ export class CameraComponent extends Component {
 
     set priority (val) {
         this._priority = val;
-        if (this._camera){
+        if (this._camera) {
             this._camera.priority = val;
         }
     }
@@ -227,17 +227,17 @@ export class CameraComponent extends Component {
         tooltip: 'i18n:camera.color',
     })
     // @constget
-    get color (): Readonly<Color>  {
+    get color (): Readonly<Color> {
         return this._color;
     }
 
     set color (val) {
         this._color.set(val);
         if (this._camera) {
-            this._camera.clearColor.r = val.x;
-            this._camera.clearColor.g = val.y;
-            this._camera.clearColor.b = val.z;
-            this._camera.clearColor.a = val.w;
+            this._camera.clearColor.r = this._color.x;
+            this._camera.clearColor.g = this._color.y;
+            this._camera.clearColor.b = this._color.z;
+            this._camera.clearColor.a = this._color.w;
         }
     }
 
@@ -353,8 +353,8 @@ export class CameraComponent extends Component {
         return this._targetTexture;
     }
 
-    set targetTexture (value){
-        if (this._targetTexture === value){
+    set targetTexture (value) {
+        if (this._targetTexture === value) {
             return;
         }
 
@@ -363,7 +363,7 @@ export class CameraComponent extends Component {
         this._chechTargetTextureEvent(old);
         this._updateTargetTexture();
 
-        if (!value && this._camera){
+        if (!value && this._camera) {
             this._camera.changeTargetWindow(EDITOR ? cc.director.root.tempWindow : null);
             this._camera.isWindowSize = true;
         }
@@ -447,7 +447,7 @@ export class CameraComponent extends Component {
      * uiNode.position = out;
      * ```
      */
-    public convertToUINode (wpos: Vec3, uiNode: Node, out?: Vec3){
+    public convertToUINode (wpos: Vec3, uiNode: Node, out?: Vec3) {
         if (!out) {
             out = new Vec3();
         }
@@ -525,7 +525,7 @@ export class CameraComponent extends Component {
 
     protected _chechTargetTextureEvent (old: RenderTexture | null) {
         const resizeFunc = (window: GFXWindow) => {
-            if (this._camera){
+            if (this._camera) {
                 this._camera.setFixedSize(window.width, window.height);
             }
         };
@@ -539,7 +539,7 @@ export class CameraComponent extends Component {
         }
     }
 
-    protected _updateTargetTexture (){
+    protected _updateTargetTexture () {
         if (!this._camera) {
             return;
         }
