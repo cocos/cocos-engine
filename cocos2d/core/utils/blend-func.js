0c40e656b805c799cad9322970192392e5ecfdba
finscn <finscn@gmail.com>
2020-03-25 16:58:57 +0800

    Update blend-func.js

--- cocos2d/core/utils/blend-func.js
+++ cocos2d/core/utils/blend-func.js
@@ -64,12 +64,11 @@ let BlendFunc = cc.Class({
 
     setMaterial (index, material) {
         let materialVar = RenderComponent.prototype.setMaterial.call(this, index, material);
-        
-        if (this._srcBlendFactor === BlendFactor.SRC_ALPHA && this._dstBlendFactor === BlendFactor.ONE_MINUS_SRC_ALPHA) {
-            return materialVar;
+
+        if (this._srcBlendFactor !== BlendFactor.SRC_ALPHA || this._dstBlendFactor !== BlendFactor.ONE_MINUS_SRC_ALPHA) {
+            this._updateMaterialBlendFunc(material);
         }
-        this._updateMaterialBlendFunc(material);
-        
+
         return materialVar;
     },
 
