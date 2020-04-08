f1130a59f61231737e1ae6f38a9a62508f148df9
caryliu <caryliu1999@gmail.com>
2020-04-08 19:39:06 +0800

    add deprecated

---
+++ cocos/particle/deprecated.ts
@@ -0,0 +1,11 @@
+import { removeProperty } from '../core/utils/deprecated';
+import Burst from './burst';
+
+removeProperty(Burst.prototype, 'Burst.prototype', [
+    {
+        'name': 'minCount'
+    },
+    {
+        'name': 'maxCount',
+    }
+]);
