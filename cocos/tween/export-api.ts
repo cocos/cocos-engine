fdfb82d20e5bc504844cf56a70401fa8b636444f
jiaxin.lai <goodlaijiaxin@qq.com>
2020-04-12 17:31:27 +0800

    fix type of ITweenOption

--- cocos/tween/export-api.ts
+++ cocos/tween/export-api.ts
@@ -18,7 +18,7 @@ export type TweenEasing =
 export interface ITweenOption {
     easing?: TweenEasing | ((k: number) => number);
     progress?: (start: number, end: number, current: number, ratio: number) => number;
-    onStart?: (target?: object) => {};
-    onUpdate?: (target?: object, ratio?: number) => {};
-    onComplete?: (target?: object) => {};
+    onStart?: (target?: object) => void;
+    onUpdate?: (target?: object, ratio?: number) => void;
+    onComplete?: (target?: object) => void;
 }
