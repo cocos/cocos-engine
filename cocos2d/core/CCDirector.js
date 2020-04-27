e11b35b5bacc15e8e821c2aeb8bc06dd62853ad8
Santy-Wang <wh19930829@qq.com>
2020-04-27 21:26:38 +0800

    fix small issue

--- cocos2d/core/CCDirector.js
+++ cocos2d/core/CCDirector.js
@@ -580,7 +580,7 @@ cc.Director.prototype = {
             return bundle.getSceneInfo(sceneName);
         });
         if (bundle) {
-            return bundle.preloadScene(sceneName, options, onProgress, onLoaded);
+            return bundle.preloadScene(sceneName, null, onProgress, onLoaded);
         }
         else {
             cc.errorID(1209, sceneName);
