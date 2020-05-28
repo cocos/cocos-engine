a293773b80220101275d7d8756c0919170efb16a
minggo <nudtzxm@gmail.com>
2020-05-28 10:35:54 +0800

    remove spaces

--- cocos/core/pipeline/pipeline-state-manager.ts
+++ cocos/core/pipeline/pipeline-state-manager.ts
@@ -19,31 +19,31 @@ export class PipelineStateManager {
         ia: GFXInputAssembler
         ): GFXPipelineState {
 
-            const hash1 = psoCreateInfo.hash;
-            const hash2= renderPass.hash;
-            const hash3 = ia.attributesHash;
+        const hash1 = psoCreateInfo.hash;
+        const hash2= renderPass.hash;
+        const hash3 = ia.attributesHash;
 
-            const res = `ps,${hash1},${hash2},${hash3}`;
-            const newHash = murmurhash2_32_gc(res, 666);
-            let pso = this._PSOHashMap.get(newHash);
-            if (!pso) {
-                const createInfo: IGFXPipelineStateInfo = {
-                    primitive: psoCreateInfo.primitive,
-                    shader: psoCreateInfo.shader,
-                    inputAssember: ia,
-                    rasterizerState: psoCreateInfo.rasterizerState,
-                    depthStencilState: psoCreateInfo.depthStencilState,
-                    blendState: psoCreateInfo.blendState,
-                    dynamicStates: psoCreateInfo.dynamicStates,
-                    layout: psoCreateInfo.pipelineLayout,
-                    renderPass: renderPass,
-                    hash: newHash,
-                };
+        const res = `ps,${hash1},${hash2},${hash3}`;
+        const newHash = murmurhash2_32_gc(res, 666);
+        let pso = this._PSOHashMap.get(newHash);
+        if (!pso) {
+            const createInfo: IGFXPipelineStateInfo = {
+                primitive: psoCreateInfo.primitive,
+                shader: psoCreateInfo.shader,
+                inputAssember: ia,
+                rasterizerState: psoCreateInfo.rasterizerState,
+                depthStencilState: psoCreateInfo.depthStencilState,
+                blendState: psoCreateInfo.blendState,
+                dynamicStates: psoCreateInfo.dynamicStates,
+                layout: psoCreateInfo.pipelineLayout,
+                renderPass: renderPass,
+                hash: newHash,
+            };
 
-                pso = device.createPipelineState(createInfo);
-                this._PSOHashMap.set(newHash, pso);
-            }
-            
-            return pso;
+            pso = device.createPipelineState(createInfo);
+            this._PSOHashMap.set(newHash, pso);
+        }
+        
+        return pso;
     }
 }
