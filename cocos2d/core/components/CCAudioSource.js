62af6aea9a444a3d05d6283988615e32b11aec2b
PP_Pro <chinese1236@qq.com>
2020-06-05 16:20:56 +0800

    set Audio as a builtin module in engine
    fix

--- cocos2d/core/components/CCAudioSource.js
+++ cocos2d/core/components/CCAudioSource.js
@@ -45,8 +45,8 @@ var AudioSource = cc.Class({
 
     ctor: function () {
         // We can't require Audio here because jsb Audio is implemented outside the engine,
-        // it can only take effect rely on overwriting cc.Audio
-        this.audio = new cc.Audio();
+        // it can only take effect rely on overwriting cc._Audio
+        this.audio = new cc._Audio();
     },
 
     properties: {
@@ -77,7 +77,7 @@ var AudioSource = cc.Class({
         isPlaying: {
             get: function () {
                 var state = this.audio.getState();
-                return state === cc.Audio.State.PLAYING;
+                return state === cc._Audio.State.PLAYING;
             },
             visible: false
         },
@@ -200,7 +200,7 @@ var AudioSource = cc.Class({
 
     _pausedCallback: function () {
         var state = this.audio.getState();
-        if (state === cc.Audio.State.PLAYING) {
+        if (state === cc._Audio.State.PLAYING) {
             this.audio.pause();
             this._pausedFlag = true;
         }
