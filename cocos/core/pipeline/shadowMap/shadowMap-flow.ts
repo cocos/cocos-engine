a0ba2deed54542deb69ba15908f80fee99c47152
troublemaker <troublemaker52025@outlook.com>
2020-06-16 16:51:07 +0800

    optimization

--- cocos/core/pipeline/shadowMap/shadowMap-flow.ts
+++ cocos/core/pipeline/shadowMap/shadowMap-flow.ts
@@ -25,17 +25,16 @@ export class ShadowMapFlow extends RenderFlow {
 
     protected updateShadowMap () {
         // 如果上一帧结果被清除，则使用copyFramebuffer
-        if (this._stages[0].framebuffer) {
-            const shadowMap = this._stages[0].framebuffer!;
+        if (this._stages[this._frame % 2].framebuffer) {
+            const shadowMap = this._stages[this._frame % 2].framebuffer!;
             this._shadowMap[this._frame % 2] = shadowMap;
         }
 
-        this._frame++;
-
+        // sweap buffer
         // @ts-ignore
         this.pipeline.shadowMap = null;
         // @ts-ignore
-        director.root!.device.copyFramebufferToBuffer(this._shadowMap[this._frame % 2], this.pipeline.shadowMap, readRegions);
+        director.root!.device.copyFramebufferToBuffer(this._shadowMap[(this._frame + 1) % 2], this.pipeline.shadowMap, readRegions);
     }
 
     public static initInfo: IRenderFlowInfo = {
@@ -55,16 +54,25 @@ export class ShadowMapFlow extends RenderFlow {
         super.initialize(info);
 
         // add shadowMap-stages
-        const shadowMapStage = new ShadowMapStage();
-        this._stages.push(shadowMapStage);
+        const shadowMapStage_1 = new ShadowMapStage();
+        this._stages.push(shadowMapStage_1);
+
+        const shadowMapStage_2 = new ShadowMapStage();
+        this._stages.push(shadowMapStage_2);
 
         this._shadowMap.length = 2;
     }
 
     public render (view: RenderView) {
-        super.render(view);
 
+        // render One of the FBO
+        this.stages[this._frame % 2].render(view);
+
+        // get One of the other FBO
         this.updateShadowMap();
+
+        // control both
+        this._frame++;
     }
 
     /**
