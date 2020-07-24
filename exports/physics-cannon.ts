bc83c34d904e069e2083d0e0371cd50a408e2499
JayceLai <goodlaijiaxin@qq.com>
2020-07-24 13:42:39 +0800

    overwrite ArrayCollisionMatrix.prototype.reset

--- exports/physics-cannon.ts
+++ exports/physics-cannon.ts
@@ -5,11 +5,19 @@
 import CANNON from '@cocos/cannon';
 if (window) window.CANNON = CANNON;
 
-//polyfill config
+// polyfill config
 CANNON['CC_CONFIG'] = {
     'numSegmentsCone': 12,
     'numSegmentsCylinder': 12,
     'ignoreSelfBody': true,
 }
+
+// overwrite
+CANNON.ArrayCollisionMatrix.prototype.reset = function () {
+    for (let key in this.matrix) {
+        delete this.matrix[key];
+    }
+}
+
 
 import '../cocos/physics/cannon/instantiate';
