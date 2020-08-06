e6c3b886e8578fd200ebeec98ee84c3abb711bd3
knox <knox.h@qq.com>
2020-08-06 10:38:16 +0800

    Fix bug with exceptions in _val data for getting color from HSV.

--- cocos2d/core/value-types/color.ts
+++ cocos2d/core/value-types/color.ts
@@ -793,7 +793,7 @@ export default class Color extends ValueType {
         r *= 255;
         g *= 255;
         b *= 255;
-        this._val = ((this.a << 24) >>> 0) + (b << 16) + (g << 8) + r;
+        this._val = ((this.a << 24) >>> 0) + (b << 16) + (g << 8) + Math.floor(r);
         return this;
     }
 
