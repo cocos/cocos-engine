9b4f0ae6888b5bfa66c987f12affd11628e35f68
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2020-08-10 16:05:34 +0800

    Fix typo (#7102)

--- cocos/core/components/ui-base/canvas-component.ts
+++ cocos/core/components/ui-base/canvas-component.ts
@@ -107,7 +107,7 @@ export class CanvasComponent extends Component {
      * @zh
      * 内置相机的颜色缓冲默认值。
      */
-    @property('清理颜色缓冲区后的颜色')
+    @tooltip('清理颜色缓冲区后的颜色')
     get color () {
         return this._color;
     }
