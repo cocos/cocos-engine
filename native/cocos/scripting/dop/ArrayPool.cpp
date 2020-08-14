ecaff068a18577c8a18570c209b9ad011fc6e20f
minggo <nudtzxm@gmail.com>
2020-08-14 09:41:13 +0800

    Native array pool (#2686)

---
+++ native/cocos/scripting/dop/ArrayPool.cpp
@@ -0,0 +1,76 @@
+#include "ArrayPool.h"
+#include "base/memory/Memory.h"
+#include "base/Log.h"
+
+// Data type of array element is uint32_t
+#define BYTES_PER_ELEMENT 4
+
+namespace se {
+
+cc::map<PoolType, ArrayPool *> ArrayPool::_pools;
+
+ArrayPool::ArrayPool(PoolType type, uint size)
+: _type(type), _size(size) {
+    ArrayPool::_pools[_type] = this;
+}
+
+ArrayPool::~ArrayPool() {
+    ArrayPool::_pools.erase(_type);
+}
+
+Object *ArrayPool::alloc(uint index) {
+    uint bytes = _size * BYTES_PER_ELEMENT;
+    uint8_t *buffer = static_cast<uint8_t *>(CC_MALLOC(bytes));
+    if (!buffer)
+        return nullptr;
+
+    auto obj = Object::createArrayBufferObject(buffer, bytes);
+    CC_FREE(buffer);
+    _objects[index] = obj;
+    _indexes[obj] = index;
+
+    return obj;
+}
+
+Object *ArrayPool::resize(Object *origin, uint size) {
+    uint bytes = size * BYTES_PER_ELEMENT;
+    uint8_t *buffer = static_cast<uint8_t *>(CC_MALLOC(bytes));
+    if (!buffer) {
+        CC_LOG_ERROR("Can not resize array.");
+        return origin;
+    }
+
+    uint8_t *originData = nullptr;
+    size_t len = 0;
+    origin->getTypedArrayData(&originData, &len);
+    memcpy(buffer, originData, len);
+
+    auto obj = Object::createArrayBufferObject(buffer, bytes);
+    CC_FREE(buffer);
+    
+    uint originIndex = _indexes[origin];
+    _objects[originIndex] = obj;
+    _indexes[obj] = originIndex;
+    _indexes.erase(origin);
+
+    return obj;
+}
+
+uint8_t *ArrayPool::getArray(PoolType type, uint index) {
+    if (ArrayPool::_pools.count(type) != 0) {
+        const auto pool = ArrayPool::_pools[type];
+        if (pool->_objects.count(index) != 0) {
+            uint8_t *ret = nullptr;
+            size_t len = 0;
+            pool->_objects[index]->getTypedArrayData(&ret, &len);
+            return ret;
+        }
+        else {
+            return nullptr;
+        }
+    } else {
+        return nullptr;
+    }
+}
+
+} // namespace se
--- native/cocos/scripting/dop/PoolType.h
+++ native/cocos/scripting/dop/PoolType.h
@@ -7,12 +7,17 @@ enum class PoolType {
     DEPTH_STENCIL_STATE,
     BLEND_STATE,
     BINDING_LAYOUT,
+    DESCRIPTOR_SETS,
     SHADER,
+    INPUT_ASSEMBLER,
+    PIPELINE_LAYOUT,
     // buffers
     PASS_INFO,
     PSOCI,
     MODEL_INFO,
     SUBMODEL_INFO,
-    INPUT_ASSEMBLER_INFO
+    INPUT_ASSEMBLER_INFO,
+    // array
+    SUBMODEL_ARRAY
 };
 }
