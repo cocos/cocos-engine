519814d61504f3393750271278ea49b05910ecee
SijieWang <itharbors@outlook.com>
2020-08-17 23:02:33 +0800

    修复一些渲染相关的 bug (#7155)

--- cocos/core/renderer/core/pass.ts
+++ cocos/core/renderer/core/pass.ts
@@ -179,7 +179,6 @@ export class Pass {
     protected _dynamics: IPassDynamics = {};
     protected _propertyHandleMap: Record<string, number> = {};
     protected _blocks: IBlock[] = [];
-    protected _pipelineLayout: IPipelineLayoutInfo = null!;
     protected _shaderInfo: IProgramInfo = null!;
     protected _defines: MacroRecord = {};
     protected _properties: Record<string, IPropertyInfo> = {};
@@ -476,7 +475,7 @@ export class Pass {
         const key = programLib.getKey(this._programName, this._defines);
         this._hShaderDefault = programLib.getGFXShader(this._device, this._programName, this._defines, pipeline, key);
         if (!this._hShaderDefault) { console.warn(`create shader ${this._programName} failed`); return false; }
-        PassPool.set(this._handle, PassView.PIPELINE_LAYOUT, this._pipelineLayout.hPipelineLayout);
+        PassPool.set(this._handle, PassView.PIPELINE_LAYOUT, programLib.getPipelineLayout(this._programName).hPipelineLayout);
         PassPool.set(this._handle, PassView.HASH, Pass.getPassHash(this._handle, this._hShaderDefault));
         return true;
     }
@@ -534,7 +533,6 @@ export class Pass {
         this._programName = info.program;
         this._defines = copyDefines ? Object.assign({}, info.defines) : info.defines;
         this._shaderInfo = programLib.getTemplate(info.program);
-        this._pipelineLayout = programLib.getPipelineLayout(info.program);
         this._properties = info.properties || this._properties;
         // pipeline state
         const device = this._device;
@@ -542,7 +540,7 @@ export class Pass {
         if (info.stateOverrides) { Pass.fillPipelineInfo(handle, info.stateOverrides); }
 
         // init descriptor set
-        const setLayouts = this._pipelineLayout.setLayouts;
+        const setLayouts = programLib.getPipelineLayout(info.program).setLayouts;
         if (!setLayouts[SetIndex.MATERIAL]) {
             setLayouts[SetIndex.MATERIAL] = device.createDescriptorSetLayout({
                 bindings: this._shaderInfo.bindings,
@@ -596,7 +594,7 @@ export class Pass {
     get root () { return this._root; }
     get device () { return this._device; }
     get shaderInfo () { return this._shaderInfo; }
-    get setLayouts () { return this._pipelineLayout.setLayouts; }
+    get setLayouts () { return programLib.getPipelineLayout(this._programName).setLayouts; }
     get program () { return this._programName; }
     get properties () { return this._properties; }
     get defines () { return this._defines; }
