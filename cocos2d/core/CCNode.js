c533619776c7bb8656b42a849927298e081fe90f
Knox <knox.h@qq.com>
2020-08-19 16:32:37 +0800

    rm upgrade color.a to opacity for 2d-tasks/issues/2934 (#6891)
    * rm upgrade color.a to opacity for 2d-tasks/issues/2934
    rename _upgrade_1x_to_2x to _initProperties
    
    * refine code

--- cocos2d/core/CCNode.js
+++ cocos2d/core/CCNode.js
@@ -1853,7 +1853,7 @@ let NodeDefines = {
         }
     },
 
-    _upgrade_1x_to_2x () {
+    _initProperties () {
         if (this._is3DNode) {
             this._update3DFunction();
         }
@@ -1872,11 +1872,6 @@ let NodeDefines = {
             trs = this._trs = this._spaceInfo.trs;
         }
 
-        if (this._zIndex !== undefined) {
-            this._localZOrder = this._zIndex << 16;
-            this._zIndex = undefined;
-        }
-
         if (CC_EDITOR) {
             if (this._skewX !== 0 || this._skewY !== 0) {
                 var NodeUtils = Editor.require('scene://utils/node');
@@ -1885,17 +1880,6 @@ let NodeDefines = {
         }
 
         this._fromEuler();
-
-        if (this._localZOrder !== 0) {
-            this._zIndex = (this._localZOrder & 0xffff0000) >> 16;
-        }
-
-        // Upgrade from 2.0.0 preview 4 & earlier versions
-        // TODO: Remove after final version
-        if (this._color.a < 255 && this._opacity === 255) {
-            this._opacity = this._color.a;
-            this._color.a = 255;
-        }
 
         if (CC_JSB && CC_NATIVERENDERER) {
             this._renderFlag |= RenderFlow.FLAG_TRANSFORM | RenderFlow.FLAG_OPACITY_COLOR;
@@ -1915,7 +1899,7 @@ let NodeDefines = {
             PrefabHelper.syncWithPrefab(this);
         }
 
-        this._upgrade_1x_to_2x();
+        this._initProperties();
 
         this._updateOrderOfArrival();
 
@@ -1949,7 +1933,7 @@ let NodeDefines = {
 
     // the same as _onBatchCreated but untouch prefab
     _onBatchRestored () {
-        this._upgrade_1x_to_2x();
+        this._initProperties();
 
         // Fixed a bug where children and parent node groups were forced to synchronize, instead of only synchronizing `_cullingMask` value
         this._cullingMask = 1 << _getActualGroupIndex(this);
