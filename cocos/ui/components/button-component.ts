f9fe1e0d3573d40e051ec1f71d57041e3b9ac62c
PP_Pro <chinese1236@qq.com>
2020-08-19 14:54:12 +0800

    refine button target
    update

--- cocos/ui/components/button-component.ts
+++ cocos/ui/components/button-component.ts
@@ -171,7 +171,7 @@ export class ButtonComponent extends Component {
     @displayOrder(0)
     @tooltip('指定 Button 背景节点，Button 状态改变时会修改此节点的 Color 或 Sprite 属性')
     get target () {
-        return this._target;
+        return this._target || this.node;
     }
 
     set target (value) {
@@ -247,7 +247,6 @@ export class ButtonComponent extends Component {
         }
         this._transition = value;
         this._updateState();
-
     }
 
     // color transition
@@ -400,7 +399,7 @@ export class ButtonComponent extends Component {
         }
 
         this._normalSprite = value;
-        const sprite = this.node.getComponent(SpriteComponent);
+        const sprite = this.target.getComponent(SpriteComponent);
         if (sprite) {
             sprite.spriteFrame = value;
         }
@@ -526,11 +525,7 @@ export class ButtonComponent extends Component {
     private _targetScale: Vec3 = new Vec3();
 
     public __preload () {
-        if (!this.target) {
-            this.target = this.node;
-        }
-
-        const sprite = this.node.getComponent(SpriteComponent);
+        const sprite = this.target.getComponent(SpriteComponent);
         if (sprite) {
             this._normalSprite = sprite.spriteFrame;
         }
@@ -540,36 +535,17 @@ export class ButtonComponent extends Component {
     }
 
     public onEnable () {
-        // check sprite frames
-        //
-        if (!EDITOR || legacyCC.GAME_VIEW) {
-            this._registerEvent();
-        } else {
-            this.node.on(SpriteComponent.EventType.SPRITE_FRAME_CHANGED, this._onTargetSpriteFrameChanged, this);
-            this.node.on(SystemEventType.COLOR_CHANGED, this._onTargetColorChanged, this);
-        }
+        this._registerEvent();
     }
 
     public onDisable () {
         this._resetState();
-
-        if (!EDITOR || legacyCC.GAME_VIEW) {
-            this.node.off(SystemEventType.TOUCH_START, this._onTouchBegan, this);
-            this.node.off(SystemEventType.TOUCH_MOVE, this._onTouchMove, this);
-            this.node.off(SystemEventType.TOUCH_END, this._onTouchEnded, this);
-            this.node.off(SystemEventType.TOUCH_CANCEL, this._onTouchCancel, this);
-
-            this.node.off(SystemEventType.MOUSE_ENTER, this._onMouseMoveIn, this);
-            this.node.off(SystemEventType.MOUSE_LEAVE, this._onMouseMoveOut, this);
-        } else {
-            this.node.off(SpriteComponent.EventType.SPRITE_FRAME_CHANGED);
-            this.node.off(SystemEventType.COLOR_CHANGED);
-        }
+        this._unregisterEvent();
     }
 
     public update (dt: number) {
-        const target = this._target ? this._target : this.node;
-        if (this._transitionFinished) {
+        const target = this.target;
+        if (this._transitionFinished || !target) {
             return;
         }
 
@@ -605,7 +581,10 @@ export class ButtonComponent extends Component {
     }
 
     protected _resizeNodeToTargetNode () {
-        let targetTrans = this._target && this._target._uiProps.uiTransformComp;
+        if (!this.target) {
+            return;
+        }
+        let targetTrans = this.target._uiProps.uiTransformComp;
         if (EDITOR && targetTrans) {
             this.node._uiProps.uiTransformComp!.setContentSize(targetTrans.contentSize);
         }
@@ -615,7 +594,7 @@ export class ButtonComponent extends Component {
         this._pressed = false;
         this._hovered = false;
         // Restore button status
-        const target = this._target;
+        const target = this.target;
         if (!target) {
             return;
         }
@@ -634,13 +613,34 @@ export class ButtonComponent extends Component {
     }
 
     protected _registerEvent () {
-        this.node.on(SystemEventType.TOUCH_START, this._onTouchBegan, this);
-        this.node.on(SystemEventType.TOUCH_MOVE, this._onTouchMove, this);
-        this.node.on(SystemEventType.TOUCH_END, this._onTouchEnded, this);
-        this.node.on(SystemEventType.TOUCH_CANCEL, this._onTouchCancel, this);
+        // DIFF: this.node need to do hit test, this.target need to do the button transition work
+        if (!EDITOR || legacyCC.GAME_VIEW) {
+            this.node.on(SystemEventType.TOUCH_START, this._onTouchBegan, this);
+            this.node.on(SystemEventType.TOUCH_MOVE, this._onTouchMove, this);
+            this.node.on(SystemEventType.TOUCH_END, this._onTouchEnded, this);
+            this.node.on(SystemEventType.TOUCH_CANCEL, this._onTouchCancel, this);
+    
+            this.node.on(SystemEventType.MOUSE_ENTER, this._onMouseMoveIn, this);
+            this.node.on(SystemEventType.MOUSE_LEAVE, this._onMouseMoveOut, this);
+        } else {
+            this.target.on(SpriteComponent.EventType.SPRITE_FRAME_CHANGED, this._onTargetSpriteFrameChanged, this);
+            this.target.on(SystemEventType.COLOR_CHANGED, this._onTargetColorChanged, this);
+        }
+    }
 
-        this.node.on(SystemEventType.MOUSE_ENTER, this._onMouseMoveIn, this);
-        this.node.on(SystemEventType.MOUSE_LEAVE, this._onMouseMoveOut, this);
+    protected _unregisterEvent () {
+        if (!EDITOR || legacyCC.GAME_VIEW) {
+            this.node.off(SystemEventType.TOUCH_START, this._onTouchBegan, this);
+            this.node.off(SystemEventType.TOUCH_MOVE, this._onTouchMove, this);
+            this.node.off(SystemEventType.TOUCH_END, this._onTouchEnded, this);
+            this.node.off(SystemEventType.TOUCH_CANCEL, this._onTouchCancel, this);
+
+            this.node.off(SystemEventType.MOUSE_ENTER, this._onMouseMoveIn, this);
+            this.node.off(SystemEventType.MOUSE_LEAVE, this._onMouseMoveOut, this);
+        } else {
+            this.target.off(SpriteComponent.EventType.SPRITE_FRAME_CHANGED);
+            this.target.off(SystemEventType.COLOR_CHANGED);
+        }
     }
 
     protected _getTargetSprite (target: Node | null) {
@@ -652,9 +652,9 @@ export class ButtonComponent extends Component {
     }
 
     protected _applyTarget () {
-        this._sprite = this._getTargetSprite(this._target);
-        if (this._target) {
-            Vec3.copy(this._originalScale, this._target.getScale());
+        if (this.target) {
+            this._sprite = this._getTargetSprite(this.target);
+            Vec3.copy(this._originalScale, this.target.getScale());
         }
     }
 
@@ -733,7 +733,7 @@ export class ButtonComponent extends Component {
 
         const hit = this.node._uiProps.uiTransformComp!.isHit(touch.getUILocation());
 
-        if (this._transition === Transition.SCALE && this._target) {
+        if (this._transition === Transition.SCALE && this.target) {
             if (hit) {
                 Vec3.copy(this._fromScale, this._originalScale);
                 Vec3.multiplyScalar(this._toScale, this._originalScale, this._zoomScale);
@@ -741,9 +741,7 @@ export class ButtonComponent extends Component {
             } else {
                 this._time = 0;
                 this._transitionFinished = true;
-                if (this._target) {
-                    this._target!.setScale(this._originalScale);
-                }
+                this.target.setScale(this._originalScale);
             }
         } else {
             let state;
@@ -821,12 +819,8 @@ export class ButtonComponent extends Component {
 
     protected _updateColorTransition (state: string) {
         const color = this[state + 'Color'];
-        const target = this._target;
-        if (!target) {
-            return;
-        }
 
-        const renderComp = target.getComponent(UIRenderComponent);
+        const renderComp = this.target?.getComponent(UIRenderComponent);
         if (!renderComp) {
             return;
         }
@@ -868,11 +862,10 @@ export class ButtonComponent extends Component {
     }
 
     protected _zoomBack () {
-        if (!this._target) {
+        if (!this.target) {
             return;
         }
-
-        Vec3.copy(this._fromScale, this._target.getScale());
+        Vec3.copy(this._fromScale, this.target.getScale());
         Vec3.copy(this._toScale, this._originalScale);
         this._time = 0;
         this._transitionFinished = false;
