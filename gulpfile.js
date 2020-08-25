8415655e12f1522dbb9c4583f6fad5079817ad2b
SB <leslieleigh@outlook.com>
2020-08-25 17:42:47 +0800

    Fix npx

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ gulp.task('build-declarations', async () => {
 gulp.task('build', gulp.parallel('build-code-minified', 'build-declarations'));
 
 gulp.task('code-check', () => {
-    return cp.spawn('tsc', ['--noEmit'], {
+    return cp.spawn('npx', ['tsc', '--noEmit'], {
         shell: true,
         stdio: 'inherit',
         cwd: __dirname,
@@ -92,7 +92,7 @@ gulp.task('code-check', () => {
 });
 
 gulp.task('unit-tests', () => {
-    return cp.spawn(`jest`, [], {
+    return cp.spawn('npx', ['jest'], {
         shell: true,
         stdio: 'inherit',
         cwd: __dirname,
