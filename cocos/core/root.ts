791a5d1ec3bb7a15e5e85446563696dd621d82cf
minggo <nudtzxm@gmail.com>
2020-08-25 16:16:58 +0800

    simplify codes

--- cocos/core/root.ts
+++ cocos/core/root.ts
@@ -5,7 +5,7 @@
 import { builtinResMgr } from './3d/builtin';
 import { GFXDevice } from './gfx/device';
 import { Pool } from './memop';
-import { RenderPipeline, RenderView } from './pipeline';
+import { RenderPipeline, RenderView, ForwardPipeline } from './pipeline';
 import { IRenderViewInfo } from './pipeline/define'
 import { Camera, Light, Model } from './renderer';
 import { DataPoolManager } from './renderer/data-pool-manager';
@@ -16,7 +16,6 @@ import { SpotLight } from './renderer/scene/spot-light';
 import { UI } from './renderer/ui/ui';
 import { legacyCC } from './global-exports';
 import { RenderWindow, IRenderWindowInfo } from './renderer/core/render-window';
-import { ForwardPipeline } from './pipeline';
 import { GFXColorAttachment, GFXDepthStencilAttachment, GFXStoreOp } from './gfx';
 import { RootHandle, RootPool, RootView, NULL_HANDLE } from './renderer/core/memory-pools';
 
