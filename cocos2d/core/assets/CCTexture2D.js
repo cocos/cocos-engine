5251e4e6deb5501b249fe99c12d13e090d56048a
Santy-Wang <wh19930829@qq.com>
2020-08-27 15:55:21 +0800

    construct a empty texture when texture is deferred loading

--- cocos2d/core/assets/CCTexture2D.js
+++ cocos2d/core/assets/CCTexture2D.js
@@ -480,6 +480,7 @@ var Texture2D = cc.Class({
      * @method getImpl
      */
     getImpl () {
+        if (!this._texture) this._texture = new renderer.Texture2D(renderer.device, {});
         return this._texture;
     },
 
