7528fb806c1a86fbd30e580d102532cdb6785dc2
finscn <finscn@gmail.com>
2020-08-30 23:47:35 +0800

    完善 rectForGID 方法

--- cocos2d/tilemap/CCTMXXMLParser.js
+++ cocos2d/tilemap/CCTMXXMLParser.js
@@ -237,10 +237,10 @@ cc.TMXTilesetInfo.prototype = {
         rect.width = this._tileSize.width;
         rect.height = this._tileSize.height;
         gid &= cc.TiledMap.TileFlag.FLIPPED_MASK;
-        gid = gid - parseInt(this.firstGid, 10);
-        let max_x = Math.round(( this.imageSize.width - this.margin * 2 + this.spacing) / (this._tileSize.width + this.spacing));
-        rect.x = Math.round((gid % max_x) * (this._tileSize.width + this.spacing) + this.margin, 10);
-        rect.y = Math.round(Math.floor(gid / max_x) * (this._tileSize.height + this.spacing) + this.margin, 10);
+        gid = gid - this.firstGid;
+        let max_x = Math.round((this.imageSize.width - this.margin * 2 + this.spacing) / (this._tileSize.width + this.spacing));
+        rect.x = Math.round((gid % max_x) * (this._tileSize.width + this.spacing) + this.margin);
+        rect.y = Math.round(Math.floor(gid / max_x) * (this._tileSize.height + this.spacing) + this.margin);
         return rect;
     }
 };
