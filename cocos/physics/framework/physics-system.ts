66653b193e69fae459397439164c14185c911cfb
JayceLai <goodlaijiaxin@qq.com>
2020-09-01 22:13:39 +0800

    sync only once if accumulator not enough

--- cocos/physics/framework/physics-system.ts
+++ cocos/physics/framework/physics-system.ts
@@ -345,10 +345,11 @@ export class PhysicsSystem extends System {
                     // TODO: nesting the dirty flag reset between the syncScenetoPhysics and the simulation to reduce calling syncScenetoPhysics.
                     this.physicsWorld.syncSceneToPhysics();
                     this._accumulator -= this._fixedTimeStep;
+                    this._subStepCount++;
                 } else {
                     this.physicsWorld.syncSceneToPhysics();
+                    break;
                 }
-                this._subStepCount++;
             }
             director.emit(Director.EVENT_AFTER_PHYSICS);
         }
