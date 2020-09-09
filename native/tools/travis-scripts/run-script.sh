4df4563be30c61e7d5ab5c6b41e96e4f7c013539
吴智明 <wuzhiming@users.noreply.github.com>
2020-09-09 14:01:48 +0800

    sync template (#2754)
    * update version
    
    * add android instant and sync project structure to 2d version
    
    * revert default project name
    
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>
    
    * add instant template travis script support
    
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>

--- native/tools/travis-scripts/run-script.sh
+++ native/tools/travis-scripts/run-script.sh
@@ -49,9 +49,11 @@ function build_android()
     echo "Compiling Android ... "
     cd $COCOS2DX_ROOT/templates/template-link/platforms/android
     sed -i "s@\${COCOS_X_ROOT}@$COCOS2DX_ROOT@g" app/build.gradle
+    sed -i "s@\${COCOS_X_ROOT}@$COCOS2DX_ROOT@g" game/build.gradle
     sed -i "s@\${COCOS_X_ROOT}@$COCOS2DX_ROOT@g" ../../common/CMakeLists.txt
     sed -i "s@\${COCOS_X_ROOT}@$COCOS2DX_ROOT@g" settings.gradle
     sed -i "s@\${COCOS_PROJ_COMMON}@$COCOS2DX_ROOT/templates/template-link/common@g" app/build.gradle
+    sed -i "s@\${COCOS_PROJ_COMMON}@$COCOS2DX_ROOT/templates/template-link/common@g" game/build.gradle
     sed -i "s/^RELEASE_/#RELEASE_/g" gradle.properties
 
     #echo "Compile Android - ndk-build ..."
