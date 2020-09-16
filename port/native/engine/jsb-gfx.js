f169a15591fad10f172863d940e215b50962de0b
minggo <nudtzxm@gmail.com>
2020-09-16 13:40:20 +0800

    revert Buffer adapter modification

--- port/native/engine/jsb-gfx.js
+++ port/native/engine/jsb-gfx.js
@@ -482,6 +482,38 @@ cc.js.get(shaderProto, 'id', function () {
 });
 
 let bufferProto = gfx.Buffer.prototype;
+
+let oldUpdate = bufferProto.update;
+bufferProto.update = function(buffer, offset, size) {
+    let buffSize;
+    if (size !== undefined ) {
+        buffSize = size;
+    } else if (this.usage & 0x40) { // BufferUsageBit.INDIRECT
+        // It is a IGFXIndirectBuffer object.
+        let drawInfos = buffer.drawInfos;
+        buffer = new Uint32Array(drawInfos.length * 7);
+        let baseIndex = 0;
+        let drawInfo;
+        for (let i = 0; i < drawInfos.length; ++i) {
+            baseIndex = i * 7;
+            drawInfo = drawInfos[i];
+            buffer[baseIndex] = drawInfo.vertexCount;
+            buffer[baseIndex + 1] = drawInfo.firstVertex;
+            buffer[baseIndex + 2] = drawInfo.indexCount;
+            buffer[baseIndex + 3] = drawInfo.firstIndex;
+            buffer[baseIndex + 4] = drawInfo.vertexOffset;
+            buffer[baseIndex + 5] = drawInfo.instanceCount;
+            buffer[baseIndex + 6] = drawInfo.firstInstance;
+        }
+
+        buffSize = buffer.byteLength;
+    } else {
+        buffSize = buffer.byteLength;
+    }
+
+    oldUpdate.call(this, buffer, offset || 0, buffSize);
+}
+
 let oldBufferInitializeFunc = bufferProto.initialize;
 bufferProto.initialize = function(info) {
     if (info.buffer) {
