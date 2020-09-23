72364416ab76b272609c7dae0c845536ae1e939e
minggo <nudtzxm@gmail.com>
2020-09-23 15:18:15 +0800

    fix crash on android (#2826)

--- native/cocos/renderer/core/gfx/GFXDevice.cpp
+++ native/cocos/renderer/core/gfx/GFXDevice.cpp
@@ -16,7 +16,16 @@ Device::Device() {
 }
 
 Device::~Device() {
-    Device::_instance = nullptr;
+    // On Android, engine will check vulkan, gles3, gles2 compatibility in sequence.
+    // Device::_instance will be the supported device, it may not be equal to this.
+    // For example, if a vulkan is not supported and gles3 is supported by a Android
+    // device. Then the function invoking sequence is:
+    // - new VKDevice() -> Device::_instance == VKDevice
+    // - GLES3Device() -> Device::_instance == GLES3Device
+    // delete VKDevice -> Device::_instance != this
+    if (this == Device::_instance) {
+        Device::_instance = nullptr;
+    }
 }
 
 Format Device::getColorFormat() const {
