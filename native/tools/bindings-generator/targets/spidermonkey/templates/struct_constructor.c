a12ef80cba85d77853306cf653c6d423042341ff
江战 <40414978+PatriceJiang@users.noreply.github.com>
2020-10-10 15:12:43 +0800

    3dgfx struct constructor allow varargs (#2879)
    * Revert "update constructor (#2873)"
    
    This reverts commit cfedd696b158b15c9a02771f029295fb970e7970.
    
    * allow variable argument list

--- native/tools/bindings-generator/targets/spidermonkey/templates/struct_constructor.c
+++ native/tools/bindings-generator/targets/spidermonkey/templates/struct_constructor.c
@@ -15,6 +15,61 @@ static bool ${struct_constructor_name}(se::State& s)
         se::NonRefNativePtrCreatedByCtorMap::emplace(cobj);
         return true;
     }
+    #if len($public_fields) > 1
+    else if(argc == 1 && args[0].isObject())
+    {
+        se::Object *json = args[0].toObject();
+        se::Value field;
+
+        ${namespaced_class_name}* cobj = JSB_ALLOC(${namespaced_class_name});
+        #set arg_idx = 0
+        #set conv_text_array = []
+        #for field in $public_fields
+        #set field_type = field.ntype.to_string($generator)
+        #set conv_text = $field.ntype.to_native({"generator": $generator, \
+                             "arg" : $field.ntype, \
+                             "arg_type": $field_type, \
+                             "in_value": "field", \
+                             "out_value": "arg" + str(arg_idx), \
+                             "class_name": $class_name, \
+                             "level": 3, \
+                             "is_static": False, \
+                             "is_pointer": $field.ntype.is_pointer, \
+                             "is_persistent": $is_persistent, \
+                             "ntype": str($field_type)})
+        #set conv_text_array += [$conv_text]
+            #if "seval_to_reference" in $conv_text
+        ${field_type}* arg${arg_idx} = nullptr;
+            #elif $field.ntype.is_numeric
+        ${field_type} arg${arg_idx} = 0;
+            #elif $field.ntype.is_pointer
+        ${field_type} arg${arg_idx} = nullptr;
+            #else
+        ${field_type} arg${arg_idx};
+            #end if
+        json->getProperty("${field.name}", &field);
+        if(!field.isUndefined()) {
+            $conv_text;
+            #if "seval_to_reference" in $conv_text_array[$arg_idx]
+            cobj->${field.name} = *arg${arg_idx};
+            #else
+            cobj->${field.name} = arg${arg_idx};
+            #end if
+        }
+        #set $arg_idx = $arg_idx + 1
+        #end for 
+
+        if(!ok) {
+            JSB_FREE(cobj);
+            SE_REPORT_ERROR("argument convertion error");
+            return false;
+        }
+
+        s.thisObject()->setPrivateData(cobj);
+        se::NonRefNativePtrCreatedByCtorMap::emplace(cobj);
+        return true;
+    }
+    #end if
     else
     {
         ${namespaced_class_name}* cobj = JSB_ALLOC(${namespaced_class_name});
@@ -34,17 +89,16 @@ static bool ${struct_constructor_name}(se::State& s)
                              "is_persistent": $is_persistent, \
                              "ntype": str($field_type)})
         #set conv_text_array += [$conv_text]
-
-        if (!args[${arg_idx}].isUndefined()) {
             #if "seval_to_reference" in $conv_text_array[$arg_idx]
-            ${field_type}* arg${arg_idx} = nullptr;
+        ${field_type}* arg${arg_idx} = nullptr;
             #elif $field.ntype.is_numeric
-            ${field_type} arg${arg_idx} = 0;
+        ${field_type} arg${arg_idx} = 0;
             #elif $field.ntype.is_pointer
-            ${field_type} arg${arg_idx} = nullptr;
+        ${field_type} arg${arg_idx} = nullptr;
             #else
-            ${field_type} arg${arg_idx};
+        ${field_type} arg${arg_idx};
             #end if
+        if (!args[${arg_idx}].isUndefined()) {
             $conv_text;
             #if "seval_to_reference" in $conv_text_array[$arg_idx]
             cobj->${field.name} = *arg${arg_idx};
