8819954eda775bb2a49114c100f8eb66b00949ee
minggo <nudtzxm@gmail.com>
2020-10-20 11:27:18 +0800

    remove unneeded files (#2926)

--- native/auto-build-src.bat
+++
@@ -1,3 +0,0 @@
-git pull fireball v1.4
-git submodule update --init
-gulp make-cocos2d-x
--- native/auto-build.bat
+++
@@ -1,5 +0,0 @@
-git checkout -- .
-git pull fireball v1.4
-git submodule update --init
-gulp make-simulator
-gulp make-prebuilt
