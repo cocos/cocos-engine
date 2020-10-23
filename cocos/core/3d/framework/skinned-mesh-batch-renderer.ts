9bfbfc81eceb649b0c388cefa9dd1873d745d017
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2020-10-23 17:15:11 +0800

    don't default to trilinear filter

--- cocos/core/3d/framework/skinned-mesh-batch-renderer.ts
+++ cocos/core/3d/framework/skinned-mesh-batch-renderer.ts
@@ -477,7 +477,7 @@ export class SkinnedMeshBatchRenderer extends SkinnedMeshRenderer {
     protected createTexture (prop: string) {
         const tex = new Texture2D();
         tex.setFilters(Filter.LINEAR, Filter.LINEAR);
-        tex.setMipFilter(Filter.LINEAR);
+        tex.setMipFilter(Filter.NEAREST);
         tex.reset({
             width: this.atlasSize,
             height: this.atlasSize,
