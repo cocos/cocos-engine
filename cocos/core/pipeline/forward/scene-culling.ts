347831267c04c857d664d416e6d05116b60983d1
coulsonwang <coulsonwang@126.com>
2020-10-30 18:39:33 +0800

    remove useless handler

--- cocos/core/pipeline/forward/scene-culling.ts
+++ cocos/core/pipeline/forward/scene-culling.ts
@@ -202,11 +202,9 @@ export function sceneCulling (pipeline: ForwardPipeline, view: RenderView) {
 
     if (_castWorldBounds) {
         aabb.toBoundingSphere(shadows.sphere, _castWorldBounds);
-        ShadowsPool.set(shadows.handle, ShadowsView.SPHERE, shadows.sphere.handle);
     }
 
     if (_receiveWorldBounds) {
         aabb.toBoundingSphere(shadows.receiveSphere, _receiveWorldBounds);
-        ShadowsPool.set(shadows.handle, ShadowsView.RECEIVE_SPHERE, shadows.receiveSphere.handle);
     }
 }
