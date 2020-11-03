1827080ec5277d16c007c1783b258ff0b673de59
yeshao2069 <yezi636shao@163.com>
2020-11-03 21:08:46 +0800

    fix ui transform setContentSize

--- cocos/core/components/ui-base/ui-transform.ts
+++ cocos/core/components/ui-base/ui-transform.ts
@@ -271,21 +271,45 @@ export class UITransform extends Component {
      * @zh
      * 设置节点原始大小，不受该节点是否被缩放或者旋转的影响。
      *
-     * @param size - 节点内容变换的尺寸或者宽度。
-     * @param height - 节点内容未变换的高度。
+     * @param size - 节点内容变换的尺寸
      * @example
      * ```ts
      * import { Size } from 'cc';
      * node.setContentSize(new Size(100, 100));
+     * ```
+     */
+    public setContentSize (size: Size): void;
+
+    /**
+     * @en
+     * Sets the untransformed size of the node.<br/>
+     * The contentSize remains the same no matter if the node is scaled or rotated.<br/>
+     * All nodes have a size. Layer and Scene have the same size of the screen.
+     *
+     * @zh
+     * 设置节点原始大小，不受该节点是否被缩放或者旋转的影响。
+     *
+     * @param width - 节点内容变换的宽度。
+     * @param height - 节点内容变换的高度。
+     * @example
+     * ```ts
+     * import { Size } from 'cc';
      * node.setContentSize(100, 100);
      * ```
      */
-    public setContentSize (size: Size | number, height?: number) {
+    public setContentSize (width: number, height: number): void;
+
+    public setContentSize (val: Size | number, height?: number) {
         const locContentSize = this._contentSize;
         let clone: Size;
         if (height === undefined) {
-            size = size as Size;
-            if ((size.width === locContentSize.width) && (size.height === locContentSize.height)) {
+            if(typeof val === 'number' || val instanceof Number){
+                val = new Size(val as number, locContentSize.height);
+            } else {
+                val = new Size(val as Size);
+            }
+            
+            if ((val.width === locContentSize.width) && (val.height === locContentSize.height)) {
                 return;
             }
 
@@ -293,10 +317,9 @@ export class UITransform extends Component {
                 clone = new Size(this._contentSize);
             }
 
-            locContentSize.width = size.width;
-            locContentSize.height = size.height;
         } else {
-            if ((size === locContentSize.width) && (height === locContentSize.height)) {
+            val = new Size(val as number, height);
+            if ((val.width === locContentSize.width) && (val.height === locContentSize.height)) {
                 return;
             }
 
@@ -304,9 +327,9 @@ export class UITransform extends Component {
                 clone = new Size(this._contentSize);
             }
 
-            locContentSize.width = size as number;
-            locContentSize.height = height;
         }
+        locContentSize.width = val.width;
+        locContentSize.height = val.height;
 
         if (EDITOR) {
             // @ts-ignore
