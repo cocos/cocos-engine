ab96e5f81a66ac561f228403f9cf0d72b8b37c5a
pandamicro <lphuabin@gmail.com>
2020-11-10 17:09:29 +0800

    update license for core modules
    math, memop, pipeline, platform, primitive, renderer, scene-graph, utils, value-types

--- cocos/core/math/mat3.ts
+++ cocos/core/math/mat3.ts
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2018-2020 Xiamen Yaji Software Co., Ltd.
 
  http://www.cocos.com
 
--- cocos/core/utils/array.ts
+++ cocos/core/utils/array.ts
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2018-2020 Xiamen Yaji Software Co., Ltd.
 
  https://www.cocos.com/
 
