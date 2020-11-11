11f4789218a81ab75526121529317e232916f360
minggo <nudtzxm@gmail.com>
2020-11-11 15:21:56 +0800

    use JS ShaderStage/Uniform/UniformBlock/UniformSampler/ShaderInfo

--- cocos/core/gfx/index.jsb.ts
+++ cocos/core/gfx/index.jsb.ts
@@ -41,11 +41,7 @@ export const Device = gfx.Device;
 export { DeviceInfo, BindingMappingInfo } from './device';
 
 export const Shader = gfx.Shader;
-export const Uniform = gfx.Uniform;
-export const UniformBlock = gfx.UniformBlock;
-export const UniformSampler = gfx.UniformSampler;
-export const ShaderInfo = gfx.ShaderInfo;
-export { ShaderStage } from './shader';
+export { ShaderStage, UniformSampler, UniformBlock, Uniform, ShaderInfo } from './shader';
 
 export const Attribute = gfx.Attribute;
 export const InputAssemblerInfo = gfx.InputAssemblerInfo;
