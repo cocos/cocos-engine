43f7e9a1efe94d8962adfbc664f4295de08bd5b0
吴智明 <wuzhiming@users.noreply.github.com>
2020-11-12 12:53:20 +0800

    comment sdkwrapper (#3016)

--- native/templates/template-link/platforms/ios/AppDelegate.mm
+++ native/templates/template-link/platforms/ios/AppDelegate.mm
@@ -2,19 +2,19 @@
  Copyright (c) 2010-2013 cocos2d-x.org
  Copyright (c) 2013-2016 Chukong Technologies Inc.
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,7 +40,7 @@ Game* game = nullptr;
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [[SDKWrapper getInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+//     [[SDKWrapper getInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     // Add the view controller's view to the window and display.
     CGRect bounds = [[UIScreen mainScreen] bounds];
     self.window = [[UIWindow alloc] initWithFrame: bounds];
@@ -50,13 +50,13 @@ Game* game = nullptr;
     _viewController.view = [[View alloc] initWithFrame:bounds];
     _viewController.view.contentScaleFactor = UIScreen.mainScreen.scale;
     [self.window setRootViewController:_viewController];
-    
+
     // cocos2d application instance
     game = new Game(bounds.size.width, bounds.size.height);
     game->init();
-    
+
     [self.window makeKeyAndVisible];
-    
+
     return YES;
 }
 
@@ -65,7 +65,7 @@ Game* game = nullptr;
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-    [[SDKWrapper getInstance] applicationWillResignActive:application];
+//     [[SDKWrapper getInstance] applicationWillResignActive:application];
     game->onPause();
 }
 
@@ -73,7 +73,7 @@ Game* game = nullptr;
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-    [[SDKWrapper getInstance] applicationDidBecomeActive:application];
+//     [[SDKWrapper getInstance] applicationDidBecomeActive:application];
     game->onResume();
 }
 
@@ -82,19 +82,19 @@ Game* game = nullptr;
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
      */
-    [[SDKWrapper getInstance] applicationDidEnterBackground:application];
+//     [[SDKWrapper getInstance] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
-    [[SDKWrapper getInstance] applicationWillEnterForeground:application];
+//     [[SDKWrapper getInstance] applicationWillEnterForeground:application];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    [[SDKWrapper getInstance] applicationWillTerminate:application];
+//     [[SDKWrapper getInstance] applicationWillTerminate:application];
     delete game;
     game = nullptr;
 }
