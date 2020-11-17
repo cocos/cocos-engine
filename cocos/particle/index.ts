9a47137a952901065626091ef34d23a4a3026a9b
yeshao2069 <yezi636shao@163.com>
2020-11-17 14:16:35 +0800

    export GradientRange from cc.

--- cocos/particle/index.ts
+++ cocos/particle/index.ts
@@ -9,13 +9,15 @@ import { ParticleSystem } from './particle-system';
 import { ParticleUtils } from './particle-utils';
 import CurveRange from './animator/curve-range';
 import { legacyCC } from '../core/global-exports';
+import GradientRange from "./animator/gradient-range"
 
 export {
     Billboard,
     Line,
     ParticleSystem,
     ParticleUtils,
-    CurveRange
+    CurveRange,
+    GradientRange,
 };
 
 export * from './deprecated';
