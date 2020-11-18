c3c83dbfde80c1b90f204ac4095bf629728d3834
coulsonwang <coulsonwang@126.com>
2020-11-18 10:54:38 +0800

    skip create texture when width or height is zero (#7781)

--- cocos/core/assets/simple-texture.ts
+++ cocos/core/assets/simple-texture.ts
@@ -228,6 +228,7 @@ export class SimpleTexture extends TextureBase {
     }
 
     protected _createTexture (device: Device) {
+        if (this._width === 0 || this._height === 0) { return; }
         let flags = TextureFlagBit.NONE;
         if (this._mipFilter !== Filter.NONE && canGenerateMipmap(device, this._width, this._height)) {
             this._mipmapLevel = getMipLevel(this._width, this._height);
