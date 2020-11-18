48d35644843f122e570adea9218f8f805d53eb77
SB <leslieleigh@outlook.com>
2020-11-18 21:28:37 +0800

    Introduce the `EXPORT_TO_GLOBAL`

--- cocos/core/default-constants.ts
+++ cocos/core/default-constants.ts
@@ -37,6 +37,8 @@ function tryDefineGlobal (name: string, value: boolean): boolean {
     }
 }
 
+// No export to global required since we have already done here.
+export const EXPORT_TO_GLOBAL = false;
 export const BUILD = tryDefineGlobal('CC_BUILD', false);
 export const TEST = tryDefineGlobal('CC_TEST', defined('tap') || defined('QUnit'));
 export const EDITOR = tryDefineGlobal('CC_EDITOR', defined('Editor') && defined('process') && ('electron' in process.versions));
