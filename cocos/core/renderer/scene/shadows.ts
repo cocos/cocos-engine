330f67881bbfdd6130533d7302b087490a9938e2
troublemaker52025 <troublemaker52025@outlook.com>
2020-11-19 21:14:12 +0800

    fix planar shadow shader error

--- cocos/core/renderer/scene/shadows.ts
+++ cocos/core/renderer/scene/shadows.ts
@@ -30,11 +30,6 @@ import { legacyCC } from '../../global-exports';
 import { Enum } from '../../value-types';
 import { ShadowsPool, NULL_HANDLE, ShadowsView, ShadowsHandle } from '../core/memory-pools';
 import { ShadowsInfo } from '../../scene-graph/scene-globals';
-import { IMacroPatch } from '../core/pass';
-
-const shadowPatches: IMacroPatch[] = [
-    { name: 'CC_RECEIVE_SHADOW', value: true },
-];
 
 /**
  * @zh 阴影类型。
@@ -352,7 +347,7 @@ export class Shadows {
             this._material = new Material();
             this._material.initialize({ effectName: 'planar-shadow' });
             ShadowsPool.set(this._handle, ShadowsView.PLANAR_PASS, this._material.passes[0].handle);
-            ShadowsPool.set(this._handle, ShadowsView.PLANAR_SHADER,this._material.passes[0].getShaderVariant(shadowPatches));
+            ShadowsPool.set(this._handle, ShadowsView.PLANAR_SHADER,this._material.passes[0].getShaderVariant(null));
         }
         if (!this._instancingMaterial) {
             this._instancingMaterial = new Material();
