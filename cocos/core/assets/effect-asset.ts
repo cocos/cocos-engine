0768cfda73d0be1158f6b9e10abf24805dd9528c
Santy-Wang <wh19930829@qq.com>
2020-11-20 17:37:53 +0800

    refine

--- cocos/core/assets/effect-asset.ts
+++ cocos/core/assets/effect-asset.ts
@@ -28,7 +28,7 @@
  * @module material
  */
 
-import { ccclass, serializable, editable } from 'cc.decorator';
+import { ccclass, serializable, editable, editorOnly } from 'cc.decorator';
 import { EDITOR } from 'internal:constants';
 import { Root } from '../root';
 import { BlendState, DepthStencilState, RasterizerState, DescriptorType,
@@ -192,6 +192,8 @@ export class EffectAsset extends Asset {
     @editable
     public combinations: IPreCompileInfo[] = [];
 
+    @serializable
+    @editorOnly
     public hideInEditor = false;
 
     /**
