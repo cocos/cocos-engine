807a0d446a86bce980b9346a4d5d0f0e31438d49
2youyou2 <501251991@qq.com>
2020-12-04 10:15:27 +0800

    refine code

--- cocos/physics-2d/framework/physics-system.ts
+++ cocos/physics-2d/framework/physics-system.ts
@@ -8,7 +8,7 @@ import { IPhysicsConfig, ICollisionMatrix } from "../../physics/framework/physic
 import { CollisionMatrix } from "../../physics/framework/collision-matrix";
 import { ERaycast2DType, RaycastResult2D } from './physics-types';
 import { Collider2D } from "./components/colliders/collider-2d";
-import { PHYSICS_2D_PTM_RATIO } from 'exports/physics-2d-framework';
+import { PHYSICS_2D_PTM_RATIO } from './physics-types';
 
 let instance: PhysicsSystem2D | null = null;
 
