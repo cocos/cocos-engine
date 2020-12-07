6dd0a414b2d82a7b180cb7129a05865e7a7de66d
Santy-Wang <wh19930829@qq.com>
2020-12-07 13:32:11 +0800

    Revert "Optimize build time constants implementation (#7882)"
    This reverts commit 6f57c3ad8fbdc257fa222bf0a417feace6b6652a.

--- cocos/core/global-exports.ts
+++ cocos/core/global-exports.ts
@@ -48,13 +48,17 @@ import {
     HUAWEI,
     OPPO,
     VIVO,
+    EXPORT_TO_GLOBAL,
 } from 'internal:constants';
 
 const _global = typeof window === 'undefined' ? global : window;
 
 /**
- * The internal variable to strike some circular reference problem.
- * This also records the content of legacy global variable `cc`(prior to 3.0).
+ * @en
+ * The main namespace of Cocos2d-JS, all engine core classes, functions, properties and constants are defined in this namespace.
+ * @zh
+ * Cocos 引擎的主要命名空间，引擎代码中所有的类，函数，属性和常量都在这个命名空间中定义。
+ * @deprecated
  */
 export const legacyCC: Record<string, any> & {
     _global: typeof globalThis;
@@ -65,27 +69,105 @@ export const legacyCC: Record<string, any> & {
 // For internal usage
 legacyCC.internal = {};
 
-// These constants are also exported into global namespace for compatible with Creator prior to 3.0.
-// Use `'cce.env'` from external, `'internal-constants'` from within internal instead.
-_global.CC_BUILD = BUILD;
-_global.CC_TEST = TEST;
-_global.CC_EDITOR = EDITOR;
-_global.CC_PREVIEW = PREVIEW;
-_global.CC_DEV = DEV;
-_global.CC_DEBUG = DEBUG;
-_global.CC_JSB = JSB;
-_global.CC_BYTEDANCE = BYTEDANCE;
-_global.CC_WECHAT = WECHAT;
-_global.CC_ALIPAY = ALIPAY;
-_global.CC_XIAOMI = XIAOMI;
-_global.CC_BAIDU = BAIDU;
-_global.CC_COCOSPLAY = COCOSPLAY;
-_global.CC_HUAWEI = HUAWEI;
-_global.CC_OPPO = OPPO;
-_global.CC_VIVO = VIVO;
-_global.CC_MINIGAME = MINIGAME;
-_global.CC_RUNTIME_BASED = RUNTIME_BASED;
-_global.CC_SUPPORT_JIT = SUPPORT_JIT;
+if (EXPORT_TO_GLOBAL) {
+    // Supports dynamically access from external scripts such as adapters and debugger.
+    // So macros should still defined in global even if inlined in engine.
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_BUILD (Available for built package)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_BUILD（在构建后生效）
+     */
+    _global.CC_BUILD = BUILD;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_TEST (Available for ci test environment)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_TEST（在 CI 测试环境下生效）
+     */
+    _global.CC_TEST = TEST;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_EDITOR (Available for editor environment)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_EDITOR（在编辑器环境下生效）
+     */
+    _global.CC_EDITOR = EDITOR;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_PREVIEW (Available for preview)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_PREVIEW（预览时生效）
+     */
+    _global.CC_PREVIEW = PREVIEW;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_DEV (Internal)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_DEV（内部使用）
+     */
+    _global.CC_DEV = DEV;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_DEBUG (Available for debug environment)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_DEBUG（在调试模式下生效）
+     */
+    _global.CC_DEBUG = DEBUG;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_JSB (Available for native application environment)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_JSB（在原生应用环境下生效）
+     */
+    _global.CC_JSB = JSB;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_BYTEDANCE (Available for Bytedance platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_BYTEDANCE（在字节平台上生效）
+     */
+    _global.CC_BYTEDANCE = BYTEDANCE;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_WECHAT (Available for Wechat mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_WECHAT（在微信小游戏平台上生效）
+     */
+    _global.CC_WECHAT = WECHAT;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_ALIPAY (Available for Alipay mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_ALIPAY（在支付宝小游戏平台上生效）
+     */
+    _global.CC_ALIPAY = ALIPAY;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_XIAOMI (Available for MI mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_XIAOMI（在小米小游戏平台上生效）
+     */
+    _global.CC_XIAOMI = XIAOMI;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_BAIDU (Available for Baidu mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_BAIDU（在百度小游戏平台上生效）
+     */
+    _global.CC_BAIDU = BAIDU;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_COCOSPLAY (Available for Cocos Play platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_COCOSPLAY（在 CocosPlay 小游戏平台上生效）
+     */
+    _global.CC_COCOSPLAY = COCOSPLAY;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_HUAWEI (Available for Huawei mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_HUAWEI（在华为快游戏平台上生效）
+     */
+    _global.CC_HUAWEI = HUAWEI;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_OPPO (Available for OPPO mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_OPPO（在 OPPO 小游戏平台上生效）
+     */
+    _global.CC_OPPO = OPPO;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_VIVO (Available for Vivo mini game platform)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_VIVO（在 Vivo 小游戏平台上生效）
+     */
+    _global.CC_VIVO = VIVO;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_MINIGAME (Available for general mini game platforms)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_MINIGAME（在各个小游戏平台上生效）
+     */
+    _global.CC_MINIGAME = MINIGAME;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_RUNTIME_BASED (Available for Huawei, OPPO, Vivo and Cocos Play)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_RUNTIME_BASED（在华为、OPPO、Vivo 和 CocosPlay 平台上生效）
+     */
+    _global.CC_RUNTIME_BASED = RUNTIME_BASED;
+    /**
+     * @en The pre-compilation constant for code tree shaking: CC_SUPPORT_JIT (Available for platforms support JIT)
+     * @zh 预编译宏变量，通常用来做平台或环境相关自动代码剔除：CC_SUPPORT_JIT（在支持 JIT 的平台上生效）
+     */
+    _global.CC_SUPPORT_JIT = SUPPORT_JIT;
+}
 
 if (DEV) {
     legacyCC._Test = {};
