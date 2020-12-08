acbe1530860e0a35f5caa07071c4be618a36652f
PP_Pro <chinese1236@qq.com>
2020-12-08 13:40:58 +0800

    add link-sure adaption

---
+++ adapters/runtime/platforms/link-sure/jsb/index.js
@@ -0,0 +1,59 @@
+import "./accelerometer"
+import "./input-touch"
+import "./opengl"
+import "./window-resize"
+
+window.jsb = window.jsb || {};
+
+let _systemInfo = wuji.getSystemInfoSync();
+
+if (!jsb.setPreferredFramesPerSecond) {
+    if (wuji.setPreferredFramesPerSecond) {
+        jsb.setPreferredFramesPerSecond = wuji.setPreferredFramesPerSecond;
+    } else {
+        jsb.setPreferredFramesPerSecond = function () {
+            console.error("The jsb.setPreferredFramesPerSecond is not define!");
+        }
+    }
+}
+
+jsb.AudioEngine = wuji.AudioEngine;
+jsb.createCanvas = wuji.createCanvas;
+jsb.createImage = wuji.createImage;
+jsb.loadImageData = wuji.loadImageData;
+jsb.loadSubpackage = wuji.loadSubpackage;
+jsb.showKeyboard = wuji.showKeyboard;
+jsb.hideKeyboard = wuji.hideKeyboard;
+jsb.platform = _systemInfo.platform;
+jsb.language = _systemInfo.language;
+if (!jsb.loadFont) {
+    if (wuji.loadFont) {
+        jsb.loadFont = wuji.loadFont;
+    } else {
+        jsb.loadFont = function () {
+            console.warn("jsb.loadFont() is not support!");
+        };
+    }
+}
+jsb.model = _systemInfo.model;
+jsb.system = _systemInfo.system;
+jsb.height = _systemInfo.windowHeight;
+jsb.width = _systemInfo.windowWidth;
+jsb.pixelRatio = _systemInfo.pixelRatio;
+jsb.onKeyboardConfirm = wuji.onKeyboardConfirm;
+jsb.onKeyboardComplete = wuji.onKeyboardComplete;
+jsb.onKeyboardInput = wuji.onKeyboardInput;
+
+jsb.offKeyboardInput = wuji.offKeyboardInput;
+jsb.offKeyboardConfirm = wuji.offKeyboardConfirm;
+jsb.offKeyboardComplete = wuji.offKeyboardComplete;
+
+jsb.env = wuji.env;
+jsb.getFileSystemManager = wuji.getFileSystemManager;
+jsb.downloadFile = wuji.downloadFile;
+jsb.getBatteryInfoSync = wuji.getBatteryInfoSync;
+
+jsb.onShow = wuji.onShow;
+jsb.onHide = wuji.onHide;
+jsb.offShow = wuji.offShow;
+jsb.offHide = wuji.offHide;
---
+++ adapters/runtime/platforms/link-sure/jsb/window-resize.js
@@ -0,0 +1,11 @@
+window.jsb = window.jsb || {};
+
+const _onWindowResize = wuji.onWindowResize;
+jsb.onWindowResize = function (callBack) {
+    _onWindowResize(function (size) {
+        callBack(size.width || size.windowWidth, size.height || size.windowHeight);
+    });
+};
+
+// 屏蔽废弃的函数
+window.resize = function () {};
