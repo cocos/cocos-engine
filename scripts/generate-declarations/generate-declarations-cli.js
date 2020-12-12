7cbefe8e2a98852c7dbf37bb83030534650a6e19
SB <leslieleigh@outlook.com>
2020-12-12 15:09:23 +0800

    Optimize .d.ts generation; prepare for sub modules

--- scripts/generate-declarations/generate-declarations-cli.js
+++ scripts/generate-declarations/generate-declarations-cli.js
@@ -4,8 +4,20 @@
 const { generate } = require('./generate-declarations'); 
 const fs = require('fs-extra'); 
  
-const outDir = './bin/.declarations'; 
-(async () => { 
+const outDir = './bin/.declarations';
+(async () => {
+    const options = {
+        outDir,
+    };
+    if (!process.argv.some((arg) => arg.startsWith('--with'))) {
+        options.withIndex = true;
+        options.withExports = false;
+        options.withEditorExports = false;
+    } else {
+        options.withIndex = !!process.argv.includes('--with-index');
+        options.withExports = !!process.argv.includes('--with-exports');
+        options.withEditorExports = !!process.argv.includes('--with-editor-exports');
+    }
     await fs.emptyDir(outDir); 
-    await generate({ outDir }); 
+    await generate(options); 
 })();
