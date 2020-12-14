ae32603393275877faa9dce87523bfcffbe09a39
minggo <nudtzxm@gmail.com>
2020-12-14 15:15:43 +0800

    remove anysdk support (#3113)

--- native/templates/template-link/platforms/android/instantapp/src/com/cocos/game/InstantActivity.java
+++ native/templates/template-link/platforms/android/instantapp/src/com/cocos/game/InstantActivity.java
@@ -43,21 +43,6 @@ public class InstantActivity extends CocosActivity {
             // Don't need to finish it again since it's finished in super.onCreate .
             return;
         }
-        // DO OTHER INITIALIZATION BELOW
-        SDKWrapper.getInstance().init(this);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SDKWrapper.getInstance().onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SDKWrapper.getInstance().onPause();
     }
 
     @Override
@@ -67,60 +52,5 @@ public class InstantActivity extends CocosActivity {
         if (!isTaskRoot()) {
             return;
         }
-        SDKWrapper.getInstance().onDestroy();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        SDKWrapper.getInstance().onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        SDKWrapper.getInstance().onNewIntent(intent);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        SDKWrapper.getInstance().onRestart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SDKWrapper.getInstance().onStop();
-    }
-
-    @Override
-    public void onBackPressed() {
-        SDKWrapper.getInstance().onBackPressed();
-        super.onBackPressed();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        SDKWrapper.getInstance().onConfigurationChanged(newConfig);
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        SDKWrapper.getInstance().onRestoreInstanceState(savedInstanceState);
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        SDKWrapper.getInstance().onSaveInstanceState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onStart() {
-        SDKWrapper.getInstance().onStart();
-        super.onStart();
     }
 }
--- native/templates/template-link/platforms/ios/AppDelegate.mm
+++ native/templates/template-link/platforms/ios/AppDelegate.mm
@@ -28,7 +28,6 @@
 #include "platform/ios/View.h"
 #include "cocos/bindings/event/EventDispatcher.h"
 
-#include "SDKWrapper.h"
 #include "Game.h"
 
 cc::Device::Orientation _lastOrientation;
@@ -42,7 +41,6 @@ Game* game = nullptr;
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//     [[SDKWrapper getInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     // Add the view controller's view to the window and display.
     CGRect bounds = [[UIScreen mainScreen] bounds];
     self.window = [[UIWindow alloc] initWithFrame: bounds];
@@ -66,15 +64,13 @@ Game* game = nullptr;
 }
 
 
-- (void) statusBarOrientationChanged:(NSNotification *)note
-{
+- (void) statusBarOrientationChanged:(NSNotification *)note {
     // https://developer.apple.com/documentation/uikit/uideviceorientation
     // NOTE: do not use API [UIDevice currentDevice].orientation to get the device orientation
     // when the device rotates to LandscapeLeft, device.orientation returns UIDeviceOrientationLandscapeRight
     // when the device rotates to LandscapeRight, device.orientation returns UIDeviceOrientationLandscapeLeft
     cc::Device::Orientation orientation;
-    switch([[UIApplication sharedApplication] statusBarOrientation])
-    {
+    switch([[UIApplication sharedApplication] statusBarOrientation]) {
         case UIInterfaceOrientationLandscapeRight:
             orientation = cc::Device::Orientation::LANDSCAPE_RIGHT;
             break;
@@ -97,40 +93,20 @@ Game* game = nullptr;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-//     [[SDKWrapper getInstance] applicationWillResignActive:application];
     game->onPause();
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-//     [[SDKWrapper getInstance] applicationDidBecomeActive:application];
     game->onResume();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
-//     [[SDKWrapper getInstance] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    /*
-     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
-     */
-//     [[SDKWrapper getInstance] applicationWillEnterForeground:application];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-//     [[SDKWrapper getInstance] applicationWillTerminate:application];
+- (void)applicationWillTerminate:(UIApplication *)application {
     delete game;
     game = nullptr;
 }
