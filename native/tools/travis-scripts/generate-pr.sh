7c8fd18aec11531fbc746c05e9293dd753f5b7a9
江战 <40414978+PatriceJiang@users.noreply.github.com>
2020-12-18 10:47:28 +0800

    fix base repo (#3127)

--- native/tools/travis-scripts/generate-pr.sh
+++ native/tools/travis-scripts/generate-pr.sh
@@ -23,7 +23,7 @@ COMMITTAG="[ci skip][AUTO]: updating jsbinding automatically"
 ELAPSEDSECS=`date +%s`
 COCOS_BRANCH="update_js_bindings_$ELAPSEDSECS"
 COCOS_ROBOT_REMOTE="https://${GH_USER}:${GH_PASSWORD}@github.com/${GH_USER}/cocos2d-x-lite.git"
-PULL_REQUEST_REPO="https://api.github.com/repos/cocos-robot/cocos2d-x-lite/pulls"
+PULL_REQUEST_REPO="https://api.github.com/repos/cocos-creator/cocos2d-x-lite/pulls"
 FETCH_REMOTE_BRANCH=$1
 JS_COMMIT_PATH="cocos/bindings/auto"
 
