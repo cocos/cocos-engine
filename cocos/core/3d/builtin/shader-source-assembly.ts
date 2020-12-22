8c6d7613d9a79f1231136dbd9c53297981fb8d47
SB <leslieleigh@outlook.com>
2020-12-22 12:46:54 +0800

    HTML5, WeChat branch

--- cocos/core/3d/builtin/shader-source-assembly.ts
+++ cocos/core/3d/builtin/shader-source-assembly.ts
@@ -1,4 +1,4 @@
-import { MINIGAME, RUNTIME_BASED } from 'internal:constants';
+import { HTML5, MINIGAME, RUNTIME_BASED, WECHAT } from 'internal:constants';
 import glsl1 from './shader-sources/glsl1';
 import glsl3 from './shader-sources/glsl3';
 import glsl4 from './shader-sources/glsl4';
@@ -15,7 +15,12 @@ type ShaderSource = unknown[][];
  * The shader sources assembled in this build.
  */
 const assembly: Partial<Record<ShaderVersion, ShaderSource>> = (() => {
-    if (MINIGAME || RUNTIME_BASED) {
+    if (HTML5 || WECHAT) {
+        return {
+            glsl1,
+            glsl3,
+        };
+    } else if (MINIGAME || RUNTIME_BASED) {
         return {
             glsl1,
         };
