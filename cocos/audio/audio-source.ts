964ddcf2ad64f4931eb3d488995c449cc1304212
PP_Pro <chinese1236@qq.com>
2020-12-29 17:30:24 +0800

    play audio on awake at the onEnable phase

--- cocos/audio/audio-source.ts
+++ cocos/audio/audio-source.ts
@@ -132,6 +132,9 @@ export class AudioSource extends Component {
 
     public onLoad () {
         this._syncStates();
+    }
+
+    public onEnable () {
         if (this._playOnAwake) { this.play(); }
     }
 
