52bf6ff194a46a60dda349657d38cf3dd63a4b3b
PP <chinese1236@qq.com>
2021-01-08 10:18:19 +0800

    fix atob undefined (#8090)
    * fix atob undefined
    
    * fix lint
    
    * remove atob in physics-ammon.ts

--- cocos/core/utils/atob.ts
+++ cocos/core/utils/atob.ts
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2020 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2018-2020 Xiamen Yaji Software Co., Ltd.
 
  https://www.cocos.com/
 
@@ -21,23 +21,31 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
- */
+*/
 
-/**
- * @packageDocumentation
- * @hidden
- */
-
-import Ammo from '../cocos/physics/ammo/ammo-instantiated';
-import '../cocos/physics/ammo/instantiate';
-
-window.Ammo = Ammo;
-
-// polyfill config
-(Ammo as any).CC_CONFIG = {
-    ignoreSelfBody: true,
-};
-
-(Ammo as any).CC_CACHE = {
-    btTriangleMesh: { enable: false },
-};
+export function atob (input: string): string {
+    const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    let output = '';
+    let chr1 = 0; let chr2 = 0; let chr3 = 0;
+    let enc1 = 0; let enc2 = 0; let enc3 = 0; let enc4 = 0;
+    let i = 0;
+    // eslint-disable-next-line no-useless-escape
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+    do {
+        enc1 = keyStr.indexOf(input.charAt(i++));
+        enc2 = keyStr.indexOf(input.charAt(i++));
+        enc3 = keyStr.indexOf(input.charAt(i++));
+        enc4 = keyStr.indexOf(input.charAt(i++));
+        chr1 = enc1 << 2 | enc2 >> 4;
+        chr2 = (enc2 & 15) << 4 | enc3 >> 2;
+        chr3 = (enc3 & 3) << 6 | enc4;
+        output += String.fromCharCode(chr1);
+        if (enc3 !== 64) {
+            output += String.fromCharCode(chr2);
+        }
+        if (enc4 !== 64) {
+            output += String.fromCharCode(chr3);
+        }
+    } while (i < input.length);
+    return output;
+}
