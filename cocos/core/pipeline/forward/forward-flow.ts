b0a603c61a8f6e76d0e66cafc9111b5f2b8f963d
troublemaker52025 <troublemaker52025@outlook.com>
2021-01-12 20:31:01 +0800

    update

--- cocos/core/pipeline/forward/forward-flow.ts
+++ cocos/core/pipeline/forward/forward-flow.ts
@@ -71,7 +71,7 @@ export class ForwardFlow extends RenderFlow {
     public render (camera: Camera) {
         const pipeline = this._pipeline as ForwardPipeline;
         sceneCulling(pipeline, camera);
-        (this._pipeline as ForwardPipeline).updateShadowUBO(camera);
+        pipeline.updateShadowUBO(camera);
         super.render(camera);
     }
 
