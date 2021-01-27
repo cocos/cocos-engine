c505a8edf36c4950fd7e6020fb093e2c59759897
PP_Pro <chinese1236@qq.com>
2021-01-27 14:50:11 +0800

    cache camera priority in event listener

--- cocos/core/platform/event-manager/event-listener.ts
+++ cocos/core/platform/event-manager/event-listener.ts
@@ -56,6 +56,11 @@ export interface IListenerMask {
  */
 export class EventListener {
     /**
+     * to cache camera priority
+     * @internal
+     */
+    public _cameraPriority: number = 0;
+    /**
      * @en The type code of unknown event listener.<br/>
      * @zh 未知的事件监听器类型
      */
