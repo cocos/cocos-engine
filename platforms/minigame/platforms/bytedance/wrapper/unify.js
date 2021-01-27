c971f7ca81b2d392f9ba267f237295442c6c7096
PP_Pro <chinese1236@qq.com>
2021-01-27 15:14:48 +0800

    Revert "cut down touch move callback"
    This reverts commit 19da61e3f16c3d1c14c1e8111b0da98b4997dea4.

--- platforms/minigame/platforms/bytedance/wrapper/unify.js
+++ platforms/minigame/platforms/bytedance/wrapper/unify.js
@@ -13,21 +13,7 @@ if (window.__globalAdapter) {
 
     // TouchEvent
     utils.cloneMethod(globalAdapter, tt, 'onTouchStart');
-    if (systemInfo.platform.toLowerCase() === 'ios') {
-        let skipTouchMove = false;
-        globalAdapter.onTouchMove = cb => {
-            tt.onTouchMove(res => {
-                skipTouchMove = !skipTouchMove;
-                if (skipTouchMove) {
-                    return;
-                }
-                cb && cb(res);
-            });
-        };
-    }
-    else {
-        utils.cloneMethod(globalAdapter, tt, 'onTouchMove');
-    }
+    utils.cloneMethod(globalAdapter, tt, 'onTouchMove');
     utils.cloneMethod(globalAdapter, tt, 'onTouchEnd');
     utils.cloneMethod(globalAdapter, tt, 'onTouchCancel');
 
