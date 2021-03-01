7f720e122ee955391d0a6f842d49729bfb8e3651
zhefeng.zhang <337031709@qq.com>
2021-03-01 16:29:18 +0800

    correction code

--- cocos2d/core/components/CCScrollView.js
+++ cocos2d/core/components/CCScrollView.js
@@ -781,10 +781,14 @@ let ScrollView = cc.Class({
 
         let deltaMove = cc.v2(0, 0);
         let wheelPrecision = 0;
-        if (cc.sys.browserType === cc.sys.BROWSER_TYPE_CHROME) {
-          wheelPrecision = -0.1;
-        } else if (cc.sys.browserType === cc.sys.BROWSER_TYPE_FIREFOX) {
-          wheelPrecision = -0.1/3;
+        //On the windows platform, the scrolling speed of the mouse wheel of ScrollView on chrome and firbox is different
+        if (cc.sys.os === cc.sys.OS_WINDOWS) {
+            if (cc.sys.browserType === cc.sys.BROWSER_TYPE_CHROME) {
+                wheelPrecision = -0.1;
+                //ScrollView 在 chrome 和 firbox 上鼠标滚轮滑动速度不同
+              } else if (cc.sys.browserType === cc.sys.BROWSER_TYPE_FIREFOX) {
+                wheelPrecision = -0.1/3;
+              }
         }
         if(CC_JSB || CC_RUNTIME) {
             wheelPrecision = -7;
