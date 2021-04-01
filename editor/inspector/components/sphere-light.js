27e06afb7017c5b92e4f2bf98c71046218d1de29
zhangxin <59186205+chichinohaha@users.noreply.github.com>
2021-04-01 11:18:45 +0800

    add sphere light inspector (#31)
    * add sphere light
    
    * format html
    
    * Update sphere-light.js
    
    Co-authored-by: SijieWang <itharbors@outlook.com>

---
+++ editor/inspector/components/sphere-light.js
@@ -0,0 +1,102 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+const propUtils = require('../utils/prop');
+
+const excludeList = [
+    'color', 'useColorTemperature', 'colorTemperature', 'size',
+    'range', 'term', 'luminance', 'luminousPower', 'staticSettings',
+];
+
+exports.template = `
+<div class="sphere-light-component">
+    <ui-prop type="dump" key="color"></ui-prop>
+    <ui-prop type="dump" key="useColorTemperature"></ui-prop>
+    <ui-prop type="dump" key="colorTemperature"></ui-prop>
+    <ui-prop type="dump" key="size"></ui-prop>
+    <ui-prop type="dump" key="range"></ui-prop>
+    <ui-prop type="dump" key="term"></ui-prop>
+    <ui-prop type="dump" showflag="term" key="luminance"></ui-prop>
+    <ui-prop type="dump" showflag="notTerm" key="luminousPower"></ui-prop>
+    <ui-prop type="dump" key="staticSettings"></ui-prop>
+    <!-- Render other data that is not taken over -->
+    <div id="customProps"></div>
+</div>
+`;
+
+// dump.value.term.value && (!dumps || dumps.every(item => !!item.value.term.value))
+
+exports.$ = {
+    customProps: '#customProps',
+    term: 'ui-prop[key="term"]',
+};
+
+const uiElements = {
+    term: {
+        ready () {
+            this.$.term.addEventListener('change-dump', () => {
+                uiElements.baseProps.update.call(this);
+            });
+        },
+    },
+    baseProps: {
+        ready () {
+            this.$.baseProps = this.$this.querySelectorAll('ui-prop:not(.customProp)');
+        },
+        update () {
+            if (!this.$.baseProps) {
+                uiElements.baseProps.ready.call(this);
+            }
+            this.$.baseProps.forEach((element) => {
+                const key = element.getAttribute('key');
+                let isShow = this.dump.value[key].visible;
+                if (isShow) {
+                    element.render(this.dump.value[key]);
+                }
+                if (element.hasAttribute('showflag')) {
+                    const showflag = element.getAttribute('showflag');
+                    if (showflag === 'term') {
+                        isShow = isShow && this.dump.value.term.value && (!this.dump.values || this.dump.values.length === 1 || this.dump.values.every((item) => item.value.term.value));
+                    } else if (showflag === 'notTerm') {
+                        isShow = isShow && !this.dump.value.term.value && (!this.dump.values || this.dump.values.length === 1 || this.dump.values.every((item) => !item.value.term.value));
+                    }
+                }
+                element.style = isShow ? '' : 'display: none;';
+            });
+        },
+    },
+    customProps: {
+        update () {
+            this.$.customProps.replaceChildren(...propUtils.getCustomPropElements(excludeList, this.dump, (element, prop) => {
+                element.className = 'customProp';
+                if (prop.dump.visible) {
+                    element.render(prop.dump);
+                }
+                element.style = prop.dump.visible ? '' : 'display: none;';
+            }));
+        },
+    },
+};
+
+exports.ready = function () {
+    for (const key in uiElements) {
+        const element = uiElements[key];
+        if (typeof element.ready === 'function') {
+            element.ready.call(this);
+        }
+    }
+};
+
+exports.update = function (dump) {
+    for (const key in dump.value) {
+        const info = dump.value[key];
+        if (dump.values) {
+            info.values = dump.values.map((value) => value[key].value);
+        }
+    }
+    this.dump = dump;
+    for (const key in uiElements) {
+        const element = uiElements[key];
+        if (typeof element.update === 'function') {
+            element.update.call(this);
+        }
+    }
+};
