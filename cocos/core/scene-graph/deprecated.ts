a4235461b84185a0eb10ea10531d9e5d56f59fa1
troublemaker52025 <troublemaker52025@outlook.com>
2021-04-02 14:40:24 +0800

    update

--- cocos/core/scene-graph/deprecated.ts
+++ cocos/core/scene-graph/deprecated.ts
@@ -28,6 +28,8 @@
  * @hidden
  */
 
+import { EDITOR } from 'internal:constants';
+import { ccclass } from 'cc.decorator';
 import { BaseNode } from './base-node';
 import { replaceProperty, removeProperty } from '../utils/x-deprecated';
 import { Layers } from './layers';
@@ -35,7 +37,8 @@ import { Node } from './node';
 import { Vec2 } from '../math/vec2';
 import { Size } from '../math/size';
 import { legacyCC } from '../global-exports';
-import { js } from '../utils/js';
+import { CCObject } from '../data/object';
+import { warnID } from '../platform/debug';
 
 replaceProperty(BaseNode.prototype, 'BaseNode', [
     {
@@ -226,3 +229,51 @@ removeProperty(Layers.BitMask, 'Layers.BitMask', [
         name: 'ALWAYS',
     },
 ]);
+
+const HideInHierarchy = CCObject.Flags.HideInHierarchy;
+const DontSave = CCObject.Flags.DontSave;
+
+/**
+ * @en
+ * Class of private entities in Cocos Creator scenes.<br/>
+ * The PrivateNode is hidden in editor, and completely transparent to users.<br/>
+ * It's normally used as Node's private content created by components in parent node.<br/>
+ * So in theory private nodes are not children, they are part of the parent node.<br/>
+ * Private node have two important characteristics:<br/>
+ * 1. It has the minimum z index and cannot be modified, because they can't be displayed over real children.<br/>
+ * 2. The positioning of private nodes is also special, they will consider the left bottom corner of the parent node's bounding box as the origin of local coordinates.<br/>
+ *    In this way, they can be easily kept inside the bounding box.<br/>
+ * Currently, it's used by RichText component and TileMap component.
+ * @zh
+ * Cocos Creator 场景中的私有节点类。<br/>
+ * 私有节点在编辑器中不可见，对用户透明。<br/>
+ * 通常私有节点是被一些特殊的组件创建出来作为父节点的一部分而存在的，理论上来说，它们不是子节点，而是父节点的组成部分。<br/>
+ * 私有节点有两个非常重要的特性：<br/>
+ * 1. 它有着最小的渲染排序的 Z 轴深度，并且无法被更改，因为它们不能被显示在其他正常子节点之上。<br/>
+ * 2. 它的定位也是特殊的，对于私有节点来说，父节点包围盒的左下角是它的局部坐标系原点，这个原点相当于父节点的位置减去它锚点的偏移。这样私有节点可以比较容易被控制在包围盒之中。<br/>
+ * 目前在引擎中，RichText 和 TileMap 都有可能生成私有节点。
+ */
+@ccclass('cc.PrivateNode')
+export class PrivateNode extends Node {
+    constructor (name?: string) {
+        super(name);
+        warnID(4500, this.name);
+
+        this.hideFlags |= DontSave | HideInHierarchy;
+    }
+}
+
+if (EDITOR) {
+    // check components to avoid missing node reference serialied in previous version
+    PrivateNode.prototype._onBatchCreated = function onBatchCreated (dontSyncChildPrefab: boolean) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        for (const comp of this._components) {
+            comp.node = this;
+        }
+
+        Node.prototype._onBatchCreated.call(this, dontSyncChildPrefab);
+    };
+}
+
+legacyCC.PrivateNode = PrivateNode;
