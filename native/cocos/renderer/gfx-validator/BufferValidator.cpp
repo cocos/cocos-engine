740c0526daf3cba697d87c256247a86b1d649302
minggo <nudtzxm@gmail.com>
2021-04-21 15:22:03 +0800

    Revert "disable log temporaly (#3438)" (#3441)
    This reverts commit 8d97f6a79d23db8019b5b05242a42442fbb44333.

--- native/cocos/renderer/gfx-validator/BufferValidator.cpp
+++ native/cocos/renderer/gfx-validator/BufferValidator.cpp
@@ -110,9 +110,7 @@ void BufferValidator::updateRedundencyCheck() {
 
     if (cur == _lastUpdateFrame) {
         CC_LOG_WARNING(utils::getStacktraceJS().c_str());
-        // FIXME: minggo: as current implementation need to update some buffers more than once, so disable it.
-        // Should enable it when it is fixed.
-        // CC_LOG_WARNING("performance warning: buffer updated more than once per frame");
+        CC_LOG_WARNING("performance warning: buffer updated more than once per frame");
     }
 
     _lastUpdateFrame = cur;
