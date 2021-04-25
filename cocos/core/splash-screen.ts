5495aa98e23767975b1680dcddfb7132b5aea7f4
jiaxin.lai <goodlaijiaxin@qq.com>
2021-04-25 11:16:48 +0800

    flush commands before submit

--- cocos/core/splash-screen.ts
+++ cocos/core/splash-screen.ts
@@ -355,6 +355,7 @@ export class SplashScreen {
 
         cmdBuff.endRenderPass();
         cmdBuff.end();
+        device.flushCommands([cmdBuff]);
         device.queue.submit([cmdBuff]);
         device.present();
     }
