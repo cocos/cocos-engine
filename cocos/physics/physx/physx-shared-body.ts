edb52786b0d6f1a73c39edbdf72c44e8e2b58681
jiaxin.lai <goodlaijiaxin@qq.com>
2021-04-25 20:51:14 +0800

    try js transform optimized by bytedance (#8537)
    * try js transform
    
    * adapt
    
    * using getWorldBoundsArray

--- cocos/physics/physx/physx-shared-body.ts
+++ cocos/physics/physx/physx-shared-body.ts
@@ -35,7 +35,7 @@ import { PhysXWorld } from './physx-world';
 import { PhysXShape } from './shapes/physx-shape';
 import { TransformBit } from '../../core/scene-graph/node-enum';
 import {
-    addActorToScene, copyPhysXTransform, getTempTransform, physXEqualsCocosQuat,
+    addActorToScene, copyPhysXTransform, getJsTransform, getTempTransform, physXEqualsCocosQuat,
     physXEqualsCocosVec3, PX, setMassAndUpdateInertia,
 } from './export-physx';
 import { VEC3_0 } from '../utils/util';
@@ -283,7 +283,7 @@ export class PhysXSharedBody {
         const node = this.node;
         if (node.hasChangedFlags) {
             if (node.hasChangedFlags & TransformBit.SCALE) this.syncScale();
-            const trans = getTempTransform(node.worldPosition, node.worldRotation);
+            const trans = getJsTransform(node.worldPosition, node.worldRotation);
             if (this._isKinematic) {
                 this.impl.setKinematicTarget(trans);
             } else {
@@ -299,9 +299,9 @@ export class PhysXSharedBody {
             const wp = node.worldPosition;
             const wr = node.worldRotation;
             const pose = this.impl.getGlobalPose();
-            const DontUpdate = physXEqualsCocosVec3(pose, wp) && physXEqualsCocosQuat(pose, wr);
-            if (!DontUpdate) {
-                const trans = getTempTransform(node.worldPosition, node.worldRotation);
+            const dontUpdate = physXEqualsCocosVec3(pose, wp) && physXEqualsCocosQuat(pose, wr);
+            if (!dontUpdate) {
+                const trans = getJsTransform(node.worldPosition, node.worldRotation);
                 if (this._isKinematic) {
                     this.impl.setKinematicTarget(trans);
                 } else {
