cdc0781d43ff7ebb7b603a24663395cfd6f45294
zxx43 <zxx43@hotmail.com>
2021-05-07 15:53:11 +0800

    change branch

--- cocos2d/videoplayer/video-player-impl.js
+++ cocos2d/videoplayer/video-player-impl.js
@@ -21,7 +21,7 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
- ****************************************************************************/
+ ****************************************************************************/ 
 
 const utils = require('../core/platform/utils');
 const sys = require('../core/platform/CCSys');
