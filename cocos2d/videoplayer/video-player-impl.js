8ddcdab1c6c5de2ca01f862336afb78b463b8cfb
zxx43 <zxx43@hotmail.com>
2021-05-08 18:57:39 +0800

    fix visible & hide (#8651)
    * fix visible & hide
    
    * modify show & hide
    
    * adjust code format

--- cocos2d/videoplayer/video-player-impl.js
+++ cocos2d/videoplayer/video-player-impl.js
@@ -46,6 +46,9 @@ let VideoPlayerImpl = cc.Class({
 
         this._video = null;
         this._url = '';
+		
+        this.playerWidth = '0px';
+        this.playerHeight = '0px';
 
         this._waitingFullscreen = false;
         this._fullScreenEnabled = false;
@@ -155,6 +158,9 @@ let VideoPlayerImpl = cc.Class({
 
         video.style.width = width + 'px';
         video.style.height = height + 'px';
+		
+        this.playerWidth = video.style.width;
+        this.playerHeight = video.style.height;
     },
 
     _createDom (muted) {
@@ -407,8 +413,12 @@ let VideoPlayerImpl = cc.Class({
         if (video.setShowRawFrame !== undefined)
             video.setShowRawFrame(show);
         else {
-            if (show) video.style.visibility = 'visible';
-            else video.style.visibility = 'hidden';
+			if (!show) {
+				video.style.width = '0%';
+			}
+			else {
+				video.style.width = this.videoWidth;
+			}
         }
     },
 
