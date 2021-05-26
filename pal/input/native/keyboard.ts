fa32a1ae62dc672bbf2b13c4207db20fac20b020
PP_Pro <chinese1236@qq.com>
2021-05-26 16:19:32 +0800

    support native keyboard KEYBOARD_DOWN event

--- pal/input/native/keyboard.ts
+++ pal/input/native/keyboard.ts
@@ -7,25 +7,45 @@ export class KeyboardInputSource {
     public support: boolean;
     private _eventTarget: EventTarget = new EventTarget();
 
+    // On native platform, KeyboardEvent.repeat is always false, so we need a map to manage the key state.
+    private _keyStateMap: Record<number, boolean> = {};
+
     constructor () {
         this.support = !system.isMobile;
         this._registerEvent();
     }
 
     private _registerEvent () {
-        jsb.onKeyDown = this._createCallback(SystemEventType.KEYBOARD_DOWN);
-        jsb.onKeyUp =  this._createCallback(SystemEventType.KEYBOARD_UP);
-    }
-
-    private _createCallback (eventType: SystemEventType) {
-        return (event: jsb.KeyboardEvent) => {
+        jsb.onKeyDown = (event: jsb.KeyboardEvent) => {
+            const keyCode = event.keyCode;
+            if (!this._keyStateMap[keyCode]) {
+                const keyDownInputEvent = this._getInputEvent(event, SystemEventType.KEYBOARD_DOWN);
+                this._eventTarget.emit(SystemEventType.KEYBOARD_DOWN, keyDownInputEvent);
+            }
+            // @ts-expect-error Compability for key pressing callback
+            const keyPressingInputEvent = this._getInputEvent(event, 'keydown');
+            this._eventTarget.emit('keydown', keyPressingInputEvent);
+            this._keyStateMap[keyCode] = true;
+        };
+        jsb.onKeyUp =  (event: jsb.KeyboardEvent) => {
+            const keyCode = event.keyCode;
             const inputEvent: KeyboardInputEvent = {
-                type: eventType,
-                code: event.keyCode,
+                type: SystemEventType.KEYBOARD_UP,
+                code: keyCode,
                 timestamp: performance.now(),
             };
-            this._eventTarget.emit(eventType, inputEvent);
+            this._keyStateMap[keyCode] = false;
+            this._eventTarget.emit(SystemEventType.KEYBOARD_UP, inputEvent);
         };
+    }
+
+    private _getInputEvent (event: jsb.KeyboardEvent, eventType: SystemEventType) {
+        const inputEvent: KeyboardInputEvent = {
+            type: eventType,
+            code: event.keyCode,  // TODO: keyCode is deprecated on Web standard
+            timestamp: performance.now(),
+        };
+        return inputEvent;
     }
 
     public onDown (cb: KeyboardCallback) {
