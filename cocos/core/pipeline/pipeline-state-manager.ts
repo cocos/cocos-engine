0fc8e6fbbe26e5654e8b497222ba6934167c1727
Yun Hsiao Wu <yunhsiaow@gmail.com>
2021-05-27 10:19:37 +0800

    node related optimizations (#8716)
    * opt: node.invalidateChildren
    
    * opt: cache pass states
    
    * make hasChangedFlags visible to native
    
    * fix switch case indent rule

--- cocos/core/pipeline/pipeline-state-manager.ts
+++ cocos/core/pipeline/pipeline-state-manager.ts
@@ -29,7 +29,6 @@
  */
 
 import { Shader, RenderPass, InputAssembler, Device, PipelineState, InputState, PipelineStateInfo } from '../gfx';
-import { PassPool, PassView, PassHandle, PipelineLayoutPool } from '../renderer/core/memory-pools';
 import { Pass } from '../renderer/core/pass';
 
 export class PipelineStateManager {
@@ -37,8 +36,7 @@ export class PipelineStateManager {
 
     // pass is only needed on TS.
     static getOrCreatePipelineState (device: Device, pass: Pass, shader: Shader, renderPass: RenderPass, ia: InputAssembler) {
-        const hPass = pass.handle;
-        const hash1 = PassPool.get(hPass, PassView.HASH);
+        const hash1 = pass.hash;
         const hash2 = renderPass.hash;
         const hash3 = ia.attributesHash;
         const hash4 = shader.id;
@@ -46,15 +44,13 @@ export class PipelineStateManager {
         const newHash = hash1 ^ hash2 ^ hash3 ^ hash4;
         let pso = this._PSOHashMap.get(newHash);
         if (!pso) {
-            const pipelineLayout = PipelineLayoutPool.get(PassPool.get(hPass, PassView.PIPELINE_LAYOUT));
             const inputState = new InputState(ia.attributes);
             const psoInfo = new PipelineStateInfo(
-                shader, pipelineLayout, renderPass, inputState,
+                shader, pass.pipelineLayout, renderPass, inputState,
                 pass.rasterizerState,
                 pass.depthStencilState,
                 pass.blendState,
-                PassPool.get(hPass, PassView.PRIMITIVE),
-                PassPool.get(hPass, PassView.DYNAMIC_STATES),
+                pass.primitive, pass.dynamicStates,
             );
             pso = device.createPipelineState(psoInfo);
             this._PSOHashMap.set(newHash, pso);
