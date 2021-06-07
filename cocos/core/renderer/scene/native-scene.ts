e84c36ad24e0b52536a0a0d59f2c10360bee4ad0
minggo <nudtzxm@gmail.com>
2021-06-07 11:24:40 +0800

    fix compiling errors on all platforms but native (#8769)

--- cocos/core/renderer/scene/native-scene.ts
+++ cocos/core/renderer/scene/native-scene.ts
@@ -1,208 +1,225 @@
-import { RenderScene } from '.';
-import { BatchView2D, Pass } from '..';
+import { BatchView2D } from '..';
 import { IFlatBuffer } from '../../assets/rendering-sub-mesh';
 import { AABB, Frustum } from '../../geometry';
 import { Attribute, Buffer, ClearFlags, Color as GFXColor, DescriptorSet, Framebuffer, InputAssembler, Shader } from '../../gfx';
 import { Color, Mat4, Rect, Vec2 } from '../../math';
 import { RenderPriority } from '../../pipeline/define';
 import { LightType } from './light';
-import { SubModel } from './submodel';
 
-export declare class NativeNode {
-    public initWithData (data: TypedArray): void;
-}
+export const NativeNode: Constructor<{
+    initWithData (data: TypedArray): void;
+}> = null!;
+export type NativeNode = InstanceType<typeof NativeNode>;
 
-export declare class NativeModel {
-    public setReceiveShadow (val: boolean): void;
-    public setEnabled (val: boolean): void;
-    public seVisFlag (val: number): void;
-    public setTransform (n: Node): void;
-    public setNode (n: Node): void;
-    public setCastShadow (val: boolean): void;
-    public setLocalBuffer (buf: Buffer | null): void;
-    public setWolrdBounds (val: AABB | null): void;
-    public addSubModel (val: NativeSubModel): void;
-    public setInstmatWorldIdx (idx: number): void;
-    public setInstancedBuffer (buffer: ArrayBuffer): void;
-    public setInstanceAttributes (attrs: Attribute[]): void;
-}
+export const NativeModel: Constructor<{
+     setReceiveShadow (val: boolean): void;
+     setEnabled (val: boolean): void;
+     seVisFlag (val: number): void;
+     setTransform (n: Node): void;
+     setNode (n: Node): void;
+     setCastShadow (val: boolean): void;
+     setLocalBuffer (buf: Buffer | null): void;
+     setWolrdBounds (val: AABB | null): void;
+     addSubModel (val: NativeSubModel): void;
+     setInstmatWorldIdx (idx: number): void;
+     setInstancedBuffer (buffer: ArrayBuffer): void;
+     setInstanceAttributes (attrs: Attribute[]): void;
+}> = null!;
+export type NativeModel = InstanceType<typeof NativeModel>;
 
-export declare class NativeLight {
-    public setType (type: LightType): void;
-    public setColor (color: Vec3): void;
-    public setUseColorTemperature (enable: boolean): void;
-    public setColorTemperatureRGB (color: Vec3): void;
-    public setNode (n: Node): void;
-}
+export const NativeLight: Constructor<{
+     setType (type: LightType): void;
+     setColor (color: Vec3): void;
+     setUseColorTemperature (enable: boolean): void;
+     setColorTemperatureRGB (color: Vec3): void;
+     setNode (n: Node): void;
+}> = null!;
+export type NativeLight = InstanceType<typeof NativeLight>;
 
-export declare class NativeDirectionalLight extends NativeLight {
-    public setDirection (dir: Vec3): void;
-    public setIlluminance (lum: number): void;
-}
+export const NativeDirectionalLight: Constructor<{
+     setDirection (dir: Vec3): void;
+     setIlluminance (lum: number): void;
+} & NativeLight> = null!;
+export type NativeDirectionalLight = InstanceType<typeof NativeDirectionalLight>;
 
-export declare class NativeSphereLight extends NativeLight {
-    public setPosition (pos: Vec3): void;
-    public setAABB (aabb: AABB): void;
-    public setSize (size: number): void;
-    public setRange (range: number): void;
-    public setIlluminance (lum: number): void;
-}
+export const NativeSphereLight: Constructor<{
+     setPosition (pos: Vec3): void;
+     setAABB (aabb: AABB): void;
+     setSize (size: number): void;
+     setRange (range: number): void;
+     setIlluminance (lum: number): void;
+}  & NativeLight> = null!;
+export type NativeSphereLight = InstanceType<typeof NativeSphereLight>;
 
-export declare class NativeSpotLight extends NativeLight {
-    public setDirection (dir: Vec3): void;
-    public setFrustum (frs: Frustum): void;
-    public setAABB (aabb: AABB): void;
-    public setPosition (pos: Vec3): void;
-    public setSize (size: number): void;
-    public setRange (range: number): void;
-    public setAspect (aspect: number): void;
-    public setAngle (angle: number): void;
-    public setIlluminance (lum: number): void;
-}
+export const NativeSpotLight: Constructor<{
+     setDirection (dir: Vec3): void;
+     setFrustum (frs: Frustum): void;
+     setAABB (aabb: AABB): void;
+     setPosition (pos: Vec3): void;
+     setSize (size: number): void;
+     setRange (range: number): void;
+     setAspect (aspect: number): void;
+     setAngle (angle: number): void;
+     setIlluminance (lum: number): void;
+} & NativeLight> = null!;
+export type NativeSpotLight = InstanceType<typeof NativeSpotLight>;
 
-export declare class NaitveSkybox {
-    public enabled: boolean;
-    public useIBL: boolean;
-    public isRGBE: boolean;
-    public model: NativeModel | null;
-}
+export const NaitveSkybox: Constructor<{
+     enabled: boolean;
+     useIBL: boolean;
+     isRGBE: boolean;
+     model: NativeModel | null;
+}> = null!;
+export type NaitveSkybox = InstanceType<typeof NaitveSkybox>;
 
-export declare class NativeFog {
-    public type: number;
-    public enabled: boolean;
-    public color: Color;
-    public density: number;
-    public start: number;
-    public end: number;
-    public atten: number;
-    public top: number;
-    public range: number;
-}
+export const NativeFog: Constructor<{
+     type: number;
+     enabled: boolean;
+     color: Color;
+     density: number;
+     start: number;
+     end: number;
+     atten: number;
+     top: number;
+     range: number;
+}> = null!;
+export type NativeFog = InstanceType<typeof NativeFog>;
 
-export declare class NativeRenderWindow {
-    public hasOffScreenAttachments: boolean;
-    public hasOnScreenAttachments: boolean;
-    public frameBuffer: Framebuffer;
-}
+export const NativeRenderWindow: Constructor<{
+     hasOffScreenAttachments: boolean;
+     hasOnScreenAttachments: boolean;
+     frameBuffer: Framebuffer;
+}> = null!;
+export type NativeRenderWindow = InstanceType<typeof NativeRenderWindow>;
 
-export declare class NativeCamera {
-    public width: number;
-    public height: number;
-    public scene: NativeRenderScene | null;
-    public frustum: Frustum;
-    public matView: Mat4;
-    public matViewProj: Mat4;
-    public matViewProjInv: Mat4;
-    public matProj: Mat4;
-    public matProjInv: Mat4;
-    public position: Vec3;
-    public forward: Vec3;
-    public node: NativeNode | null;
-    public clearColor: GFXColor;
-    public viewPort: Rect;
-    public window: NativeRenderWindow | null;
-    public visibility: number;
-    public clearFlag: ClearFlags;
-    public clearDepth: number;
-    public clearStencil: number;
-    public exposure: number;
-}
+export const NativeCamera: Constructor<{
+     width: number;
+     height: number;
+     scene: NativeRenderScene | null;
+     frustum: Frustum;
+     matView: Mat4;
+     matViewProj: Mat4;
+     matViewProjInv: Mat4;
+     matProj: Mat4;
+     matProjInv: Mat4;
+     position: Vec3;
+     forward: Vec3;
+     node: NativeNode | null;
+     clearColor: GFXColor;
+     viewPort: Rect;
+     window: NativeRenderWindow | null;
+     visibility: number;
+     clearFlag: ClearFlags;
+     clearDepth: number;
+     clearStencil: number;
+     exposure: number;
+}> = null!;
+export type NativeCamera = InstanceType<typeof NativeCamera>;
 
-export declare class NativePass {
-    public update(): void;
-    public setPriority(val: number): void;
-    public setStage(val: number): void;
-    public setPhase(val: number): void;
-    public setPrimitive(val: number): void;
-    public setRasterizerState(val): void;
-    public setDepthStencilState(val): void;
-    public setBlendState(val): void;
-    public setDescriptorSet(val): void;
-    public setBatchingScheme(val: number): void;
-    public setDynamicState(val: number): void;
-    public setHash(val: number): void;
-    public setPipelineLayout(val): void;
-}
+export const NativePass: Constructor<{
+     update(): void;
+     setPriority(val: number): void;
+     setStage(val: number): void;
+     setPhase(val: number): void;
+     setPrimitive(val: number): void;
+     setRasterizerState(val): void;
+     setDepthStencilState(val): void;
+     setBlendState(val): void;
+     setDescriptorSet(val): void;
+     setBatchingScheme(val: number): void;
+     setDynamicState(val: number): void;
+     setHash(val: number): void;
+     setPipelineLayout(val): void;
+}> = null!;
+export type NativePass = InstanceType<typeof NativePass>;
 
-export declare class NativeSubModel {
-    public setDescriptorSet(val: DescriptorSet | null): void;
-    public setInputAssembler(val: InputAssembler | null): void;
-    public setRenderingSubMesh(val: IFlatBuffer[]): void;
-    public setPlanarShader(val: Shader | null): void;
-    public setPlanarInstanceShader(val: Shader | null): void;
-    public setPasses(val: NativePass[]): void;
-    public setShaders(val: (Shader | null)[]): void;
-    public setPriority(val: RenderPriority): void;
-}
-export declare class NativeDrawBatch2D {
-    public visFlags: BatchView2D;
-    public inputAssembler: InputAssembler | null;
-    public descriptorSet: DescriptorSet | null;
-    public passes: NativePass[];
-    public shaders: Shader[];
-}
+export const NativeSubModel: Constructor<{
+     setDescriptorSet(val: DescriptorSet | null): void;
+     setInputAssembler(val: InputAssembler | null): void;
+     setRenderingSubMesh(val: IFlatBuffer[]): void;
+     setPlanarShader(val: Shader | null): void;
+     setPlanarInstanceShader(val: Shader | null): void;
+     setPasses(val: NativePass[]): void;
+     setShaders(val: (Shader | null)[]): void;
+     setPriority(val: RenderPriority): void;
+}> = null!;
+export type NativeSubModel = InstanceType<typeof NativeSubModel>;
 
-export declare class NativeRenderScene {
-    public setMainLight (l: NativeLight | null): void;
-    public addSphereLight (l: NativeLight | null): void;
-    public removeSphereLight (l: NativeLight | null): void;
-    public addSpotLight (l: NativeLight | null): void;
-    public removeSpotLight (l: NativeLight | null): void;
-    public removeSphereLights (): void;
-    public removeSpotLights (): void;
-    public addModel (m: NativeModel): void;
-    public removeModel (m: NativeModel): void;
-    public removeModels (): void;
-    public addBatch (batch: NativeDrawBatch2D): void;
-    public removeBatch (index: number): void;
-    public removeBatches (): void;
-}
+export const NativeDrawBatch2D: Constructor<{
+     visFlags: BatchView2D;
+     inputAssembler: InputAssembler | null;
+     descriptorSet: DescriptorSet | null;
+     passes: NativePass[];
+     shaders: Shader[];
+}> = null!;
+export type NativeDrawBatch2D = InstanceType<typeof NativeDrawBatch2D>;
 
-export declare class NativeAmbient {
-    public enabled: boolean;
-    public skyColor: Color;
-    public skyIllum: number;
-    public groundAlbedo: Color;
-}
+export const NativeRenderScene: Constructor<{
+     setMainLight (l: NativeLight | null): void;
+     addSphereLight (l: NativeLight | null): void;
+     removeSphereLight (l: NativeLight | null): void;
+     addSpotLight (l: NativeLight | null): void;
+     removeSpotLight (l: NativeLight | null): void;
+     removeSphereLights (): void;
+     removeSpotLights (): void;
+     addModel (m: NativeModel): void;
+     removeModel (m: NativeModel): void;
+     removeModels (): void;
+     addBatch (batch: NativeDrawBatch2D): void;
+     removeBatch (index: number): void;
+     removeBatches (): void;
+}> = null!;
+export type NativeRenderScene = InstanceType<typeof NativeRenderScene>;
 
-export declare class NativeShadow {
-    public normal: Vec3;
-    public distance: number;
-    public color: Color;
-    public nearValue: number;
-    public farValue: number;
-    public aspect: number;
-    public orthoSize: number;
-    public size: Vec2;
-    public pcfType: number;
-    public shadowMapDirty: boolean;
-    public bias: number;
-    public packing: boolean;
-    public linear: boolean;
-    public selfShadow: boolean;
-    public normalBias: number;
-    public autoAdapt: boolean;
-    public planarPass: NativePass;
-    public instancePass: NativePass;
-    public enabled: boolean;
-    public shadowType: number;
-}
+export const NativeAmbient: Constructor<{
+    enabled: boolean;
+    skyColor: Color;
+    skyIllum: number;
+    groundAlbedo: Color;
+}> = null!;
+export type NativeAmbient = InstanceType<typeof NativeAmbient>;
 
-export declare class NativeRoot {
-    public cumulativeTime: number;
-    public frameTime: number;
-}
+export const NativeShadow: Constructor<{
+     normal: Vec3;
+     distance: number;
+     color: Color;
+     nearValue: number;
+     farValue: number;
+     aspect: number;
+     orthoSize: number;
+     size: Vec2;
+     pcfType: number;
+     shadowMapDirty: boolean;
+     bias: number;
+     packing: boolean;
+     linear: boolean;
+     selfShadow: boolean;
+     normalBias: number;
+     autoAdapt: boolean;
+     planarPass: NativePass;
+     instancePass: NativePass;
+     enabled: boolean;
+     shadowType: number;
+}> = null!;
+export type NativeShadow = InstanceType<typeof NativeShadow>;
 
-export declare class NativePipelineSharedSceneData {
-    public isHDR: boolean;
-    public shadingScale: number;
-    public fpScale: number;
-    public fog: NativeFog;
-    public ambient: NativeAmbient;
-    public skybox: NaitveSkybox;
-    public shadow: NativeShadow;
-    public deferredLightPassShader: Shader | null;
-    public deferredLightPass: NativePass;
-    public deferredPostPassShader: Shader | null;
-    public deferredPostPass: NativePass;
-}
+export const NativeRoot: Constructor<{
+     cumulativeTime: number;
+     frameTime: number;
+}> = null!;
+export type NativeRoot = InstanceType<typeof NativeRoot>;
+
+export const NativePipelineSharedSceneData: Constructor<{
+     isHDR: boolean;
+     shadingScale: number;
+     fpScale: number;
+     fog: NativeFog;
+     ambient: NativeAmbient;
+     skybox: NaitveSkybox;
+     shadow: NativeShadow;
+     deferredLightPassShader: Shader | null;
+     deferredLightPass: NativePass;
+     deferredPostPassShader: Shader | null;
+     deferredPostPass: NativePass;
+}> = null!;
+export type NativePipelineSharedSceneData = InstanceType<typeof NativePipelineSharedSceneData>;
