7e55ddf13314666d33eba20d7720575f3722cc60
PP <chinese1236@qq.com>
2021-06-09 15:12:51 +0800

    support SystemEvent.Keyboard enum type (#8785)
    * support SystemEvent.Keyboard enum type
    
    update
    
    update
    
    update
    
    update
    
    # Conflicts:
    #	cocos/core/platform/event-manager/system-event.ts
    
    update pal
    
    mark macro.KEY as warning
    
    mark
    
    update
    
    update
    
    * support right shift, alt and ctrl enum
    
    * support web keyCode map
    
    # Conflicts:
    #	pal/input/web/keyboard.ts
    
    * support num enter key enum
    
    * rename num_slash to num_divide
    
    * support native keyCode map
    
    # Conflicts:
    #	pal/input/native/keyboard.ts
    
    * update enum
    
    * rename Keyboard to KeyCode
    
    * update deprecated
    
    * update

--- pal/input/native/keyboard.ts
+++ pal/input/native/keyboard.ts
@@ -2,6 +2,29 @@ import { KeyboardCallback, KeyboardInputEvent } from 'pal/input';
 import { system } from 'pal/system';
 import { KeyboardEvent } from '../../../cocos/core/platform/event-manager/event-enum';
 import { EventTarget } from '../../../cocos/core/event/event-target';
+import { KeyCode } from '../../../cocos/core/platform/event-manager/key-code';
+
+const nativeKeyCode2Keycode: Record<number, KeyCode> = {
+    12: KeyCode.NUM_LOCK,
+    10048: KeyCode.NUM_0,
+    10049: KeyCode.NUM_1,
+    10050: KeyCode.NUM_2,
+    10051: KeyCode.NUM_3,
+    10052: KeyCode.NUM_4,
+    10053: KeyCode.NUM_5,
+    10054: KeyCode.NUM_6,
+    10055: KeyCode.NUM_7,
+    10056: KeyCode.NUM_8,
+    10057: KeyCode.NUM_9,
+    20013: KeyCode.NUM_ENTER,
+    20016: KeyCode.SHIFT_RIGHT,
+    20017: KeyCode.CTRL_RIGHT,
+    20018: KeyCode.ALT_RIGHT,
+};
+
+function getKeyCode (keyCode: number): KeyCode {
+    return nativeKeyCode2Keycode[keyCode] || keyCode;
+}
 
 export class KeyboardInputSource {
     public support: boolean;
@@ -17,32 +40,33 @@ export class KeyboardInputSource {
 
     private _registerEvent () {
         jsb.onKeyDown = (event: jsb.KeyboardEvent) => {
-            const keyCode = event.keyCode;
-            if (!this._keyStateMap[keyCode]) {
+            const keycode = getKeyCode(event.keyCode);
+            if (!this._keyStateMap[keycode]) {
                 const keyDownInputEvent = this._getInputEvent(event, KeyboardEvent.KEY_DOWN);
                 this._eventTarget.emit(KeyboardEvent.KEY_DOWN, keyDownInputEvent);
             }
             // @ts-expect-error Compability for key pressing callback
             const keyPressingInputEvent = this._getInputEvent(event, 'keydown');
             this._eventTarget.emit('keydown', keyPressingInputEvent);
-            this._keyStateMap[keyCode] = true;
+            this._keyStateMap[keycode] = true;
         };
         jsb.onKeyUp =  (event: jsb.KeyboardEvent) => {
-            const keyCode = event.keyCode;
+            const keycode = getKeyCode(event.keyCode);
             const inputEvent: KeyboardInputEvent = {
                 type: KeyboardEvent.KEY_UP,
-                code: keyCode,
+                code: keycode,
                 timestamp: performance.now(),
             };
-            this._keyStateMap[keyCode] = false;
+            this._keyStateMap[keycode] = false;
             this._eventTarget.emit(KeyboardEvent.KEY_UP, inputEvent);
         };
     }
 
     private _getInputEvent (event: jsb.KeyboardEvent, eventType: KeyboardEvent) {
+        const keycode = getKeyCode(event.keyCode);
         const inputEvent: KeyboardInputEvent = {
             type: eventType,
-            code: event.keyCode,  // TODO: keyCode is deprecated on Web standard
+            code: keycode,
             timestamp: performance.now(),
         };
         return inputEvent;
