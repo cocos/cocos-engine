93e76714c61b3d8d0e0aa73e8f371c0b099521bb
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2021-06-29 16:06:40 +0800

    Class decorator uniquelyReferenced (#8894)
    * Class decorator unique
    
    * unique -> uniquelyReferenced
    
    * Add unit test
    
    * Update doc
    
    * Optimize for non-serialization env
    
    * Update

--- cocos/core/data/decorators/serializable.ts
+++ cocos/core/data/decorators/serializable.ts
@@ -28,8 +28,15 @@
  * @module decorator
  */
 
-import { LegacyPropertyDecorator } from './utils';
+import { EDITOR, TEST } from 'internal:constants';
+import { emptyDecorator, LegacyPropertyDecorator } from './utils';
 import { property, IPropertyOptions } from './property';
+import { getOrCreateSerializationMetadata } from '../serialization-metadata';
+
+/**
+ * True if serialization feature is enabled in current environment.
+ */
+const WITH_SERIALIZATION = EDITOR || TEST;
 
 export const serializable: LegacyPropertyDecorator = (target, propertyKey, descriptor) => property(makeSerializable({ }))(target, propertyKey, descriptor);
 
@@ -56,3 +63,61 @@ function makeSerializable (options: IPropertyOptions) {
     }
     return options;
 }
+
+/**
+ * @en
+ * Marks the target class as "uniquely referenced" which means, in the aspect of serialization,
+ * no more than one objects should reference to same instance of that class.
+ * When serializing references to objects of such class,
+ * they're treated as different object even they point to actually the same.
+ * While deserializing, these two references would point two distinct objects.
+ * For example:
+ * ```ts
+ * import { _decorator } from 'cc';
+ * \@_decorator.ccclass
+ * \@_decorator.uniquelyReferenced
+ * class Foo { }
+ *
+ * \@_decorator.ccclass
+ * class Bar {
+ *   \@_decorator.property
+ *   public foo = new Foo();
+ * }
+ *
+ * const bar1 = new Bar();
+ * const bar2 = new Bar();
+ * bar2.foo = bar1.foo; // Programmatically let them reference to the same
+ * ```
+ * `bar1` and `bar2` reference to the same `Foo` object.
+ * However, after deserializing, `bar1.foo === bar2.foo` always evaluates to `false`.
+ * @zh
+ * 将目标类标记为“被唯一引用”的，其意味着就序列化而言，不会有多个对象引用该类的同一实例。
+ * 当序列化到该类的对象引用时，即使它们明面上指向同一对象，也会被当作是不同对象；
+ * 而当反序列化后，这两个引用将指向截然不同的两个对象。
+ * 例如：
+ * ```ts
+ * import { _decorator } from 'cc';
+ * \@_decorator.ccclass
+ * \@_decorator.uniquelyReferenced
+ * class Foo { }
+ *
+ * \@_decorator.ccclass
+ * class Bar {
+ *   \@_decorator.property
+ *   public foo = new Foo();
+ * }
+ *
+ * const bar1 = new Bar();
+ * const bar2 = new Bar();
+ * bar2.foo = bar1.foo; // 由程序逻辑让它们引用同一个对象
+ * ```
+ * `bar1` 和 `bar2` 引用同一个 `Foo` 对象。
+ * 但在反序列化之后，`bar1.foo === bar2.foo` 永不成立。
+ */
+export const uniquelyReferenced: ClassDecorator = !WITH_SERIALIZATION
+    ? emptyDecorator
+    // eslint-disable-next-line func-names, @typescript-eslint/ban-types
+    : function uniquelyReferenced<TFunction extends Function> (constructor: TFunction) {
+        const metadata = getOrCreateSerializationMetadata(constructor);
+        metadata.uniquelyReferenced = true;
+    };
---
+++ cocos/core/data/serialization-metadata.ts
@@ -0,0 +1,27 @@
+const sMetadataTag = Symbol('cc:SerializationMetadata');
+
+/**
+ * For internal usage only. DO NOT USE IT IN YOUR CODES.
+ * @param constructor
+ * @returns
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function getSerializationMetadata (constructor: Function): SerializationMetadata | undefined {
+    return (constructor as MayBeInjected)[sMetadataTag];
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function getOrCreateSerializationMetadata (constructor: Function): SerializationMetadata {
+    return (constructor as MayBeInjected)[sMetadataTag] ??= {};
+}
+
+/**
+ * For internal usage only. DO NOT USE IT IN YOUR CODES.
+ */
+export interface SerializationMetadata {
+    uniquelyReferenced?: boolean;
+}
+
+interface MayBeInjected {
+    [sMetadataTag]?: SerializationMetadata;
+}
