5aa11a1e21220ee5e10120bb7076689a8f018413
pandamicro <lphuabin@gmail.com>
2021-07-15 17:50:15 +0800

    Fix ci

--- cocos/core/event/event-target-factory.ts
+++ cocos/core/event/event-target-factory.ts
@@ -34,6 +34,7 @@ export function applyMixins (derivedCtor: any, baseCtors: any[]) {
     baseCtors.forEach((baseCtor) => {
         Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
             if (name !== 'constructor') {
+                // @ts-expect-error
                 Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name));
             }
         });
