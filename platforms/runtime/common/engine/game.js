09ae44ee71077690091fe162017d18f2ed78cf5a
PP_Pro <chinese1236@qq.com>
2021-07-29 18:06:32 +0800

    fix hide show event on runtime platform

--- platforms/runtime/common/engine/game.js
+++ platforms/runtime/common/engine/game.js
@@ -10,14 +10,6 @@ cc.game._setAnimFrame = function () {
     window.cAF = window.cancelAnimationFrame;
 };
 
-ral.onHide(function (data) {
-    cc.game.emit(cc.Game.EVENT_HIDE);
-});
-
-ral.onShow(function () {
-    cc.game.emit(cc.Game.EVENT_SHOW);
-});
-
 ral.onWindowResize && ral.onWindowResize(function (width, height) {
     // Since the initialization of the creator engine may not take place until after the onWindowResize call,
     // you need to determine whether the canvas already exists before you can call the setCanvasSize method
