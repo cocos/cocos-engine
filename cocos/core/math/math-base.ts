fb0c6d6e3f339c31d516562b602bacb43110d01a
PatriceJiang <397136899@qq.com>
2021-08-05 11:37:18 +0800

    simplify

--- cocos/core/math/math-base.ts
+++ cocos/core/math/math-base.ts
@@ -2,15 +2,11 @@ import { JSB } from 'internal:constants';
 import { ValueType } from '../value-types/value-type';
 import { FloatArray } from './type-define';
 
-export const MATH_FLOAT_ARRAY = Float32Array;
+export const MATH_FLOAT_ARRAY = JSB ? Float32Array : Float64Array;
 
 export class MathBase extends ValueType {
-    public static createFloatArray (size: number) : FloatArray {
-        if (JSB) {
-            return new Float32Array(size);
-        } else {
-            return new Float64Array(size);
-        }
+    public static createFloatArray (size: number) {
+        return new MATH_FLOAT_ARRAY();
     }
 
     /**
