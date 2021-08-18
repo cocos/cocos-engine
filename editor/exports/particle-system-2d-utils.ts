8695ccc181d0171b5a3b47f198237010dd0e7213
shrinktofit <leslieleigh@outlook.com>
2021-08-18 16:04:30 +0800

    Match Creator's universal module resolution algorithm(2)

--- editor/exports/particle-system-2d-utils.ts
+++ editor/exports/particle-system-2d-utils.ts
@@ -1,4 +1,4 @@
 export { PNGReader } from '../../cocos/particle-2d/png-reader';
 export { TiffReader } from '../../cocos/particle-2d/tiff-reader';
-export { default as codec } from '../../external/compression/ZipUtils';
+export { default as codec } from '../../external/compression/ZipUtils.js';
 export { getImageFormatByData, ImageFormat } from '../../cocos/particle-2d/particle-system-2d';
