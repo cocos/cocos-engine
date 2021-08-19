d120ba3ace4d547f872b236092624e03964b4dff
zhakesi <liqiao@cocos.com>
2021-08-19 15:17:33 +0800

    fix:root node scene has no rts info. (#9167)
    * fix:root node scene has no rts info.
    
    * add lpos lrot lscale attributes for scene

---
+++ tests/core/node.test.ts
@@ -0,0 +1,17 @@
+import { Node, Scene} from "../../cocos/core/scene-graph"
+import {Vec3} from "../../cocos/core/math"
+
+describe(`Node`, () => {
+    test('@inverseTransformPoint', () => {
+        let scene = new Scene('temp');
+        let parentNode = new Node();
+        let subNode = new Node();
+        parentNode.setPosition(20, -30, 100);
+        subNode.setPosition(55, 35, 22);
+        parentNode.parent = scene;
+        subNode.parent = parentNode;
+        let p = new Vec3(100, 200, 0);
+        subNode.inverseTransformPoint(p, p);
+        expect(p).toStrictEqual(new Vec3(25, 195, -122));
+    });
+});
