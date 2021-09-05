85823efa882aee1514b7df1c42a1cb1722cfc33a
Zeqiang Li <zeqiang-li@outlook.com>
2021-09-05 12:24:02 +0800

    swapchain texture

--- cocos/core/gfx/webgpu/webgpu-swapchain.ts
+++ cocos/core/gfx/webgpu/webgpu-swapchain.ts
@@ -3,11 +3,17 @@ import {
     TextureInfo,
     IsPowerOf2,
     SwapchainInfo,
+    TextureType,
+    TextureFlagBit,
+    TextureUsageBit,
+    SampleCount,
+    Format,
 } from '../base/define';
 import { Texture } from '../base/texture';
 import { wgpuWasmModule } from './webgpu-utils';
 import { WebGPUDevice } from './webgpu-device';
 import { Swapchain } from '../base/swapchain';
+import { WebGPUTexture } from './webgpu-texture';
 
 export class WebGPUSwapchain extends Swapchain {
     private _nativeSwapchain;
@@ -25,6 +31,42 @@ export class WebGPUSwapchain extends Swapchain {
 
         const nativeDevice = wgpuWasmModule.nativeDevice;
         this._nativeSwapchain = nativeDevice.createSwapchain(swapchainInfo);
+
+        this._colorTexture = new WebGPUTexture();
+        const colorTex = this._colorTexture as WebGPUTexture;
+        colorTex.swapchain = this;
+        const swapchainColorTexInfo = new TextureInfo(
+            TextureType.TEX1D,
+            TextureUsageBit.COLOR_ATTACHMENT,
+            Format.BGRA8,
+            info.width,
+            info.height,
+            TextureFlagBit.NONE,
+            1,
+            1,
+            SampleCount.ONE,
+            1,
+            0,
+        );
+        colorTex.initialize(swapchainColorTexInfo);
+
+        this._depthStencilTexture = new WebGPUTexture();
+        const dsTex = this._depthStencilTexture as WebGPUTexture;
+        dsTex.swapchain = this;
+        const swapchainDSTextureInfo = new TextureInfo(
+            TextureType.TEX1D,
+            TextureUsageBit.DEPTH_STENCIL_ATTACHMENT,
+            Format.DEPTH_STENCIL,
+            info.width,
+            info.height,
+            TextureFlagBit.NONE,
+            1,
+            1,
+            SampleCount.ONE,
+            1,
+            0,
+        );
+        dsTex.initialize(swapchainDSTextureInfo);
     }
 
     public resize (width: number, height: number): void {
--- cocos/core/gfx/webgpu/webgpu-texture.ts
+++ cocos/core/gfx/webgpu/webgpu-texture.ts
@@ -1,22 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import {
     FormatSurfaceSize,
     TextureInfo,
     IsPowerOf2,
     TextureViewInfo,
+    Format,
 } from '../base/define';
 import { Texture } from '../base/texture';
 import { wgpuWasmModule } from './webgpu-utils';
 import { WebGPUDevice } from './webgpu-device';
+import { WebGPUSwapchain } from './webgpu-swapchain';
 
 export class WebGPUTexture extends Texture {
     private _nativeTexture;
+    private _swapchain;
 
-    get nativeTexture () {
-        return this._nativeTexture;
+    set swapchain (swapchain: WebGPUSwapchain) {
+        this._swapchain = swapchain;
     }
+
     public initialize (info: TextureInfo | TextureViewInfo): boolean {
         const nativeDevice = (this._device as WebGPUDevice).nativeDevice();
         if ('texture' in info) {
+            this._type = info.type;
+            this._format = info.format;
+            this._layerCount = info.layerCount;
+            this._levelCount = info.layerCount;
+
             const texViewInfo = new wgpuWasmModule.TextureViewInfoInstance();
             texViewInfo.setTexture((info.texture as WebGPUTexture).nativeTexture().getThis());
             texViewInfo.setType(info.type);
@@ -27,18 +37,34 @@ export class WebGPUTexture extends Texture {
             texViewInfo.setLayerCount(info.layerCount);
             this._nativeTexture = nativeDevice.createTexture(texViewInfo);
         } else {
-            const texInfo = new wgpuWasmModule.TextureInfoInstance();
-            texInfo.type = info.type;
-            texInfo.usage = info.usage;
-            texInfo.format = info.format;
-            texInfo.width = info.width;
-            texInfo.height = info.height;
-            texInfo.flags = info.flags;
-            texInfo.layerCount = info.layerCount;
-            texInfo.levelCount = info.levelCount;
-            texInfo.samples = info.samples;
-            texInfo.depth = info.depth;
-            this._nativeTexture = nativeDevice.createTexture(texInfo);
+            this._type = info.type;
+            this._format = info.format;
+            this._width = info.width;
+            this._height = info.height;
+            this._usage = info.usage;
+            this._flags = info.flags;
+            this._layerCount = info.layerCount;
+            this._levelCount = info.levelCount;
+            this._samples = info.samples;
+            this._depth = info.depth;
+
+            if (this._swapchain) {
+                this._nativeTexture = info.format === Format.DEPTH || info.format === Format.DEPTH_STENCIL
+                    ? this._swapchain.nativeSwapchain.getDepthStencilTexture() : this._swapchain.nativeSwapchain.getColorTexture();
+            } else {
+                const texInfo = new wgpuWasmModule.TextureInfoInstance();
+                texInfo.type = info.type;
+                texInfo.usage = info.usage;
+                texInfo.format = info.format;
+                texInfo.width = info.width;
+                texInfo.height = info.height;
+                texInfo.flags = info.flags;
+                texInfo.layerCount = info.layerCount;
+                texInfo.levelCount = info.levelCount;
+                texInfo.samples = info.samples;
+                texInfo.depth = info.depth;
+                this._nativeTexture = nativeDevice.createTexture(texInfo);
+            }
         }
 
         return true;
