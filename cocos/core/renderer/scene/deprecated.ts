40a40a00ef9a6642bcef0bb4e976b8c43ed3e0d9
troublemaker52025 <troublemaker52025@outlook.com>
2021-09-06 18:48:26 +0800

    Remove shadow.autoAdapt property for v3.3.1 (#9286)
    * Remove shadow.autoAdapt property for v3.3.1
    
    * update

--- cocos/core/renderer/scene/deprecated.ts
+++ cocos/core/renderer/scene/deprecated.ts
@@ -29,6 +29,7 @@ import { Layers } from '../../scene-graph/layers';
 import { legacyCC } from '../../global-exports';
 import { Pass } from '../core/pass';
 import { Camera } from './camera';
+import { Shadows } from './shadows';
 
 removeProperty(RenderScene.prototype, 'RenderScene.prototype', [
     { name: 'raycastUI2DNode' },
@@ -152,5 +153,23 @@ replaceProperty(Pass.prototype, 'Pass.prototype', [
 removeProperty(Camera.prototype, 'Camera.prototype', [
     {
         name: 'getSplitFrustum',
+    },
+]);
+
+removeProperty(Shadows.prototype, 'Shadows.prototype', [
+    {
+        name: 'aspect',
+    },
+    {
+        name: 'selfShadow',
+    },
+    {
+        name: 'linear',
+    },
+    {
+        name: 'packing',
+    },
+    {
+        name: 'autoAdapt',
     },
 ]);
