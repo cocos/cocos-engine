c412d3a81592bc6ce29024088043bc5e9be250d4
PP <chinese1236@qq.com>
2021-09-07 14:10:04 +0800

    support touch manager on PAL (#9281)
    * support touch manager
    
    * update inputmanager
    
    * update event manager
    
    * optimize code

--- cocos/core/platform/event-manager/input-manager.ts
+++ cocos/core/platform/event-manager/input-manager.ts
@@ -29,41 +29,32 @@
  * @hidden
  */
 
-import { AccelerometerInputEvent, input, MouseInputEvent, MouseWheelInputEvent, TouchInputEvent, KeyboardInputEvent } from 'pal/input';
+import { AccelerometerInputEvent, input, MouseInputEvent, MouseWheelInputEvent, TouchInputEvent, KeyboardInputEvent, MouseInputSource, TouchData } from 'pal/input';
 import { Vec2 } from '../../math/index';
 import { macro } from '../macro';
 import { eventManager } from './event-manager';
 import { EventAcceleration, EventKeyboard, EventMouse, EventTouch } from './events';
 import { Touch } from './touch';
 import { legacyCC } from '../../global-exports';
-import { logID } from '../debug';
 import { Acceleration } from './acceleration';
 import { SystemEventType } from './event-enum';
-
-const TOUCH_TIMEOUT = macro.TOUCH_TIMEOUT;
-
-const _vec2 = new Vec2();
-const _preLocation = new Vec2();
+import { touchManager } from '../../../../pal/input/touch-manager';
 
 interface IView {
     _devicePixelRatio: number;
 }
 
+const pointerEventTypeMap = {
+    [SystemEventType.MOUSE_DOWN]: SystemEventType.TOUCH_START,
+    [SystemEventType.MOUSE_MOVE]: SystemEventType.TOUCH_MOVE,
+    [SystemEventType.MOUSE_UP]: SystemEventType.TOUCH_END,
+};
+
 /**
  *  This class manages all events of input. include: touch, mouse, accelerometer, keyboard
  */
 class InputManager {
-    private _preTouchPoint = new Vec2();
     private _prevMousePoint = new Vec2();
-
-    private _preTouchPool: Touch[] = [];
-    private _preTouchPoolPointer = 0;
-
-    private _touches: Touch[] = [];
-    private _touchesIntegerDict: { [index: number]: number | undefined; } = { };
-
-    private _indexBitsUsed = 0;
-    private _maxTouches = 8;
 
     // TODO: remove this property
     private _glView: IView | null = null;
@@ -110,25 +101,25 @@ class InputManager {
 
     // #region Mouse Handle
     private _dispatchMouseEvent (inputEvent: MouseInputEvent) {
+        let touchInputEvent: TouchInputEvent;
         let mouseEvent: EventMouse;
-        let touch: Touch;
         switch (inputEvent.type) {
         case SystemEventType.MOUSE_DOWN:
             mouseEvent = this._getMouseEvent(inputEvent);
-            touch = this._getTouch(inputEvent);
-            this._handleTouchesStart([touch]);
+            touchInputEvent = this._simulateTouchInputEvent(inputEvent);
+            this._handleTouchesStart(touchInputEvent);
             eventManager.dispatchEvent(mouseEvent);
             break;
         case SystemEventType.MOUSE_MOVE:
             mouseEvent = this._getMouseEvent(inputEvent);
-            touch = this._getTouch(inputEvent);
-            this._handleTouchesMove([touch]);
+            touchInputEvent = this._simulateTouchInputEvent(inputEvent);
+            this._handleTouchesMove(touchInputEvent);
             eventManager.dispatchEvent(mouseEvent);
             break;
         case SystemEventType.MOUSE_UP:
             mouseEvent = this._getMouseEvent(inputEvent);
-            touch = this._getTouch(inputEvent);
-            this._handleTouchesEnd([touch]);
+            touchInputEvent = this._simulateTouchInputEvent(inputEvent);
+            this._handleTouchesEnd(touchInputEvent);
             eventManager.dispatchEvent(mouseEvent);
             break;
         case SystemEventType.MOUSE_WHEEL:
@@ -144,166 +135,114 @@ class InputManager {
 
     // #region Touch Handle
     private _dispatchTouchEvent (inputEvent: TouchInputEvent) {
-        const touchList = this._getTouchList(inputEvent);
         switch (inputEvent.type) {
         case SystemEventType.TOUCH_START:
-            this._handleTouchesStart(touchList);
+            this._handleTouchesStart(inputEvent);
             break;
         case SystemEventType.TOUCH_MOVE:
-            this._handleTouchesMove(touchList);
+            this._handleTouchesMove(inputEvent);
             break;
         case SystemEventType.TOUCH_END:
-            this._handleTouchesEnd(touchList);
+            this._handleTouchesEnd(inputEvent);
             break;
         case SystemEventType.TOUCH_CANCEL:
-            this._handleTouchesCancel(touchList);
+            this._handleTouchesCancel(inputEvent);
             break;
         default:
             break;
         }
     }
 
-    private _handleTouchesStart (touches: Touch[]) {
+    private _handleTouchesStart (inputEvent: TouchInputEvent) {
         const handleTouches: Touch[] = [];
-        const locTouchIntDict = this._touchesIntegerDict;
-        for (let i = 0; i < touches.length; ++i) {
-            const touch = touches[i];
-            const touchID = touch.getID();
+        const changedTouches = inputEvent.changedTouches;
+        const dpr = this._getViewPixelRatio();
+        for (let i = 0; i < changedTouches.length; ++i) {
+            const changedTouch = changedTouches[i];
+            const touchID = changedTouch.identifier;
             if (touchID === null) {
                 continue;
             }
-            const index = locTouchIntDict[touchID];
-            if (index === undefined) {
-                const unusedIndex = this._getUnUsedIndex();
-                if (unusedIndex === -1) {
-                    logID(2300, unusedIndex);
-                    continue;
-                }
-                // curTouch = this._touches[unusedIndex] = touch;
-                touch.getLocation(_vec2);
-                const curTouch = new Touch(_vec2.x, _vec2.y, touchID);
-                this._touches[unusedIndex] = curTouch;
-                touch.getPreviousLocation(_vec2);
-                curTouch.setPrevPoint(_vec2);
-                locTouchIntDict[touchID] = unusedIndex;
-                handleTouches.push(curTouch);
+
+            const touch = touchManager.createTouch(touchID, changedTouch.x * dpr, changedTouch.y * dpr);
+            if (!touch) {
+                continue;
+            }
+            handleTouches.push(touch);
+            if (!macro.ENABLE_MULTI_TOUCH) {
+                break;
             }
         }
         if (handleTouches.length > 0) {
-            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_START, macro.ENABLE_MULTI_TOUCH ? this._getUsefulTouches() : handleTouches);
+            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_START, macro.ENABLE_MULTI_TOUCH ? touchManager.getAllTouches() : handleTouches);
             eventManager.dispatchEvent(touchEvent);
         }
     }
 
-    private _handleTouchesMove (touches: Touch[]) {
+    private _handleTouchesMove (inputEvent: TouchInputEvent) {
         const handleTouches: Touch[] = [];
-        const locTouches = this._touches;
-        for (let i = 0; i < touches.length; ++i) {
-            const touch = touches[i];
-            const touchID = touch.getID();
+        const changedTouches = inputEvent.changedTouches;
+        const dpr = this._getViewPixelRatio();
+        for (let i = 0; i < changedTouches.length; ++i) {
+            const changedTouch = changedTouches[i];
+            const touchID = changedTouch.identifier;
             if (touchID === null) {
                 continue;
             }
-            const index = this._touchesIntegerDict[touchID];
-            if (index === undefined) {
-                // cc.log("if the index doesn't exist, it is an error");
+            touchManager.updateTouch(touchID, changedTouch.x * dpr, changedTouch.y * dpr);
+            const touch = touchManager.getTouch(touchID);
+            if (!touch) {
                 continue;
             }
-            if (locTouches[index]) {
-                touch.getLocation(_vec2);
-                locTouches[index].setPoint(_vec2);
-                touch.getPreviousLocation(_vec2);
-                locTouches[index].setPrevPoint(_vec2);
-                handleTouches.push(locTouches[index]);
+            handleTouches.push(touch);
+            if (!macro.ENABLE_MULTI_TOUCH) {
+                break;
             }
         }
         if (handleTouches.length > 0) {
-            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_MOVE, macro.ENABLE_MULTI_TOUCH ? this._getUsefulTouches() : handleTouches);
+            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_MOVE, macro.ENABLE_MULTI_TOUCH ? touchManager.getAllTouches() : handleTouches);
             eventManager.dispatchEvent(touchEvent);
         }
     }
 
-    private _handleTouchesEnd (touches: Touch[]) {
-        const handleTouches = this.getSetOfTouchesEndOrCancel(touches);
+    private _handleTouchesEnd (inputEvent: TouchInputEvent) {
+        const handleTouches = this._getSetOfTouchesEndOrCancel(inputEvent);
         if (handleTouches.length > 0) {
-            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_END, macro.ENABLE_MULTI_TOUCH ? this._getUsefulTouches() : handleTouches);
+            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_END, macro.ENABLE_MULTI_TOUCH ? touchManager.getAllTouches() : handleTouches);
             eventManager.dispatchEvent(touchEvent);
         }
-        this._preTouchPool.length = 0;
     }
 
-    private _handleTouchesCancel (touches: Touch[]) {
-        const handleTouches = this.getSetOfTouchesEndOrCancel(touches);
+    private _handleTouchesCancel (inputEvent: TouchInputEvent) {
+        const handleTouches = this._getSetOfTouchesEndOrCancel(inputEvent);
         if (handleTouches.length > 0) {
-            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_CANCEL, macro.ENABLE_MULTI_TOUCH ? this._getUsefulTouches() : handleTouches);
+            const touchEvent = new EventTouch(handleTouches, false, SystemEventType.TOUCH_CANCEL, macro.ENABLE_MULTI_TOUCH ? touchManager.getAllTouches() : handleTouches);
             eventManager.dispatchEvent(touchEvent);
         }
-        this._preTouchPool.length = 0;
     }
 
-    public getSetOfTouchesEndOrCancel (touches: Touch[]) {
+    private _getSetOfTouchesEndOrCancel (inputEvent: TouchInputEvent) {
         const handleTouches: Touch[] = [];
-        const locTouches = this._touches;
-        const locTouchesIntDict = this._touchesIntegerDict;
-        for (let i = 0; i < touches.length; ++i) {
-            const touch = touches[i];
-            const touchID = touch.getID();
+        const changedTouches = inputEvent.changedTouches;
+        const dpr = this._getViewPixelRatio();
+        for (let i = 0; i < changedTouches.length; ++i) {
+            const changedTouch = changedTouches[i];
+            const touchID = changedTouch.identifier;
             if (touchID === null) {
                 continue;
             }
-            const index = locTouchesIntDict[touchID];
-            if (index === undefined) {
-                // cc.log("if the index doesn't exist, it is an error");
+            touchManager.updateTouch(touchID, changedTouch.x * dpr, changedTouch.y * dpr);
+            const touch = touchManager.getTouch(touchID);
+            if (!touch) {
                 continue;
             }
-            if (locTouches[index]) {
-                touch.getLocation(_vec2);
-                locTouches[index].setPoint(_vec2);
-                touch.getPreviousLocation(_vec2);
-                locTouches[index].setPrevPoint(_vec2);
-                handleTouches.push(locTouches[index]);
-                this._removeUsedIndexBit(index);
-                delete locTouchesIntDict[touchID];
+            handleTouches.push(touch);
+            touchManager.releaseTouch(touchID);
+            if (!macro.ENABLE_MULTI_TOUCH) {
+                break;
             }
         }
         return handleTouches;
-    }
-
-    private _getPreTouch (touch: Touch) {
-        let preTouch: Touch | null = null;
-        const locPreTouchPool = this._preTouchPool;
-        const id = touch.getID();
-        for (let i = locPreTouchPool.length - 1; i >= 0; i--) {
-            if (locPreTouchPool[i].getID() === id) {
-                preTouch = locPreTouchPool[i];
-                break;
-            }
-        }
-        if (!preTouch) {
-            preTouch = touch;
-        }
-        return preTouch;
-    }
-
-    private _setPreTouch (touch: Touch) {
-        let find = false;
-        const locPreTouchPool = this._preTouchPool;
-        const id = touch.getID();
-        for (let i = locPreTouchPool.length - 1; i >= 0; i--) {
-            if (locPreTouchPool[i].getID() === id) {
-                locPreTouchPool[i] = touch;
-                find = true;
-                break;
-            }
-        }
-        if (!find) {
-            if (locPreTouchPool.length <= 50) {
-                locPreTouchPool.push(touch);
-            } else {
-                locPreTouchPool[this._preTouchPoolPointer] = touch;
-                this._preTouchPoolPointer = (this._preTouchPoolPointer + 1) % 50;
-            }
-        }
     }
 
     // TODO: remove this private method
@@ -314,16 +253,20 @@ class InputManager {
         return this._glView ? this._glView._devicePixelRatio : 1;
     }
 
-    private _getTouch (inputEvent: MouseInputEvent): Touch {
-        const locPreTouch = this._preTouchPoint;
-        const pixelRatio = this._getViewPixelRatio();
-        const x = inputEvent.x * pixelRatio;
-        const y = inputEvent.y * pixelRatio;
-        const touch = new Touch(x, y, 0);
-        touch.setPrevPoint(locPreTouch.x, locPreTouch.y);
-        locPreTouch.x = x;
-        locPreTouch.y = y;
-        return touch;
+    private _simulateTouchInputEvent (mouseInputEvent: MouseInputEvent): TouchInputEvent {
+        const touchData: TouchData = {
+            identifier: 0,
+            x: mouseInputEvent.x,
+            y: mouseInputEvent.y,
+            force: 0,
+        };
+
+        const touchInputEvent: TouchInputEvent = {
+            type: pointerEventTypeMap[mouseInputEvent.type],
+            changedTouches: [touchData],
+            timestamp: mouseInputEvent.timestamp,
+        };
+        return touchInputEvent;
     }
 
     private _getMouseEvent (inputEvent: MouseInputEvent): EventMouse {
@@ -349,85 +292,6 @@ class InputManager {
             mouseEvent.movementY = inputEvent.movementY;
         }
         return mouseEvent;
-    }
-
-    private _getTouchList (inputEvent: TouchInputEvent) {
-        const touchList: Touch[] = [];
-        const locPreTouch = this._preTouchPoint;
-        const length = inputEvent.changedTouches.length;
-        const pixelRatio = this._getViewPixelRatio();
-        for (let i = 0; i < length; i++) {
-            const touchData = inputEvent.changedTouches[i];
-            const x = touchData.x * pixelRatio;
-            const y = touchData.y * pixelRatio;
-            // TODO: what if touchData.identifier is undefined
-            const touch = new Touch(x, y, touchData.identifier);
-            this._getPreTouch(touch).getLocation(_preLocation);
-            touch.setPrevPoint(_preLocation.x, _preLocation.y);
-            this._setPreTouch(touch);
-            // update previous location
-            locPreTouch.x = x;
-            locPreTouch.y = y;
-            touchList.push(touch);
-
-            if (!macro.ENABLE_MULTI_TOUCH) {
-                break;
-            }
-        }
-        return touchList;
-    }
-
-    private _getUnUsedIndex () {
-        let temp = this._indexBitsUsed;
-        const now = legacyCC.game.frameStartTime;
-
-        for (let i = 0; i < this._maxTouches; i++) {
-            if (!(temp & 0x00000001)) {
-                this._indexBitsUsed |= (1 << i);
-                return i;
-            } else {
-                const touch = this._touches[i];
-                if (now - touch.lastModified > TOUCH_TIMEOUT) {
-                    this._removeUsedIndexBit(i);
-                    const touchID = touch.getID();
-                    if (touchID !== null) {
-                        delete this._touchesIntegerDict[touchID];
-                    }
-                    return i;
-                }
-            }
-            temp >>= 1;
-        }
-
-        // all bits are used
-        return -1;
-    }
-
-    private _removeUsedIndexBit (index) {
-        if (index < 0 || index >= this._maxTouches) {
-            return;
-        }
-
-        let temp = 1 << index;
-        temp = ~temp;
-        this._indexBitsUsed &= temp;
-    }
-
-    private _getUsefulTouches () {
-        const touches: Touch[] = [];
-        const touchDict = this._touchesIntegerDict;
-        for (const id in touchDict) {
-            const index = parseInt(id);
-            const usedID = touchDict[index];
-            if (usedID === undefined || usedID === null) {
-                continue;
-            }
-
-            const touch = this._touches[usedID];
-            touches.push(touch);
-        }
-
-        return touches;
     }
     // #endregion Touch Handle
 
