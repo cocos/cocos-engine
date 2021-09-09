32ecb37d1d100fdb1120eb57ed52a15dd7ff5f0f
troublemaker52025 <troublemaker52025@outlook.com>
2021-09-09 13:48:14 +0800

    fix replceProperty error for v3.3.1 (#9306)
    * fix replceProperty error for v3.3.1
    
    * update

--- cocos/core/scene-graph/deprecated.ts
+++ cocos/core/scene-graph/deprecated.ts
@@ -275,17 +275,4 @@ if (EDITOR) {
     };
 }
 
-replaceProperty(SceneGlobals, 'SceneGlobals', [
-    {
-        name: 'autoAdapt',
-        targetName: 'fixedArea',
-        customGetter (this: SceneGlobals) {
-            return !this.shadows.fixedArea;
-        },
-        customSetter (this: SceneGlobals, value: boolean) {
-            this.shadows.fixedArea = !value;
-        },
-    },
-]);
-
 legacyCC.PrivateNode = PrivateNode;
