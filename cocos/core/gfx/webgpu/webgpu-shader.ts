87b58bfc42e2a3c1467335658ea60e5286cf917f
Zeqiang Li <zeqiang-li@outlook.com>
2021-09-10 17:54:15 +0800

    ready for cmdbuff

--- cocos/core/gfx/webgpu/webgpu-shader.ts
+++ cocos/core/gfx/webgpu/webgpu-shader.ts
@@ -1,54 +1,158 @@
 import { Shader } from '../base/shader';
-import { WebGPUCmdFuncCreateShader, WebGPUCmdFuncDestroyShader } from './webgpu-commands';
 import { WebGPUDevice } from './webgpu-device';
-import { IWebGPUGPUShader, IWebGPUGPUShaderStage } from './webgpu-gpu-objects';
-import { ShaderInfo } from '../base/define';
+import { Format, MemoryAccessBit, ShaderInfo, ShaderStageFlagBit, Type } from '../base/define';
+import { wgpuWasmModule } from './webgpu-utils';
 
 export class WebGPUShader extends Shader {
-    get gpuShader(): IWebGPUGPUShader {
-        return this._gpuShader!;
-    }
+    private _nativeShader;
 
-    private _gpuShader: IWebGPUGPUShader | null = null;
-
-    public initialize(info: ShaderInfo): boolean {
+    public initialize (info: ShaderInfo): boolean {
         this._name = info.name;
         this._stages = info.stages;
         this._attributes = info.attributes;
         this._blocks = info.blocks;
         this._samplers = info.samplers;
 
-        this._gpuShader = {
-            name: info.name,
-            blocks: info.blocks,
-            samplers: info.samplers,
+        const nativeDevice = wgpuWasmModule.nativeDevice;
+        const shaderInfo = new wgpuWasmModule.ShaderInfoInstance();
+        shaderInfo.name = info.name;
 
-            gpuStages: new Array<IWebGPUGPUShaderStage>(info.stages.length),
-            glProgram: null,
-            glInputs: [],
-            glUniforms: [],
-            glBlocks: [],
-            glSamplers: [],
-        };
-
-        for (let i = 0; i < info.stages.length; ++i) {
-            const stage = info.stages[i];
-            this._gpuShader.gpuStages[i] = {
-                type: stage.stage,
-                source: stage.source,
-                glShader: null,
-            };
+        const shaderStageList = new wgpuWasmModule.ShaderStageList();
+        for (let i = 0; i < info.stages.length; i++) {
+            const shaderStage = new wgpuWasmModule.ShaderStageInstance();
+            const stageName = ShaderStageFlagBit[info.stages[i].stage];
+            shaderStage.stage = wgpuWasmModule.ShaderStageFlags[stageName];
+            shaderStage.source = info.stages[i].source;
+            shaderStageList.push_back(shaderStage);
         }
+        shaderInfo.stages = shaderStageList;
 
-        WebGPUCmdFuncCreateShader(this._device as WebGPUDevice, this._gpuShader);
+        const attributeList = new wgpuWasmModule.AttributeList();
+        for (let i = 0; i < info.attributes.length; i++) {
+            const attribute = new wgpuWasmModule.AttributeInstance();
+            attribute.name = info.attributes[i].name;
 
+            const formatStr = Format[info.attributes[i].format];
+            attribute.format = wgpuWasmModule.Format[formatStr];
+
+            attribute.isNormalized = info.attributes[i].isNormalized;
+            attribute.stream = info.attributes[i].stream;
+            attribute.isInstanced = info.attributes[i].isInstanced;
+            attribute.location = info.attributes[i].location;
+
+            attributeList.push_back(attribute);
+        }
+        shaderInfo.attributes = attributeList;
+
+        const uniformBlockList = new wgpuWasmModule.UniformBlockList();
+        for (let i = 0; i < info.blocks.length; i++) {
+            const block = new wgpuWasmModule.UniformBlockInstance();
+            block.set = info.blocks[i].set;
+            block.binding = info.blocks[i].binding;
+            block.name = info.blocks[i].name;
+            for (let j = 0; j < info.blocks[i].members.length; j++) {
+                const uniformInfo = info.blocks[i].members[j];
+                const uniform = new wgpuWasmModule.UniformInstance();
+                uniform.name = uniformInfo.name;
+
+                const typeStr = Type[uniformInfo.type];
+                uniform.type = wgpuWasmModule.Type[typeStr];
+                block.members.push_back(uniform);
+            }
+            block.count = info.blocks[i].count;
+            uniformBlockList.push_back(block);
+        }
+        shaderInfo.blocks = uniformBlockList;
+
+        const uniformStorageBufferList = new wgpuWasmModule.UniformStorageBufferList();
+        for (let i = 0; i < info.buffers.length; i++) {
+            const buffer = new wgpuWasmModule.UniformStorageBufferInstance();
+            buffer.set = info.buffers[i].set;
+            buffer.binding = info.buffers[i].binding;
+            buffer.name = info.buffers[i].name;
+            buffer.count = info.buffers[i].count;
+
+            const memAccName = MemoryAccessBit[info.buffers[i].memoryAccess];
+            buffer.memoryAccess = wgpuWasmModule.MemoryAccess[memAccName];
+            uniformStorageBufferList.push_back(buffer);
+        }
+        shaderInfo.buffers = uniformStorageBufferList;
+
+        const uniformSamplerTextureList = new wgpuWasmModule.UniformSamplerTextureList();
+        for (let i = 0; i < info.samplerTextures.length; i++) {
+            const uniformSamplerTexture = new wgpuWasmModule.UniformSamplerTextureInstance();
+            uniformSamplerTexture.set = info.samplerTextures[i].set;
+            uniformSamplerTexture.binding = info.samplerTextures[i].binding;
+            uniformSamplerTexture.name = info.samplerTextures[i].name;
+            uniformSamplerTexture.count = info.samplerTextures[i].count;
+
+            const typeStr = Type[info.samplerTextures[i].type];
+            uniformSamplerTexture.type = wgpuWasmModule.Type[typeStr];
+            uniformSamplerTextureList.push_back(uniformSamplerTexture);
+        }
+        shaderInfo.samplerTextures = uniformSamplerTextureList;
+
+        const uniformSamplerList = new wgpuWasmModule.UniformSamplerList();
+        for (let i = 0; i < info.samplers.length; i++) {
+            const uniformSampler = new wgpuWasmModule.UniformSamplerInstance();
+            uniformSampler.set = info.samplers[i].set;
+            uniformSampler.binding = info.samplers[i].binding;
+            uniformSampler.name = info.samplers[i].name;
+            uniformSampler.count = info.samplers[i].count;
+
+            uniformSamplerList.push_back(uniformSampler);
+        }
+        shaderInfo.samplers = uniformSamplerList;
+
+        const uniformTextureList = new wgpuWasmModule.UniformTextureList();
+        for (let i = 0; i < info.textures.length; i++) {
+            const uniformTexture = new wgpuWasmModule.UniformTextureInstance();
+            uniformTexture.set = info.textures[i].set;
+            uniformTexture.binding = info.textures[i].binding;
+            uniformTexture.name = info.textures[i].name;
+            uniformTexture.count = info.textures[i].count;
+
+            const typeStr = Type[info.textures[i].type];
+            uniformTexture.type = wgpuWasmModule.Type[typeStr];
+            uniformTextureList.push_back(uniformTexture);
+        }
+        shaderInfo.textures = uniformTextureList;
+
+        const uniformStorageImageList = new wgpuWasmModule.UniformStorageImageList();
+        for (let i = 0; i < info.images.length; i++) {
+            const uniformStorageImage = new wgpuWasmModule.UniformStorageImageInstance();
+            uniformStorageImage.set = info.images[i].set;
+            uniformStorageImage.binding = info.images[i].binding;
+            uniformStorageImage.name = info.images[i].name;
+            uniformStorageImage.count = info.images[i].count;
+
+            const typeStr = Type[info.images[i].type];
+            uniformStorageImage.type = wgpuWasmModule.Type[typeStr];
+            const memAccStr = MemoryAccessBit[info.images[i].memoryAccess];
+            uniformStorageImage.memoryAccess = wgpuWasmModule.MemoryAccess[memAccStr];
+
+            uniformStorageImageList.push_back(uniformStorageImage);
+        }
+        shaderInfo.images = uniformStorageImageList;
+
+        const uniformInputAttachmentList = new wgpuWasmModule.UniformInputAttachmentList();
+        for (let i = 0; i < info.subpassInputs.length; i++) {
+            const uniformInputAttachment = new wgpuWasmModule.UniformInputAttachmentInstance();
+            uniformInputAttachment.set = info.subpassInputs[i].set;
+            uniformInputAttachment.binding = info.subpassInputs[i].binding;
+            uniformInputAttachment.name = info.subpassInputs[i].name;
+            uniformInputAttachment.count = info.subpassInputs[i].count;
+
+            uniformInputAttachmentList.push_back(uniformInputAttachment);
+        }
+        shaderInfo.subpassInputs = uniformInputAttachmentList;
+
+        this._nativeShader = nativeDevice.createShader(shaderInfo);
         return true;
     }
 
-    public destroy() {
-        if (this._gpuShader) {
-            WebGPUCmdFuncDestroyShader(this._device as WebGPUDevice, this._gpuShader);
-            this._gpuShader = null;
-        }
+    public destroy () {
+        this._nativeShader.destroy();
+        this._nativeShader.delete();
     }
 }
