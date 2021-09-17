f8db8848049e1560feedb811c466434604bc8eb9
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2021-09-17 10:29:05 +0800

    Expose blend algorithm to editor (#47)

---
+++ editor/exports/new-gen-anim.ts
@@ -0,0 +1,9 @@
+
+export {
+    blend1D,
+} from '../../cocos/core/animation/newgen-anim/blend-1d';
+
+export {
+    blendSimpleDirectional,
+    validateSimpleDirectionalSamples,
+} from '../../cocos/core/animation/newgen-anim/blend-2d';
