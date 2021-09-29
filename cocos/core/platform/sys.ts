6ce394e21ab9d446782a7f1b20ed31b7eb131509
PP_Pro <chinese1236@qq.com>
2021-09-29 22:58:20 +0800

    support sys.hasFeature and deprecate sys.capabilities

--- cocos/core/platform/sys.ts
+++ cocos/core/platform/sys.ts
@@ -33,11 +33,21 @@ import { screenAdapter } from 'pal/screen-adapter';
 import { legacyCC } from '../global-exports';
 import { Rect } from '../math/rect';
 import { warnID, log } from './debug';
-import { NetworkType, Language, OS, Platform, BrowserType } from '../../../pal/system-info/enum-type';
+import { NetworkType, Language, OS, Platform, BrowserType, Feature } from '../../../pal/system-info/enum-type';
 import { Vec2 } from '../math';
 
 const windowSize = screenAdapter.windowSize;
 const pixelRatio = systemInfo.pixelRatio;
+
+export declare namespace sys {
+    /**
+     * @en
+     * Platform related feature enum type.
+     * @zh
+     * 平台相关的特性枚举类型。
+     */
+    export type Feature = EnumAlias<typeof Feature>;
+}
 
 /**
  * @en A set of system related variables
@@ -45,6 +55,18 @@ const pixelRatio = systemInfo.pixelRatio;
  * @main
  */
 export const sys = {
+    Feature,
+
+    /**
+     * @en
+     * Returns if the specified platform related feature is supported.
+     * @zn
+     * 返回指定的平台相关的特性是否支持。
+     */
+    hasFeature (feature: sys.Feature): boolean {
+        return systemInfo.hasFeature(feature);
+    },
+
     /**
      * @en
      * Network type enumeration
@@ -176,12 +198,14 @@ export const sys = {
     /**
      * @en The capabilities of the current platform
      * @zh 当前平台的功能可用性
+     *
+     * @deprecated since v3.4.0, please use sys.hasFeature() instead.
      */
     capabilities: {
-        canvas: systemInfo.supportCapability.canvas,
-        opengl: systemInfo.supportCapability.gl,
-        webp: systemInfo.supportCapability.webp,
-        imageBitmap: systemInfo.supportCapability.imageBitmap,
+        canvas: systemInfo.hasFeature(Feature.CANVAS),
+        opengl: systemInfo.hasFeature(Feature.WEBGL),
+        webp: systemInfo.hasFeature(Feature.WEBP),
+        imageBitmap: systemInfo.hasFeature(Feature.IMAGE_BIT_MAP),
         // TODO: move into pal/input
         touches: false,
         mouse: false,
