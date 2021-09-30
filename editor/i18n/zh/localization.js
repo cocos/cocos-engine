0fd3a5a24e1ea0a611be6507b4da9605166fc76c
ChiaNing <sun.cloud.mo@gmail.com>
2021-09-30 17:35:05 +0800

    GPUDriven i18n add

--- editor/i18n/zh/localization.js
+++ editor/i18n/zh/localization.js
@@ -707,6 +707,10 @@ module.exports = {
             label: "用户界面",
             description: "用户界面支持。",
         },
+        gpu_driven: {
+            label: "GPU驱动",
+            description: "是否启用GPU驱动方案（暂时只对打包后的工程起效）",
+        },
         base_3d: {
             label: "基础 3D 功能",
             description: "常用于一般 3D 应用的工具与组件。",
