ab1db8f642b7bea8d2bc5a36b58e9a8bbaec9b90
lawrence-liuyue <32774732+lawrence-liuyue@users.noreply.github.com>
2021-10-12 23:38:39 +0800

    DeferredPipeline: enable shadowmap for main light (#9463)
    * DeferredPipeline: enable shadowmap for main light
    
    * Update deferred-pipeline-scene-data.ts

--- cocos/core/pipeline/deferred/lighting-stage.ts
+++ cocos/core/pipeline/deferred/lighting-stage.ts
@@ -256,6 +256,8 @@ export class LightingStage extends RenderStage {
         const framebuffer = deferredData.outputFrameBuffer;
         const renderPass = framebuffer.renderPass;
 
+        pipeline.pipelineUBO.updateShadowUBO(camera);
+
         cmdBuff.beginRenderPass(renderPass, framebuffer, this._renderArea,
             colors, camera.clearDepth, camera.clearStencil);
 
