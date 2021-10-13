c1236784224f4d6ec44985274c67327d2be87b5d
Myway3D <MSoft1115@hotmail.com>
2021-10-13 17:07:52 +0800

    revert render callback

--- cocos/core/pipeline/forward/forward-pipeline.ts
+++ cocos/core/pipeline/forward/forward-pipeline.ts
@@ -133,19 +133,10 @@ export class ForwardPipeline extends RenderPipeline {
             const camera = cameras[i];
             if (camera.scene) {
                 sceneCulling(this, camera);
-
-                for (let k = 0; k < RenderPipeline._renderCallbacks.length; ++k) {
-                    RenderPipeline._renderCallbacks[k].onPreRender(camera);
-                }
-
                 this._pipelineUBO.updateGlobalUBO(camera.window!);
                 this._pipelineUBO.updateCameraUBO(camera);
                 for (let j = 0; j < this._flows.length; j++) {
                     this._flows[j].render(camera);
-                }
-
-                for (let k = 0; k < RenderPipeline._renderCallbacks.length; ++k) {
-                    RenderPipeline._renderCallbacks[k].onPostRender(camera);
                 }
             }
         }
