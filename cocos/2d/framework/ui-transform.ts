6f1c083ba443e43435cb28e2fd9698926fa7b99e
ChiaNing <sun.cloud.mo@gmail.com>
2021-10-13 15:59:19 +0800

    fix renderData null bug (#9476)

--- cocos/2d/framework/ui-transform.ts
+++ cocos/2d/framework/ui-transform.ts
@@ -682,8 +682,8 @@ export class UITransform extends Component {
         const uiComp = this.node._uiProps.uiComp;
         if (uiComp) {
             uiComp.markForUpdateRenderData();
-            // @ts-expect-error hack for canRender is false
-            uiComp.renderData.vertDirty = true;
+            // @ts-expect-error hack for canRender is false // HACK,Need remove
+            if (uiComp.renderData) { uiComp.renderData.vertDirty = true; }
         }
     }
 
