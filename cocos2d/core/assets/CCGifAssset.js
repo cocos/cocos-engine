a60d4542af0d00afe561c94b7c235e2fb39b30c1
xyf-mac <xu_yanfeng@126.com>
2021-10-19 15:28:39 +0800

    add gif support

--- cocos2d/core/assets/CCGifAssset.js
+++ cocos2d/core/assets/CCGifAssset.js
@@ -6,11 +6,11 @@
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated engine source code (the "Software"), a limited,
-  worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
  to use Cocos Creator solely to develop games on your target platforms. You shall
-  not use Cocos Creator software for developing other software or tools that's
-  used for developing games. You are not granted to publish, distribute,
-  sublicense, and/or sell copies of Cocos Creator.
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
 
  The software or tools in this License Agreement are licensed, not sold.
  Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
@@ -24,22 +24,18 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-require('./CCAsset');
-require('./CCFont');
-require('./CCPrefab');
-require('./CCAudioClip');
-require('./CCVideoClip');
-require('./CCScripts');
-require('./CCSceneAsset');
-require('./CCSpriteFrame');
-require('./CCTexture2D');
-require('./CCRenderTexture');
-require('./CCTTFFont');
-require('./CCSpriteAtlas');
-require('./CCBitmapFont');
-require('./CCLabelAtlas');
-require('./CCTextAsset');
-require('./CCJsonAsset');
-require('./CCBufferAsset');
-require('./material');
-require('./CCGifAssset');
+const Asset = require('./CCAsset');
+
+var GifAsset = cc.Class({
+    name: 'cc.GifAsset',
+    extends: Asset,
+
+    ctor(){
+
+    },
+    properties:{
+
+    },
+});
+cc.GifAsset = GifAsset;
+module.exports = GifAsset;
