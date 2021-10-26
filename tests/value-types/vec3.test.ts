cedc47e0d4907e234d814621c703d2117466a1fd
Leslie Leigh <leslieleigh@outlook.com>
2021-10-26 16:00:14 +0800

    Update

--- tests/value-types/vec3.test.ts
+++ tests/value-types/vec3.test.ts
@@ -1,3 +1,42 @@
+import { toRadian } from '../../cocos/core/math/utils';
+import { Vec3 } from '../../cocos/core/math/vec3';
+
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
+});
+
+test('Angle', () => {
+    expect(Vec3.angle(
+        Vec3.UNIT_X,
+        new Vec3(1.0, Math.tan(toRadian(30.0))),
+    )).toBeCloseTo(toRadian(30.0));
+
+    expect(Vec3.angle(
+        Vec3.UNIT_X,
+        Vec3.UNIT_Z,
+    )).toBeCloseTo(toRadian(90.0));
+
+    expect(Vec3.angle(
+        Vec3.UNIT_X,
+        new Vec3(1.0, Math.tan(toRadian(30.0))),
+        false, // Assume normalized
+    )).toBeCloseTo(toRadian(30.0));
+
+    expect(Vec3.angle(
+        Vec3.UNIT_X,
+        Vec3.normalize(new Vec3(), new Vec3(1.0, Math.tan(toRadian(30.0)))),
+        true, // Assume normalized
+    )).toBeCloseTo(toRadian(30.0));
+
+    // TODO:
+    
+    // expect(Vec3.angle(
+    //     Vec3.ZERO,
+    //     new Vec3(1.0, Math.tan(toRadian(30.0))),
+    // )).toBeCloseTo(Math.PI);
+
+    // expect(Vec3.angle(
+    //     Vec3.ZERO,
+    //     Vec3.ZERO,
+    // )).toBeCloseTo(Math.PI);
 });
