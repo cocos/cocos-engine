c8bbda6277b0bae82a012ea9957ac7f33e016c7a
江战 <40414978+PatriceJiang@users.noreply.github.com>
2021-10-27 18:43:23 +0800

    fix se crash (#4012)

--- native/cocos/bindings/manual/jsb_cocos_manual.cpp
+++ native/cocos/bindings/manual/jsb_cocos_manual.cpp
@@ -631,6 +631,7 @@ static bool js_se_setExceptionCallback(se::State &s) { // NOLINT(readability-ide
     }
 
     se::ScriptEngine::getInstance()->setJSExceptionCallback([objFunc](const char *location, const char *message, const char *stack) {
+        se::AutoHandleScope scope;
         se::ValueArray jsArgs;
         jsArgs.resize(3);
         jsArgs[0] = se::Value(location);
