6c84515909c56ef7020ed569145940c9260e8d84
zhangmingzhen <mingzhen.zhang@cocos.com>
2021-10-30 11:08:07 +0800

    ignore grammar error

--- tests/core/node.test.ts
+++ tests/core/node.test.ts
@@ -60,10 +60,10 @@ describe(`Node`, () => {
         node.active = null;
         expect(node.active).toBe(false);
 
-        // // @ts-expect-error
-        // node.active = '' as boolean;
-        // // @ts-expect-error
-        // node.active = 0 as boolean;
+        // @ts-expect-error
+        node.active = '' as boolean;
+        // @ts-expect-error
+        node.active = 0 as boolean;
 
         node.active = true;
         expect(node.active).toBe(true);
