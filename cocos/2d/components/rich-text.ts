2038de16aeaea4c8879a194834f6f794c19ed34e
zhangmingzhen <mingzhen.zhang@cocos.com>
2021-11-02 18:51:15 +0800

    Add '\n' toughly, to be optimized.

--- cocos/2d/components/rich-text.ts
+++ cocos/2d/components/rich-text.ts
@@ -451,8 +451,6 @@ export class RichText extends UIComponent {
     protected _lineOffsetX = 0;
     protected _updateRichTextStatus: () => void;
 
-    protected _splitLongStrArr: Array<string> = [];
-
     constructor () {
         super();
         if (EDITOR) {
@@ -549,28 +547,40 @@ export class RichText extends UIComponent {
     }
 
     protected splitLongStringIntoSeveralShortString (styleIndex: number, s: string) {
-        const labelSize = this._calculateSize(styleIndex, s);
+        const splitLongStrArr: string[] = [];
 
-        if (labelSize.x >= 2048) {
-            let tempString = s.substring(0, s.length / 2);
-            let tempSize = this._calculateSize(styleIndex, tempString);
-            while (tempSize.x >= 2048) {
-                tempString = tempString.substring(0, tempString.length / 2);
-                tempSize = this._calculateSize(styleIndex, tempString);
-            }
-            //console.log(`tempString = ${tempString}, tempSize = ${tempSize.x}`);
+        //这里输入的字符串还没有解析用户的'\n'
+        const userMultilineString: string[] = s.split('\n');
+        for (let i = 0; i < userMultilineString.length; i++) {
+            const labelSize = this._calculateSize(styleIndex, userMultilineString[i]);
+            if (labelSize.x < 2048) {
+                splitLongStrArr.push(userMultilineString[i]);
+            } else {
+                const longStr = userMultilineString[i];
+                let tempString = longStr.substring(0, longStr.length / 2);
+                let tempSize = this._calculateSize(styleIndex, tempString);
+                let tempLength = tempString.length;
+                while (tempSize.x >= 2048) {
+                    tempString = tempString.substring(0, tempString.length / 2);
+                    tempSize = this._calculateSize(styleIndex, tempString);
+                    tempLength = tempString.length;
+                }
 
-            let tempIndex = 0;
-            while (tempIndex < s.length) {
-                const thisLength = Math.min(tempSize.x, s.length - tempIndex);
-                this._splitLongStrArr.push(s.substring(tempIndex, thisLength));
-                tempIndex += thisLength;
-            }
-
-            for (let i = 0; i < this._splitLongStrArr.length; i++) {
-                console.log(this._splitLongStrArr[i]);
+                let tempIndex = 0;
+                while (tempIndex < longStr.length) {
+                    const thisLength = Math.min(tempLength, longStr.length - tempIndex);
+                    //substring return characters from indexA to indexB(not contain indexB)
+                    splitLongStrArr.push(longStr.substring(tempIndex, tempIndex + thisLength));
+                    tempIndex += thisLength;
+                }
             }
         }
+
+        for (let i = 0; i < splitLongStrArr.length; i++) {
+            console.log(splitLongStrArr[i]);
+        }
+
+        return splitLongStrArr;
     }
 
     protected _measureText (styleIndex: number, string?: string) {
@@ -904,7 +914,7 @@ export class RichText extends UIComponent {
 
         for (let i = 0; i < this._textArray.length; ++i) {
             const richTextElement = this._textArray[i];
-            const text = richTextElement.text;
+            let text = richTextElement.text;
             if (text === undefined) {
                 continue;
             }
@@ -921,9 +931,8 @@ export class RichText extends UIComponent {
                 }
             }
 
-            this.splitLongStringIntoSeveralShortString(i, text);
-
-            //在split分行展示
+            const splitArr: string[] = this.splitLongStringIntoSeveralShortString(i, text);
+            text = splitArr.join('\n');
 
             const multilineTexts = text.split('\n');
 
