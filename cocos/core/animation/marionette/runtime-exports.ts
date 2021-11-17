05484abc30227e89b5c04096590b7ee73f7bb9fb
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2021-11-17 15:55:03 +0800

    Rename animation.StateStatus -> animation.MotionStateStatus (#9675)

--- cocos/core/animation/marionette/runtime-exports.ts
+++ cocos/core/animation/marionette/runtime-exports.ts
@@ -4,7 +4,14 @@ import './animation-blend-1d';
 import './animation-blend-2d';
 import './animation-blend-direct';
 
+import type { MotionStateStatus } from './animation-controller';
+
 export type { AnimationGraphRunTime } from './animation-graph';
 export { AnimationController } from './animation-controller';
-export type { ClipStatus, TransitionStatus, StateStatus } from './animation-controller';
+export type { ClipStatus, TransitionStatus, MotionStateStatus } from './animation-controller';
 export { StateMachineComponent } from './state-machine-component';
+
+/**
+ * @deprecated Since v3.4-preview-2. Use `MotionStateStatus` instead.
+ */
+export type StateStatus = MotionStateStatus;
--- cocos/core/animation/marionette/state-machine-component.ts
+++ cocos/core/animation/marionette/state-machine-component.ts
@@ -1,40 +1,39 @@
 import { ccclass } from 'cc.decorator';
 import { CLASS_NAME_PREFIX_ANIM } from '../define';
-import type { AnimationController, StateStatus } from './animation-controller';
+import type { AnimationController, MotionStateStatus } from './animation-controller';
 
 @ccclass(`${CLASS_NAME_PREFIX_ANIM}StateMachineComponent`)
 export class StateMachineComponent {
     /**
      * Called when a motion state right after it entered.
      * @param controller The animation controller it within.
-     * @param stateStatus The status of the motion.
+     * @param motionStateStatus The status of the motion.
      */
-    public onMotionStateEnter (controller: AnimationController, stateStatus: Readonly<StateStatus>): void {
+    public onMotionStateEnter (controller: AnimationController, motionStateStatus: Readonly<MotionStateStatus>): void {
         // Can be overrode
     }
 
     /**
      * Called when a motion state is going to be exited.
      * @param controller The animation controller it within.
-     * @param stateStatus The status of the motion.
+     * @param motionStateStatus The status of the motion.
      */
-    public onMotionStateExit (controller: AnimationController, stateStatus: Readonly<StateStatus>): void {
+    public onMotionStateExit (controller: AnimationController, motionStateStatus: Readonly<MotionStateStatus>): void {
         // Can be overrode
     }
 
     /**
      * Called when a motion state updated except for the first and last frame.
      * @param controller The animation controller it within.
-     * @param stateStatus The status of the motion.
+     * @param motionStateStatus The status of the motion.
      */
-    public onMotionStateUpdate (controller: AnimationController, stateStatus: Readonly<StateStatus>): void {
+    public onMotionStateUpdate (controller: AnimationController, motionStateStatus: Readonly<MotionStateStatus>): void {
         // Can be overrode
     }
 
     /**
      * Called when a state machine right after it entered.
      * @param controller The animation controller it within.
-     * @param stateStatus The status of the motion.
      */
     public onStateMachineEnter (controller: AnimationController) {
         // Can be overrode
@@ -43,7 +42,6 @@ export class StateMachineComponent {
     /**
      * Called when a state machine right after it entered.
      * @param controller The animation controller it within.
-     * @param stateStatus The status of the motion.
      */
     public onStateMachineExit (controller: AnimationController) {
         // Can be overrode
