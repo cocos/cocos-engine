5ec9830a63e2a0dc8115f7d924d4a7cd88ffb952
fish9167 <674821954@qq.com>
2021-11-23 09:37:10 +0800

    Create interface-check-report.js

---
+++ .github/workflows/interface-check-report.js
@@ -0,0 +1,19 @@
+const ps = require('path');
+const fs = require('fs');
+
+const interfaceDiffPath = ps.join(__dirname, '../../interface-diff.txt');
+let reportContent = fs.readFileSync(interfaceDiffPath, 'utf8');
+
+if (reportContent.includes('@')) {
+    reportContent = reportContent.split('\n').slice(3).join('\n');
+    reportContent = `## Interface Check Report
+\`\`\`diff
+! WARNING this pull request has changed these public interface:
+${reportContent}
+\`\`\`
+`;
+} else {
+    reportContent = `## Interface Check Report
+This pull request does not change any public interface !`;
+}
+fs.writeFileSync(interfaceDiffPath, reportContent, 'utf8');
