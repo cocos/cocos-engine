ce1e8e6072e9b1ab8465632f928613eddc7e578d
troublemaker52025 <troublemaker52025@outlook.com>
2021-11-25 15:57:38 +0800

    RenderAdditiveLightQueue exclude transparent model for v3.4.0 (#4066)
    * RenderAdditiveLightQueue exclude transparent model for v3.4.0
    
    * update

--- native/cocos/renderer/pipeline/RenderAdditiveLightQueue.cpp
+++ native/cocos/renderer/pipeline/RenderAdditiveLightQueue.cpp
@@ -131,6 +131,10 @@ void RenderAdditiveLightQueue::gatherLightPasses(const scene::Camera *camera, gf
             const auto lightPassIdx = lightPassIndices[i];
             if (lightPassIdx == UINT_MAX) continue;
             const auto *pass          = subModel->getPass(lightPassIdx);
+            const bool  isTransparent = subModel->getPass(0)->getBlendState()->targets[0].blend;
+            if (isTransparent) {
+                continue;
+            }
             auto *      descriptorSet = subModel->getDescriptorSet();
             descriptorSet->bindBuffer(UBOForwardLight::BINDING, _firstLightBufferView);
             descriptorSet->update();
