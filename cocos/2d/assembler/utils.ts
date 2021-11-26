3673752872d3ed57f19a13e69d77ff4315b0eca7
ChiaNing <sun.cloud.mo@gmail.com>
2021-11-26 14:01:52 +0800

    Revert "fix mesh buffer cache update bug (#9734)"
    This reverts commit f1c8e2528ac935a0bb9743750ef5a451504bf6bb.

--- cocos/2d/assembler/utils.ts
+++ cocos/2d/assembler/utils.ts
@@ -101,7 +101,6 @@ export function updateCachedVertices (node: Node, renderData: RenderData, color:
         Color.toArray(vBuf, color, vertexOffset);
         vertexOffset += 4;
     }
-    buffer.setDirty();
 }
 
 export function fillMeshVertices3D (node: Node, renderer: IBatcher, renderData: RenderData, color: Color) {
@@ -215,5 +214,4 @@ export function updateCachedVerticesWithoutCalc3D (node: Node, renderData: Rende
         Color.toArray(vBuf, color, vertexOffset);
         vertexOffset += 4;
     }
-    buffer.setDirty();
 }
