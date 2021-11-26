fa0f40350314f6544b27554a59bd95d332b68151
Knox <knox.h@qq.com>
2021-11-26 16:31:58 +0800

    Fix onPaused naming error in video player code (#9733)
    * Fix onPaused naming error in video player code
    
    * add deprecated in video
    
    * refine code
    
    * refine code
    
    * refine code..

--- cocos/video/deprecated.ts
+++ cocos/video/deprecated.ts
@@ -24,6 +24,15 @@
  */
 
 /**
+ * @packageDocumentation
  * @hidden
  */
-export * from '../cocos/video';
+import { replaceProperty } from '../core/utils/x-deprecated';
+import { VideoPlayer } from './video-player';
+
+replaceProperty(VideoPlayer.prototype, 'VideoPlayer.prototype', [
+    {
+        name: 'onPasued',
+        newName: 'onPaused',
+    },
+]);
