75c9f61b022398d0c397531bd2cd6bfa75df6c72
minggo <nudtzxm@gmail.com>
2021-11-29 16:27:23 +0800

    add deserialization function

--- cocos/core/assets/texture-cube.jsb.ts
+++ cocos/core/assets/texture-cube.jsb.ts
@@ -30,8 +30,30 @@ import {
 } from '../data/utils/decorator-jsb-utils';
 import { legacyCC } from '../global-exports';
 import { Filter, PixelFormat, WrapMode } from './asset-enum';
+import { js } from '../utils/js';
 
 const textureCubeProto: any = jsb.TextureCube.prototype;
+interface ITextureCubeSerializeData {
+    base: string;
+    rgbe: boolean;
+    mipmaps: {
+        front: string;
+        back: string;
+        left: string;
+        right: string;
+        top: string;
+        bottom: string;
+    }[];
+}
+
+enum FaceIndex {
+    right = 0,
+    left = 1,
+    top = 2,
+    bottom = 3,
+    front = 4,
+    back = 5,
+}
 
 textureCubeProto.createNode = null!;
 
@@ -67,6 +89,82 @@ textureCubeProto._ctor = function () {
     // _initializerDefineProperty(_this, 'isRGBE', _descriptor$b, _assertThisInitialized(_this));
     // _initializerDefineProperty(_this, '_mipmaps', _descriptor2$7, _assertThisInitialized(_this));
 };
+
+function _forEachFace (mipmap, callback: (face, faceIndex) => void) {
+    callback(mipmap.front, FaceIndex.front);
+    callback(mipmap.back, FaceIndex.back);
+    callback(mipmap.left, FaceIndex.left);
+    callback(mipmap.right, FaceIndex.right);
+    callback(mipmap.top, FaceIndex.top);
+    callback(mipmap.bottom, FaceIndex.bottom);
+}
+
+Object.defineProperty(textureCubeProto, 'mipmaps', {
+    get () {
+        return this._mipmaps;
+    },
+    set (value) {
+        this._mipmaps = value;
+        this.setMipmaps(value);
+        this.mipmapLevel = this._mipmaps.length;
+        if (this._mipmaps.length > 0) {
+            const imageAsset = this._mipmaps[0].front;
+            this.reset({
+                width: imageAsset.width,
+                height: imageAsset.height,
+                format: imageAsset.format,
+                mipmapLevel: this._mipmaps.length,
+            });
+            this._mipmaps.forEach((mipmap, level) => {
+                _forEachFace(mipmap, (face, faceIndex) => {
+                    this.assignImage(face, level, faceIndex);
+                });
+            });
+        } else {
+            this.reset({
+                width: 0,
+                height: 0,
+                mipmapLevel: this._mipmaps.length,
+            });
+        }
+    }
+});
+
+Object.defineProperty(textureCubeProto, 'image', {
+    get () {
+        return this._mipmaps.length === 0 ? null : this._mipmaps[0];
+    },
+    set (value) {
+        this.mipmaps = value ? [value] : [];
+    }
+});
+
+textureCubeProto._deserialize = function (serializedData: ITextureCubeSerializeData, handle: any) {
+    const data = serializedData;
+    jsb.TextureBase.prototype._deserialize.call(this, data.base, handle);
+    this.isRGBE = data.rgbe;
+    this._mipmaps = new Array(data.mipmaps.length);
+    for (let i = 0; i < data.mipmaps.length; ++i) {
+        // Prevent resource load failed
+        this._mipmaps[i] = {
+            front: new jsb.ImageAsset(),
+            back: new jsb.ImageAsset(),
+            left: new jsb.ImageAsset(),
+            right: new jsb.ImageAsset(),
+            top: new jsb.ImageAsset(),
+            bottom: new jsb.ImageAsset(),
+        };
+        const mipmap = data.mipmaps[i];
+        const imageAssetClassId = js._getClassId(jsb.ImageAsset);
+        handle.result.push(this._mipmaps[i], `front`, mipmap.front, imageAssetClassId);
+        handle.result.push(this._mipmaps[i], `back`, mipmap.back, imageAssetClassId);
+        handle.result.push(this._mipmaps[i], `left`, mipmap.left, imageAssetClassId);
+        handle.result.push(this._mipmaps[i], `right`, mipmap.right, imageAssetClassId);
+        handle.result.push(this._mipmaps[i], `top`, mipmap.top, imageAssetClassId);
+        handle.result.push(this._mipmaps[i], `bottom`, mipmap.bottom, imageAssetClassId);
+    }
+    this.setMipmaps(this._mipmaps);
+}
 
 clsDecorator(TextureCube);
 
