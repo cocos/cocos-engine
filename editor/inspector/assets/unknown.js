b0df058cb2e41a067fb88f4afb447546a2320df4
dogodo-cc <741484865@qq.com>
2021-12-06 11:31:59 +0800

    style: delete useless code

--- editor/inspector/assets/unknown.js
+++ editor/inspector/assets/unknown.js
@@ -1,14 +1,11 @@
 'use strict';
-exports.update = exports.$ = exports.template = void 0;
 exports.template = '';
 exports.$ = {};
 /**
  * 自动渲染组件的方法
  * @param dump
  */
-async function update(infos) {
-    // @ts-ignore
-    const $panel = this;
-    // $panel.$.section.innerHTML = 'unknown';
+async function update() {
+
 }
 exports.update = update;
