0c430fdeffaef6338cc45b272bb7b9e6d36edc6f
Patrice Jiang <397136899@qq.com>
2021-12-08 15:59:56 +0800

    save]

--- platforms/native/engine/jsb-object.js
+++ platforms/native/engine/jsb-object.js
@@ -3,21 +3,21 @@
 
 const oldDestroy = jsb.CCObject.prototype.destroy;
 const oldDestroyImmediate = jsb.CCObject.prototype._destroyImmediate;
-const deferredDestroyObjects = [[],[]];
+const deferredDestroyObjects = [];
 let deferredArrayIndex = 0;
 
 jsb.CCObject.prototype.destroy = function() {
-    deferredDestroyObjects[deferredArrayIndex].push(this);
+    deferredDestroyObjects.push(this);
     oldDestroy.call(this);
 };
 
 
 jsb.CCObject._deferredDestroyReleaseObjects = function() {
-    deferredArrayIndex = (deferredArrayIndex + 1) % deferredDestroyObjects.length;
-    deferredDestroyObjects[deferredArrayIndex].length = 0;
+    jsb.CCObject.deferredDestroy();
+    deferredDestroyObjects.length = 0;
 };
 
 jsb.CCObject.prototype._destroyImmediate = function() {  
-    deferredDestroyObjects[deferredArrayIndex].push(this);
+    deferredDestroyObjects.push(this);
     oldDestroyImmediate.call(this);
 };
