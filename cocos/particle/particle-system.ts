2038419f918a8a4a2527a303d50050ffcc346b5c
zxx43 <zxx43@hotmail.com>
2021-12-17 11:12:43 +0800

    add burst restart

--- cocos/particle/particle-system.ts
+++ cocos/particle/particle-system.ts
@@ -894,6 +894,10 @@ export class ParticleSystem extends RenderableComponent {
 
         // if stop emit modify the refresh flag to true
         this._needRefresh = true;
+
+        for (const burst of this.bursts) {
+            burst.reset();
+        }
     }
 
     // remove all particles from current particle system.
