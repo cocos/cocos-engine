84e2046b19d26c63beb1af50a6d07c4713cb3afe
PP_Pro <chinese1236@qq.com>
2021-12-22 17:20:03 +0800

    publish beta6

--- scripts/build-engine/src/build-declarations/index.ts
+++ scripts/build-engine/src/build-declarations/index.ts
@@ -219,8 +219,8 @@ export async function build (options: {
 
         interfaceFilter.cullInterface({
             inputDts: indexOutputPath,
-            privateTag: 'private_cc',
-            deprecateTag: 'marked_as_engine_private',
+            privateTag: 'engine_internal',
+            deprecateTag: 'legacy_public',
             deprecateTip: 'since v3.5.0, this is an engine private interface that will be removed in the future.',
         });
     } catch (error) {
