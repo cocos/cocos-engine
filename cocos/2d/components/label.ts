6601d1d2de590953dffb33a17bf6515e628072bf
Zhang mingzhen <32831993+Zhmz@users.noreply.github.com>
2021-12-28 11:27:58 +0800

    Label string setter optimization (#9880)
    * remove rebundant onDisable
    
    * update label.string.setter and add unit test case
    
    Co-authored-by: zhangmingzhen <mingzhen.zhang@cocos.com>

--- cocos/2d/components/label.ts
+++ cocos/2d/components/label.ts
@@ -206,11 +206,12 @@ export class Label extends Renderable2D {
         return this._string;
     }
     set string (value) {
-        if (value) {
-            value += '';
-        } else {
+        if (value === null || value === undefined) {
             value = '';
+        } else {
+            value = value.toString();
         }
+
         if (this._string === value) {
             return;
         }
@@ -720,10 +721,6 @@ export class Label extends Renderable2D {
         }
 
         this._applyFontTexture();
-    }
-
-    public onDisable () {
-        super.onDisable();
     }
 
     public onDestroy () {
