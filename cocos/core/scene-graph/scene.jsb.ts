833aa736f4cf07b44fcd615df602f40b020388e9
dumganhar <dumganhar@gmail.com>
2021-12-28 14:43:58 +0800

    Invoke RenderScene.activate for JSB.

--- cocos/core/scene-graph/scene.jsb.ts
+++ cocos/core/scene-graph/scene.jsb.ts
@@ -154,9 +154,12 @@ sceneProto._activate = function (active: boolean) {
     // }
     legacyCC.director._nodeActivator.activateNode(this, active);
     // The test environment does not currently support the renderer
-    // if (!TEST) {
-    this._globals.activate();
-    // }
+    if (!TEST) {
+        this._globals.activate();
+        if (this._renderScene) {
+            this._renderScene.activate();
+        }
+    }
 };
 
 clsDecorator(Scene);
