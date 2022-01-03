4a5d9fe03ef44c7ab0c34b8eca631054cb4d4b53
pandamicro <lphuabin@gmail.com>
2022-01-03 23:08:55 +0800

    Apply suggestions from code review
    Co-authored-by: yufang.wu <2435731537@qq.com>

--- cocos/core/animation/marionette/animation-controller.ts
+++ cocos/core/animation/marionette/animation-controller.ts
@@ -20,17 +20,17 @@ export type {
  * When the controller starts, the animation graph is instantiated.
  * Then you may set variables or query the running statuses of the animation graph instance.
  * @zh
- * 动画控制器组件应用动画图到它所附加到的结点。
- * 当控制器开始时，动画图会被实例化。接着，可以设置动画图实例中的变量或查询动画图的运作状态。
+ * 将动画图应用到动画控制器组件所挂载的节点上。
+ * 当动画控制器开始运行时，动画图会被实例化。然后便可以设置动画图实例中的变量或者查询动画图的运行状况。
  */
 @ccclass('cc.animation.AnimationController')
 @menu('Animation/Animation Controller')
 export class AnimationController extends Component {
     /**
      * @zh
-     * 该控制器关联的动画图。
+     * 动画控制器所关联的动画图。
      * @en
-     * The animation graph this controller associate with.
+     * The animation graph associated with the animation controller.
      */
     @property(AnimationGraph)
     public graph: AnimationGraphRunTime | null = null;
@@ -49,7 +49,7 @@ export class AnimationController extends Component {
 
     /**
      * @zh 获取动画图中的所有变量。
-     * @en Gets variables of the animation graph.
+     * @en Gets all the variables in the animation graph.
      * @returns The iterator to the variables.
      * @example
      * ```ts
@@ -66,9 +66,9 @@ export class AnimationController extends Component {
 
     /**
      * @zh 设置动画图实例中变量的值。
-     * @en Sets value of the specified variable.
+     * @en Sets the value of the variable in the animation graph instance.
      * @param name 变量名称。
-     * @param value 要设置的值。
+     * @param value 设置变量的值。
      */
     public setValue (name: string, value: Value) {
         const { _graphEval: graphEval } = this;
@@ -78,7 +78,7 @@ export class AnimationController extends Component {
 
     /**
      * @zh 获取动画图实例中变量的值。
-     * @en Gets value of the specified variable.
+     * @en Gets the value of the variable in the animation graph instance.
      * @param name 变量名称。
      * @returns 变量当前的值。
      */
@@ -89,8 +89,8 @@ export class AnimationController extends Component {
     }
 
     /**
-     * @zh 获取动画图实例中当前动画状态的运作状态。
-     * @en Gets the status of the current motion state.
+     * @zh 获取动画图实例中当前状态的运行状况。
+     * @en Gets the running status of the current state in the animation graph instance.
      * @param layer 层级索引。（必须为 `0`）
      * @returns 当前的状态运作状态对象。
      */
@@ -101,8 +101,8 @@ export class AnimationController extends Component {
     }
 
     /**
-     * @zh 获取动画图实例中当前状态上包含的所有动画剪辑的运作状态。
-     * @en Gets the clip statuses contained in current motion state.
+     * @zh 获取动画图实例中当前状态上包含的所有动画剪辑的运行状况。
+     * @en Gets the running status of all the animation clips added on the current state in the animation graph instance.
      * @param layer 层级索引。（必须为 `0`）
      * @returns 到动画剪辑运作状态的迭代器。
      */
@@ -113,10 +113,10 @@ export class AnimationController extends Component {
     }
 
     /**
-     * @zh 获取动画图实例中当前正在进行的过渡的运作状态。
+     * @zh 获取动画图实例中当前正在进行的过渡的运行状况。
      * @en Gets the status of the current transition.
      * @param layer 层级索引。（必须为 `0`）
-     * @returns 当前正在进行的过渡，若未在进行过渡，则返回 `null`。
+     * @returns 当前正在进行的过渡，若没有进行任何过渡，则返回 `null`。
      */
     public getCurrentTransition (layer: number) {
         const { _graphEval: graphEval } = this;
@@ -125,8 +125,8 @@ export class AnimationController extends Component {
     }
 
     /**
-     * @zh 获取动画图实例中下一状态的运作状态。
-     * @en Gets the status of the next motion state.
+     * @zh 获取动画图实例中下一个状态的运行状况。
+     * @en Gets the running status of the next state in the animation graph instance.
      * @param layer 层级索引。（必须为 `0`）
      * @returns 下一状态运作状态对象，若未在进行过渡，则返回 `null`。
      */
@@ -137,8 +137,8 @@ export class AnimationController extends Component {
     }
 
     /**
-     * @zh 获取动画图实例中下一状态上包含的所有动画剪辑的运作状态。
-     * @en Gets the clip statuses contained in current motion state.
+     * @zh 获取动画图实例中下一个状态上添加的所有动画剪辑的运行状况。
+     * @en Gets the running status of all the animation clips added on the next state in the animation graph instance.
      * @param layer 层级索引。（必须为 `0`）
      * @returns 到下一状态上包含的动画剪辑运作状态的迭代器，若未在进行过渡，则返回一个空的迭代器。
      */
