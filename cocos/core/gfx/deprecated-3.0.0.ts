adc80163ffeab62ed37e569cee0b2d84ae0e6a45
yiwenxue <15225434259xue@gmail.com>
2022-01-24 11:54:04 +0800

    Add deprecation information to help developers move to the new interface. Every time a deprecated property is accessed, an error will present. So developers must move to the new interface following the instruction in the error info.

--- cocos/core/gfx/deprecated-3.0.0.ts
+++ cocos/core/gfx/deprecated-3.0.0.ts
@@ -27,8 +27,9 @@
  * @hidden
  */
 
-import { replaceProperty } from '../utils/x-deprecated';
+import { replaceProperty, removeProperty } from '../utils/x-deprecated';
 import { legacyCC } from '../global-exports';
+import { Feature } from './base/define';
 
 // Deprecated CC polyfill
 const customMappings = {
@@ -57,3 +58,78 @@ for (const api in legacyCC.gfx) {
         },
     ]);
 }
+
+removeProperty(Feature, 'Feature', [
+    {
+        name: 'COLOR_FLOAT',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.R32F) & FormatFeatureBit.RENDER_TARGET;',
+    },
+    {
+        name: 'COLOR_HALF_FLOAT',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.R16F) & FormatFeatureBit.RENDER_TARGET;',
+    },
+    {
+        name: 'TEXTURE_FLOAT',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = (device.getFormatFeatures(Format.R32F) & (FormatFeatureBit.RENDER_TARGET'
+            + ' | FormatFeatureBit.SAMPLED_TEXTURE)) === (FormatFeatureBit.RENDER_TARGET | FormatFeatureBit.SAMPLED_TEXTURE);',
+    },
+    {
+        name: 'TEXTURE_HALF_FLOAT',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = (device.getFormatFeatures(Format.R16F) & (FormatFeatureBit.RENDER_TARGET'
+            + ' | FormatFeatureBit.SAMPLED_TEXTURE)) === (FormatFeatureBit.RENDER_TARGET | FormatFeatureBit.SAMPLED_TEXTURE);',
+    },
+    {
+        name: 'TEXTURE_FLOAT_LINEAR',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.R32F) & FormatFeatureBit.LINEAR_FILTER;',
+    },
+    {
+        name: 'TEXTURE_HALF_FLOAT_LINEAR',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.R16F) & FormatFeatureBit.LINEAR_FILTER;',
+    },
+    {
+        name: 'FORMAT_R11G11B10F',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.R11G11B10F) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_SRGB',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.SRGB8) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_ETC1',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.ETC_RGB8) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_ETC2',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.ETC2_RGB8) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_DXT',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.BC1) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_PVRTC',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.PVRTC_RGB2) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_ASTC',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.ASTC_RGBA_4x4) !== FormatFeatureBit.NONE;',
+    },
+    {
+        name: 'FORMAT_RGB8',
+        suggest: 'Please use device.getFormatFeatures() instead, like: \n'
+            + 'let isSupported = device.getFormatFeatures(Format.RGB8) !== FormatFeatureBit.NONE;',
+    },
+]);
