bf8e801b742305a9665e6b604fab4921c38269c2
chengwenjun <chengwenjun@sofish.com>
2022-02-11 15:04:44 +0800

    [bug]fix constant 最后的k=1的时候，返回是1，不是0。 问题： 若k=1的情况下，返回0.在重复播放的动画中，出现k=1这一帧时候，显示不正确的问题。

--- cocos/core/animation/easing.ts
+++ cocos/core/animation/easing.ts
@@ -28,23 +28,23 @@
  * @module animation
  */
 
-export function constant () {
-    return 0;
+export function constant(k: number) {
+    return k < 1 ? 0 : 1;
 }
 
-export function linear (k: number) {
+export function linear(k: number) {
     return k;
 }
 
-export function quadIn (k: number) {
+export function quadIn(k: number) {
     return k * k;
 }
 
-export function quadOut (k: number) {
+export function quadOut(k: number) {
     return k * (2 - k);
 }
 
-export function quadInOut (k: number) {
+export function quadInOut(k: number) {
     k *= 2;
     if (k < 1) {
         return 0.5 * k * k;
@@ -52,15 +52,15 @@ export function quadInOut (k: number) {
     return -0.5 * (--k * (k - 2) - 1);
 }
 
-export function cubicIn (k: number) {
+export function cubicIn(k: number) {
     return k * k * k;
 }
 
-export function cubicOut (k: number) {
+export function cubicOut(k: number) {
     return --k * k * k + 1;
 }
 
-export function cubicInOut (k: number) {
+export function cubicInOut(k: number) {
     k *= 2;
     if (k < 1) {
         return 0.5 * k * k * k;
@@ -68,15 +68,15 @@ export function cubicInOut (k: number) {
     return 0.5 * ((k -= 2) * k * k + 2);
 }
 
-export function quartIn (k: number) {
+export function quartIn(k: number) {
     return k * k * k * k;
 }
 
-export function quartOut (k: number) {
+export function quartOut(k: number) {
     return 1 - (--k * k * k * k);
 }
 
-export function quartInOut (k: number) {
+export function quartInOut(k: number) {
     k *= 2;
     if (k < 1) {
         return 0.5 * k * k * k * k;
@@ -84,15 +84,15 @@ export function quartInOut (k: number) {
     return -0.5 * ((k -= 2) * k * k * k - 2);
 }
 
-export function quintIn (k: number) {
+export function quintIn(k: number) {
     return k * k * k * k * k;
 }
 
-export function quintOut (k: number) {
+export function quintOut(k: number) {
     return --k * k * k * k * k + 1;
 }
 
-export function quintInOut (k: number) {
+export function quintInOut(k: number) {
     k *= 2;
     if (k < 1) {
         return 0.5 * k * k * k * k * k;
@@ -100,30 +100,30 @@ export function quintInOut (k: number) {
     return 0.5 * ((k -= 2) * k * k * k * k + 2);
 }
 
-export function sineIn (k: number) {
+export function sineIn(k: number) {
     if (k === 1) {
         return 1;
     }
     return 1 - Math.cos(k * Math.PI / 2);
 }
 
-export function sineOut (k: number) {
+export function sineOut(k: number) {
     return Math.sin(k * Math.PI / 2);
 }
 
-export function sineInOut (k: number) {
+export function sineInOut(k: number) {
     return 0.5 * (1 - Math.cos(Math.PI * k));
 }
 
-export function expoIn (k: number) {
+export function expoIn(k: number) {
     return k === 0 ? 0 : Math.pow(1024, k - 1);
 }
 
-export function expoOut (k: number) {
+export function expoOut(k: number) {
     return k === 1 ? 1 : 1 - Math.pow(2, -10 * k);
 }
 
-export function expoInOut (k: number) {
+export function expoInOut(k: number) {
     if (k === 0) {
         return 0;
     }
@@ -137,15 +137,15 @@ export function expoInOut (k: number) {
     return 0.5 * (-Math.pow(2, -10 * (k - 1)) + 2);
 }
 
-export function circIn (k: number) {
+export function circIn(k: number) {
     return 1 - Math.sqrt(1 - k * k);
 }
 
-export function circOut (k: number) {
+export function circOut(k: number) {
     return Math.sqrt(1 - (--k * k));
 }
 
-export function circInOut (k: number) {
+export function circInOut(k: number) {
     k *= 2;
     if (k < 1) {
         return -0.5 * (Math.sqrt(1 - k * k) - 1);
@@ -153,7 +153,7 @@ export function circInOut (k: number) {
     return 0.5 * (Math.sqrt(1 - (k -= 2) * k) + 1);
 }
 
-export function elasticIn (k: number) {
+export function elasticIn(k: number) {
     let s;
     let a = 0.1;
     const p = 0.4;
@@ -172,7 +172,7 @@ export function elasticIn (k: number) {
     return -(a * Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p));
 }
 
-export function elasticOut (k: number) {
+export function elasticOut(k: number) {
     let s;
     let a = 0.1;
     const p = 0.4;
@@ -191,7 +191,7 @@ export function elasticOut (k: number) {
     return (a * Math.pow(2, -10 * k) * Math.sin((k - s) * (2 * Math.PI) / p) + 1);
 }
 
-export function elasticInOut (k: number) {
+export function elasticInOut(k: number) {
     let s;
     let a = 0.1;
     const p = 0.4;
@@ -215,7 +215,7 @@ export function elasticInOut (k: number) {
     return a * Math.pow(2, -10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p) * 0.5 + 1;
 }
 
-export function backIn (k: number) {
+export function backIn(k: number) {
     if (k === 1) {
         return 1;
     }
@@ -223,7 +223,7 @@ export function backIn (k: number) {
     return k * k * ((s + 1) * k - s);
 }
 
-export function backOut (k: number) {
+export function backOut(k: number) {
     if (k === 0) {
         return 0;
     }
@@ -231,7 +231,7 @@ export function backOut (k: number) {
     return --k * k * ((s + 1) * k + s) + 1;
 }
 
-export function backInOut (k: number) {
+export function backInOut(k: number) {
     const s = 1.70158 * 1.525;
     k *= 2;
     if (k < 1) {
@@ -240,11 +240,11 @@ export function backInOut (k: number) {
     return 0.5 * ((k -= 2) * k * ((s + 1) * k + s) + 2);
 }
 
-export function bounceIn (k: number) {
+export function bounceIn(k: number) {
     return 1 - bounceOut(1 - k);
 }
 
-export function bounceOut (k: number) {
+export function bounceOut(k: number) {
     if (k < (1 / 2.75)) {
         return 7.5625 * k * k;
     } else if (k < (2 / 2.75)) {
@@ -256,14 +256,14 @@ export function bounceOut (k: number) {
     }
 }
 
-export function bounceInOut (k: number) {
+export function bounceInOut(k: number) {
     if (k < 0.5) {
         return bounceIn(k * 2) * 0.5;
     }
     return bounceOut(k * 2 - 1) * 0.5 + 0.5;
 }
 
-export function smooth (k: number) {
+export function smooth(k: number) {
     if (k <= 0) {
         return 0;
     }
@@ -273,7 +273,7 @@ export function smooth (k: number) {
     return k * k * (3 - 2 * k);
 }
 
-export function fade (k: number) {
+export function fade(k: number) {
     if (k <= 0) {
         return 0;
     }
@@ -294,7 +294,7 @@ export const elasticOutIn = _makeOutIn(elasticIn, elasticOut);
 export const backOutIn = _makeOutIn(backIn, backOut);
 export const bounceOutIn = _makeOutIn(bounceIn, bounceOut);
 
-function _makeOutIn (fnIn: (k: number) => number, fnOut: (k: number) => number) {
+function _makeOutIn(fnIn: (k: number) => number, fnOut: (k: number) => number) {
     return (k: number) => {
         if (k < 0.5) {
             return fnOut(k * 2) / 2;
