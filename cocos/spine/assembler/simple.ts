4a4769f27a6010984a4bf4e29aba5f5bb4b4c874
pandamicro <lphuabin@gmail.com>
2022-02-16 15:48:38 +0800

    Fix spine tint accessor registartion

--- cocos/spine/assembler/simple.ts
+++ cocos/spine/assembler/simple.ts
@@ -173,11 +173,11 @@ export const simple: IAssembler = {
             if (useTint) {
                 accessor = _tintAccessor = new StaticVBAccessor(device, attributes, this.vCount);
                 // Register to batcher so that batcher can upload buffers after batching process
-                batcher.registerBufferAccessor(Number.parseInt('SPINE', 36), _accessor);
+                batcher.registerBufferAccessor(Number.parseInt('SPINE_TINT', 36), _tintAccessor);
             } else {
                 accessor = _accessor = new StaticVBAccessor(device, attributes, this.vCount);
                 // Register to batcher so that batcher can upload buffers after batching process
-                batcher.registerBufferAccessor(Number.parseInt('SPINE_TINT', 36), _accessor);
+                batcher.registerBufferAccessor(Number.parseInt('SPINE', 36), _accessor);
             }
         }
         return accessor;
