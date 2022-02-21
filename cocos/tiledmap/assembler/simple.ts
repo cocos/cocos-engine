c51684608ed072cab379920f5de76f657fff7b7f
zhakesi <liqiao@cocos.com>
2022-02-21 16:14:40 +0800

    fix getTiledTileAt cause layer show wrong. (#10137)
    * fix getTiledTileAt cause layer show wrong.
    
    * fix code style

--- cocos/tiledmap/assembler/simple.ts
+++ cocos/tiledmap/assembler/simple.ts
@@ -534,9 +534,11 @@ function fillByTiledNode (tiledNode: Node, color: Float32Array, vbuf: Float32Arr
     const vertStep3 = vertStep * 3;
 
     tiledNode.updateWorldTransform();
-    Mat4.fromRTS(_mat4_temp, tiledNode.getRotation(), tiledNode.getPosition(), tiledNode.getScale());
+    Mat4.fromRTS(_mat4_temp, tiledNode.rotation, tiledNode.position, tiledNode.scale);
     Vec3.set(_vec3u_temp, -(left + _moveX), -(bottom + _moveY), 0);
     Mat4.transform(_mat4_temp, _mat4_temp, _vec3u_temp);
+    Mat4.multiply(_mat4_temp, tiledNode.parent!.worldMatrix, _mat4_temp);
+
     const m = _mat4_temp;
     const tx = m.m12;
     const ty = m.m13;
