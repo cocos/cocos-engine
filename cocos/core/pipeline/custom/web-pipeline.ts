0481ee26bf0cdc0ab94d44fe8c848a6af6f800ec
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-02-23 18:41:09 +0800

    fix web-pipeline diamond inheritance problem

--- cocos/core/pipeline/custom/web-pipeline.ts
+++ cocos/core/pipeline/custom/web-pipeline.ts
@@ -35,9 +35,8 @@ import { RenderScene } from '../../renderer/scene';
 import { legacyCC } from '../../global-exports';
 import { LayoutGraphData } from './layout-graph';
 
-export class WebSetter extends Setter {
+export class WebSetter {
     constructor (data: RenderData) {
-        super();
         this._data = data;
     }
     public setMat4 (name: string, mat: Mat4): void {}
