890986656bad9c319707d5788a19f8947eb1c2b1
dumganhar <dumganhar@gmail.com>
2022-02-24 15:30:02 +0800

    Make missing return type as an error.

--- native/cmake/predefine.cmake
+++ native/cmake/predefine.cmake
@@ -100,13 +100,13 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 if("$ENV{COCOS_ENGINE_DEV}" EQUAL "1")
-    set(WERROR_FLAGS "-Werror") # -Wshorten-64-to-32 -Werror=return-type
+    set(WERROR_FLAGS "-Werror -Werror=return-type") # -Wshorten-64-to-32 -Werror=return-type
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         set(WERROR_FLAGS "/WX")
     endif()
     message(STATUS "Enable NO_WERROR")
 else()
-    set(WERROR_FLAGS "")
+    set(WERROR_FLAGS "-Werror=return-type")
     message(STATUS "Ignore NO_WERROR")
 endif()
 
