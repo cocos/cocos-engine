3a93d12c1812e04ea55c3ddf1ee36658c8df4d33
timlyeee <timelyeee@gmail.com>
2022-03-03 16:16:29 +0800

    fix win64 restart crash
    * supply restart fix from v3.4.2/v3.5(supply a default context)
    * temp hack for v8::Arraybuffer(abi conflict).
    * remove use of windows dll for x86

--- native/cocos/bindings/manual/jsb_global.cpp
+++ native/cocos/bindings/manual/jsb_global.cpp
@@ -821,13 +821,17 @@ static bool jsb_createExternalArrayBuffer(se::State& s) {
         ok &= sevalue_to_native(args[0], &byteLength, s.thisObject());
         SE_PRECONDITION2(ok, false, "jsb_createExternalArrayBuffer : Error processing arguments");
         if (byteLength > 0) {
+#ifdef SCRIPT_ENGINE_TYPE == SCRIPT_ENGINE_V8
+            se::HandleObject arrayBuffer{se::Object::createArrayBufferObject(nullptr, byteLength)};
+#else
             void *buffer = malloc(byteLength);
             memset(buffer, 0x00, byteLength);
-            se::HandleObject arrayBuffer{se::Object::createExternalArrayBufferObject(buffer, byteLength, [](void* contents, size_t /*byteLength*/, void*/* userData */){
+            se::HandleObject arrayBuffer{se::Object::createExternalArrayBufferObject(buffer, byteLength, [](void *contents, size_t /*byteLength*/, void * /* userData */) {
                 if (contents != nullptr) {
                     free(contents);
                 }
             })};
+#endif // SCRIPT_ENGINE_TYPE == SCRIPT_ENGINE_V8
             s.rval().setObject(arrayBuffer);
         } else {
             s.rval().setNull();
