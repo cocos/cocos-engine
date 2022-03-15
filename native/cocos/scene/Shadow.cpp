1e24ae301231b8394240c90440e011436894126c
troublemaker52025 <troublemaker52025@outlook.com>
2022-03-15 16:36:54 +0800

    Fix shadow switching crash for v3.6.0

--- native/cocos/scene/Shadow.cpp
+++ native/cocos/scene/Shadow.cpp
@@ -115,6 +115,7 @@ void Shadows::initialize(const ShadowsInfo &shadowsInfo) {
     setMaxReceived(shadowsInfo.getMaxReceived());
     setSize(shadowsInfo.getSize());
     setShadowColor(shadowsInfo.getShadowColor());
+    this->_shadowMapDirty = false;
 }
 
 void Shadows::destroy() {
