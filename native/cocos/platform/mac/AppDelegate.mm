ecb0533c666055a408298e668a80d24ef2fe363f
qiuguohua <qiuguohua111@126.com>
2022-03-26 15:38:57 +0800

    V3.5.0 window position (#10513)
    * Mac,windows,linux create lower left window by default
    
    * Mac creates bottom left window by default

--- native/cocos/platform/mac/AppDelegate.mm
+++ native/cocos/platform/mac/AppDelegate.mm
@@ -40,14 +40,10 @@
 
 @implementation AppDelegate
 
-- (void)createWindowPosCentered:(NSString*)title width:(int)w height:(int)h {
+- (void)createWindowPosBottomLeft:(NSString*)title width:(int)w height:(int)h {
     CGRect screenFrame = CGDisplayBounds(kCGDirectMainDisplay);
-    int x = 0;
-    int y = 0;
-    if(w < screenFrame.size.width)
-        x = screenFrame.origin.x + (screenFrame.size.width - w)/2;
-    if(h < screenFrame.size.height)
-        y = screenFrame.origin.y + (screenFrame.size.height - h)/2;
+    int x = screenFrame.origin.x;
+    int y = screenFrame.origin.y;
     [self createWindow :title xPos:x yPos:y width:w height:h];
 }
 
