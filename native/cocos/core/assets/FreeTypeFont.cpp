a43d17ad3129cd0f3e10a99b3494b7aca8ef455b
yangchao <1162485779@qq.com>
2022-03-31 19:34:32 +0800

    revert

--- native/cocos/core/assets/FreeTypeFont.cpp
+++ native/cocos/core/assets/FreeTypeFont.cpp
@@ -27,11 +27,7 @@
 ****************************************************************************/
 
 #include "FreeTypeFont.h"
-#if (CC_PLATFORM == CC_PLATFORM_LINUX)
-#include <freetype2/ft2build.h>
-#else
 #include <freetype/ft2build.h>
-#endif
 #include FT_FREETYPE_H
 #include <cstdint>
 #include "base/Log.h"
