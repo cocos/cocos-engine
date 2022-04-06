7c8a4f0bca1394a80f750d8be492629ba4679ef3
James Chen <dumganhar@gmail.com>
2022-04-06 15:15:48 +0800

    Add support for TextEncoder & TextDecoder.

--- native/cocos/bindings/jswrapper/v8/Object.cpp
+++ native/cocos/bindings/jswrapper/v8/Object.cpp
@@ -564,20 +564,25 @@ void Object::setPrivateObject(PrivateObjectBase *data) {
     assert(_privateObject == nullptr);
     #if CC_DEBUG
     //assert(NativePtrToObjectMap::find(data->getRaw()) == NativePtrToObjectMap::end());
-    auto it = NativePtrToObjectMap::find(data->getRaw());
-    if (it != NativePtrToObjectMap::end()) {
-        auto *pri = it->second->getPrivateObject();
-        SE_LOGE("Already exists object %s/[%s], trying to add %s/[%s]\n", pri->getName(), typeid(*pri).name(), data->getName(), typeid(*data).name());
-        #if JSB_TRACK_OBJECT_CREATION
-        SE_LOGE(" previous object created at %s\n", it->second->_objectCreationStackFrame.c_str());
-        #endif
-        assert(false);
+    if (data != nullptr) {
+        auto it = NativePtrToObjectMap::find(data->getRaw());
+        if (it != NativePtrToObjectMap::end()) {
+            auto *pri = it->second->getPrivateObject();
+            SE_LOGE("Already exists object %s/[%s], trying to add %s/[%s]\n", pri->getName(), typeid(*pri).name(), data->getName(), typeid(*data).name());
+            #if JSB_TRACK_OBJECT_CREATION
+            SE_LOGE(" previous object created at %s\n", it->second->_objectCreationStackFrame.c_str());
+            #endif
+            assert(false);
+        }
     }
     #endif
     internal::setPrivate(__isolate, _obj, data, this, &_internalData);
-    NativePtrToObjectMap::emplace(data->getRaw(), this);
     _privateObject = data;
-    defineOwnProperty("__native_ptr__", se::Value(static_cast<uint64_t>(reinterpret_cast<uintptr_t>(data->getRaw()))), false, false, false);
+
+    if (data != nullptr) {
+        NativePtrToObjectMap::emplace(data->getRaw(), this);
+        defineOwnProperty("__native_ptr__", se::Value(static_cast<uint64_t>(reinterpret_cast<uintptr_t>(data->getRaw()))), false, false, false);
+    }
 }
 
 PrivateObjectBase *Object::getPrivateObject() const {
