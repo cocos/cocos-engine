b67050e6b33ab9fe2c9da56fb9fcc176255d60c3
bofeng.song <bofeng.song@chukong-inc.com>
2022-04-14 10:42:58 +0800

    fix wrong include due to capital name

--- native/cocos/platform/android/GameActivityInclude.cpp
+++ native/cocos/platform/android/GameActivityInclude.cpp
@@ -25,7 +25,7 @@
 
 
 #include "game-activity/GameActivity.cpp"
-#include "game-text-input/GameTextInput.cpp"
+#include "game-text-input/gametextinput.cpp"
 extern "C" {
 #include "game-activity/native_app_glue/android_native_app_glue.c"
 }
