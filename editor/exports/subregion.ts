33e244074722c6d7bd810b8e3bee7ad7e757efac
Leslie Leigh <leslieleigh@outlook.com>
2022-05-05 10:26:41 +0800

    Update

--- editor/exports/subregion.ts
+++ editor/exports/subregion.ts
@@ -5,7 +5,9 @@ export {
     removeSubRegionTag,
 } from '../../cocos/core/animation/animation-clip';
 
-export type { Subregion } from '../../cocos/core/animation/subregion/subregion';
+export { Subregion } from '../../cocos/core/animation/subregion/subregion';
+
+export type { Su } from '../../cocos/core/animation/subregion/subregion';
 
 export { ParticleSystemSubRegionPlayer } from '../../cocos/core/animation/subregion/particle-system-subregion';
 
