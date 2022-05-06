d3c044f50a33ddf9e8d42ea05258e4c9eab203dd
Zeqiang Li <zeqiang-li@outlook.com>
2022-05-06 17:22:19 +0800

    i18n

--- cocos/core/geometry/sphere.ts
+++ cocos/core/geometry/sphere.ts
@@ -50,7 +50,7 @@ export class Sphere {
      * @param cy @en X-Coordinate of center point relative to the origin.  @zh 中心点的相对于原点的 Y 坐标。
      * @param cz @en X-Coordinate of center point relative to the origin.  @zh 中心点的相对于原点的 Z 坐标。
      * @param r @en Radius of the sphere. @zh 球体的半径
-     * @return {Sphere} 返回一个 sphere。
+     * @return 返回一个 sphere。
      */
     public static create (cx: number, cy: number, cz: number, r: number): Sphere {
         return new Sphere(cx, cy, cz, r);
@@ -61,8 +61,8 @@ export class Sphere {
      * clone a new sphere
      * @zh
      * 克隆一个新的 sphere 实例。
-     * @param {Sphere} p @en Sphere which cloned from. @zh 克隆的目标。
-     * @return {Sphere} @en Sphere which cloned to. @zh 克隆出的示例。
+     * @param p @en Sphere which cloned from. @zh 克隆的目标。
+     * @return @en Sphere which cloned to. @zh 克隆出的示例。
      */
     public static clone (p: Sphere): Sphere {
         return new Sphere(p.center.x, p.center.y, p.center.z, p.radius);
@@ -73,9 +73,9 @@ export class Sphere {
      * copy the values from one sphere to another
      * @zh
      * 将从一个 sphere 的值复制到另一个 sphere。
-     * @param {Sphere} out @en Sphere which copied to. @zh 接受操作的 sphere。
-     * @param {Sphere} a @en Sphere which copied from. @zh 被复制的 sphere。
-     * @return {Sphere} out @en Sphere which copied to. @zh 接受操作的 sphere。
+     * @param out @en The sphere object to copy to. @zh 接受操作的 sphere。
+     * @param a @en The sphere object to copy from. @zh 被复制的 sphere。
+     * @return @en The sphere object to copy to. @zh 接受操作的 sphere。
      */
     public static copy (out: Sphere, p: Sphere): Sphere {
         Vec3.copy(out.center, p.center);
@@ -92,7 +92,7 @@ export class Sphere {
      * @param out - @en Sphere created from points. @zh 接受操作的 sphere。
      * @param minPos - @en Lower point of the sphere. @zh sphere 的较小点。
      * @param maxPos - @en Upper point of the sphere. @zh sphere 的较大点。
-     * @returns {Sphere} @en Sphere created from points. @zh out 接受操作的 sphere。
+     * @returns @en The output sphere object to save the created sphere data. @zh 接受操作的 sphere。
      */
     public static fromPoints (out: Sphere, minPos: Vec3, maxPos: Vec3): Sphere {
         Vec3.multiplyScalar(out.center, Vec3.add(_v3_tmp, minPos, maxPos), 0.5);
@@ -105,12 +105,12 @@ export class Sphere {
      * Set the components of a sphere to the given values
      * @zh
      * 将球体的属性设置为给定的值。
-     * @param {Sphere} out @en Sphere which the properties set to. @zh 接受操作的 sphere。
+     * @param out @en The sphere to set properties to. @zh 接受操作的 sphere。
      * @param cx @en X-Coordinate of center point relative to the origin.  @zh 中心点的相对于原点的 X 坐标。
      * @param cy @en Y-Coordinate of center point relative to the origin.  @zh 中心点的相对于原点的 Y 坐标。
      * @param cz @en Z-Coordinate of center point relative to the origin.  @zh 中心点的相对于原点的 Z 坐标。
      * @param {number} r @en Radius of the sphere. @zh 半径。
-     * @return {Sphere} out @en Sphere which the properties set to. @zh 接受操作的 sphere。
+     * @return out @en Sphere which the properties set to. @zh 接受操作的 sphere。
      * @function
      */
     public static set (out: Sphere, cx: number, cy: number, cz: number, r: number): Sphere {
@@ -170,9 +170,9 @@ export class Sphere {
      * Construct a sphere.
      * @zh
      * 构造一个球。
-     * @param cx @en The X-Coordinate of sphere @zh 该球的世界坐标的 X 坐标。
-     * @param cy @en The Y-Coordinate of sphere @zh 该球的世界坐标的 Y 坐标。
-     * @param cz @en The Z-Coordinate of sphere @zh 该球的世界坐标的 Z 坐标。
+     * @param cx @en The X-Coordinate of the sphere. @zh 该球的世界坐标的 X 坐标。
+     * @param cy @en The Y-Coordinate of the sphere. @zh 该球的世界坐标的 Y 坐标。
+     * @param cz @en The Z-Coordinate of the sphere. @zh 该球的世界坐标的 Z 坐标。
      * @param {number} r 半径。
      */
     constructor (cx = 0, cy = 0, cz = 0, r = 1) {
@@ -199,7 +199,7 @@ export class Sphere {
      * Copy a sphere.
      * @zh
      * 拷贝对象。
-     * @param a @en Sphere which copied from. @zh 拷贝的目标。
+     * @param a @en The sphere to copy from. @zh 拷贝的目标。
      */
     public copy (a: Sphere) {
         return Sphere.copy(this, a);
@@ -210,8 +210,8 @@ export class Sphere {
      * Get the bounding points of this shape
      * @zh
      * 获取此形状的边界点。
-     * @param {Vec3} minPos @en Bottom(negative axis) point of the sphere. @zh 最小点。
-     * @param {Vec3} maxPos @en Top(positive axis) point of the sphere. @zh 最大点。
+     * @param {Vec3} minPos @en Bottom(negative direction) point of the sphere. @zh 最小点。
+     * @param {Vec3} maxPos @en Top(positive directin) point of the sphere. @zh 最大点。
      */
     public getBoundary (minPos: Vec3, maxPos: Vec3) {
         Vec3.set(minPos, this.center.x - this.radius, this.center.y - this.radius, this.center.z - this.radius);
@@ -223,11 +223,11 @@ export class Sphere {
      * Transform this shape
      * @zh
      * 将 out 根据这个 sphere 的数据进行变换。
-     * @param m @en Transform matrix. @zh 变换的矩阵。
-     * @param pos @en Position. @zh 变换的位置部分。
-     * @param rot @en Rotation. @zh 变换的旋转部分。
-     * @param scale @en Scale. @zh 变换的缩放部分。
-     * @param out @en Sphere the transform applied to. @zh 变换的目标。
+     * @param m @en The transform matrix. @zh 变换的矩阵。
+     * @param pos @en The position. @zh 变换的位置部分。
+     * @param rot @en The rotation. @zh 变换的旋转部分。
+     * @param scale @en The scale. @zh 变换的缩放部分。
+     * @param out @en The sphere which the transform will be applied to. @zh 变换的目标。
      */
     public transform (m: Mat4, pos: Vec3, rot: Quat, scale: Vec3, out: Sphere) {
         Vec3.transformMat4(out.center, this.center, m);
@@ -239,9 +239,9 @@ export class Sphere {
      * Translate and rotate this sphere.
      * @zh
      * 将 out 根据这个 sphere 的数据进行变换。
-     * @param m @en Transform matrix. @zh 变换的矩阵。
-     * @param rot @en Rotation. @zh 变换的旋转部分。
-     * @param out @en Sphere the transform applied to. @zh 变换的目标。
+     * @param m @en The transform matrix. @zh 变换的矩阵。
+     * @param rot @en The rotation. @zh 变换的旋转部分。
+     * @param out @en The sphere which the transform will be applied to. @zh 变换的目标。
      */
     public translateAndRotate (m: Mat4, rot: Quat, out: Sphere) {
         Vec3.transformMat4(out.center, this.center, m);
@@ -252,17 +252,17 @@ export class Sphere {
      * Scaling this sphere.
      * @zh
      * 将 out 根据这个 sphere 的数据进行缩放。
-     * @param scale @en Scale. @zh 缩放值。
-     * @param out @en Sphere the scale applied to. @zh 缩放的目标。
+     * @param scale @en The scale. @zh 缩放值。
+     * @param out @en The sphere which the scale will be applied to. @zh 缩放的目标。
      */
     public setScale (scale: Vec3, out: Sphere) {
         out.radius = this.radius * maxComponent(scale);
     }
 
     /**
-     * @en Sphere and point merge.
+     * @en The point to be merged.
      * @zh 球跟点合并
-     * @param point @en Point. @zh 点
+     * @param point @en The point to be merged. @zh 点
      */
     public mergePoint (point: Vec3) {
         // if sphere.radius Less than 0,
@@ -285,9 +285,9 @@ export class Sphere {
     }
 
     /**
-     * @en Sphere and points merge.
+     * @en The sphere and points to be merged.
      * @zh 球跟一系列点合并
-     * @param points @en Points. @zh 一系列点
+     * @param points @en The point to be merged. @zh 一系列点
      */
     public mergePoints (points: Vec3[]) {
         const length = points.length;
@@ -302,7 +302,7 @@ export class Sphere {
     }
 
     /**
-     * @en Sphere and AABB merge.
+     * @en The axis-aligned bounding box to be merged.
      * @zh 球跟立方体合并
      * @param a @en Cube. @zh 立方体
      */
