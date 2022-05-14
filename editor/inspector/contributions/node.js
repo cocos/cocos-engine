d6b47b6f010312dbe15d43eca9195e5486594d4b
dogodo-cc <741484865@qq.com>
2022-05-14 15:46:41 +0800

    feat: add debounce

--- editor/inspector/contributions/node.js
+++ editor/inspector/contributions/node.js
@@ -2,11 +2,13 @@
 const fs = require('fs');
 const path = require('path');
 const utils = require('./utils');
+const { join } = require('path');
+module.paths.push(join(Editor.App.path, 'node_modules'));
+const { debounce } = require('throttle-debounce');
 
 exports.listeners = {
-    async 'change-dump'(event) {
+    'change-dump': debounce(500, async (event) => {
         const panel = this;
-
         const target = event.target;
         if (!target) {
             return;
@@ -92,7 +94,7 @@ exports.listeners = {
         } catch (error) {
             console.error(error);
         }
-    },
+    }),
     'confirm-dump'() {
         const panel = this;
 
