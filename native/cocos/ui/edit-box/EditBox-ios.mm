43d091c21cc2f00065bcf3746abd77d13ea0e2c2
timlyeee <timelyeee@gmail.com>
2022-05-16 10:22:42 +0800

    temp save

--- native/cocos/ui/edit-box/EditBox-ios.mm
+++ native/cocos/ui/edit-box/EditBox-ios.mm
@@ -27,6 +27,7 @@
 #include "cocos/bindings/jswrapper/SeApi.h"
 #include "cocos/bindings/manual/jsb_global.h"
 
+
 #import <UIKit/UIKit.h>
 
 #define TEXT_LINE_HEIGHT         40
@@ -43,71 +44,74 @@
 
 // MARK: class declaration
 
-@interface ButtonHandler : NSObject
-- (IBAction)buttonTapped:(UIButton *)button;
-@end
 
-@interface KeyboardEventHandler : NSObject
+/*******************************
+ TODO: New implementation of iOS inputbox
+ UI Structure:
+    ==[|             ][done]== >>> inputAccessoryView
+    = q w e r t y u i o p =
+    = a s d f g h j k l ; '' =
+    = virtual [    ] keyboard = >>> inputView
+ 
+ Further needs:
+    Customization of inputbox, developer DIY toolbar.
+    The principle idea is to set inputAccessoryView from JS where developer can bind selector with js callback.
+    JS API:
+        uibutton sendBtn;
+        sendBtn.onclick = () => {...}
+        uiInputField inputFld;
+        inputFld.oninput = () => {...}
+        jsb.inputbox ibox;
+        ibox.addComponent(sendBtn); //automatically set as the last element.
+        ibox.setLayout(sendBtn, inputFld);
+    JSB binding:
+        jsb_addComponent(se::Value){
+            ...
+            inputBox.addComponent(cpt);
+        }
+    Native API:
+    class InputBox{
+        ...
+        addComponent(UIView* cpt);
+        setLayout(UIView*[] views);
+        createInputBox();
+    }
+
+ *******************************/
+
+
+//TODO: A mecanism to store each customize UITextView or UITextField. For example, if keyboard and toolbar is customized, should give a hash value and store in the map with a hash value calculated.
+
+
+// Customize inputbox
+@interface Inputbox : NSObject {
+@public UITextView  *g_inputView;
+@public UITextField *g_inputField;
+}
+
+- (void) createTextField:   (const cc::EditBox::ShowInfo*)showInfo;
+- (void) createTextView:    (const cc::EditBox::ShowInfo*)showInfo;
+
+- (void) addComponent: (UIView*) component;
+- (void) setLayout: (UIView*[]) components;
+- (void) show: (const cc::EditBox::ShowInfo*)showInfo;
+//TODO: move all handlers into inputbox implementation
+- (IBAction)buttonTapped:(UIButton *)button;
 - (void)keyboardWillShow:(NSNotification *)notification;
 - (void)keyboardWillHide:(NSNotification *)notification;
-@end
-
-@interface TextFieldDelegate : NSObject <UITextFieldDelegate>
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
 - (void)textFieldDidChange:(UITextField *)textField;
 - (BOOL)textFieldShouldReturn:(UITextField *)textField;
-@end
-
-@interface TextViewDelegate : NSObject <UITextViewDelegate>
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
 - (void)textViewDidChange:(UITextView *)textView;
 @end
 
+
+static Inputbox *g_inputbox = nil;
 /*************************************************************************
- Global variables and functions.
+ Global variables and functions relative to script engine.
  ************************************************************************/
-
-// MARK: global variables and functions
-
 namespace {
-bool g_isMultiline = false;
-bool g_confirmHold = false;
-unsigned int g_maxLength = INT_MAX;
-KeyboardEventHandler *g_keyboardHandler = nil;
-
-// "#1fa014", a color of dark green, was used for confirm button background
-static UIColor *g_darkGreen = [UIColor colorWithRed:31 / 255.0 green:160 / 255.0 blue:20 / 255.0 alpha:0.8];
-
-UITextField *g_textField = nil;
-TextFieldDelegate *g_textFieldDelegate = nil;
-UIButton *g_textFieldConfirmButton = nil;
-ButtonHandler *g_textFieldConfirmButtonHandler = nil;
-
-UITextView *g_textView = nil;
-TextViewDelegate *g_textViewDelegate = nil;
-UIButton *g_textViewConfirmButton = nil;
-ButtonHandler *g_textViewConfirmButtonHander = nil;
-
-UIView *getCurrentView() {
-    if (g_isMultiline)
-        return g_textView;
-    else
-        return g_textField;
-}
-
-NSString *getCurrentText() {
-    if (g_isMultiline)
-        return g_textView.text;
-    else
-        return g_textField.text;
-}
-
-void setText(NSString *text) {
-    if (g_isMultiline)
-        g_textView.text = text;
-    else
-        g_textField.text = text;
-}
 
 se::Value textInputCallback;
 
@@ -136,46 +140,13 @@ void callJSFunc(const ccstd::string &type, const ccstd::string &text) {
     textInputCallback.toObject()->call(args, nullptr);
 }
 
-int getTextInputHeight() {
-    if (g_isMultiline)
+int getTextInputHeight(bool isMultiline) {
+    if (isMultiline)
         return TEXT_LINE_HEIGHT * TEXT_VIEW_MAX_LINE_SHOWN;
     else
         return TEXT_LINE_HEIGHT;
 }
 
-void createButton(UIButton **button, ButtonHandler **buttonHandler, const CGRect &viewRect, const ccstd::string &title) {
-    ButtonHandler *btnHandler = [[ButtonHandler alloc] init];
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [btn addTarget:btnHandler
-                  action:@selector(buttonTapped:)
-        forControlEvents:UIControlEventTouchUpInside];
-    btn.frame = CGRectMake(0, 0, BUTTON_WIDTH, BUTTON_HIGHT);
-    btn.backgroundColor = g_darkGreen;
-    [btn setTitle:[NSString stringWithUTF8String:title.c_str()]
-         forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor whiteColor]
-              forState:UIControlStateNormal];
-
-    *button = btn;
-    *buttonHandler = btnHandler;
-}
-
-void setTexFiledKeyboardType(UITextField *textField, const ccstd::string &inputType) {
-    if (0 == inputType.compare("password")) {
-        textField.secureTextEntry = TRUE;
-        textField.keyboardType = UIKeyboardTypeDefault;
-    } else {
-        textField.secureTextEntry = FALSE;
-        if (0 == inputType.compare("email"))
-            textField.keyboardType = UIKeyboardTypeEmailAddress;
-        else if (0 == inputType.compare("number"))
-            textField.keyboardType = UIKeyboardTypeDecimalPad;
-        else if (0 == inputType.compare("url"))
-            textField.keyboardType = UIKeyboardTypeURL;
-        else if (0 == inputType.compare("text"))
-            textField.keyboardType = UIKeyboardTypeDefault;
-    }
-}
 
 void setTextFieldReturnType(UITextField *textField, const ccstd::string &returnType) {
     if (0 == returnType.compare("done"))
@@ -195,47 +166,20 @@ NSString *getConfirmButtonTitle(const ccstd::string &returnType) {
     return NSLocalizedString(titleKey, nil); // get i18n string to be the title
 }
 
-void initTextField(const CGRect &rect, const cc::EditBox::ShowInfo &showInfo) {
-    if (!g_textField) {
-        g_textField = [[UITextField alloc] initWithFrame:rect];
-        g_textField.textColor = [UIColor blackColor];
-        g_textField.backgroundColor = [UIColor whiteColor];
-        [g_textField setBorderStyle:UITextBorderStyleLine];
-        g_textField.backgroundColor = [UIColor whiteColor];
-
-        g_textFieldDelegate = [[TextFieldDelegate alloc] init];
-        g_textField.delegate = g_textFieldDelegate;
-
-        // Assign the overlay button to a stored text field
-        createButton(&g_textFieldConfirmButton, &g_textFieldConfirmButtonHandler, rect, showInfo.confirmType);
-        g_textField.rightView = g_textFieldConfirmButton;
-        g_textField.rightViewMode = UITextFieldViewModeAlways;
-        [g_textField addTarget:g_textFieldDelegate action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+void initTextField(const cc::EditBox::ShowInfo &showInfo) {
+    if(!g_inputbox) {
+        g_inputbox = [[Inputbox alloc] init];
     }
-
-    g_textField.frame = rect;
-    setTextFieldReturnType(g_textField, showInfo.confirmType);
-    setTexFiledKeyboardType(g_textField, showInfo.inputType);
-    g_textField.text = [NSString stringWithUTF8String:showInfo.defaultValue.c_str()];
-    [g_textFieldConfirmButton setTitle:getConfirmButtonTitle(showInfo.confirmType) forState:UIControlStateNormal];
+    [g_inputbox createTextField:&showInfo];
+    return;
 }
 
-void initTextView(const CGRect &viewRect, const CGRect &btnRect, const cc::EditBox::ShowInfo &showInfo) {
-    if (!g_textView) {
-        g_textView = [[UITextView alloc] initWithFrame:btnRect];
-        g_textView.textColor = [UIColor blackColor];
-        g_textView.backgroundColor = [UIColor whiteColor];
-        g_textViewDelegate = [[TextViewDelegate alloc] init];
-        g_textView.delegate = g_textViewDelegate;
-
-        createButton(&g_textViewConfirmButton, &g_textViewConfirmButtonHander, btnRect, showInfo.confirmType);
-        g_textViewConfirmButton.frame = CGRectMake(viewRect.size.width - BUTTON_WIDTH, 0, BUTTON_WIDTH, BUTTON_HIGHT);
-        [g_textView addSubview:g_textViewConfirmButton];
+void initTextView(const cc::EditBox::ShowInfo &showInfo) {
+    if(!g_inputbox) {
+        g_inputbox = [[Inputbox alloc] init];
     }
-
-    g_textView.frame = btnRect;
-    g_textView.text = [NSString stringWithUTF8String:showInfo.defaultValue.c_str()];
-    [g_textViewConfirmButton setTitle:getConfirmButtonTitle(showInfo.confirmType) forState:UIControlStateNormal];
+    [g_inputbox createTextView:&showInfo];
+    return;
 }
 
 CGRect getSafeAreaRect() {
@@ -258,87 +202,102 @@ CGRect getSafeAreaRect() {
 
     return viewRect;
 }
+UIView *getCurrentView(bool isMultiline) {
+    if(isMultiline)
+        return g_inputbox->g_inputView;
+    return g_inputbox->g_inputField;
+}
 
+NSString* getCurrentText(bool isMultiline){
+    if(isMultiline)
+        return g_inputbox->g_inputView.text;
+    return g_inputbox->g_inputField.text;
+}
 void addTextInput(const cc::EditBox::ShowInfo &showInfo) {
     auto safeAreaRect = getSafeAreaRect();
-    int height = getTextInputHeight();
-    CGRect rect = CGRectMake(safeAreaRect.origin.x,
-                             safeAreaRect.size.height - height,
+    int height = getTextInputHeight(showInfo.isMultiline);
+    CGRect btnRect = CGRectMake(safeAreaRect.origin.x,
+                             safeAreaRect.size.height - height/2,
                              safeAreaRect.size.width,
                              height);
     if (showInfo.isMultiline)
-        initTextView(safeAreaRect, rect, showInfo);
+        initTextView(showInfo);
     else
-        initTextField(rect, showInfo);
+        initTextField(showInfo);
 
-    UIView *textInput = getCurrentView();
+    UIView *textInput = getCurrentView(showInfo.isMultiline);
     UIView *view = UIApplication.sharedApplication.delegate.window.rootViewController.view;
     [view addSubview:textInput];
     [textInput becomeFirstResponder];
 }
 
 void addKeyboardEventLisnters() {
-    if (!g_keyboardHandler)
-        g_keyboardHandler = [[KeyboardEventHandler alloc] init];
 
-    [[NSNotificationCenter defaultCenter] addObserver:g_keyboardHandler
-                                             selector:@selector(keyboardWillShow:)
-                                                 name:UIKeyboardWillShowNotification
-                                               object:nil];
-
-    [[NSNotificationCenter defaultCenter] addObserver:g_keyboardHandler
-                                             selector:@selector(keyboardWillHide:)
-                                                 name:UIKeyboardWillHideNotification
-                                               object:nil];
 }
 
 void removeKeyboardEventLisnters() {
-    if (!g_keyboardHandler)
-        return;
 
-    [[NSNotificationCenter defaultCenter] removeObserver:g_keyboardHandler];
 }
 } // namespace
 
 /*************************************************************************
  Class implementations.
  ************************************************************************/
+@implementation Inputbox
+{
 
-// MARK: class implementation
-
-@implementation KeyboardEventHandler
-- (void)keyboardWillShow:(NSNotification *)notification {
-    UIView *textView = getCurrentView();
-    if (!textView)
-        return;
-
-    NSDictionary *keyboardInfo = [notification userInfo];
-    NSValue *keyboardFrame = [keyboardInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
-    CGSize kbSize = [keyboardFrame CGRectValue].size;
-
-    int textHeight = getTextInputHeight();
-
-    CGRect screenRect = getSafeAreaRect();
-    textView.frame = CGRectMake(screenRect.origin.x,
-                                screenRect.size.height - textHeight - kbSize.height,
-                                screenRect.size.width,
-                                textHeight);
 }
-
-- (void)keyboardWillHide:(NSNotification *)notification {
-    NSDictionary *info = [notification userInfo];
-
-    CGRect beginKeyboardRect = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
-    CGRect endKeyboardRect = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    CGFloat yOffset = endKeyboardRect.origin.y - beginKeyboardRect.origin.y;
-
-    if (yOffset <= 0) {
-        cc::EditBox::hide();
+- (void) createTextView:    (const cc::EditBox::ShowInfo *)showInfo{
+    g_inputView = [[UITextView alloc]
+                   initWithFrame:CGRectMake(showInfo->x,
+                                            showInfo->y,
+                                            showInfo->width,
+                                            showInfo->height)];
+    //TODO: bind with specified inputView and inputAccessoryView
+    return;
+}
+- (void) createTextField:    (const cc::EditBox::ShowInfo*)showInfo{
+    g_inputField = [[UITextField alloc]
+                   initWithFrame:CGRectMake(showInfo->x,
+                                            showInfo->y,
+                                            showInfo->width,
+                                            showInfo->height)];
+    //TODO: bind with specified inputView and inputAccessoryView
+    return;
+}
+- (void) show: (const cc::EditBox::ShowInfo*)showInfo {
+    if (showInfo->isMultiline) {
+        createTextField:showInfo;
+    } else {
+        createTextView:showInfo;
     }
+        
 }
-@end
+- (void) hide {
+    
+}
+- (id) init {
+    self = [super init];
+    return self;
+}
+- (void) addComponent:(UIView *)component {
+    //TODO: Add component into inputAccessoryView
+    return;
+}
 
-@implementation TextFieldDelegate
+
+- (void) setLayout:(UIView *[])components {
+    //TODO: Set layout of inputAccessoryView
+    return;
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+
+}
+- (void)keyboardWillHide:(NSNotification *)notification {
+    
+}
+
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     // REFINE: check length limit before text changed
     return YES;
@@ -349,10 +308,10 @@ void removeKeyboardEventLisnters() {
         return;
 
     // check length limit after text changed, a little rude
-    if (textField.text.length > g_maxLength) {
-        NSRange rangeIndex = [textField.text rangeOfComposedCharacterSequenceAtIndex:g_maxLength];
-        textField.text = [textField.text substringToIndex:rangeIndex.location];
-    }
+//    if (textField.text.length > g_maxLength) {
+//        NSRange rangeIndex = [textField.text rangeOfComposedCharacterSequenceAtIndex:g_maxLength];
+//        textField.text = [textField.text substringToIndex:rangeIndex.location];
+//    }
 
     callJSFunc("input", [textField.text UTF8String]);
 }
@@ -361,18 +320,14 @@ void removeKeyboardEventLisnters() {
     cc::EditBox::complete();
     return YES;
 }
-@end
 
-@implementation ButtonHandler
 - (IBAction)buttonTapped:(UIButton *)button {
-    const ccstd::string text([getCurrentText() UTF8String]);
-    callJSFunc("confirm", text);
-    if (!g_confirmHold)
-        cc::EditBox::complete();
-}
-@end
 
-@implementation TextViewDelegate
+    callJSFunc("confirm", "sss");
+
+    cc::EditBox::complete();
+}
+
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
     // REFINE: check length limit before text changed
     return YES;
@@ -383,8 +338,6 @@ void removeKeyboardEventLisnters() {
         return;
 
     // check length limit after text changed, a little rude
-    if (textView.text.length > g_maxLength)
-        textView.text = [textView.text substringToIndex:g_maxLength];
 
     callJSFunc("input", [textView.text UTF8String]);
 }
@@ -396,39 +349,24 @@ void removeKeyboardEventLisnters() {
 
 // MARK: EditBox
 
-namespace cc {
-
+namespace cc{
 bool EditBox::_isShown = false;
-
 void EditBox::show(const cc::EditBox::ShowInfo &showInfo) {
-    // Should initialize them at first.
-    g_maxLength = showInfo.maxLength;
-    g_isMultiline = showInfo.isMultiline;
-    g_confirmHold = showInfo.confirmHold;
-
-    addKeyboardEventLisnters();
-    addTextInput(showInfo);
-
-    _isShown = true;
+    if(!g_inputbox){
+        g_inputbox = [[Inputbox alloc] init];
+    }
+    [g_inputbox show:&showInfo];
 }
 
 void EditBox::hide() {
-    removeKeyboardEventLisnters();
-
-    UIView *view = getCurrentView();
-    if (view) {
-        [view removeFromSuperview];
-        [view resignFirstResponder];
-    }
-
-    _isShown = false;
+    [g_inputbox hide];
 }
 
 bool EditBox::complete() {
     if (!_isShown)
         return false;
 
-    NSString *text = getCurrentText();
+    NSString *text = getCurrentText(sinfo.isMultiline);
     callJSFunc("complete", [text UTF8String]);
     EditBox::hide();
 
