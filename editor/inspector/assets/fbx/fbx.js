9bf41d1635d4119f548ff8002ef291a6565f5b75
arsen2010 <arsen2010@126.com>
2022-05-16 16:27:54 +0800

    change getConfig position

--- editor/inspector/assets/fbx/fbx.js
+++ editor/inspector/assets/fbx/fbx.js
@@ -185,7 +185,7 @@ const Elements = {
         async update() {
             const panel = this;
 
-            const laboratoryExpectedValue = await Editor.Profile.getConfig('inspector', 'asset.fbx.material.smart');
+            const laboratoryExpectedValue = await Editor.Profile.getConfig('engine', 'asset.fbx.material.smart');
             if (!laboratoryExpectedValue) {
                 panel.$.smartMaterialEnabledProp.setAttribute('readonly', '');
             } else {
