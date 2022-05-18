73048877bd2a0b258ed47cea1fb1d676e1bf41c4
mmyduckx <308303735@qq.com>
2022-05-18 18:53:46 +0800

    remove all CC_TARGET_OS_TVOS related codes

--- native/cocos/platform/ios/modules/Accelerometer.mm
+++ native/cocos/platform/ios/modules/Accelerometer.mm
@@ -145,24 +145,15 @@ static CCMotionDispatcher *__motionDispatcher = nullptr;
 
 namespace cc {
 void Accelerometer::setAccelerometerEnabled(bool isEnabled) {
-#if !defined(CC_TARGET_OS_TVOS)
     [[CCMotionDispatcher sharedMotionDispatcher] setMotionEnabled:isEnabled];
-#endif
 }
 
 void Accelerometer::setAccelerometerInterval(float interval) {
-#if !defined(CC_TARGET_OS_TVOS)
     [[CCMotionDispatcher sharedMotionDispatcher] setMotionInterval:interval];
-#endif
 }
 
 const Accelerometer::MotionValue &Accelerometer::getDeviceMotionValue() {
-#if !defined(CC_TARGET_OS_TVOS)
     return [[CCMotionDispatcher sharedMotionDispatcher] getMotionValue];
-#else
-    static Device::MotionValue ret;
-    return ret;
-#endif
 }
 
 } // namespace cc
