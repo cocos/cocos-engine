865d63073f08d8f9177ea871292b6d37f2f2e257
ChiaNing <sun.clooud.mo@gmail.com>
2022-05-19 15:11:23 +0800

    Fill the Z Position in UI

--- cocos/2d/assembler/sprite/bar-filled.ts
+++ cocos/2d/assembler/sprite/bar-filled.ts
@@ -222,9 +222,11 @@ export const barFilled: IAssembler = {
         const vData = chunk.vb;
 
         let offset = 0;
+        const z = node.getPosition().z;
         for (let i = 0; i < 4; i++) {
             const local = dataList[i];
-            Vec3.transformMat4(vec3_temp, local, matrix);
+            Vec3.set(vec3_temp, local.x, local.y, 0);
+            Vec3.transformMat4(vec3_temp, vec3_temp, matrix);
             offset = i * stride;
             vData[offset] = vec3_temp.x;
             vData[offset + 1] = vec3_temp.y;
