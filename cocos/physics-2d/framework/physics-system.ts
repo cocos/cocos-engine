bb2b4b3e539ce2cb0112f7fa02a42d2925060c69
changhua <39176194+changhua0118@users.noreply.github.com>
2022-05-24 10:29:27 +0800

    [3.5.1]fix physics collision matrix (#11181)
    * fix physics collision matrix pyhsicsGroup2D
    
    * fix import legacyCC

--- cocos/physics-2d/framework/physics-system.ts
+++ cocos/physics-2d/framework/physics-system.ts
@@ -10,8 +10,10 @@ import { IPhysicsConfig, ICollisionMatrix } from '../../physics/framework/physic
 import { CollisionMatrix } from '../../physics/framework/collision-matrix';
 import { ERaycast2DType, RaycastResult2D, PHYSICS_2D_PTM_RATIO, PhysicsGroup } from './physics-types';
 import { Collider2D } from './components/colliders/collider-2d';
+import { legacyCC } from '../../core/global-exports';
 
 let instance: PhysicsSystem2D | null = null;
+legacyCC.internal.PhysicsGroup2D = PhysicsGroup;
 
 export class PhysicsSystem2D extends Eventify(System) {
     /**
