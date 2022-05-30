388f9fd3601830fb9b1ef550005b536b1996eac7
PP_Pro <chinese1236@qq.com>
2022-05-30 17:14:06 +0800

    add comment for input-source

--- pal/input/input-source.ts
+++ pal/input/input-source.ts
@@ -26,36 +26,84 @@
 
 import { Vec2, Vec3 } from '../../cocos/core/math';
 
+/**
+ * @en The abstract class for input source, which is used to control the input signal of a mono input source
+ * @zh InputSource 抽象类，该类用于控制单一输入源的输入信号
+ */
 export abstract class InputSource<T> {
+    /**
+     * @en Get the signal value of the input source
+     * @zh 获取输入源的信号值
+     */
     abstract getValue (): T;
 }
 
+/**
+ * @en The class for input source of 1D Axis, which is used to control the input signal of a mono input source
+ * @zh 1D 轴的 InputSource 类，该类用于控制单一输入源的输入信号
+ */
 export class InputSourceAxis1D extends InputSource<number> {
+    /**
+     * @en Get the signal value of the input source, ranged from -1 to 1
+     * @zh 获取输入源的信号值，取值范围从 -1 到 1
+     */
     getValue (): number {
         throw new Error('Method not implemented.');
     }
 }
 
+/**
+ * @en The class for input source of 2D Axis, which is used to control the input signal of a mono input source
+ * @zh 2D 轴的 InputSource 类，该类用于控制单一输入源的输入信号
+ */
 export class InputSourceAxis2D extends InputSource<Vec2> {
+    /**
+     * @en Get the signal value of the input source, which returns a Vec2 object.
+     * @zh 获取输入源的信号值，该方法返回一个 Vec2 对象
+     */
     getValue (): Vec2 {
         throw new Error('Method not implemented.');
     }
 }
 
+/**
+ * @en The class for input source of 3D Axis, which is used to control the input signal of a mono input source
+ * @zh 3D 轴的 InputSource 类，该类用于控制单一输入源的输入信号
+ */
 export class InputSourceAxis3D extends InputSource<Vec3> {
+    /**
+     * @en Get the signal value of the input source, which returns a Vec3 object.
+     * @zh 获取输入源的信号值，该方法返回一个 Vec3 对象
+     */
     getValue (): Vec3 {
         throw new Error('Method not implemented.');
     }
 }
 
+/**
+ * @en The class for input source of 1D Axis, which is used to control the input signal of a composite of multi input source
+ * @zh 1D 轴的 InputSource 类，该类用于控制多个输入源组合的输入信号
+ */
 export class CompositeInputSourceAxis1D extends InputSourceAxis1D {
+    /**
+     * @en Get the input signal value of positive 1D axis, ranged from 0 to 1
+     * @zh 获取 1D 轴的正向的输入信号值，取值范围从 0 到 1
+     */
     public positive: InputSourceButton;
+    /**
+     * @en Get the input signal value of negative 1D axis, ranged from 0 to 1
+     * @zh 获取 1D 轴的负向的输入信号值，取值范围从 0 到 1
+     */
     public negative: InputSourceButton;
     constructor (options: {positive: InputSourceButton, negative: InputSourceButton}) {
         super();
         this.positive = options.positive;
         this.negative = options.negative;
     }
+    /**
+     * @en Get the signal value of the input source, ranged from -1 to 1.
+     * @zh 获取输入源的信号值，取值范围从 -1 到 1
+     */
     public getValue (): number {
         const positiveValue = this.positive.getValue();
         const negativeValue = this.negative.getValue();
@@ -65,13 +113,42 @@ export class CompositeInputSourceAxis1D extends InputSourceAxis1D {
         return -negativeValue;
     }
 }
+
+/**
+ * @en The class for input source of 2D Axis, which is used to control the input signal of a composite of multi input source
+ * @zh 2D 轴的 InputSource 类，该类用于控制多个输入源组合的输入信号
+ */
 export class CompositeInputSourceAxis2D extends InputSourceAxis2D {
+    /**
+     * @en Get the input signal value of up direction of 2D axis, ranged from 0 to 1
+     * @zh 获取 2D 轴的向上的输入信号值，取值范围从 0 到 1
+     */
     public up: InputSourceButton;
+    /**
+     * @en Get the input signal value of down direction of 2D axis, ranged from 0 to 1
+     * @zh 获取 2D 轴的向下的输入信号值，取值范围从 0 到 1
+     */
     public down: InputSourceButton;
+    /**
+     * @en Get the input signal value of left direction of 2D axis, ranged from 0 to 1
+     * @zh 获取 2D 轴的向左的输入信号值，取值范围从 0 到 1
+     */
     public left: InputSourceButton;
+    /**
+     * @en Get the input signal value of right direction of 2D axis, ranged from 0 to 1
+     * @zh 获取 2D 轴的向右的输入信号值，取值范围从 0 到 1
+     */
     public right: InputSourceButton;
 
+    /**
+     * @en Get the input signal value on the x axis of 2D axis, ranged from -1 to 1
+     * @zh 获取 2D 轴在 x 轴上的输入信号值，取值范围从 -1 到 1
+     */
     public xAxis: CompositeInputSourceAxis1D;
+    /**
+     * @en Get the input signal value on the y axis of 2D axis, ranged from -1 to 1
+     * @zh 获取 2D 轴在 y 轴上的输入信号值，取值范围从 -1 到 1
+     */
     public yAxis: CompositeInputSourceAxis1D;
     constructor (options: {up: InputSourceButton, down: InputSourceButton, left: InputSourceButton, right: InputSourceButton}) {
         super();
@@ -82,20 +159,65 @@ export class CompositeInputSourceAxis2D extends InputSourceAxis2D {
         this.xAxis = new CompositeInputSourceAxis1D({ positive: this.right, negative: this.left });
         this.yAxis = new CompositeInputSourceAxis1D({ positive: this.up, negative: this.down });
     }
+    /**
+     * @en Get the signal value of the input source, which returns a Vec2 object.
+     * @zh 获取输入源的信号值，该方法返回一个 Vec2 对象
+     */
     getValue (): Vec2 {
         return new Vec2(this.xAxis.getValue(), this.yAxis.getValue());
     }
 }
+
+/**
+ * @en The class for input source of 3D Axis, which is used to control the input signal of a composite of multi input source
+ * @zh 3D 轴的 InputSource 类，该类用于控制多个输入源组合的输入信号
+ */
 export class CompositeInputSourceAxis3D extends InputSourceAxis3D {
+    /**
+     * @en Get the input signal value of up direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向上的输入信号值，取值范围从 0 到 1
+     */
     public up: InputSourceButton;
+    /**
+     * @en Get the input signal value of down direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向下的输入信号值，取值范围从 0 到 1
+     */
     public down: InputSourceButton;
+    /**
+     * @en Get the input signal value of left direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向左的输入信号值，取值范围从 0 到 1
+     */
     public left: InputSourceButton;
+    /**
+     * @en Get the input signal value of right direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向右的输入信号值，取值范围从 0 到 1
+     */
     public right: InputSourceButton;
+    /**
+     * @en Get the input signal value of forward direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向前的输入信号值，取值范围从 0 到 1
+     */
     public forward: InputSourceButton;
+    /**
+     * @en Get the input signal value of backward direction of 3D axis, ranged from 0 to 1
+     * @zh 获取 3D 轴的向后的输入信号值，取值范围从 0 到 1
+     */
     public backward: InputSourceButton;
 
+    /**
+     * @en Get the input signal value on the x axis of 3D axis, ranged from -1 to 1
+     * @zh 获取 3D 轴在 x 轴上的输入信号值，取值范围从 -1 到 1
+     */
     public xAxis: CompositeInputSourceAxis1D;
+    /**
+     * @en Get the input signal value on the y axis of 3D axis, ranged from -1 to 1
+     * @zh 获取 3D 轴在 y 轴上的输入信号值，取值范围从 -1 到 1
+     */
     public yAxis: CompositeInputSourceAxis1D;
+    /**
+     * @en Get the input signal value on the z axis of 3D axis, ranged from -1 to 1
+     * @zh 获取 3D 轴在 z 轴上的输入信号值，取值范围从 -1 到 1
+     */
     public zAxis: CompositeInputSourceAxis1D;
     constructor (options: {up: InputSourceButton, down: InputSourceButton, left: InputSourceButton,
         right: InputSourceButton, forward: InputSourceButton, backward: InputSourceButton}) {
@@ -112,16 +234,39 @@ export class CompositeInputSourceAxis3D extends InputSourceAxis3D {
         this.zAxis = new CompositeInputSourceAxis1D({ positive: this.forward, negative: this.backward });
     }
 
+    /**
+     * @en Get the signal value of the input source, which returns a Vec3 object.
+     * @zh 获取输入源的信号值，该方法返回一个 Vec3 对象
+     */
     getValue (): Vec3 {
         return new Vec3(this.xAxis.getValue(), this.yAxis.getValue(), this.zAxis.getValue());
     }
 }
 
+/**
+ * @en The class for input source of mono button, whose input signal value is ranged from 0 to 1
+ * @zh 单一按键输入源类, 输入信号源的取值范围是 0 到 1
+ */
 export class InputSourceButton extends InputSourceAxis1D {
+    /**
+     * @en Get the signal value of the input source, ranged from 0 to 1
+     * @zh 获取输入源的信号值，取值范围从 0 到 1
+     */
+    getValue (): number {
+        return super.getValue();
+    }
 }
 
+/**
+ * @en The class for input source of dpad, whose input signal value a Vec2 object
+ * @zh 手柄方向键输入源类, 输入信号源的取值是一个 Vec2 对象
+ */
 export class InputSourceDpad extends CompositeInputSourceAxis2D {
 }
 
+/**
+ * @en The class for input source of stick, whose input signal value a Vec2 object
+ * @zh 摇杆输入源类, 输入信号源的取值是一个 Vec2 对象
+ */
 export class InputSourceStick extends CompositeInputSourceAxis2D {
 }
