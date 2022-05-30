657ebe36f81cf96488d4b32fc10239c24b26e035
PP_Pro <chinese1236@qq.com>
2022-05-30 15:11:59 +0800

    remove useless code

--- cocos/input/gamepad.ts
+++
@@ -1,8 +0,0 @@
-export namespace Input {
-    export class Gamepad {
-        private constructor () {}
-        static current: Gamepad;
-        static all: Gamepad[];
-        public test!: number;
-    }
-}
