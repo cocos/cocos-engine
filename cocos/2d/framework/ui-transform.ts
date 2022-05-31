814cc6a37e4d985a2aa9048c74b34c39eb47451f
PP_Pro <chinese1236@qq.com>
2022-05-31 14:13:18 +0800

    ban UI hitTest on renderTexture

--- cocos/2d/framework/ui-transform.ts
+++ cocos/2d/framework/ui-transform.ts
@@ -461,7 +461,7 @@ export class UITransform extends Component {
         const cameras = this._getRenderScene().cameras;
         for (let i = 0; i < cameras.length; i++) {
             const camera = cameras[i];
-            if (!(camera.visibility & this.node.layer)) continue;
+            if (!(camera.visibility & this.node.layer) || (camera.window && !camera.window.swapchain)) continue;
 
             // Convert Screen Space into World Space.
             Vec3.set(v3WorldPt, screenPoint.x, screenPoint.y, 0);  // vec3 screen pos
