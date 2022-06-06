zhakesi/engine

fix skinning model split

native/cocos/renderer/pipeline/Define.h
@@ -45,7 +45,10 @@ class RenderFlow;
 // Skinning models with number of bones more than this capacity will be automatically switched to texture skinning.
 // But still, you can tweak this for your own need by changing the number below
 // and the JOINT_UNIFORM_CAPACITY macro in cc-skinning shader header.
-constexpr int JOINT_UNIFORM_CAPACITY = 30;
+class CC_DLL SkinningJointCapacity {
+public:
+    static uint JOINT_UNIFORM_CAPACITY;
+};
 
 constexpr float SHADOW_CAMERA_MAX_FAR    = 2000.0F;
 const float     COEFFICIENT_OF_EXPANSION = 2.0F * sqrtf(3.0F);
@@ -330,14 +333,15 @@ struct CC_DLL UBOSkinningAnimation {
     static const String                          NAME;
 };
 
-struct CC_DLL UBOSkinning {
-    static constexpr uint                        JOINTS_OFFSET = 0;
-    static constexpr uint                        COUNT         = UBOSkinning::JOINTS_OFFSET + JOINT_UNIFORM_CAPACITY * 12;
-    static constexpr uint                        SIZE          = UBOSkinning::COUNT * 4;
-    static constexpr uint                        BINDING       = static_cast<uint>(ModelLocalBindings::UBO_SKINNING_TEXTURE);
+class CC_DLL UBOSkinning {
+public:
+    static uint                                  COUNT;
+    static uint                                  SIZE;
+    static constexpr uint                        BINDING            = static_cast<uint>(ModelLocalBindings::UBO_SKINNING_TEXTURE);
     static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
-    static const gfx::UniformBlock               LAYOUT;
+    static gfx::UniformBlock                     LAYOUT;
     static const String                          NAME;
+    static void InitLayout (uint capacity);
 };
 
 struct CC_DLL UBOMorph {
@@ -522,6 +526,13 @@ struct CC_DLL DIFFUSEMAP : public Object {
 };
 
 struct CC_DLL JOINTTEXTURE : public Object {
+    static constexpr uint                        BINDING = static_cast<uint>(ModelLocalBindings::SAMPLER_JOINTS);
+    static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
+    static const gfx::UniformSamplerTexture      LAYOUT;
+    static const String                          NAME;
+};
+
+struct CC_DLL REALTIMEJOINTTEXTURE : public Object {
     static constexpr uint                        BINDING = static_cast<uint>(ModelLocalBindings::SAMPLER_JOINTS);
     static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
     static const gfx::UniformSamplerTexture      LAYOUT;

native/cocos/renderer/pipeline/RenderPipeline.cpp
@@ -291,12 +291,14 @@ void RenderPipeline::generateConstantMacros() {
 #define CC_DEVICE_CAN_BENEFIT_FROM_INPUT_ATTACHMENT %d
 #define CC_PLATFORM_ANDROID_AND_WEBGL 0
 #define CC_ENABLE_WEBGL_HIGHP_STRUCT_VALUES 0
+#define CC_JOINT_UNIFORM_CAPACITY %d
         )",
         hasAnyFlags(_device->getFormatFeatures(gfx::Format::RGBA32F), gfx::FormatFeature::RENDER_TARGET | gfx::FormatFeature::SAMPLED_TEXTURE),
         _clusterEnabled ? 1 : 0,
         _device->getCapabilities().maxVertexUniformVectors,
         _device->getCapabilities().maxFragmentUniformVectors,
-        _device->hasFeature(gfx::Feature::INPUT_ATTACHMENT_BENEFIT));
+        _device->hasFeature(gfx::Feature::INPUT_ATTACHMENT_BENEFIT),
+        SkinningJointCapacity::JOINT_UNIFORM_CAPACITY);
 }
 
 RenderStage *RenderPipeline::getRenderstageByName(const String &name) const {

native/cocos/scene/SkinningModel.h
@@ -32,6 +32,7 @@
 #include "renderer/gfx-base/GFXDescriptorSet.h"
 #include "renderer/pipeline/Define.h"
 #include "scene/Model.h"
+#include "../renderer/gfx-validator/DeviceValidator.h"
 namespace cc {
 namespace scene {
 
@@ -52,6 +53,25 @@ struct JointInfo {
     std::vector<uint32_t>       indices;
 };
 
+class RealTimeJointTexture {
+public:
+    RealTimeJointTexture() {
+        textures.clear();
+    }
+
+    ~RealTimeJointTexture() {
+        textures.clear();
+        if (buffer) {
+            delete buffer;
+        }
+    }
+
+    static const int WIDTH = 256;
+    static const int HEIGHT = 3;
+    std::vector<gfx::Texture *> textures;
+    float *buffer = nullptr;
+};
+
 class SkinningModel : public Model {
 public:
     SkinningModel()                      = default;
@@ -67,9 +87,27 @@ public:
         _joints        = std::move(joints);
     }
     inline void updateLocalDescriptors(uint32_t submodelIdx, gfx::DescriptorSet *descriptorset) {
-        gfx::Buffer *buffer = _buffers[_bufferIndices[submodelIdx]];
+        uint32_t idx = _bufferIndices[submodelIdx];
+        gfx::Buffer *buffer = _buffers[idx];
         if (buffer) {
             descriptorset->bindBuffer(pipeline::UBOSkinning::BINDING, buffer);
+        }
+        if (!_realTimeTextureMode) return;
+        if (_realTimeJointTexture->textures.size() < idx + 1) return;
+        gfx::Texture *texture = _realTimeJointTexture->textures[idx];
+        if (texture) {
+            gfx::SamplerInfo info{
+                gfx::Filter::POINT,
+                gfx::Filter::POINT,
+                gfx::Filter::NONE,
+                gfx::Address::CLAMP,
+                gfx::Address::CLAMP,
+                gfx::Address::CLAMP,
+            };
+            auto device = cc::gfx::Device::getInstance();
+            auto sampler = device->getSampler(info);
+            descriptorset->bindTexture(pipeline::UBOSkinning::REALTIME_JOINTS, texture);
+            descriptorset->bindSampler(pipeline::UBOSkinning::REALTIME_JOINTS, sampler);
         }
     }
     inline void setNeedUpdate(bool needUpdate) {
@@ -77,6 +115,7 @@ public:
     }
     void updateTransform(uint32_t stamp) override;
     void updateUBOs(uint32_t stamp) override;
+    void setRealTimeJointTextures(std::vector<gfx::Texture *> textures);
 
 protected:
     ModelType _type{ModelType::SKINNING};
@@ -84,13 +123,17 @@ protected:
 private:
     static void                                                    uploadJointData(uint32_t base, const Mat4 &mat, float *dst);
     void                                                           updateWorldMatrix(JointInfo *info, uint32_t stamp);
+    void                                                           updateRealTimeJointTextureBuffer();
     bool                                                           _needUpdate{false};
     Mat4                                                           _worldMatrix;
     std::vector<uint32_t>                                          _bufferIndices;
     std::vector<gfx::Buffer *>                                     _buffers;
     std::vector<JointInfo>                                         _joints;
-    std::vector<std::array<float, pipeline::UBOSkinning::COUNT> *> _dataArray;
+    std::vector<float *>                                           _dataArray;
     static std::vector<JointTransform *>                           transStacks;
+    bool                                                           _realTimeTextureMode = false;
+    RealTimeJointTexture                                           *_realTimeJointTexture;
+};
 };
 
 } // namespace scene

cocos/cocos-engine

native/cocos/renderer/pipeline/Define.h
@@ -45,7 +45,10 @@ class RenderFlow;
 // Skinning models with number of bones more than this capacity will be automatically switched to texture skinning.
 // But still, you can tweak this for your own need by changing the number below
 // and the JOINT_UNIFORM_CAPACITY macro in cc-skinning shader header.
-constexpr int JOINT_UNIFORM_CAPACITY = 30;
+class CC_DLL SkinningJointCapacity {
+public:
+    static uint jointUniformCapacity;
+};
 
 constexpr float SHADOW_CAMERA_MAX_FAR    = 2000.0F;
 const float     COEFFICIENT_OF_EXPANSION = 2.0F * sqrtf(3.0F);
@@ -330,14 +333,15 @@ struct CC_DLL UBOSkinningAnimation {
     static const String                          NAME;
 };
 
-struct CC_DLL UBOSkinning {
-    static constexpr uint                        JOINTS_OFFSET = 0;
-    static constexpr uint                        COUNT         = UBOSkinning::JOINTS_OFFSET + JOINT_UNIFORM_CAPACITY * 12;
-    static constexpr uint                        SIZE          = UBOSkinning::COUNT * 4;
-    static constexpr uint                        BINDING       = static_cast<uint>(ModelLocalBindings::UBO_SKINNING_TEXTURE);
+class CC_DLL UBOSkinning {
+public:
+    static uint                                  count;
+    static uint                                  size;
+    static constexpr uint                        BINDING            = static_cast<uint>(ModelLocalBindings::UBO_SKINNING_TEXTURE);
     static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
-    static const gfx::UniformBlock               LAYOUT;
+    static gfx::UniformBlock                     layout;
     static const String                          NAME;
+    static void initLayout (uint capacity);
 };
 
 struct CC_DLL UBOMorph {
@@ -522,6 +526,13 @@ struct CC_DLL DIFFUSEMAP : public Object {
 };
 
 struct CC_DLL JOINTTEXTURE : public Object {
+    static constexpr uint                        BINDING = static_cast<uint>(ModelLocalBindings::SAMPLER_JOINTS);
+    static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
+    static const gfx::UniformSamplerTexture      LAYOUT;
+    static const String                          NAME;
+};
+
+struct CC_DLL REALTIMEJOINTTEXTURE : public Object {
     static constexpr uint                        BINDING = static_cast<uint>(ModelLocalBindings::SAMPLER_JOINTS);
     static const gfx::DescriptorSetLayoutBinding DESCRIPTOR;
     static const gfx::UniformSamplerTexture      LAYOUT;

native/cocos/renderer/pipeline/RenderPipeline.cpp
@@ -291,12 +291,14 @@ void RenderPipeline::generateConstantMacros() {
 #define CC_DEVICE_CAN_BENEFIT_FROM_INPUT_ATTACHMENT %d
 #define CC_PLATFORM_ANDROID_AND_WEBGL 0
 #define CC_ENABLE_WEBGL_HIGHP_STRUCT_VALUES 0
+#define CC_JOINT_UNIFORM_CAPACITY %d
         )",
         hasAnyFlags(_device->getFormatFeatures(gfx::Format::RGBA32F), gfx::FormatFeature::RENDER_TARGET | gfx::FormatFeature::SAMPLED_TEXTURE),
         _clusterEnabled ? 1 : 0,
         _device->getCapabilities().maxVertexUniformVectors,
         _device->getCapabilities().maxFragmentUniformVectors,
-        _device->hasFeature(gfx::Feature::INPUT_ATTACHMENT_BENEFIT));
+        _device->hasFeature(gfx::Feature::INPUT_ATTACHMENT_BENEFIT),
+        SkinningJointCapacity::jointUniformCapacity);
 }
 
 RenderStage *RenderPipeline::getRenderstageByName(const String &name) const {

native/cocos/scene/SkinningModel.h
@@ -32,6 +32,7 @@
 #include "renderer/gfx-base/GFXDescriptorSet.h"
 #include "renderer/pipeline/Define.h"
 #include "scene/Model.h"
+#include "../renderer/gfx-validator/DeviceValidator.h"
 namespace cc {
 namespace scene {
 
@@ -52,6 +53,23 @@ struct JointInfo {
     std::vector<uint32_t>       indices;
 };
 
+class RealTimeJointTexture {
+public:
+    RealTimeJointTexture() {
+        textures.clear();
+    }
+
+    ~RealTimeJointTexture() {
+        textures.clear();
+        delete buffer;
+    }
+
+    static const uint32_t WIDTH = 256;
+    static const uint32_t HEIGHT = 3;
+    std::vector<gfx::Texture *> textures;
+    float *buffer = nullptr;
+};
+
 class SkinningModel : public Model {
 public:
     SkinningModel()                      = default;
@@ -67,9 +85,27 @@ public:
         _joints        = std::move(joints);
     }
     inline void updateLocalDescriptors(uint32_t submodelIdx, gfx::DescriptorSet *descriptorset) {
-        gfx::Buffer *buffer = _buffers[_bufferIndices[submodelIdx]];
+        uint32_t idx = _bufferIndices[submodelIdx];
+        gfx::Buffer *buffer = _buffers[idx];
         if (buffer) {
             descriptorset->bindBuffer(pipeline::UBOSkinning::BINDING, buffer);
+        }
+        if (!_realTimeTextureMode) return;
+        if (_realTimeJointTexture->textures.size() < idx + 1) return;
+        gfx::Texture *texture = _realTimeJointTexture->textures[idx];
+        if (texture) {
+            gfx::SamplerInfo info{
+                gfx::Filter::POINT,
+                gfx::Filter::POINT,
+                gfx::Filter::NONE,
+                gfx::Address::CLAMP,
+                gfx::Address::CLAMP,
+                gfx::Address::CLAMP,
+            };
+            auto *device = cc::gfx::Device::getInstance();
+            auto *sampler = device->getSampler(info);
+            descriptorset->bindTexture(pipeline::REALTIMEJOINTTEXTURE::BINDING, texture);
+            descriptorset->bindSampler(pipeline::REALTIMEJOINTTEXTURE::BINDING, sampler);
         }
     }
     inline void setNeedUpdate(bool needUpdate) {
@@ -77,6 +113,7 @@ public:
     }
     void updateTransform(uint32_t stamp) override;
     void updateUBOs(uint32_t stamp) override;
+    void setRealTimeJointTextures(std::vector<gfx::Texture *> textures);
 
 protected:
     ModelType _type{ModelType::SKINNING};
@@ -84,13 +121,16 @@ protected:
 private:
     static void                                                    uploadJointData(uint32_t base, const Mat4 &mat, float *dst);
     void                                                           updateWorldMatrix(JointInfo *info, uint32_t stamp);
+    void                                                           updateRealTimeJointTextureBuffer();
     bool                                                           _needUpdate{false};
     Mat4                                                           _worldMatrix;
     std::vector<uint32_t>                                          _bufferIndices;
     std::vector<gfx::Buffer *>                                     _buffers;
     std::vector<JointInfo>                                         _joints;
-    std::vector<std::array<float, pipeline::UBOSkinning::COUNT> *> _dataArray;
+    std::vector<float *>                                           _dataArray;
     static std::vector<JointTransform *>                           transStacks;
+    bool                                                           _realTimeTextureMode = false;
+    RealTimeJointTexture                                           *_realTimeJointTexture;
 };
 
 } // namespace scene