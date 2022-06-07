f7229701215c4321769ac7c2c2a00d2e28d257eb
yanOO1497 <yuanyuan1497@foxmail.com>
2022-06-07 17:23:30 +0800

    remove linux

--- scripts/native-pack-tool/source/index.ts
+++ scripts/native-pack-tool/source/index.ts
@@ -3,7 +3,6 @@ import { nativePackToolMg } from './base/manager';
 import { MacPackTool } from './platforms/mac';
 import { WindowsPackTool } from './platforms/windows';
 import { AndroidPackTool } from './platforms/android';
-import { LinuxPackTool } from './platforms/linux';
 import { OHOSPackTool } from './platforms/ohos';
 import { IOSPackTool } from './platforms/ios';
 
@@ -11,7 +10,6 @@ nativePackToolMg.register('ios', new IOSPackTool());
 nativePackToolMg.register('mac', new MacPackTool());
 nativePackToolMg.register('windows', new WindowsPackTool());
 nativePackToolMg.register('android', new AndroidPackTool());
-nativePackToolMg.register('linux', new LinuxPackTool());
 nativePackToolMg.register('ohos', new OHOSPackTool());
 
 export * from './base/manager';
