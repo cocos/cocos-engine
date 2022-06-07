69434d6ee9c3ab66d00749a620a597369dc7f7df
troublemaker52025 <troublemaker52025@outlook.com>
2022-06-07 10:17:27 +0800

    fix ci error

--- cocos/core/renderer/scene/index.jsb.ts
+++ cocos/core/renderer/scene/index.jsb.ts
@@ -233,9 +233,9 @@ export const CSMLevel = Enum({
 /**
  * @zh 级联阴影性能优化模式。
  * @en The CSM performance optimization mode
- * @enum Shadows.CSMPerformanceOptimizationMode
+ * @enum Shadows.CSMOptimizationMode
  */
- export const CSMPerformanceOptimizationMode = Enum({
+ export const CSMOptimizationMode = Enum({
     /**
      * @zh 没有性能优化
      * @en has no performance optimization
