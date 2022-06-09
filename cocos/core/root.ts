2493fb95a702fac2e2c704a425d534b06ee1a833
PP_Pro <chinese1236@qq.com>
2022-06-09 18:30:50 +0800

    Revert "remove SERVER_MODE usage"
    This reverts commit 7e561c9f585e16ef9b319018ec87f51bc195fa49.

--- cocos/core/root.ts
+++ cocos/core/root.ts
@@ -23,6 +23,7 @@
  THE SOFTWARE.
  */
 
+import { JSB, SERVER_MODE } from 'internal:constants';
 import { builtinResMgr } from './builtin';
 import { Pool } from './memop';
 import { RenderPipeline, createDefaultPipeline, DeferredPipeline } from './pipeline';
@@ -489,6 +490,9 @@ export class Root {
 
             legacyCC.director.emit(legacyCC.Director.EVENT_BEFORE_COMMIT);
             cameraList.sort((a: Camera, b: Camera) => a.priority - b.priority);
+            if (!SERVER_MODE) {
+                this._pipeline.render(cameraList);
+            }
             this._device.present();
         }
 
