d2ada3f13f450ba0589aa8968d92a6fed9c7a2d1
PP_Pro <chinese1236@qq.com>
2022-06-11 16:07:44 +0800

    use windowSize instead of screenSize for minigame platforms

--- pal/minigame/runtime.ts
+++ pal/minigame/runtime.ts
@@ -42,16 +42,7 @@ Object.defineProperty(minigame, 'orientation', {
     },
 });
 
-if (VIVO) {
-    // TODO: need to be handled in ral lib.
-    minigame.getSystemInfoSync = function () {
-        const sys = ral.getSystemInfoSync() as SystemInfo;
-        // on VIVO, windowWidth should be windowHeight when it is landscape
-        sys.windowWidth = sys.screenWidth;
-        sys.windowHeight = sys.screenHeight;
-        return sys;
-    };
-} else if (LINKSURE || COCOSPLAY) {
+if (LINKSURE || COCOSPLAY) {
     // TODO: update system info when view resized, currently the resize callback is not supported.
     let cachedSystemInfo = ral.getSystemInfoSync() as SystemInfo;
     minigame.onWindowResize?.(() => {
