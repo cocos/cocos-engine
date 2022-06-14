16001ddd0800472f386ec3d0af0de15010444f48
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-06-14 15:25:43 +0800

    refactoring pass

--- cocos/core/renderer/core/pass.ts
+++ cocos/core/renderer/core/pass.ts
@@ -162,11 +162,6 @@ export class Pass {
     protected _buffers: Buffer[] = [];
     protected _descriptorSet: DescriptorSet = null!;
     protected _pipelineLayout: PipelineLayout = null!;
-    // transient resources
-    protected _textures: Map<string, Texture> = new Map<string, Texture>();
-    protected _samplers: Map<string, Sampler> = new Map<string, Sampler>();
-    protected _textureArrays: Map<string, Texture[]> = new Map<string, Texture[]>();
-    protected _samplerArrays: Map<string, Sampler[]> = new Map<string, Sampler[]>();
     // internal data
     protected _passIndex = 0;
     protected _propertyIndex = 0;
@@ -401,11 +396,11 @@ export class Pass {
         for (let i = 0; i < textures.length; ++i) {
             const texture = textures[i];
             if (texture) {
-                this.bindTexture(binding, textures[i], i);
+                this.bindTexture(binding, texture, i);
             }
             const sampler = samplers[i];
             if (sampler) {
-                this.bindSampler(binding, samplers[i], i);
+                this.bindSampler(binding, sampler, i);
             }
         }
     }
