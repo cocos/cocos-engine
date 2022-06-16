80edcb487adcb7920ab8af39e9f8e0c8124c654a
minggo <nudtzxm@gmail.com>
2022-06-16 11:54:21 +0800

    fix clang tidy error

--- native/cocos/base/memory/Memory.h
+++ native/cocos/base/memory/Memory.h
@@ -64,8 +64,8 @@ public:
 };
 } // namespace cc
 
-#define ccnew                new (std::nothrow)
-#define ccnew_placement(...) new (__VA_ARGS__)
+#define ccnew                new (std::nothrow) //NOLINT(readability-identifier-naming)
+#define ccnew_placement(...) new (__VA_ARGS__) //NOLINT(readability-identifier-naming)
 
 #define CC_SAFE_DELETE(ptr) \
     if (ptr) {              \
