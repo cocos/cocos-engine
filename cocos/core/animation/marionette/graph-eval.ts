3184aaebb1c2830318a4285197e1f3703ddda4d5
Leslie Leigh <leslieleigh@outlook.com>
2022-06-17 10:47:47 +0800

    Tweak

--- cocos/core/animation/marionette/graph-eval.ts
+++ cocos/core/animation/marionette/graph-eval.ts
@@ -5,7 +5,7 @@ import {
 } from './animation-graph';
 import { assertIsTrue, assertIsNonNullable } from '../../data/utils/asserts';
 import { MotionEval, MotionEvalContext } from './motion';
-import { Node } from '../../scene-graph/node';
+import type { Node } from '../../scene-graph/node';
 import { createEval } from './create-eval';
 import { Value, VarInstance, TriggerResetMode } from './variable';
 import { BindContext, bindOr, validateVariableExistence, validateVariableType, VariableType } from './parametric';
