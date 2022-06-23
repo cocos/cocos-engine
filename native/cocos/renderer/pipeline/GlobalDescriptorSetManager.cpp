fcc39c7e4640a94517522944e5fd9fac5819e3f9
minggo <nudtzxm@gmail.com>
2022-06-23 17:48:37 +0800

    use IntrusivePtr to share objects

--- native/cocos/renderer/pipeline/GlobalDescriptorSetManager.cpp
+++ native/cocos/renderer/pipeline/GlobalDescriptorSetManager.cpp
@@ -53,9 +53,7 @@ void GlobalDSManager::activate(gfx::Device *device) {
     });
 
     setDescriptorSetLayout();
-    CC_SAFE_DESTROY_NULL(_descriptorSetLayout)
     _descriptorSetLayout = device->createDescriptorSetLayout({globalDescriptorSetLayout.bindings});
-    CC_SAFE_DESTROY_NULL(_globalDescriptorSet)
     _globalDescriptorSet = device->createDescriptorSet({_descriptorSetLayout});
 }
 
@@ -147,16 +145,12 @@ gfx::DescriptorSet *GlobalDSManager::getOrCreateDescriptorSet(uint32_t idx) {
 }
 
 void GlobalDSManager::destroy() {
-    for (auto *shadowUBO : _shadowUBOs) {
-        CC_SAFE_DELETE(shadowUBO)
-    }
-    for (auto &pair : _descriptorSetMap) {
-        CC_SAFE_DELETE(pair.second)
-    }
+    _shadowUBOs.clear();
     _descriptorSetMap.clear();
-
-    CC_SAFE_DESTROY_NULL(_descriptorSetLayout)
-    CC_SAFE_DELETE(_globalDescriptorSet)
+    _descriptorSetLayout = nullptr;
+    _globalDescriptorSet = nullptr;
+    _linearSampler = nullptr;
+    _pointSampler = nullptr;
 }
 
 void GlobalDSManager::setDescriptorSetLayout() {
