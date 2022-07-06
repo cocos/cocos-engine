0759483c6f5f47cee7d1fa01503ae6ead2f98dea
yiwenxue <15225434259xue@gmail.com>
2022-07-06 12:11:31 +0800

    fix es lint

--- cocos/core/pipeline/render-instanced-queue.ts
+++ cocos/core/pipeline/render-instanced-queue.ts
@@ -89,8 +89,7 @@ export class RenderInstancedQueue {
      */
     public recordCommandBuffer (device: Device, renderPass: RenderPass, cmdBuff: CommandBuffer,
         descriptorSet: DescriptorSet | null = null, dynamicOffsets?: Readonly<number[]>) {
-
-        const it = this._renderQueue.length === 0 ? this.queue.values() : this._renderQueue.values(); 
+        const it = this._renderQueue.length === 0 ? this.queue.values() : this._renderQueue.values();
         let res = it.next();
 
         while (!res.done) {
