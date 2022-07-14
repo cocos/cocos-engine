016418626df79f545c6b8c7d1cda7f24fc37e406
PP <chinese1236@qq.com>
2022-07-14 17:17:32 +0800

    update deprecated info (#11985)

--- cocos/input/deprecated-3.3.0.ts
+++ cocos/input/deprecated-3.3.0.ts
@@ -2,7 +2,7 @@ import { deprecateModuleExportedName } from '../core/utils/x-deprecated';
 
 deprecateModuleExportedName({
     SystemEventType: {
-        newName: 'SystemEvent.EventType',
+        newName: 'Input.EventType',
         since: '3.3.0',
         removed: false,
     },
