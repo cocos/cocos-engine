e44e58b68aba2db4175c50a3007e6887961d2ea7
Patrice Jiang <397136899@qq.com>
2022-07-18 09:07:51 +0800

    optimize fillVertexBuffers

--- native/cocos/2d/renderer/Batcher2d.h
+++ native/cocos/2d/renderer/Batcher2d.h
@@ -95,17 +95,10 @@ private:
         uint8_t stride = drawInfo->getStride();
         uint32_t size = drawInfo->getVbCount() * stride;
         float* vbBuffer = drawInfo->getVbBuffer();
-
-        Vec3 temp;
-        uint32_t offset = 0;
         for (int i = 0; i < size; i += stride) {
             Render2dLayout* curLayout = drawInfo->getRender2dLayout(i);
-            temp.transformMat4(curLayout->position, matrix);
-
-            offset = i;
-            vbBuffer[offset++] = temp.x;
-            vbBuffer[offset++] = temp.y;
-            vbBuffer[offset++] = temp.z;
+            static_assert(sizeof(Vec3) == 3 * sizeof(float));
+            reinterpret_cast<Vec3*>(vbBuffer + i)->transformMat4(curLayout->position, matrix);
         }
     }
 
