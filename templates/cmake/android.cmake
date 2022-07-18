826fb77601075c8f145b9f7d1135fe1bee06799e
James Chen <dumganhar@gmail.com>
2022-07-18 11:53:07 +0800

    Set -mfpu=neon-fp16 only for armeabi-v7a.

--- templates/cmake/android.cmake
+++ templates/cmake/android.cmake
@@ -3,8 +3,14 @@
 macro(cc_android_before_target target_name)
     list(APPEND CC_ALL_SOURCES ${CC_COMMON_SOURCES} ${CC_PROJ_SOURCES})
 
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon-fp16 -fsigned-char -ffunction-sections -fdata-sections -fstrict-aliasing")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp16 -fsigned-char -ffunction-sections -fdata-sections -fstrict-aliasing -frtti -fexceptions")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsigned-char -ffunction-sections -fdata-sections -fstrict-aliasing")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsigned-char -ffunction-sections -fdata-sections -fstrict-aliasing -frtti -fexceptions")
+
+if("${ANDORID_ABI}" STREQUAL "armeabi-v7a")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon-fp16")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp16")
+endif()
+
     set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fvisibility=default -fno-omit-frame-pointer")
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -fvisibility=hidden")
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fvisibility=default -fno-omit-frame-pointer")
