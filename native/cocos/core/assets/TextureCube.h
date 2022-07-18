ae6ec2407bf2fb869170ee2f411e7173ea84ba41
James Chen <dumganhar@gmail.com>
2022-07-18 11:35:52 +0800

    Update TextureCube.cpp

--- native/cocos/core/assets/TextureCube.h
+++ native/cocos/core/assets/TextureCube.h
@@ -203,7 +203,7 @@ public:
         return _mipmaps.empty() ? nullptr : &_mipmaps[0];
     }
 
-    void setImage(const ITextureCubeMipmap &value);
+    void setImage(const ITextureCubeMipmap *value);
 
     /**
      * @en Reset the current texture with given size, pixel format and mipmap images.
