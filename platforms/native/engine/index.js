14f52def5ea36614072b9d12cdf75866285c0686
timlyeee <timelyeee@gmail.com>
2022-07-20 15:10:59 +0800

    Move reflection and bridge implementation from jsb to native

--- platforms/native/engine/index.js
+++ platforms/native/engine/index.js
@@ -22,9 +22,6 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-require('./jsb-reflection.js');
-require('./jsb-bridge.js');
-require('./jsb-bridge-wrapper.js');
 require('./jsb-assets-manager.js');
 
 require('./jsb-game.js');
