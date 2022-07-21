28641051744956e3035f0b848675917efd804111
troublemaker52025 <troublemaker52025@outlook.com>
2022-07-21 13:06:21 +0800

    fix ci error

--- native/cocos/renderer/pipeline/PipelineUBO.cpp
+++ native/cocos/renderer/pipeline/PipelineUBO.cpp
@@ -245,8 +245,9 @@ void PipelineUBO::updateShadowUBOView(const RenderPipeline *pipeline, ccstd::arr
                         const float csmViewDir2[4] = {matShadowView.m[2], matShadowView.m[6], matShadowView.m[10], 0.0F};
                         memcpy(cv.data() + UBOCSM::CSM_VIEW_DIR_2_OFFSET + i * 4, &csmViewDir2, sizeof(csmViewDir2));
 
-
-                        memcpy(cv.data() + UBOCSM::CSM_ATLAS_OFFSET + i * 4, &csmLayers->getLayers()[i]->getCSMAtlas(), sizeof(Vec4));
+                        const auto &csmAtlas = csmLayers->getLayers()[i]->getCSMAtlas();
+                        const float csmAtlasOffsets[4] = {csmAtlas.x, csmAtlas.y, csmAtlas.z, csmAtlas.w};
+                        memcpy(cv.data() + UBOCSM::CSM_ATLAS_OFFSET + i * 4, &csmAtlasOffsets, sizeof(csmAtlasOffsets));
 
 
                         cv[UBOCSM::CSM_SPLITS_INFO_OFFSET + i] = csmLayers->getLayers()[i]->getSplitCameraFar() / mainLight->getShadowDistance();
