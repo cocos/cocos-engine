689d49cfec9e2132fb0f5363da294df4843af5e0
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-07-22 10:51:43 +0800

    fix build error

--- native/cocos/renderer/pipeline/custom/NativePipelineImpl.cpp
+++ native/cocos/renderer/pipeline/custom/NativePipelineImpl.cpp
@@ -338,8 +338,9 @@ bool NativePipeline::activate(gfx::Swapchain *swapchainIn) {
     macros["CC_PIPELINE_TYPE"] = 0;
     globalDSManager->activate(device);
     pipelineSceneData->activate(device);
+#if CC_USE_DEBUG_RENDERER
     DebugRenderer::getInstance()->activate(device);
-
+#endif
     // generate macros here rather than construct func because _clusterEnabled
     // switch may be changed in root.ts setRenderPipeline() function which is after
     // pipeline construct.
