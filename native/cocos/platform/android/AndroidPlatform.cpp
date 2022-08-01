aae31337c31dfac31da1349dcc9f65bb825a13f6
bofeng.song <bofeng.song@chukong-inc.com>
2022-08-01 11:49:10 +0800

    refine

--- native/cocos/platform/android/AndroidPlatform.cpp
+++ native/cocos/platform/android/AndroidPlatform.cpp
@@ -206,23 +206,23 @@ public:
         if (motionEvent->pointerCount > 0) {
             int action = motionEvent->action;
             int actionMasked = action & AMOTION_EVENT_ACTION_MASK;
-            int index = -1;
+            int eventChangedIndex = -1;
 
             if (actionMasked == AMOTION_EVENT_ACTION_DOWN ||
                 actionMasked == AMOTION_EVENT_ACTION_POINTER_DOWN) {
                 if (actionMasked == AMOTION_EVENT_ACTION_POINTER_DOWN) {
-                    index = action >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+                    eventChangedIndex = action >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
                 } else {
-                    index = 0;
+                    eventChangedIndex = 0;
                 }
                 touchEvent.type = cc::TouchEvent::Type::BEGAN;
             } else if (actionMasked == AMOTION_EVENT_ACTION_UP ||
                        actionMasked == AMOTION_EVENT_ACTION_POINTER_UP) {
                 touchEvent.type = cc::TouchEvent::Type::ENDED;
                 if (actionMasked == AMOTION_EVENT_ACTION_POINTER_UP) {
-                    index = action >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+                    eventChangedIndex = action >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
                 } else {
-                    index = 0;
+                    eventChangedIndex = 0;
                 }
             } else if (actionMasked == AMOTION_EVENT_ACTION_CANCEL) {
                 touchEvent.type = cc::TouchEvent::Type::CANCELLED;
@@ -232,8 +232,8 @@ public:
                 return false;
             }
 
-            if (index >= 0) {
-                addTouchEvent(index, motionEvent);
+            if (eventChangedIndex >= 0) {
+                addTouchEvent(eventChangedIndex, motionEvent);
             } else {
                 for (int i = 0; i < motionEvent->pointerCount; i++) {
                     addTouchEvent(i, motionEvent);
@@ -430,7 +430,6 @@ private:
         touchEvent.touches.emplace_back(x, y, id);
     }
 
-private:
     AppEventCallback _eventCallback{nullptr};
     AndroidPlatform *_androidPlatform{nullptr};
     JNIEnv *_jniEnv{nullptr};         // JNI environment
