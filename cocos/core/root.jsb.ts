b931d1443ca812d3d84534d25c88598a24f77fb9
ChiaNing <sun.clooud.mo@gmail.com>
2022-08-03 19:10:29 +0800

    remove

--- cocos/core/root.jsb.ts
+++ cocos/core/root.jsb.ts
@@ -209,3 +209,15 @@ rootProto.setRenderPipeline = function (pipeline) {
     this._createBatcher2D();
     return ppl;
 }
+
+rootProto.addBatch = function (batch) {
+    console.error('The Draw Batch class is implemented differently in the native platform and does not support calling this interface using jsb.')
+}
+
+rootProto.removeBatch = function (batch) {
+    console.error('The Draw Batch class is implemented differently in the native platform and does not support calling this interface using jsb.')
+}
+
+rootProto.removeBatches = function () {
+    console.error('The Draw Batch class is implemented differently in the native platform and does not support calling this interface using jsb.')
+}
