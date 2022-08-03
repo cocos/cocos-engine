c0c0ec4d511c9f1c5986629d5c691e07fc2e7c64
mmyduckx <308303735@qq.com>
2022-08-03 18:24:56 +0800

    add Node::getPathInHierarchy

--- native/cocos/core/scene-graph/Node.cpp
+++ native/cocos/core/scene-graph/Node.cpp
@@ -886,6 +886,16 @@ void Node::clearNodeArray() {
     }
 }
 
+ccstd::string Node::getPathInHierarchy() const {
+    ccstd::string result = getName();
+    Node *curNode = getParent();
+    while (curNode && dynamic_cast<Scene *>(curNode) == nullptr) {
+        result = ccstd::string(curNode->getName()).append("/").append(result);
+        curNode = curNode->getParent();
+    }
+    return result;
+}
+
 void Node::translate(const Vec3 &trans, NodeSpace ns) {
     Vec3 v3Temp{trans};
     if (ns == NodeSpace::LOCAL) {
