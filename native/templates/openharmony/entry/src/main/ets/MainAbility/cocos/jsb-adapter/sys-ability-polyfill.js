16da2f731b94e5c50fc32344f5c12a410c4cfe3e
yangchao <1162485779@qq.com>
2022-08-12 18:49:09 +0800

    Add oh template  (#12408)

---
+++ native/templates/openharmony/entry/src/main/ets/MainAbility/cocos/jsb-adapter/sys-ability-polyfill.js
@@ -0,0 +1,21 @@
+import display from '@ohos.display';
+
+window.oh = {};
+
+module.exports = function systemReady () {
+    return new Promise(resolve => {
+        if (typeof XMLHttpRequest === 'undefined') {
+            window.XMLHttpRequest = function () {}
+        }
+        display.getDefaultDisplay((err, data) => {
+            window.oh.display = data;
+            let device = jsb.device;
+            
+            //https://developer.harmonyos.com/cn/docs/documentation/doc-references/js-apis-display-0000001281001106
+            device.getDevicePixelRatio = function () { return data.densityPixels; }
+            device.getDeviceOrientation = function () { return data.rotation; }
+        });
+        resolve();
+    })
+
+}
