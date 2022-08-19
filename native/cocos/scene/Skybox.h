4cd005b967376d6299dc398d1e6e6cd970fcef54
xubing <58895777@qq.com>
2022-08-19 16:30:30 +0800

    fix ci error

--- native/cocos/scene/Skybox.h
+++ native/cocos/scene/Skybox.h
@@ -190,7 +190,7 @@ public:
     bool _useHDR{true};
     EnvironmentLightingType _envLightingType{EnvironmentLightingType::HEMISPHERE_DIFFUSE};
     IntrusivePtr<Material> _editableMaterial;
-    float _rotationAngle{0.F};
+    float _rotationAngle{0.0F};
     Skybox *_resource{nullptr};
 };
 
@@ -310,7 +310,7 @@ private:
     bool _useDiffuseMap{false};
     bool _activated{false};
     IntrusivePtr<Material> _editableMaterial;
-    float _rotationAngle{0.F};
+    float _rotationAngle{0.0F};
     CC_DISALLOW_COPY_MOVE_ASSIGN(Skybox);
 };
 
