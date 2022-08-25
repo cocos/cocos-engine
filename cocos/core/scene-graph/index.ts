95afd9a3ba351df98c97e3b503bcdf6620ad899f
mmyduckx <308303735@qq.com>
2022-08-25 17:55:36 +0800

    fix remove BaseNode PR install fail

--- cocos/core/scene-graph/index.ts
+++ cocos/core/scene-graph/index.ts
@@ -27,8 +27,8 @@ import './node-event-processor';
 import './deprecated';
 import './deprecated-3.7.0';
 // @deprecated since v3.7, please use Node instead.
-export { Node as BaseNode } from './node'; //reserve BaseNode for compatibility
 export { Node } from './node';
+export { Node as BaseNode } from './node'; //reserve BaseNode for compatibility
 export { Scene } from './scene';
 export { Layers } from './layers';
 export { find } from './find';
