b6536813a569701c06389729e3dc8515eb400c65
zhangmingzhen <mingzhen.zhang@cocos.com>
2022-08-26 15:05:41 +0800

    use assert and remove unreasonable and deprecated condition

--- cocos/2d/components/rich-text.ts
+++ cocos/2d/components/rich-text.ts
@@ -758,13 +758,8 @@ export class RichText extends Component {
         for (let i = children.length - 1; i >= 0; i--) {
             const child = children[i];
             if (child.name === RichTextChildName || child.name === RichTextChildImageName) {
-                if (child.parent === this.node) {
-                    child.parent = null;
-                } else {
-                    // In case child.parent !== this.node, child cannot be removed from children
-
-                    children.splice(i, 1);
-                }
+                assert(child.parent === this.node);
+                child.parent = null;
 
                 const segment = createSegment(child.name);
                 segment.node = child;
