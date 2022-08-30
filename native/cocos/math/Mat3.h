400db65d028a02c5912dfa6624544cf5376b975d
James Chen <dumganhar@gmail.com>
2022-08-30 16:58:07 +0800

    Don't inline approxEquals for Mat3 & Mat4.

--- native/cocos/math/Mat3.h
+++ native/cocos/math/Mat3.h
@@ -222,11 +222,7 @@ public:
     /**
      * Determines if this matrix is approximately equal to the given matrix.
      */
-    inline bool approxEquals(const Mat3& v, float precision = CC_FLOAT_CMP_PRECISION) const {
-        return math::isEqualF(m[0], v.m[0], precision) && math::isEqualF(m[1], v.m[1], precision) && math::isEqualF(m[2], v.m[2], precision)
-            && math::isEqualF(m[3], v.m[3], precision) && math::isEqualF(m[4], v.m[4], precision) && math::isEqualF(m[5], v.m[5], precision)
-            && math::isEqualF(m[6], v.m[6], precision) && math::isEqualF(m[7], v.m[7], precision) && math::isEqualF(m[8], v.m[8], precision);
-    }
+    bool approxEquals(const Mat3& v, float precision = CC_FLOAT_CMP_PRECISION) const;
 
     /** equals to a matrix full of zeros */
     static const Mat3 ZERO;
