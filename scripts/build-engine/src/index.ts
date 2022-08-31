fe680def3309343882e010f50ac26b1b2ba6c342
PP_Pro <chinese1236@qq.com>
2022-08-31 15:39:42 +0800

    feat: support webgpu

--- scripts/build-engine/src/index.ts
+++ scripts/build-engine/src/index.ts
@@ -275,7 +275,7 @@ async function doBuild ({
 
     // HACK: get platform, mode, flags from build time constants
     const flags: Record<string, any> = {};
-    ['SERVER_MODE', 'NOT_PACK_PHYSX_LIBS', 'DEBUG', 'NET_MODE'].forEach((key) => {
+    ['SERVER_MODE', 'NOT_PACK_PHYSX_LIBS', 'DEBUG', 'NET_MODE', 'WEBGPU'].forEach((key) => {
         flags[key] = buildTimeConstants[key];
     });
     let platform = options.platform as PlatformType;
