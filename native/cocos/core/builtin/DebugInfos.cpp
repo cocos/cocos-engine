c923074bcad56b68a2bf9bc80389713e67043b7c
Cocos Robot <48829427+cocos-robot@users.noreply.github.com>
2022-09-02 13:39:01 +0800

    [ci skip][AUTO]: Automated code generating update: b4a084b208061fefd566798f56a7ff999173fcb8  (#12656) (#12657)
    Co-authored-by: cocos-robot <cocos-robot@cocos.com>

--- native/cocos/core/builtin/DebugInfos.cpp
+++ native/cocos/core/builtin/DebugInfos.cpp
@@ -454,6 +454,7 @@ ccstd::unordered_map<int, ccstd::string> debugInfos = {
 { 16002, "'%s' is removed since v%s." },
 { 16003, "'%s' is removed since v%s, please use '%s' instead." },
 { 16101, "The effect('%s') you are looking for does not exist, please confirm the effect name in the editor. NOTE: Since 3.6, the name of the built-in effect has been changed to its name in the editor, please check it out. More information please refer to https://docs.cocos.com/creator/manual/en/shader/effect-inspector.html" },
+{ 16201, "The asset replacing failed, can not found override asset('%s') for '%s'" },
 
 };
 }//namespace cc
