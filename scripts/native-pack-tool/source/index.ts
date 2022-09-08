b4be6500eac80bf1dcb2c7972a7b46c2fb9fe0d5
oahcy <1162485779@qq.com>
2022-09-08 17:11:59 +0800

    add oh packtool

--- scripts/native-pack-tool/source/index.ts
+++ scripts/native-pack-tool/source/index.ts
@@ -3,6 +3,7 @@ import { nativePackToolMg } from './base/manager';
 import { MacPackTool } from './platforms/mac';
 import { WindowsPackTool } from './platforms/windows';
 import { AndroidPackTool } from './platforms/android';
+import { OpenHarmonyPackTool } from './platforms/openharmony';
 import { OHOSPackTool } from './platforms/ohos';
 import { IOSPackTool } from './platforms/ios';
 import { HuaweiAGCPackTool } from './platforms/huawei-agc';
@@ -11,6 +12,7 @@ nativePackToolMg.register('ios', new IOSPackTool());
 nativePackToolMg.register('mac', new MacPackTool());
 nativePackToolMg.register('windows', new WindowsPackTool());
 nativePackToolMg.register('android', new AndroidPackTool());
+nativePackToolMg.register('openharmony', new OpenHarmonyPackTool());
 nativePackToolMg.register('ohos', new OHOSPackTool());
 nativePackToolMg.register('huawei-agc', new HuaweiAGCPackTool());
 
