5e2af03bd571befdeceb33bd05ccf10010fef0cb
minggo <nudtzxm@gmail.com>
2022-09-16 15:10:30 +0800

    fix crash on native

--- cocos/core/root.jsb.ts
+++ cocos/core/root.jsb.ts
@@ -6,6 +6,7 @@ import { Device, deviceManager } from '../gfx';
 import { DebugView } from '../rendering/debug-view';
 import { buildDeferredLayout, buildForwardLayout } from '../rendering/custom/effect';
 import { settings, Settings } from './settings';
+import { ForwardPipeline } from '../rendering';
 
 declare const nr: any;
 declare const jsb: any;
@@ -203,7 +204,7 @@ rootProto.setRenderPipeline = function (pipeline) {
     } else {
         if (!pipeline) {
             // pipeline should not be created in C++, ._ctor need to be triggered
-            pipeline = new nr.ForwardPipeline();
+            pipeline = new ForwardPipeline();
             pipeline.init();
         }
         ppl =  oldSetPipeline.call(this, pipeline);
