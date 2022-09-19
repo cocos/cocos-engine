0e2fe0c343bb44d1b11992d3e9fa46f7d483d0f3
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-09-19 17:51:39 +0800

    adding resource graph execution

--- native/cocos/renderer/pipeline/custom/NativeResourceGraph.cpp
+++ native/cocos/renderer/pipeline/custom/NativeResourceGraph.cpp
@@ -26,15 +26,106 @@
 #include "Range.h"
 #include "RenderGraphGraphs.h"
 #include "RenderGraphTypes.h"
-
+#include "cocos/renderer/gfx-base/GFXDevice.h"
+#include "gfx-base/GFXDef-common.h"
+#include "pipeline/custom/RenderCommonFwd.h"
 
 namespace cc {
 
 namespace render {
 
+namespace {
+
+gfx::BufferInfo getBufferInfo(const ResourceDesc& desc) {
+    using namespace gfx; // NOLINT(google-build-using-namespace)
+
+    BufferUsage usage = BufferUsage::TRANSFER_SRC | BufferUsage::TRANSFER_DST;
+    if (any(desc.flags & ResourceFlags::UNIFORM)) {
+        usage |= BufferUsage::UNIFORM;
+    }
+    if (any(desc.flags & ResourceFlags::STORAGE)) {
+        usage |= BufferUsage::STORAGE;
+    }
+    if (any(desc.flags & ResourceFlags::INDIRECT)) {
+        usage |= BufferUsage::INDIRECT;
+    }
+
+    return {
+        usage,
+        MemoryUsage::DEVICE,
+        desc.width,
+        1U,
+        BufferFlagBit::NONE,
+    };
+}
+
+gfx::TextureInfo getTextureInfo(const ResourceDesc& desc, bool bCube = false) {
+    using namespace gfx; // NOLINT(google-build-using-namespace)
+    // type
+    auto type = TextureType::TEX1D;
+    switch (desc.dimension) {
+        case ResourceDimension::TEXTURE1D:
+            if (desc.depthOrArraySize > 1) {
+                type = TextureType::TEX1D_ARRAY;
+            } else {
+                type = TextureType::TEX1D;
+            }
+            break;
+        case ResourceDimension::TEXTURE2D:
+            if (desc.depthOrArraySize > 1) {
+                if (bCube) {
+                    type = TextureType::CUBE;
+                } else {
+                    type = TextureType::TEX2D_ARRAY;
+                }
+            } else {
+                type = TextureType::TEX2D;
+            }
+            break;
+        case ResourceDimension::TEXTURE3D:
+            type = TextureType::TEX3D;
+            break;
+        case ResourceDimension::BUFFER:
+            CC_EXPECTS(false);
+    }
+
+    // usage
+    TextureUsage usage = TextureUsage::SAMPLED | TextureUsage::TRANSFER_SRC | TextureUsage::TRANSFER_DST;
+    if (any(desc.flags & ResourceFlags::COLOR_ATTACHMENT)) {
+        usage |= TextureUsage::COLOR_ATTACHMENT;
+    }
+    if (any(desc.flags & ResourceFlags::DEPTH_STENCIL_ATTACHMENT)) {
+        CC_EXPECTS(!any(desc.flags & ResourceFlags::COLOR_ATTACHMENT));
+        usage |= TextureUsage::DEPTH_STENCIL_ATTACHMENT;
+    }
+    if (any(desc.flags & ResourceFlags::INPUT_ATTACHMENT)) {
+        usage |= TextureUsage::INPUT_ATTACHMENT;
+    }
+    if (any(desc.flags & ResourceFlags::STORAGE)) {
+        usage |= TextureUsage::STORAGE;
+    }
+
+    return {
+        type,
+        usage,
+        desc.format,
+        desc.width,
+        desc.height,
+        desc.textureFlags,
+        type == TextureType::TEX3D ? 1U : desc.depthOrArraySize,
+        desc.mipLevels,
+        desc.sampleCount,
+        type == TextureType::TEX3D ? desc.depthOrArraySize : 1U,
+        nullptr,
+    };
+}
+
+} // namespace
+
 void ResourceGraph::mount(gfx::Device* device, vertex_descriptor vertID) {
     std::ignore = device;
     auto& resg = *this;
+    const auto& desc = get(ResourceGraph::DescTag{}, *this, vertID);
     visitObject(
         vertID, resg,
         [&](const ManagedResource& resource) {
@@ -42,14 +133,18 @@ void ResourceGraph::mount(gfx::Device* device, vertex_descriptor vertID) {
         },
         [&](ManagedBuffer& buffer) {
             if (!buffer.buffer) {
-                // do mount
+                auto info = getBufferInfo(desc);
+                buffer.buffer = device->createBuffer(info);
             }
+            CC_ENSURES(buffer.buffer);
             buffer.fenceValue = nextFenceValue;
         },
         [&](ManagedTexture& texture) {
             if (!texture.texture) {
-                // do mount
+                auto info = getTextureInfo(desc);
+                texture.texture = device->createTexture(info);
             }
+            CC_ENSURES(texture.texture);
             texture.fenceValue = nextFenceValue;
         },
         [&](const IntrusivePtr<gfx::Buffer>& pass) {
