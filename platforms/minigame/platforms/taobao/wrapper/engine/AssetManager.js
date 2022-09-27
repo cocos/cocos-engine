6bb3c084cd083130345c57dd2de7bbf29f1e581e
PP <chinese1236@qq.com>
2022-09-27 10:13:50 +0800

    fix: undefined window.__globalAdapter && loading remote image on Taobao IDE (#12901)
    * fix: undefined window.__globalAdapter
    
    * fix: loading remote image on Taobao IDE

--- platforms/minigame/platforms/taobao/wrapper/engine/AssetManager.js
+++ platforms/minigame/platforms/taobao/wrapper/engine/AssetManager.js
@@ -2,25 +2,48 @@ const parser = cc.assetManager.parser;
 const downloader = cc.assetManager.downloader;
 const debug = cc.debug;
 
-function downloadImage (url, options, onComplete) {
+function downloadImage(url, options, onComplete) {
+    onComplete(null, url);
+}
+
+if (my.isIDE) {
+    // NOTE: image can't load from cached downloaded file on Taobao IDE
+    downloader.register({
+        // Image
+        '.png': downloadImage,
+        '.jpg': downloadImage,
+        '.bmp': downloadImage,
+        '.jpeg': downloadImage,
+        '.gif': downloadImage,
+        '.ico': downloadImage,
+        '.tiff': downloadImage,
+        '.image': downloadImage,
+        '.webp': downloadImage,
+        '.pvr': downloadImage,
+        '.pkm': downloadImage,
+        '.astc': downloadImage,
+    });
+}
+
+function loadImage(url, options, onComplete) {
     const img = window.screencanvas.createImage();
 
     function loadCallback() {
-      img.onload = null;
-      img.onerror = null;
+        img.onload = null;
+        img.onerror = null;
 
-      if (onComplete) {
-        onComplete(null, img);
-      }
+        if (onComplete) {
+            onComplete(null, img);
+        }
     }
 
     function errorCallback() {
-      img.onload = null;
-      img.onerror = null;
+        img.onload = null;
+        img.onerror = null;
 
-      if (onComplete) {
-        onComplete(new Error(debug.getError(4930, url)));
-      }
+        if (onComplete) {
+            onComplete(new Error(debug.getError(4930, url)));
+        }
     }
 
     img.onload = loadCallback;
@@ -29,16 +52,16 @@ function downloadImage (url, options, onComplete) {
     return img;
 }
 
-downloader.downloadDomImage = downloadImage;
+downloader.downloadDomImage = loadImage;
 
 parser.register({
-    '.png': downloadImage,
-    '.jpg': downloadImage,
-    '.bmp': downloadImage,
-    '.jpeg': downloadImage,
-    '.gif': downloadImage,
-    '.ico': downloadImage,
-    '.tiff': downloadImage,
-    '.image': downloadImage,
-    '.webp': downloadImage,
+    '.png': loadImage,
+    '.jpg': loadImage,
+    '.bmp': loadImage,
+    '.jpeg': loadImage,
+    '.gif': loadImage,
+    '.ico': loadImage,
+    '.tiff': loadImage,
+    '.image': loadImage,
+    '.webp': loadImage,
 });
