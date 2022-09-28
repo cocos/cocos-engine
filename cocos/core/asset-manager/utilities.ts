be59381eaf9b9fd422617ce7deb5437e527aad60
zhangxin <13622872688@163.com>
2022-09-28 15:20:50 +0800

    Revert "use confole.error for editor"
    This reverts commit 38844a2e247f91d1f00767dca5ea5af471e103ef.

--- cocos/core/asset-manager/utilities.ts
+++ cocos/core/asset-manager/utilities.ts
@@ -127,27 +127,20 @@ export function setProperties (uuid: string, asset: Asset, assetsMap: Record<str
             const depend = depends[i];
             const dependAsset = assetsMap[`${depend.uuid}@import`];
             if (!dependAsset) {
-                let warningForAssetOverride: string | undefined;
                 if (DEBUG || EDITOR) {
                     const assetManger = (legacyCC.assetManager as typeof import('./asset-manager').default);
                     for (const [origin, target] of assetManger.assetsOverrideMap.entries()) {
-                        warningForAssetOverride = `Asset with UUID  '${target}' is the value of assetManger.assetsOverrideMap['${origin}'],try to use the asset with UUID '${origin}' instead.`;
+                        error(`Asset with UUID  '${target}' is the value of assetManger.assetsOverrideMap['${origin}'],try to use the asset with UUID '${origin}' instead.`);
                         break;
                     }
                 }
                 if (EDITOR) {
-                    if (warningForAssetOverride) {
-                        console.error(warningForAssetOverride);
-                    }
                     if (!missingAssetReporter) {
                         // eslint-disable-next-line new-cap
                         missingAssetReporter = new EditorExtends.MissingReporter.object(asset);
                     }
                     missingAssetReporter.stashByOwner(depend.owner, depend.prop, EditorExtends.serialize.asAsset(depend.uuid));
                 } else {
-                    if (warningForAssetOverride) {
-                        error(warningForAssetOverride);
-                    }
                     error(`The asset ${depend.uuid} is missing!`);
                 }
                 if (depend.type && depend.type !== Asset) {
