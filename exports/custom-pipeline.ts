26c2945f3600763a87b5af6b2fa6c02ac60efde0
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-10-09 14:39:42 +0800

    add rendering module

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -2,8 +2,11 @@ import { createCustomPipeline, addCustomBuiltinPipelines } from '../cocos/render
 import { legacyCC } from '../cocos/core/global-exports';
 import { PipelineBuilder } from '../cocos/rendering/custom/pipeline';
 
-export * as rendering from '../cocos/rendering/custom/public';
+import * as rendering from '../cocos/rendering/custom/public';
 
 legacyCC.internal.createCustomPipeline = createCustomPipeline;
 legacyCC.internal.customPipelineBuilderMap = new Map<string, PipelineBuilder>();
 addCustomBuiltinPipelines(legacyCC.internal.customPipelineBuilderMap);
+
+legacyCC.rendering = rendering;
+export { rendering };
