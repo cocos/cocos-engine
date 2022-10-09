1d914c12415486707af76645ef24f3a4f67c1445
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-10-09 13:31:15 +0800

    try fix global variables are sealed

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -2,7 +2,7 @@ import { createCustomPipeline, addCustomBuiltinPipelines } from '../cocos/render
 import { legacyCC } from '../cocos/core/global-exports';
 import { PipelineBuilder } from '../cocos/rendering/custom/pipeline';
 
-export * as rendering from '../cocos/rendering/custom/public';
+export * as rendering2 from '../cocos/rendering/custom/public';
 
 legacyCC.internal.createCustomPipeline = createCustomPipeline;
 legacyCC.internal.customPipelineBuilderMap = new Map<string, PipelineBuilder>();
