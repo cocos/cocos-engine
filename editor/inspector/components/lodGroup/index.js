99ca65c211bb3cf71128da97ad74782d086a3c7c
changhua <39176194+changhua0118@users.noreply.github.com>
2022-10-13 09:43:19 +0800

    add lod panel to inspector;update lodGroup component;
    (cherry picked from commit 6c09a2d8838f79a6f23830f1754b13aa8dc77719)

---
+++ editor/inspector/components/lodGroup/index.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const lodItem = require('./components/lod-item/lod-item');
+
+module.paths.push(join(Editor.App.path, 'node_modules'));
+const Vue = require('vue/dist/vue.min.js');
+let vm;
+
+exports.style = readFileSync(join(__dirname, './index.css'), 'utf8');
+
+exports.template = '<div id="app"></div>';
+
+exports.methods = {};
+
+exports.$ = {
+    app: '#app',
+};
+
+exports.update = function(dump) {
+    vm.dump = dump;
+    console.log(dump);
+}
+
+exports.ready = function() {
+    vm = new Vue({
+        el: this.$.app,
+        template: readFileSync(join(__dirname, './index.html'), 'utf8'),
+        components: {
+            'lod-item': lodItem,
+        },
+        data() {
+            return {
+                dump: {},
+            }
+        },
+        methods: {
+            onObjectSizeConfirm(event) {
+                const that = this;
+                that.dump.value.size.value = event.target.value;
+                that.updateDump(that.dump.value.size);
+            },
+            updateLODs(operator, index) {
+                const that = this;
+                const LODs = that.dump.value.LODs.value;
+                // TODO: 手动处理 path
+                if (operator === 'insert') {
+                    if (LODs.length >= 8) {
+                        console.warn('Maximum 8 LOD, Can\'t add more LOD');
+                        return;
+                    }
+                    const lodItemData = Object.assign({}, that.dump.value.LODs.elementTypeData);
+                    const preValue = LODs[index].value.screenRelativeTransitionHeight.value;
+                    const nextValue = LODs[index + 1] ? LODs[index + 1].value.screenRelativeTransitionHeight.value : 0;
+                    lodItemData.value.screenRelativeTransitionHeight.value = (preValue + nextValue) / 2;
+                    LODs.splice(index + 1, 0, lodItemData);
+                } else if (operator === 'delete') {
+                    if (LODs.length === 1) {
+                        console.warn('At least one LOD, Can\'t delete any more');
+                        return;
+                    }
+                    LODs.splice(index, 1);
+                }
+                that.updateDump(that.dump.value.LODs);
+            },
+            updateDump(dump) {
+                const that = this;
+                that.$refs['lod-dump'].dump = dump;
+                that.$refs['lod-dump'].dispatch('change-dump');
+            },
+            recalculateBounds() {
+                // TODO: 
+                console.log('recalculateBounds');
+            },
+            resetObjectSize() {
+                // TODO: 
+                console.log('resetObjectSize');
+            },
+            calculateRange(range, index) {
+                const that = this;
+                const LODs = that.dump.value.LODs.value;
+                if (range === 'min') {
+                    return LODs[index + 1] ? LODs[index + 1].value.screenRelativeTransitionHeight.value * 100 : 0;
+                } else if (range === 'max') {
+                    return LODs[index - 1] ? LODs[index - 1].value.screenRelativeTransitionHeight.value * 100 : 100;
+                }
+            },
+        },
+    });
+}
+
+exports.close = function() {
+    vm = null;
+}
