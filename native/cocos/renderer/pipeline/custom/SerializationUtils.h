1333ff1f43a10d164fca06738acbae76043d2c0a
hyde zhou <zlzhou.sh@outlook.com>
2022-10-17 14:43:36 +0800

    [develop] add layout graph serialization (#13020)

--- native/cocos/renderer/pipeline/custom/SerializationUtils.h
+++ native/cocos/renderer/pipeline/custom/SerializationUtils.h
@@ -24,10 +24,15 @@
 ****************************************************************************/
 
 #pragma once
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
 #include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include "cocos/renderer/gfx-base/GFXDef-common.h"
 #include "cocos/renderer/pipeline/custom/ArchiveTypes.h"
@@ -51,6 +56,7 @@ template <class T,
               std::is_enum_v<T> || std::is_integral_v<T> || std::is_floating_point_v<T>,
               bool> = false>
 void save(OutputArchive& ar, const T& v) {
+    static_assert(!std::is_enum_v<T> || sizeof(T) <= sizeof(uint32_t)); // enum can only be 1, 2, 4 bytes
     ar.writeNumber(static_cast<double>(v));
 }
 
@@ -59,6 +65,7 @@ template <class T,
               std::is_enum_v<T> || std::is_integral_v<T> || std::is_floating_point_v<T>,
               bool> = false>
 void load(InputArchive& ar, T& v) {
+    static_assert(!std::is_enum_v<T> || sizeof(T) <= sizeof(uint32_t)); // enum can only be 1, 2, 4 bytes
     v = static_cast<T>(ar.readNumber());
 }
 
@@ -89,6 +96,107 @@ void load(InputArchive& ar, std::vector<T, Allocator>& vec) {
     vec.resize(sz);
     for (auto& v : vec) {
         load(ar, v);
+    }
+}
+
+// set
+template <class Value, class Less, class Allocator>
+void save(OutputArchive& ar, const std::set<Value, Less, Allocator>& set) {
+    save<uint32_t>(ar, static_cast<uint32_t>(set.size()));
+    for (const auto& value : set) {
+        save(ar, value);
+    }
+}
+
+template <class Value, class Less, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::set<Value, Less, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value;
+        load(ar, value);
+        set.emplace(std::move(value));
+    }
+}
+
+template <class Value, class Less, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::set<Value, Less, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value(set.get_allocator());
+        load(ar, value);
+        set.emplace(std::move(value));
+    }
+}
+
+// flat_set
+template <class Value, class Less, class Allocator>
+void save(OutputArchive& ar, const boost::container::flat_set<Value, Less, Allocator>& set) {
+    save<uint32_t>(ar, static_cast<uint32_t>(set.size()));
+    for (const auto& value : set) {
+        save(ar, value);
+    }
+}
+
+template <class Value, class Less, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_set<Value, Less, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    set.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value;
+        load(ar, value);
+        set.emplace(std::move(value));
+    }
+}
+
+template <class Value, class Less, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_set<Value, Less, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    set.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value(set.get_allocator());
+        load(ar, value);
+        set.emplace(std::move(value));
+    }
+}
+
+// unordered_set
+template <class Value, class Hash, class Pred, class Allocator>
+void save(OutputArchive& ar, const std::unordered_set<Value, Hash, Pred, Allocator>& set) {
+    save<uint32_t>(ar, static_cast<uint32_t>(set.size()));
+    for (const auto& value : set) {
+        save(ar, value);
+    }
+}
+
+template <class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_set<Value, Hash, Pred, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value;
+        load(ar, value);
+        set.emplace(std::move(value));
+    }
+}
+
+template <class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_set<Value, Hash, Pred, Allocator>& set) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Value value(set.get_allocator());
+        load(ar, value);
+        set.emplace(std::move(value));
     }
 }
 
@@ -162,19 +270,215 @@ void load(InputArchive& ar, std::map<Key, Value, Less, Allocator>& map) {
     }
 }
 
+// flat_map
+template <class Key, class Value, class Less, class Allocator>
+void save(OutputArchive& ar, const boost::container::flat_map<Key, Value, Less, Allocator>& map) {
+    save<uint32_t>(ar, static_cast<uint32_t>(map.size()));
+    for (const auto& [key, value] : map) {
+        save(ar, key);
+        save(ar, value);
+    }
+}
+
+template <class Key, class Value, class Less, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_map<Key, Value, Less, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    map.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key;
+        Value value;
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Less, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_map<Key, Value, Less, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    map.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key(map.get_allocator());
+        Value value;
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Less, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_map<Key, Value, Less, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    map.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key;
+        Value value(map.get_allocator());
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Less, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, boost::container::flat_map<Key, Value, Less, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    map.reserve(sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key(map.get_allocator());
+        Value value(map.get_allocator());
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+// unordered_map
+template <class Key, class Value, class Hash, class Pred, class Allocator>
+void save(OutputArchive& ar, const std::unordered_map<Key, Value, Hash, Pred, Allocator>& map) {
+    save<uint32_t>(ar, static_cast<uint32_t>(map.size()));
+    for (const auto& [key, value] : map) {
+        save(ar, key);
+        save(ar, value);
+    }
+}
+
+template <class Key, class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_map<Key, Value, Hash, Pred, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key;
+        Value value;
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<!std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_map<Key, Value, Hash, Pred, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key(map.get_allocator());
+        Value value;
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<!std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_map<Key, Value, Hash, Pred, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key;
+        Value value(map.get_allocator());
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
+template <class Key, class Value, class Hash, class Pred, class Allocator,
+          std::enable_if_t<std::uses_allocator_v<Key, Allocator>, bool> = false,
+          std::enable_if_t<std::uses_allocator_v<Value, Allocator>, bool> = false>
+void load(InputArchive& ar, std::unordered_map<Key, Value, Hash, Pred, Allocator>& map) {
+    uint32_t sz = 0;
+    load(ar, sz);
+    for (uint32_t i = 0; i != sz; ++i) {
+        Key key(map.get_allocator());
+        Value value(map.get_allocator());
+        load(ar, key);
+        load(ar, value);
+        map.emplace(std::move(key), std::move(value));
+    }
+}
+
 // gfx
 inline void save(OutputArchive& ar, const gfx::Color& v) {
-    ar.writeNumber(v.x);
-    ar.writeNumber(v.y);
-    ar.writeNumber(v.z);
-    ar.writeNumber(v.w);
+    save(ar, v.x);
+    save(ar, v.y);
+    save(ar, v.z);
+    save(ar, v.w);
 }
 
 inline void load(InputArchive& ar, gfx::Color& v) {
-    v.x = static_cast<float>(ar.readNumber());
-    v.y = static_cast<float>(ar.readNumber());
-    v.z = static_cast<float>(ar.readNumber());
-    v.w = static_cast<float>(ar.readNumber());
+    load(ar, v.x);
+    load(ar, v.y);
+    load(ar, v.z);
+    load(ar, v.w);
+}
+
+inline void save(OutputArchive& ar, const gfx::Uniform& v) {
+    save(ar, v.name);
+    save(ar, v.type);
+    save(ar, v.count);
+}
+
+inline void load(InputArchive& ar, gfx::Uniform& v) {
+    load(ar, v.name);
+    load(ar, v.type);
+    load(ar, v.count);
+}
+
+inline void save(OutputArchive& ar, const gfx::UniformBlock& v) {
+    save(ar, v.set);
+    save(ar, v.binding);
+    save(ar, v.name);
+    save(ar, v.members);
+    save(ar, v.count);
+}
+
+inline void load(InputArchive& ar, gfx::UniformBlock& v) {
+    load(ar, v.set);
+    load(ar, v.binding);
+    load(ar, v.name);
+    load(ar, v.members);
+    load(ar, v.count);
+}
+
+inline void save(OutputArchive& ar, const gfx::DescriptorSetLayoutBinding& v) {
+    save(ar, v.binding);
+    save(ar, v.descriptorType);
+    save(ar, v.count);
+    save(ar, v.stageFlags);
+    // skip immutableSamplers: SamplerList
+}
+
+inline void load(InputArchive& ar, gfx::DescriptorSetLayoutBinding& v) {
+    load(ar, v.binding);
+    load(ar, v.descriptorType);
+    load(ar, v.count);
+    load(ar, v.stageFlags);
+    // skip immutableSamplers: SamplerList
+}
+
+inline void save(OutputArchive& ar, const gfx::DescriptorSetLayoutInfo& v) {
+    save(ar, v.bindings);
+}
+
+inline void load(InputArchive& ar, gfx::DescriptorSetLayoutInfo& v) {
+    load(ar, v.bindings);
 }
 
 } // namespace render
