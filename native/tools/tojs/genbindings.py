b04cac18d6c7d84bd63601ba8706f50b7512e7c3
mmyduckx <308303735@qq.com>
2022-10-24 18:55:56 +0800

    remove xr in genbindings.py

--- native/tools/tojs/genbindings.py
+++ native/tools/tojs/genbindings.py
@@ -4,7 +4,7 @@
 # Android ndk version must be ndk-r16 or greater.
 
 
-import sys, os, shutil, subprocess, re
+import sys, os, subprocess, re
 from contextlib import contextmanager
 
 if sys.version_info.major >= 3:
@@ -13,7 +13,6 @@ else:
     import ConfigParser as configparser
 
 defaultSections = [
-    'xr',
 ]
 
 projectRoot = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
