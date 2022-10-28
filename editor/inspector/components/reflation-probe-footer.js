3cdf6f5519a1611d0aa1677455a6dd7484db5df6
zhangxin <13622872688@163.com>
2022-10-28 18:00:58 +0800

    update reflation inspector

---
+++ editor/inspector/components/reflation-probe-footer.js
@@ -0,0 +1,60 @@
+exports.template = /* html */`
+<div class="reflation-probe-footer">
+    <ui-button class="bake blue" >Bake</ui-button>
+</div>
+`;
+
+exports.style = /* css */`
+.reflation-probe-footer {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 6px;
+}
+
+ui-button[hidden] {
+    display: none;
+}
+
+.bake {
+    flex: 1;
+}
+
+`;
+
+exports.$ = {
+    bake: '.bake'
+};
+
+exports.update = async function(dump) {
+    if (dump) {
+        this.dump = dump;
+        /** @type {HTMLElement} */
+        const button = this.$.bake;
+        const cubeTypes = (this.dump.value.probeType.values ?? [this.dump.value.probeType.value]);
+        const cubeEnumIndex = this.dump.value.probeType.enumList.findIndex(item => item.name === 'CUBE');
+        button.hidden = cubeTypes.some(item => item !== cubeEnumIndex);
+    }
+
+};
+
+exports.ready = function() {
+    this.$.bake.addEventListener('confirm', () => {
+        const uuids = this.dump.value.uuid.values ?? [this.dump.value.uuid.value];
+        Editor.Message.send('scene', 'reflection-probe:bake-cube-map', uuids);
+    })
+};
+
+exports.close = function() {
+
+};
+
+exports.methods = {
+    /**
+     * 
+     * @param {string} uuid 
+     */
+    bake(uuid){
+        
+    }
+    
+};
