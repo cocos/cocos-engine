7ed9fd54c7da593b138430d15549e1c69df1062c
minggo <nudtzxm@gmail.com>
2022-11-01 10:43:56 +0800

    add missing files

---
+++ cocos/serialization/deserialize-dynamic-empty.ts
@@ -0,0 +1,8 @@
+
+export function deserializeDynamic() {
+    throw new Error(`Should not called`);
+}
+
+export function parseUuidDependenciesDynamic() {
+    throw new Error(`Should not called`);
+}
