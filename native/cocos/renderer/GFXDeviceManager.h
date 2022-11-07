6f711888196405284a579798413aad0656de83e5
dogeFu <609075410@qq.com>
2022-11-07 14:22:44 +0800

    fix clang-format

--- native/cocos/renderer/GFXDeviceManager.h
+++ native/cocos/renderer/GFXDeviceManager.h
@@ -30,11 +30,11 @@
 #include "gfx-agent/DeviceAgent.h"
 #include "gfx-validator/DeviceValidator.h"
 
-//#undef CC_USE_NVN
-//#undef CC_USE_VULKAN
-//#undef CC_USE_METAL
-//#undef CC_USE_GLES3
-//#undef CC_USE_GLES2
+// #undef CC_USE_NVN
+// #undef CC_USE_VULKAN
+// #undef CC_USE_METAL
+// #undef CC_USE_GLES3
+// #undef CC_USE_GLES2
 
 #ifdef CC_USE_NVN
     #include "gfx-nvn/NVNDevice.h"
@@ -104,7 +104,7 @@ public:
 #endif
 
 #ifdef CC_EDITOR
-    Device::isSupportDetachDeviceThread = false;
+        Device::isSupportDetachDeviceThread = false;
 #endif
         if (tryCreate<EmptyDevice>(info, &device)) return device;
 
