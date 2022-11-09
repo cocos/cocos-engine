d52e10c3d854e96fa2ff7a05788ab48d85db4f7d
arsen2010 <arsen2010@126.com>
2022-11-09 22:04:41 +0800

    fix animation-mask can not edit in inspector

--- cocos/animation/marionette/animation-mask.ts
+++ cocos/animation/marionette/animation-mask.ts
@@ -1,8 +1,23 @@
-import { ccclass, serializable, editable } from 'cc.decorator';
+import { ccclass, serializable, editable, type } from 'cc.decorator';
 import type { Node } from '../../scene-graph/node';
 import { Asset } from '../../asset/assets/asset';
 import { js } from '../../core';
 import { CLASS_NAME_PREFIX_ANIM } from '../define';
+
+interface JointMaskInfo {
+    readonly path: string;
+
+    enabled: boolean;
+}
+
+@ccclass('cc.JointMask')
+class JointMask {
+    @serializable
+    public path = '';
+
+    @serializable
+    public enabled = true;
+}
 
 @ccclass(`${CLASS_NAME_PREFIX_ANIM}AnimationMask`)
 export class AnimationMask extends Asset {
@@ -10,6 +25,7 @@ export class AnimationMask extends Asset {
     private _jointMasks: JointMask[] = [];
 
     @editable
+    @type(JointMask)
     get joints (): Iterable<JointMaskInfo> {
         return this._jointMasks;
     }
@@ -67,19 +83,4 @@ type JointMaskInfo_ = JointMaskInfo;
 
 export declare namespace AnimationMask {
     export type JointMaskInfo = JointMaskInfo_;
-}
-
-interface JointMaskInfo {
-    readonly path: string;
-
-    enabled: boolean;
-}
-
-@ccclass('cc.JointMask')
-class JointMask {
-    @serializable
-    public path = '';
-
-    @serializable
-    public enabled = true;
 }
