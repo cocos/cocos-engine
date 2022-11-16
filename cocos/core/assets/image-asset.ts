cb8ac4a5134b973ad075a480f3f30b394192f1f6
ycg <1670543365@qq.com>
2022-11-16 11:27:46 +0800

    Fix : imageAsset data for taobao

--- cocos/core/assets/image-asset.ts
+++ cocos/core/assets/image-asset.ts
@@ -62,7 +62,7 @@ function fetchImageSource (imageSource: ImageSource) {
 
 // 返回该图像源是否是平台提供的图像对象。
 function isNativeImage (imageSource: ImageSource): imageSource is (HTMLImageElement | HTMLCanvasElement | ImageBitmap) {
-    if (ALIPAY || XIAOMI || BAIDU) {
+    if (ALIPAY || TAOBAO || XIAOMI || BAIDU) {
         // We're unable to grab the constructors of Alipay native image or canvas object.
         return !('_data' in imageSource);
     }
