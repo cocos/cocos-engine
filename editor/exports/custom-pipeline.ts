b44db64684077e87ee88fcee1f495ecaf0faf4d6
Zhou Zhenglong <zlzhou.sh@outlook.com>
2022-11-17 14:39:25 +0800

    add types

--- editor/exports/custom-pipeline.ts
+++ editor/exports/custom-pipeline.ts
@@ -1,4 +1,5 @@
 export * from '../../cocos/rendering/custom/graph';
+export * from '../../cocos/rendering/custom/types';
 export * from '../../cocos/rendering/custom/layout-graph';
 export * from '../../cocos/rendering/custom/archive';
 export * from '../../cocos/rendering/custom/binary-archive';
