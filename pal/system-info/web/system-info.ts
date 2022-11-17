6d43ea0b33f42b9c0e267a8ca2f374d4dcbd524f
fqamic <fqamic@vip.qq.com>
2022-11-17 16:07:49 +0800

    fix: pr ts-expect-error undefined xr

--- pal/system-info/web/system-info.ts
+++ pal/system-info/web/system-info.ts
@@ -188,6 +188,7 @@ class SystemInfo extends EventTarget {
             [Feature.EVENT_GAMEPAD]: (navigator.getGamepads !== undefined || navigator.webkitGetGamepads !== undefined),
             [Feature.EVENT_HANDLE]: EDITOR || PREVIEW,
             [Feature.EVENT_HMD]: this.isXR,
+            // @ts-expect-error undefined xr
             [Feature.EVENT_HANDSET]: (typeof navigator.xr !== 'undefined'),
         };
 
