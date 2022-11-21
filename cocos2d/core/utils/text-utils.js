9f9163177d1a6d8aecf50218af1afcbbb1719651
jianting.zheng <jianting.zheng@cocos.com>
2022-11-21 14:57:58 +0800

    feat: add function to clear measureCache.

--- cocos2d/core/utils/text-utils.js
+++ cocos2d/core/utils/text-utils.js
@@ -160,6 +160,10 @@ var textUtils = {
         return ((ch >= 9 && ch <= 13) || ch === 32 || ch === 133 || ch === 160 || ch === 5760 || (ch >= 8192 && ch <= 8202) || ch === 8232 || ch === 8233 || ch === 8239 || ch === 8287 || ch === 12288);
     },
 
+    clearMeasureCache: function() {
+        measureCache.clear();
+    },
+
     safeMeasureText: function (ctx, string, desc) {
         let font = desc || ctx.font;
         let key = font + "\uD83C\uDFAE" + string;
