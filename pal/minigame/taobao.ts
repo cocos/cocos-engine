69edf6d1fa9c146349f713a9686f5e62dd82f2e9
ycg <1670543365@qq.com>
2022-11-22 16:32:18 +0800

    Fix: A filpY operation will be performed after ReadPixels on Taobao. (#13482)
    * Fix: npm test error.
    
    * Fix: A filpY operation will be performed after ReadPixels on Taobao.

--- pal/minigame/taobao.ts
+++ pal/minigame/taobao.ts
@@ -143,31 +143,48 @@ minigame.getSafeArea = function () {
 };
 // #endregion SafeArea
 
-// TODO: adapt GL.getUniformLocation
-// Android return value: undefined.   iOS return value: {ID: -1}.
-if (!my.isIDE && my.getSystemInfoSync().platform.toLocaleLowerCase() === 'ios') {
+// TODO: A filpY operation will be performed after ReadPixels on Taobao.
+if (!my.isIDE) {
     // @ts-expect-error canvas defined in global
     const locCanvas = $global.screencanvas;
     if (locCanvas) {
-        const webglRC = locCanvas.getContext('webgl');
-        const originalGetUniformLocation = webglRC.getUniformLocation.bind(webglRC);
-        webglRC.getUniformLocation = function (program, name) {
-            const glLoc = originalGetUniformLocation(program, name);
-            if (glLoc && glLoc.ID === -1) {
-                return undefined;
+        const originalGetContext = locCanvas.getContext.bind(locCanvas);
+        locCanvas.getContext = function (name, param) {
+            if (typeof name === 'string' && typeof param === 'object' && name.startsWith('webgl')) {
+                Object.assign(param, { enable_flip_y_after_read_pixels: false });
+                const gl = originalGetContext(name, param);
+                adapterGL(gl);
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                return gl;
             }
             // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            return originalGetUniformLocation(program, name);
+            return originalGetContext(name, param);
         };
     }
 }
 
-// TODO: Do not use premultiplication on the phone.
-if (!my.isIDE) {
-    const locCanvas = $global.screencanvas;
-    if (locCanvas) {
-        const webglRC = locCanvas.getContext('webgl');
-        webglRC.pixelStorei(webglRC.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+let hasAdapter = false;
+function adapterGL (gl) {
+    if (hasAdapter) { return; }
+    hasAdapter = true;
+
+    if (!my.isIDE) {
+        // TODO: Premultiplication is already used on Taobao, do not use premultiplication on the phone.
+        gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+
+        // TODO: adapter gl.getUniformLocation
+        // Android return value: undefined.   iOS return value: {ID: -1}.
+        if (my.getSystemInfoSync().platform.toLocaleLowerCase() === 'ios') {
+            const originalGetUniformLocation = gl.getUniformLocation.bind(gl);
+            gl.getUniformLocation = function (program, name) {
+                const glLoc = originalGetUniformLocation(program, name);
+                if (glLoc && glLoc.ID === -1) {
+                    return undefined;
+                }
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                return originalGetUniformLocation(program, name);
+            };
+        }
     }
 }
 
