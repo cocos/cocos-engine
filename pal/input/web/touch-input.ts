57909326dda611cd35481cd58dd121d79954ef93
MoHai <1192652394@qq.com>
2022-11-23 14:26:14 +0800

    change webxr check interface

--- pal/input/web/touch-input.ts
+++ pal/input/web/touch-input.ts
@@ -82,7 +82,7 @@ export class TouchInputSource {
 
     private _getLocation (touch: globalThis.Touch, canvasRect: Rect): Vec2 {
         // webxr has been converted to screen coordinates via camera
-        if (globalThis.__globalXR.ar && globalThis.__globalXR.ar.getAPIState() === 3) {
+        if (globalThis.__globalXR.ar && globalThis.__globalXR.ar.isWebXR()) {
             return new Vec2(touch.clientX, touch.clientY);
         }
 
