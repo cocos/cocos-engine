75f6293e03c246ff3797cd3dc9f5913f913926bf
changhua <39176194+changhua0118@users.noreply.github.com>
2022-12-06 17:00:17 +0800

    add lod metrics

--- editor/inspector/components/lod-group/lod-item.js
+++ editor/inspector/components/lod-group/lod-item.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { trackEventWithTimer } = require('../../utils/metrics');
+
 exports.template = `
 <ui-section :cache-expand="cacheExpandId">
     <header class="header" slot="header">
@@ -128,6 +130,7 @@ exports.methods = {
         }
         that.data.value.screenUsagePercentage.value = event.target.value / 100;
         that.updateDump(that.data.value.screenUsagePercentage);
+        trackEventWithTimer('LOD', 'A100009');
     },
     onMeshConfirm(event, meshIndex) {
         const that = this;
@@ -138,8 +141,10 @@ exports.methods = {
         const that = this;
         if (operator === 'insert') {
             that.data.value.renderers.value.push(that.data.value.renderers.elementTypeData);
+            trackEventWithTimer('LOD', 'A100007');
         } else if (operator === 'delete') {
             that.data.value.renderers.value.pop();
+            trackEventWithTimer('LOD', 'A100008');
         }
         that.updateDump(that.data.value.renderers);
     },
@@ -169,6 +174,7 @@ exports.methods = {
         }
         that.data.value.screenUsagePercentage.value = size;
         that.updateDump(that.data.value.screenUsagePercentage);
+        trackEventWithTimer('LOD', 'A100004');
     },
     handleTriangleLabel(meshIndex) {
         const that = this;
