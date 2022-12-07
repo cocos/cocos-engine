7f79407951abce981e9fc47bd3b4c5b886058118
江战 <40414978+PatriceJiang@users.noreply.github.com>
2022-12-07 11:12:06 +0800

    fix import

--- cocos/asset/assets/texture-base.jsb.ts
+++ cocos/asset/assets/texture-base.jsb.ts
@@ -22,7 +22,8 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 */
-import { ccclass, serializable, EDITOR, TEST } from 'cc.decorator';
+import { ccclass, serializable } from 'cc.decorator';
+import { TEST, EDITOR } from 'internal:constants';
 import { deviceManager } from '../../gfx';
 import { cclegacy } from '../../core';
 import { Filter, PixelFormat, WrapMode } from './asset-enum';
