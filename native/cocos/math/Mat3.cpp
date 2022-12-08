85395fbcd629c61d05948d89b5b442630ce69ae8
李吉林 <798080536@qq.com>
2022-12-08 20:26:02 +0800

    fix interpolation bug due to Mat3::set (#13750)
    * fix interpolation bug due to Mat3::set

--- native/cocos/math/Mat3.cpp
+++ native/cocos/math/Mat3.cpp
@@ -52,13 +52,13 @@ Mat3::Mat3(const Mat3 &copy) {
 
 void Mat3::set(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33) {
     m[0] = m11;
-    m[1] = m21;
-    m[2] = m31;
-    m[3] = m12;
+    m[1] = m12;
+    m[2] = m13;
+    m[3] = m21;
     m[4] = m22;
-    m[5] = m32;
-    m[6] = m13;
-    m[7] = m23;
+    m[5] = m23;
+    m[6] = m31;
+    m[7] = m32;
     m[8] = m33;
 }
 
