a06e735e0186d0d2d32a6a0825b5736c20645e4d
qiuguohua <qiuguohua111@126.com>
2022-12-08 11:38:20 +0800

    1、Fix the problem of Harmony not running 2、Modify setassetmanager to setAssetManager

--- native/cocos/platform/ohos/OhosPlatform.h
+++ native/cocos/platform/ohos/OhosPlatform.h
@@ -37,7 +37,7 @@ public:
     int32_t run(int argc, const char **argv) override;
     int getSdkVersion() const override;
     int32_t loop() override;
-
+    ISystemWindow *createNativeWindow(uint32_t windowId, void *externalHandle) override;
 private:
     void waitWindowInitialized();
 
