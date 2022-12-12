83e806201ba0a147e6bc78a5f2e26e088bd1a734
Leslie Leigh <leslieleigh@outlook.com>
2022-12-12 11:07:47 +0800

    Update doc

--- cocos/animation/core/animation-handle.ts
+++ cocos/animation/core/animation-handle.ts
@@ -1,6 +1,14 @@
+/**
+ * The reason why we wrap the index into an object is in that
+ * the number of bones may vary due to animation replacement/added/moved(remember we have "overrideClips").
+ */
+
 export interface TransformHandle {
     __brand: 'TransformHandle';
 
+    /**
+     * Index of the transform in pose's transform array.
+     */
     readonly index: number;
 
     destroy(): void;
@@ -9,6 +17,9 @@ export interface TransformHandle {
 export interface MetaValueHandle {
     __brand: 'MetaValueHandle';
 
+    /**
+     * Index of the meta value in pose's meta value array.
+     */
     readonly index: number;
 
     destroy(): void;
