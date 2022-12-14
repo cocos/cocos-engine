53bfa9acd9c5ea87893308d65be79c7edcdff747
minggo <nudtzxm@gmail.com>
2022-12-14 19:04:58 +0800

    simplify code

--- tests/core/math/quat.test.ts
+++ tests/core/math/quat.test.ts
@@ -1,7 +1,5 @@
 import { Mat3, Quat } from "../../../cocos/core";
 
-
-
 // test Quat
 describe('Test Quat', () => {
     test('fromMat3', () => {
@@ -10,9 +8,11 @@ describe('Test Quat', () => {
                               0, 1, 0);
         let quat = new Quat;
         Quat.fromMat3(quat, mat3);
-        expect(quat.x).toBe(-0.7071067811865475);
-        expect(quat.y).toBe(0);
-        expect(quat.z).toBe(0);
-        expect(quat.w).toBe(0.7071067811865476);
+        expect(quat).toMatchObject({
+            x: -0.7071067811865475,
+            y: 0,
+            z: 0,
+            w: 0.7071067811865476
+        });
     });
 });
