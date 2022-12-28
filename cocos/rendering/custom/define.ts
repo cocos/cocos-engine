6f308289b98fe8d77f6a481cbb73c80280518a19
minggo <nudtzxm@gmail.com>
2022-12-28 14:19:32 +0800

    fix compiling error (#13930)
    * fix compiling error

--- cocos/rendering/custom/define.ts
+++ cocos/rendering/custom/define.ts
@@ -22,7 +22,8 @@
  THE SOFTWARE.
 */
 
-import { BufferInfo, Buffer, BufferUsageBit, ClearFlagBit, Color, DescriptorSet,
+import { BufferInfo, Buffer, BufferUsageBit, ClearFlagBit, Color, DescriptorSet, LoadOp,
+    Format, Rect, Sampler, StoreOp, Texture, Viewport, MemoryUsageBit } from '../../gfx';
 import { Camera, CSMLevel, DirectionalLight, Light, LightType, ReflectionProbe, ShadowType, SKYBOX_FLAG, SpotLight } from '../../render-scene/scene';
 import { supportsR32FloatTexture } from '../define';
 import { Pipeline } from './pipeline';
@@ -33,7 +34,7 @@ import { SRGBToLinear } from '../pipeline-funcs';
 import { RenderWindow } from '../../render-scene/core/render-window';
 import { RenderData } from './render-graph';
 import { WebPipeline } from './web-pipeline';
-import { DescriptorBlockData, DescriptorSetData } from './layout-graph';
+import { DescriptorSetData } from './layout-graph';
 
 // Anti-aliasing type, other types will be gradually added in the future
 export enum AntiAliasing {
