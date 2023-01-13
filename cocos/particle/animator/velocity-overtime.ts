a613e9d219c000fde1aabc58965d0bae255594b4
zxx43 <zxx43@hotmail.com>
2023-01-13 10:37:53 +0800

    add orbital (#14057)

--- cocos/particle/animator/velocity-overtime.ts
+++ cocos/particle/animator/velocity-overtime.ts
@@ -24,7 +24,7 @@
  */
 
 import { ccclass, tooltip, displayOrder, range, type, serializable } from 'cc.decorator';
-import { Mat4, pseudoRandom, Quat, Vec3 } from '../../core/math';
+import { Mat4, Mat3, pseudoRandom, Quat, Vec3, EPSILON, approx } from '../../core/math';
 import { Space, ModuleRandSeed } from '../enum';
 import { Particle, ParticleModuleBase, PARTICLE_MODULE_NAME } from '../particle';
 import { calculateTransform } from '../particle-general-function';
@@ -35,6 +35,11 @@ const VELOCITY_Y_OVERTIME_RAND_OFFSET = ModuleRandSeed.VELOCITY_Y;
 const VELOCITY_Z_OVERTIME_RAND_OFFSET = ModuleRandSeed.VELOCITY_Z;
 
 const _temp_v3 = new Vec3();
+const _temp_quat = new Quat();
+const _temp_mat3 = new Mat3();
+const _temp_new = new Vec3();
+const _temp_normalize = new Vec3();
+const _temp_old = new Vec3();
 
 @ccclass('cc.VelocityOvertimeModule')
 export default class VelocityOvertimeModule extends ParticleModuleBase {
@@ -96,6 +101,75 @@ export default class VelocityOvertimeModule extends ParticleModuleBase {
     public speedModifier = new CurveRange();
 
     /**
+     * @zh 沿 X 轴的轨道速度。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(6)
+    @tooltip('i18n:velocityOvertimeModule.orbitX')
+    public orbitX = new CurveRange();
+    /**
+     * @zh 沿 Y 轴的轨道速度。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(7)
+    @tooltip('i18n:velocityOvertimeModule.orbitY')
+    public orbitY = new CurveRange();
+
+    /**
+     * @zh 沿 Z 轴的轨道速度。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(8)
+    @tooltip('i18n:velocityOvertimeModule.orbitZ')
+    public orbitZ = new CurveRange();
+
+    /**
+     * @zh 沿 X 轴的轨道偏移。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(9)
+    @tooltip('i18n:velocityOvertimeModule.offsetX')
+    public offsetX = new CurveRange();
+
+    /**
+     * @zh 沿 Y 轴的轨道偏移。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(10)
+    @tooltip('i18n:velocityOvertimeModule.offsetY')
+    public offsetY = new CurveRange();
+
+    /**
+     * @zh 沿 Z 轴的轨道偏移。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(11)
+    @tooltip('i18n:velocityOvertimeModule.offsetZ')
+    public offsetZ = new CurveRange();
+
+    /**
+     * @zh 轨道半径。
+     */
+    @type(CurveRange)
+    @serializable
+    @range([-1, 1])
+    @displayOrder(12)
+    @tooltip('i18n:velocityOvertimeModule.radius')
+    public radius = new CurveRange();
+
+    /**
      * @zh 速度计算时采用的坐标系[[Space]]。
      */
     @type(Space)
@@ -107,6 +181,8 @@ export default class VelocityOvertimeModule extends ParticleModuleBase {
     private rotation: Quat;
     private needTransform: boolean;
     public name = PARTICLE_MODULE_NAME.VELOCITY;
+    private worldToLocal: Mat3;
+    private localToWorld: Mat3;
 
     constructor () {
         super();
@@ -114,10 +190,53 @@ export default class VelocityOvertimeModule extends ParticleModuleBase {
         this.speedModifier.constant = 1;
         this.needTransform = false;
         this.needUpdate = true;
+        this.worldToLocal = new Mat3();
+        this.localToWorld = new Mat3();
     }
 
     public update (space: number, worldTransform: Mat4) {
         this.needTransform = calculateTransform(space, this.space, worldTransform, this.rotation);
+        if (this.needTransform) {
+            const hasOrbital = this.offsetX.getMaxAbs() > 0 || this.offsetY.getMaxAbs() > 0 || this.offsetZ.getMaxAbs() > 0
+            || this.orbitX.getMaxAbs() > 0 || this.orbitY.getMaxAbs() > 0 || this.orbitZ.getMaxAbs() > 0;
+            if (hasOrbital) {
+                Mat3.fromMat4(this.localToWorld, worldTransform);
+                Mat3.invert(this.worldToLocal, this.localToWorld);
+            }
+        }
+    }
+
+    private calculateOrbital (p: Particle, dt: number, normalizedTime: number, speedMod: number) {
+        const offX = this.offsetX.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_X_OVERTIME_RAND_OFFSET))!;
+        const offY = this.offsetY.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_X_OVERTIME_RAND_OFFSET))!;
+        const offZ = this.offsetZ.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_X_OVERTIME_RAND_OFFSET))!;
+        const offset = Vec3.set(_temp_v3, offX, offY, offZ);
+
+        const radial = this.radius.evaluate(normalizedTime, pseudoRandom(p.randomSeed)) * dt;
+
+        const avelX = this.orbitX.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_X_OVERTIME_RAND_OFFSET))! * dt * Particle.R2D;
+        const avelY = this.orbitY.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_Y_OVERTIME_RAND_OFFSET))! * dt * Particle.R2D;
+        const avelZ = this.orbitZ.evaluate(normalizedTime, pseudoRandom(p.randomSeed ^ VELOCITY_Z_OVERTIME_RAND_OFFSET))! * dt * Particle.R2D;
+        const angM = Mat3.fromQuat(_temp_mat3, Quat.fromEuler(_temp_quat, avelX, avelY, avelZ));
+
+        _temp_old.set(p.position);
+        if (this.needTransform) {
+            Vec3.transformMat3(_temp_old, _temp_old, this.worldToLocal);
+        }
+        Vec3.subtract(_temp_old, _temp_old, offset);
+
+        _temp_new.set(_temp_old);
+        Vec3.transformMat3(_temp_new, _temp_new, angM);
+        Vec3.normalize(_temp_normalize, _temp_new);
+        Vec3.multiplyScalar(_temp_normalize, _temp_normalize, radial);
+        Vec3.add(_temp_new, _temp_new, _temp_normalize);
+        let animatedVelocity = Vec3.subtract(_temp_new, _temp_new, _temp_old);
+        if (this.needTransform) {
+            animatedVelocity = Vec3.transformMat3(_temp_new, _temp_new, this.localToWorld);
+        }
+
+        Vec3.multiplyScalar(animatedVelocity, animatedVelocity, 1 / dt);
+        Vec3.add(p.animatedVelocity, p.animatedVelocity, animatedVelocity);
     }
 
     public animate (p: Particle, dt: number) {
@@ -127,7 +246,16 @@ export default class VelocityOvertimeModule extends ParticleModuleBase {
             Vec3.transformQuat(vel, vel, this.rotation);
         }
         Vec3.add(p.animatedVelocity, p.animatedVelocity, vel);
+
+        const speedMod = this.speedModifier.evaluate(normalizedTime, pseudoRandom(p.randomSeed + VELOCITY_X_OVERTIME_RAND_OFFSET))!;
+
+        const hasOrbital = this.offsetX.getMaxAbs() > 0 || this.offsetY.getMaxAbs() > 0 || this.offsetZ.getMaxAbs() > 0
+        || this.orbitX.getMaxAbs() > 0 || this.orbitY.getMaxAbs() > 0 || this.orbitZ.getMaxAbs() > 0;
+        if (hasOrbital) {
+            this.calculateOrbital(p, dt, normalizedTime, speedMod);
+        }
+
         Vec3.add(p.ultimateVelocity, p.velocity, p.animatedVelocity);
-        Vec3.multiplyScalar(p.ultimateVelocity, p.ultimateVelocity, this.speedModifier.evaluate(1 - p.remainingLifetime / p.startLifetime, pseudoRandom(p.randomSeed + VELOCITY_X_OVERTIME_RAND_OFFSET))!);
+        Vec3.multiplyScalar(p.ultimateVelocity, p.ultimateVelocity, speedMod);
     }
 }
