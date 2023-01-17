4ee94970b99ce55e89495a3ce4e8aa4c1b53507b
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-01-17 17:13:22 +0800

    enable custom pipeline in editor

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -1,9 +1,9 @@
-import { EDITOR } from 'internal:constants';
 import { legacyCC } from '../cocos/core/global-exports';
 import * as rendering from '../cocos/rendering/custom';
+import { macro } from './base';
 
 export { rendering };
 
-if (!EDITOR) {
+if (macro.CUSTOM_PIPELINE_NAME !== '') {
     legacyCC.rendering = rendering;
 }
