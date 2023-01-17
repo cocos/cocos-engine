4d7a692818867dae379dbda1f4d8a5f427ee0dac
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-01-17 17:31:03 +0800

    always enable in runtime

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -1,9 +1,10 @@
+import { EDITOR } from 'internal:constants';
 import { legacyCC } from '../cocos/core/global-exports';
 import * as rendering from '../cocos/rendering/custom';
 import { macro } from './base';
 
 export { rendering };
 
-if (macro.CUSTOM_PIPELINE_NAME !== '') {
+if (!EDITOR || macro.CUSTOM_PIPELINE_NAME !== '') {
     legacyCC.rendering = rendering;
 }
