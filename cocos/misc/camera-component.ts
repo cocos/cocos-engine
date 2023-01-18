d0d0cfa9b13f61eb3658577b600a8ac5385d6217
pandamicro <lphuabin@gmail.com>
2023-01-18 13:48:31 +0800

    Improve API doc for camera

--- cocos/misc/camera-component.ts
+++ cocos/misc/camera-component.ts
@@ -41,23 +41,42 @@ import { ClearFlagBit } from '../gfx';
 
 const _temp_vec3_1 = new Vec3();
 
-/**
- * @en The projection type.
- * @zh 投影类型。
- */
 const ProjectionType = Enum(CameraProjection);
 const FOVAxis = Enum(CameraFOVAxis);
 const Aperture = Enum(CameraAperture);
 const Shutter = Enum(CameraShutter);
 const ISO = Enum(CameraISO);
 
+/**
+ * @en Clear screen flag enumeration of the camera.
+ * @zh 相机的清屏标记枚举。
+ */
 export const ClearFlag = Enum({
+    /**
+     * @en Clear the screen with [[SceneGlobals.skybox]], will clear the depth and stencil buffer at the same time.
+     * @zh 使用指定天空盒 [[SceneGlobals.skybox]] 清屏，会同时清理深度和蒙版缓冲。
+     */
     SKYBOX: SKYBOX_FLAG | ClearFlagBit.DEPTH_STENCIL,
+    /**
+     * @en Clear the screen with the given [[Camera.clearColor]], will clear the depth and stencil buffer at the same time.
+     * @zh 使用指定的相机清屏颜色 [[Camera.clearColor]] 来清屏，会同时清理将深度和蒙版缓冲。
+     */
     SOLID_COLOR: ClearFlagBit.ALL,
+    /**
+     * @en Only clear the depth and stencil buffer while keeping the color buffer intact. Often used in UI camera.
+     * @zh 只清理深度和蒙版缓冲，同时保留颜色缓冲不变。常用于 UI 相机。
+     */
     DEPTH_ONLY: ClearFlagBit.DEPTH_STENCIL,
+    /**
+     * @en Don't clear anything and continue rendering.
+     * @zh 不清理任何内容就开始渲染，适合多 Camera 叠加渲染。
+     */
     DONT_CLEAR: ClearFlagBit.NONE,
 });
 
+/**
+ * @internal
+ */
 export declare namespace Camera {
     export type ProjectionType = EnumAlias<typeof ProjectionType>;
     export type FOVAxis = EnumAlias<typeof FOVAxis>;
@@ -281,7 +300,7 @@ export class Camera extends Component {
     }
 
     /**
-     * @en The axis on which the FOV would be fixed regardless of screen aspect changes.
+     * @en The axi on which the FOV would be fixed regardless of screen aspect changes.
      * @zh 指定视角的固定轴向，在此轴上不会跟随屏幕长宽比例变化。
      */
     @type(FOVAxis)
@@ -489,6 +508,9 @@ export class Camera extends Component {
         }
     }
 
+    /**
+     * @internal
+     */
     get cameraType () {
         return this._cameraType;
     }
@@ -503,6 +525,9 @@ export class Camera extends Component {
         }
     }
 
+    /**
+     * @internal
+     */
     get trackingType () {
         return this._trackingType;
     }
