628acb56108c6daaedc34bca93abdfee019d2408
minggo <nudtzxm@gmail.com>
2023-01-31 11:24:25 +0800

    merge container-manager.ts and scalable-container.ts

--- cocos/game/director.ts
+++ cocos/game/director.ts
@@ -35,7 +35,7 @@ import { Root } from '../root';
 import { Node, Scene } from '../scene-graph';
 import { ComponentScheduler } from '../scene-graph/component-scheduler';
 import NodeActivator from '../scene-graph/node-activator';
-import { containerManager } from '../core/memop/container-manager';
+import { containerManager } from '../core/memop/scalable-container';
 import { uiRendererManager } from '../2d/framework/ui-renderer-manager';
 import { assetManager } from '../asset/asset-manager';
 import { deviceManager } from '../gfx';
