e49c06e8d56666e7a7b6ecefb4e3c4710099b9af
santy-wang <wh19930829@qq.com>
2023-02-07 14:30:23 +0800

    fix bugs

--- tests/particle/particle-soa-data.test.ts
+++ tests/particle/particle-soa-data.test.ts
@@ -1,5 +1,14 @@
 
+import { ParticleSOAData } from '../../cocos/particle/particle-soa-data';
 
-test('particle-soa-data', () => {
+describe('particle-soa-data', () => {
+    
+    test('capacity', () => {
+        const particles = new ParticleSOAData();
+        expect(particles.capacity).toBe(16);
+        expect(particles.count).toBe(0);
+        particles.addParticles(5);
 
-})
+    });
+    
+});
