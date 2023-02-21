29d148d452db100f6fc69d76038dec79559504c5
bofeng <bofeng.song@chukong-inc.com>
2023-02-21 14:46:52 +0800

    refine

--- platforms/minigame/platforms/taobao-mini-game/wrapper/builtin/Canvas.js
+++ platforms/minigame/platforms/taobao-mini-game/wrapper/builtin/Canvas.js
@@ -1,71 +1,69 @@
-import { innerWidth, innerHeight } from './WindowProperties'
+import { innerWidth, innerHeight } from './WindowProperties';
 
 function Canvas () {}
 
-let CanvasProxy = new Proxy(Canvas, {
+const CanvasProxy = new Proxy(Canvas, {
   construct () {
-
     const canvas = my.createCanvas();
 
-    canvas.type = 'canvas'
-  
+    canvas.type = 'canvas';
+
     // canvas.__proto__.__proto__.__proto__ = new HTMLCanvasElement()
-  
-    const _getContext = canvas.getContext
-  
+
+    const _getContext = canvas.getContext;
+
     canvas.getBoundingClientRect = () => {
       const ret = {
         top: 0,
         left: 0,
         width: window.innerWidth,
-        height: window.innerHeight
-      }
-      return ret
-    }
-  
+        height: window.innerHeight,
+      };
+      return ret;
+    };
+
     canvas.style = {
       top: '0px',
       left: '0px',
-      width: innerWidth + 'px',
-      height: innerHeight + 'px',
-    }
-  
+      width: `${innerWidth}px`,
+      height: `${innerHeight}px`,
+    };
+
     canvas.addEventListener = function (type, listener, options = {}) {
       // console.log('canvas.addEventListener', type);
       $global.document.addEventListener(type, listener, options);
-    }
-  
+    };
+
     canvas.removeEventListener = function (type, listener) {
       // console.log('canvas.removeEventListener', type);
       $global.document.removeEventListener(type, listener);
-    }
-  
+    };
+
     canvas.dispatchEvent = function (event = {}) {
-      console.log('canvas.dispatchEvent' , event.type, event);
+      console.log('canvas.dispatchEvent', event.type, event);
       // nothing to do
-    }
-  
+    };
+
     Object.defineProperty(canvas, 'clientWidth', {
       enumerable: true,
-      get: function get() {
-        return innerWidth
-      }
-    })
-  
+      get: function get () {
+        return innerWidth;
+      },
+    });
+
     Object.defineProperty(canvas, 'clientHeight', {
       enumerable: true,
-      get: function get() {
-        return innerHeight
-      }
-    })
-  
-    return canvas
+      get: function get () {
+        return innerHeight;
+      },
+    });
+
+    return canvas;
   },
 });
 
-// 暴露全局的 canvas
+// Expose the global canvas
 $global.screencanvas = $global.screencanvas || new CanvasProxy();
-
 
 // NOTE: this is a hack operation
 // let canvas = new window.Canvas()
