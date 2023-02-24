bcc22893b23672c57bf899a7a6adcc292198cd11
hyde zhou <zlzhou.sh@outlook.com>
2023-02-24 16:06:32 +0800

    fix custom pipeline init (#14353)

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -1,10 +1,6 @@
-import { EDITOR } from 'internal:constants';
 import { legacyCC } from '../cocos/core/global-exports';
 import * as rendering from '../cocos/rendering/custom';
-import { macro } from './base';
 
 export { rendering };
 
-if (!EDITOR || macro.CUSTOM_PIPELINE_NAME !== '') {
-    legacyCC.rendering = rendering;
-}
+legacyCC.rendering = rendering;
