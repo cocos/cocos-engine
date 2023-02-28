cb8fc67ec96ed79e60f0879420e011a030b752a1
zhangxin <13622872688@163.com>
2023-02-28 17:25:17 +0800

    add volume edit button to all 3d collider

--- editor/inspector/components-footer.js
+++ editor/inspector/components-footer.js
@@ -1,5 +1,10 @@
 const { join } = require('path');
 
 module.exports = {
+    'cc.BoxCollider': join(__dirname, './components/collider/box-collider-footer.js'),
+    'cc.CapsuleCollider': join(__dirname, './components/collider/capsule-collider-footer.js'),
+    'cc.ConeCollider': join(__dirname, './components/collider/cone-collider-footer.js'),
+    'cc.CylinderCollider': join(__dirname, './components/collider/cylinder-collider-footer.js'),
     'cc.LightProbeGroup': join(__dirname, './components/light-probe-group-footer.js'),
+    'cc.SphereCollider': join(__dirname, './components/collider/sphere-collider-footer.js'),
 };
