7daa5e9d063ffa6869811ad3bffde0c979de95f7
AFeiYA <386665678@qq.com>
2023-03-09 18:40:11 +0800

    Fix vertex count undefined error.

--- cocos/3d/assets/mesh.ts
+++ cocos/3d/assets/mesh.ts
@@ -1104,7 +1104,7 @@ export class Mesh extends Asset {
             const vertexCount = vertexBundle.view.count;
             const { format } = vertexBundle.attributes[iAttribute];
             const StorageConstructor = getTypedArrayConstructor(FormatInfos[format]);
-            if (vertexCount === 0) {
+            if (vertexCount === 0 || vertexCount === undefined) {
                 return;
             }
 
