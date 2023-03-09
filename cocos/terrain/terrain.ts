418891b96fbda476b44fff9079df82908fdafdd7
Myway3D <MSoft1115@hotmail.com>
2023-03-09 15:39:50 +0800

    fixed the main light still takes effect after baking

--- cocos/terrain/terrain.ts
+++ cocos/terrain/terrain.ts
@@ -46,7 +46,7 @@ import { TerrainAsset, TerrainLayerInfo, TERRAIN_HEIGHT_BASE, TERRAIN_HEIGHT_FAC
     TERRAIN_MAX_LAYER_COUNT, TERRAIN_HEIGHT_FMIN, TERRAIN_HEIGHT_FMAX, TERRAIN_MAX_BLEND_LAYERS, TERRAIN_DATA_VERSION5 } from './terrain-asset';
 import { CCFloat } from '../core';
 import { PipelineEventType } from '../rendering';
-import { Node } from '../scene-graph';
+import { MobilityMode, Node } from '../scene-graph';
 
 // the same as dependentAssets: legacy/terrain.effect
 const TERRAIN_EFFECT_UUID = '1d08ef62-a503-4ce2-8b9a-46c90873f7d3';
@@ -293,6 +293,19 @@ class TerrainRenderable extends ModelRenderer {
         return false;
     }
 
+     /**
+     * @deprecated since v3.5.0, this is an engine private interface that will be removed in the future.
+     */
+    public _updateLightingmap (texture: Texture2D | null, uvParam: Vec4) {
+        if (this._model == null) {
+            return;
+        }
+
+        this._lightmap = texture;
+        this._updateReceiveDirLight();
+        this._model.updateLightingmap(texture, uvParam);
+    }
+
     /**
      * @deprecated since v3.5.0, this is an engine private interface that will be removed in the future.
      */
@@ -315,6 +328,35 @@ class TerrainRenderable extends ModelRenderer {
         }
 
         this._onMaterialModified(0, null);
+    }
+
+    protected _onUpdateReceiveDirLight (visibility: number, forceClose = false) {
+        if (!this._model) { return; }
+        if (forceClose) {
+            this._model.receiveDirLight = false;
+            return;
+        }
+        if (this.node && ((visibility & this.node.layer) === this.node.layer)
+        || (visibility & this._model.visFlags)) {
+            this._model.receiveDirLight = true;
+        } else {
+            this._model.receiveDirLight = false;
+        }
+    }
+
+    protected _updateReceiveDirLight () {
+        if (!this._model) { return; }
+        const scene = this.node.scene;
+        if (!scene || !scene.renderScene) { return; }
+        const mainLight = scene.renderScene.mainLight;
+        if (!mainLight) { return; }
+        const visibility = mainLight.visibility;
+        if (!mainLight.node) { return; }
+        if (mainLight.node.mobility === MobilityMode.Static && this._lightmap) {
+            this._onUpdateReceiveDirLight(visibility, true);
+        } else {
+            this._onUpdateReceiveDirLight(visibility);
+        }
     }
 
     private _getBuiltinMaterial () {
@@ -448,9 +490,8 @@ export class TerrainBlock {
     public update () {
         this._updateMaterial(false);
 
-        if (this._renderable._model && this.lightmap !== this._renderable._lightmap) {
-            this._renderable._lightmap = this.lightmap;
-            this._renderable._model?.updateLightingmap(this.lightmap, this.lightmapUVParam);
+        if (this.lightmap !== this._renderable._lightmap) {
+            this._renderable._updateLightingmap(this.lightmap, this.lightmapUVParam);
         }
 
         const useNormalMap = this._terrain.useNormalMap;
@@ -837,8 +878,7 @@ export class TerrainBlock {
                 this.lightmap.setWrapMode(WrapMode.CLAMP_TO_BORDER, WrapMode.CLAMP_TO_BORDER);
             }
 
-            this._renderable._lightmap = this.lightmap;
-            this._renderable._model?.updateLightingmap(this.lightmap, this.lightmapUVParam);
+            this._renderable._updateLightingmap(this.lightmap, this.lightmapUVParam);
         }
     }
 
