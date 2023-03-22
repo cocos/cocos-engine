c9d964933c5cb7e270b7cf7f4b130ab526cedbb5
pp_pro <chinese1236@qq.com>
2023-03-22 11:55:12 +0800

    optimize

--- scripts/compile-native-ts.js
+++ scripts/compile-native-ts.js
@@ -5,6 +5,12 @@ const ts = require('typescript');
 const del = require('del');
 const chalk = require('chalk').default;
 
+let clean = true;
+const argv2 = process.argv[2];
+if (argv2 === '--no-clean') {
+    clean = false;
+}
+
 const engineRoot = normalizePath(ps.join(__dirname, '..'));
 const buildOutput = normalizePath(ps.join(__dirname, '__TS_ENGINE__'));
 
@@ -79,7 +85,7 @@ async function compileTsEngine () {
     console.log('compile source file: ', entries);
 
     const compilerOptions = {
-        strict: false,
+        strict: true,
         experimentalDecorators: true,
         lib: ["lib.es2015.d.ts", "lib.es2017.d.ts"],
         types: [
@@ -134,10 +140,16 @@ async function compileTsEngine () {
         }
     }
 
-    console.log(chalk.green(`\nclean output: ${buildOutput}\n`));
-    await del(buildOutput, {force: true});
+    if (clean) {
+        console.log(chalk.green(`\nclean output: ${buildOutput}\n`));
+        await del(buildOutput, {force: true});
+    }
+
 
     if (hasError) {
+        if (clean) {
+            console.log(chalk.red(`\nNOTE: please run 'node ./scripts/compile-native-ts.js --no-clean' locally to locate the error !\n`));
+        }
         process.exit(1);
     } else {
         console.log('compile successfully!\n');
