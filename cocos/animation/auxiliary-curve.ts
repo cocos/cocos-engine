shrinktofit/engine

Introduce auxiliary curve

cocos/animation/auxiliary-curve.ts
@@ -1,0 +1,12 @@
+import { RealCurve } from '../core';
+import { ccclass, serializable } from '../core/data/decorators';
+import { CLASS_NAME_PREFIX_ANIM } from './define';
+
+@ccclass(`${CLASS_NAME_PREFIX_ANIM}AuxiliaryCurve`)
+export class AuxiliaryCurve {
+    @serializable
+    public name = '';
+
+    @serializable
+    public curve = new RealCurve();
+}

cocos/animation/marionette/animation-graph-animation-clip-binding.ts
@@ -1,9 +1,10 @@
 import { DEBUG } from 'internal:constants';
-import { error, Quat, Vec3, warnID } from '../../core';
+import { error, Quat, RealCurve, Vec3, warnID } from '../../core';
 import { assertIsTrue } from '../../core/data/utils/asserts';
 import { Node } from '../../scene-graph/node';
 import { AnimationClip, exoticAnimationTag } from '../animation-clip';
-import { TransformHandle } from '../core/animation-handle';
+import { AuxiliaryCurve } from '../auxiliary-curve';
+import { AuxiliaryCurveHandle, TransformHandle } from '../core/animation-handle';
 import { Pose } from '../core/pose';
 import { createEvalSymbol } from '../define';
 import { ExoticTrsAGEvaluation } from '../exotic-animation/exotic-animation';
@@ -39,6 +40,12 @@ export interface AnimationClipGraphBindingContext {
      * @returns The transform handle if successfully bound, `null` otherwise.
      */
     bindTransform(path: string): TransformHandle | null;
+
+    /**
+     * Binds an auxiliary curve.
+     * @param curveName Curve name.
+     */
+    bindAuxiliaryCurve(curveName: string): AuxiliaryCurveHandle;
 }
 
 /**
@@ -153,6 +160,24 @@ class PoseScaleBinding extends PoseBindingBase implements PoseBinding<Vec3> {
     }
 }
 
+class AuxiliaryCurveBinding implements PoseBinding<number> {
+    constructor (private _handle: AuxiliaryCurveHandle) {
+
+    }
+
+    public destroy (): void {
+        this._handle.destroy();
+    }
+
+    public setValue (value: number, pose: Pose): void {
+        pose.metaValues[this._handle.index] = value;
+    }
+
+    public getValue (pose: Pose) {
+        return pose.metaValues[this._handle.index];
+    }
+}
+
 /**
  * Creates a corresponding pose binding.
  * @param transformHandle Handle to the transform.
@@ -226,6 +251,7 @@ function createRuntimeBindingAG (track: TrackBinding, bindContext: AnimationClip
         origin,
     } = bindContext;
     const { path, proxy } = track;
+
     const nPaths = path.length;
     const iLastPath = nPaths - 1;
 
@@ -267,6 +293,27 @@ function createRuntimeBindingAG (track: TrackBinding, bindContext: AnimationClip
     return undefined;
 }
 
+class AuxiliaryCurveEvaluation {
+    constructor (private _binding: AuxiliaryCurveBinding, private _curve: RealCurve) {
+    }
+
+    /**
+     * Evaluates.
+     * @param time The time.
+     * @param context The evaluation context.
+     */
+    public evaluate (time: number, context: AnimationClipGraphEvaluationContext) {
+        const {
+            _curve: curve,
+            _binding: binding,
+        } = this;
+        const pose = context;
+
+        const value = curve.evaluate(time);
+        binding.setValue(value, pose);
+    }
+}
+
 /**
  * Describes the evaluation of a animation clip in sense of animation graph.
  */
@@ -279,10 +326,12 @@ export class AnimationClipAGEvaluation {
 
         const trackEvaluations: AGTrackEvaluation<unknown>[] = [];
         let exoticAnimationEvaluation: ExoticTrsAGEvaluation | undefined;
+        const auxiliaryCurveEvaluations: AuxiliaryCurveEvaluation[] = [];
 
         const {
             tracks,
             [exoticAnimationTag]: exoticAnimation,
+            auxiliaryCurves,
         } = clip;
 
         for (const track of tracks) {
@@ -306,8 +355,23 @@ export class AnimationClipAGEvaluation {
             exoticAnimationEvaluation = exoticAnimation.createEvaluatorForAnimationGraph(context);
         }
 
+        const nAuxiliaryCurves = auxiliaryCurves.length;
+        for (let iAuxiliaryCurve = 0; iAuxiliaryCurve < nAuxiliaryCurves; ++iAuxiliaryCurve) {
+            const {
+                name: curveName,
+                curve,
+            } = auxiliaryCurves[iAuxiliaryCurve];
+            const handle = context.bindAuxiliaryCurve(curveName);
+            const binding = new AuxiliaryCurveBinding(handle);
+            auxiliaryCurveEvaluations.push(new AuxiliaryCurveEvaluation(
+                binding,
+                curve,
+            ));
+        }
+
         this._trackEvaluations = trackEvaluations;
         this._exoticAnimationEvaluation = exoticAnimationEvaluation;
+        this._auxiliaryCurveEvaluations = auxiliaryCurveEvaluations;
     }
 
     /**
@@ -334,6 +398,7 @@ export class AnimationClipAGEvaluation {
         const {
             _trackEvaluations: trackEvaluations,
             _exoticAnimationEvaluation: exoticAnimationEvaluation,
+            _auxiliaryCurveEvaluations: auxiliaryCurveEvaluations,
         } = this;
 
         const pose = context;
@@ -346,9 +411,16 @@ export class AnimationClipAGEvaluation {
         if (exoticAnimationEvaluation) {
             exoticAnimationEvaluation.evaluate(time, pose);
         }
+
+        const nAuxiliaryCurveEvaluations = auxiliaryCurveEvaluations.length;
+        for (let iAuxiliaryCurveEvaluation = 0; iAuxiliaryCurveEvaluation < nAuxiliaryCurveEvaluations; ++iAuxiliaryCurveEvaluation) {
+            auxiliaryCurveEvaluations[iAuxiliaryCurveEvaluation].evaluate(time, context);
+        }
     }
 
     private _trackEvaluations: AGTrackEvaluation<any>[] = [];
 
     private _exoticAnimationEvaluation: ExoticTrsAGEvaluation | undefined;
+
+    private _auxiliaryCurveEvaluations: AuxiliaryCurveEvaluation[] = [];
 }

tests/animation/auxiliary-curve.test.ts
@@ -1,0 +1,55 @@
+import { AnimationClip, AnimationState } from "../../cocos/animation";
+import { ClipMotion } from "../../cocos/animation/marionette/clip-motion";
+import { Node } from "../../cocos/scene-graph";
+import { createAnimationGraph } from './new-gen-anim/utils/factory';
+import { LinearRealValueAnimationFixture } from "./new-gen-anim/utils/fixtures";
+import { AnimationGraphEvalMock } from './new-gen-anim/utils/eval-mock';
+import { AuxiliaryCurve } from "../../cocos/animation/auxiliary-curve";
+
+test(`Shall be ignored in legacy animation system`, () => {
+    const curve = new AuxiliaryCurve();
+    curve.name = 'xyz';
+    const clip = new AnimationClip();
+    clip.duration = 1.0;
+    clip.auxiliaryCurves.push(curve);
+
+    const state = new AnimationState(clip);
+    const node = new Node();
+    state.initialize(node);
+});
+
+describe(`Behavior in marionette`, () => {
+    test(`In clip motion`, () => {
+        const fixture = {
+            curveName: 'SomeCurve',
+            animation: new LinearRealValueAnimationFixture(0.3, 0.4, 0.5),
+        };
+    
+        const clip = new AnimationClip();
+        clip.duration = fixture.animation.duration;
+        const curve = new AuxiliaryCurve();
+        curve.name = 'SomeCurve';
+        curve.curve.assignSorted([
+            [0.0, fixture.animation.from],
+            [fixture.animation.duration, fixture.animation.to],
+        ]);
+        clip.auxiliaryCurves.push(curve);
+
+        const motion = new ClipMotion();
+        motion.clip = clip;
+    
+        const animationGraph = createAnimationGraph({
+            layers: [{
+                stateMachine: {
+                    states: { 'motion': { type: 'motion', motion } },
+                    entryTransitions: [{ to: 'motion' }],
+                },
+            }],
+        });
+    
+        const evalMock = new AnimationGraphEvalMock(new Node(), animationGraph);
+    
+        evalMock.goto(0.2);
+        expect(evalMock.controller.getAuxiliaryCurveValue_experimental(fixture.curveName)).toBeCloseTo(fixture.animation.getExpected(0.2));
+    });
+});

cocos/cocos-engine

cocos/animation/auxiliary-curve.ts
@@ -1,0 +1,12 @@
+import { RealCurve } from '../core';
+import { ccclass, serializable } from '../core/data/decorators';
+import { CLASS_NAME_PREFIX_ANIM } from './define';
+
+@ccclass(`${CLASS_NAME_PREFIX_ANIM}AuxiliaryCurveInfo`)
+export class AuxiliaryCurveInfo {
+    @serializable
+    public name = '';
+
+    @serializable
+    public curve = new RealCurve();
+}

cocos/animation/marionette/animation-graph-animation-clip-binding.ts
@@ -1,9 +1,10 @@
 import { DEBUG } from 'internal:constants';
-import { error, Quat, Vec3, warnID } from '../../core';
+import { error, Quat, RealCurve, Vec3, warnID } from '../../core';
 import { assertIsTrue } from '../../core/data/utils/asserts';
 import { Node } from '../../scene-graph/node';
 import { AnimationClip, exoticAnimationTag } from '../animation-clip';
-import { TransformHandle } from '../core/animation-handle';
+import { AuxiliaryCurveInfo } from '../auxiliary-curve';
+import { AuxiliaryCurveHandle, TransformHandle } from '../core/animation-handle';
 import { Pose } from '../core/pose';
 import { createEvalSymbol } from '../define';
 import { ExoticTrsAGEvaluation } from '../exotic-animation/exotic-animation';
@@ -39,6 +40,12 @@ export interface AnimationClipGraphBindingContext {
      * @returns The transform handle if successfully bound, `null` otherwise.
      */
     bindTransform(path: string): TransformHandle | null;
+
+    /**
+     * Binds an auxiliary curve.
+     * @param curveName Curve name.
+     */
+    bindAuxiliaryCurve(curveName: string): AuxiliaryCurveHandle;
 }
 
 /**
@@ -153,6 +160,24 @@ class PoseScaleBinding extends PoseBindingBase implements PoseBinding<Vec3> {
     }
 }
 
+class AuxiliaryCurveBinding implements PoseBinding<number> {
+    constructor (private _handle: AuxiliaryCurveHandle) {
+
+    }
+
+    public destroy (): void {
+        this._handle.destroy();
+    }
+
+    public setValue (value: number, pose: Pose): void {
+        pose.metaValues[this._handle.index] = value;
+    }
+
+    public getValue (pose: Pose) {
+        return pose.metaValues[this._handle.index];
+    }
+}
+
 /**
  * Creates a corresponding pose binding.
  * @param transformHandle Handle to the transform.
@@ -226,6 +251,7 @@ function createRuntimeBindingAG (track: TrackBinding, bindContext: AnimationClip
         origin,
     } = bindContext;
     const { path, proxy } = track;
+
     const nPaths = path.length;
     const iLastPath = nPaths - 1;
 
@@ -267,6 +293,27 @@ function createRuntimeBindingAG (track: TrackBinding, bindContext: AnimationClip
     return undefined;
 }
 
+class AuxiliaryCurveEvaluation {
+    constructor (private _binding: AuxiliaryCurveBinding, private _curve: RealCurve) {
+    }
+
+    /**
+     * Evaluates.
+     * @param time The time.
+     * @param context The evaluation context.
+     */
+    public evaluate (time: number, context: AnimationClipGraphEvaluationContext) {
+        const {
+            _curve: curve,
+            _binding: binding,
+        } = this;
+        const pose = context;
+
+        const value = curve.evaluate(time);
+        binding.setValue(value, pose);
+    }
+}
+
 /**
  * Describes the evaluation of a animation clip in sense of animation graph.
  */
@@ -279,10 +326,12 @@ export class AnimationClipAGEvaluation {
 
         const trackEvaluations: AGTrackEvaluation<unknown>[] = [];
         let exoticAnimationEvaluation: ExoticTrsAGEvaluation | undefined;
+        const auxiliaryCurveEvaluations: AuxiliaryCurveEvaluation[] = [];
 
         const {
             tracks,
             [exoticAnimationTag]: exoticAnimation,
+            auxiliaryCurveInfos_experimental: auxiliaryCurves,
         } = clip;
 
         for (const track of tracks) {
@@ -306,8 +355,23 @@ export class AnimationClipAGEvaluation {
             exoticAnimationEvaluation = exoticAnimation.createEvaluatorForAnimationGraph(context);
         }
 
+        const nAuxiliaryCurves = auxiliaryCurves.length;
+        for (let iAuxiliaryCurve = 0; iAuxiliaryCurve < nAuxiliaryCurves; ++iAuxiliaryCurve) {
+            const {
+                name: curveName,
+                curve,
+            } = auxiliaryCurves[iAuxiliaryCurve];
+            const handle = context.bindAuxiliaryCurve(curveName);
+            const binding = new AuxiliaryCurveBinding(handle);
+            auxiliaryCurveEvaluations.push(new AuxiliaryCurveEvaluation(
+                binding,
+                curve,
+            ));
+        }
+
         this._trackEvaluations = trackEvaluations;
         this._exoticAnimationEvaluation = exoticAnimationEvaluation;
+        this._auxiliaryCurveEvaluations = auxiliaryCurveEvaluations;
     }
 
     /**
@@ -334,6 +398,7 @@ export class AnimationClipAGEvaluation {
         const {
             _trackEvaluations: trackEvaluations,
             _exoticAnimationEvaluation: exoticAnimationEvaluation,
+            _auxiliaryCurveEvaluations: auxiliaryCurveEvaluations,
         } = this;
 
         const pose = context;
@@ -346,9 +411,16 @@ export class AnimationClipAGEvaluation {
         if (exoticAnimationEvaluation) {
             exoticAnimationEvaluation.evaluate(time, pose);
         }
+
+        const nAuxiliaryCurveEvaluations = auxiliaryCurveEvaluations.length;
+        for (let iAuxiliaryCurveEvaluation = 0; iAuxiliaryCurveEvaluation < nAuxiliaryCurveEvaluations; ++iAuxiliaryCurveEvaluation) {
+            auxiliaryCurveEvaluations[iAuxiliaryCurveEvaluation].evaluate(time, context);
+        }
     }
 
     private _trackEvaluations: AGTrackEvaluation<any>[] = [];
 
     private _exoticAnimationEvaluation: ExoticTrsAGEvaluation | undefined;
+
+    private _auxiliaryCurveEvaluations: AuxiliaryCurveEvaluation[] = [];
 }

tests/animation/auxiliary-curve.test.ts
@@ -1,0 +1,55 @@
+import { AnimationClip, AnimationState } from "../../cocos/animation";
+import { ClipMotion } from "../../cocos/animation/marionette/clip-motion";
+import { Node } from "../../cocos/scene-graph";
+import { createAnimationGraph } from './new-gen-anim/utils/factory';
+import { LinearRealValueAnimationFixture } from "./new-gen-anim/utils/fixtures";
+import { AnimationGraphEvalMock } from './new-gen-anim/utils/eval-mock';
+import { AuxiliaryCurveInfo } from "../../cocos/animation/auxiliary-curve";
+
+test(`Shall be ignored in legacy animation system`, () => {
+    const curveInfo = new AuxiliaryCurveInfo();
+    curveInfo.name = 'xyz';
+    const clip = new AnimationClip();
+    clip.duration = 1.0;
+    clip.auxiliaryCurveInfos_experimental = [curveInfo];
+
+    const state = new AnimationState(clip);
+    const node = new Node();
+    state.initialize(node);
+});
+
+describe(`Behavior in marionette`, () => {
+    test(`In clip motion`, () => {
+        const fixture = {
+            curveName: 'SomeCurve',
+            animation: new LinearRealValueAnimationFixture(0.3, 0.4, 0.5),
+        };
+    
+        const clip = new AnimationClip();
+        clip.duration = fixture.animation.duration;
+        const curveInfo = new AuxiliaryCurveInfo();
+        curveInfo.name = 'SomeCurve';
+        curveInfo.curve.assignSorted([
+            [0.0, fixture.animation.from],
+            [fixture.animation.duration, fixture.animation.to],
+        ]);
+        clip.auxiliaryCurveInfos_experimental = [curveInfo];
+
+        const motion = new ClipMotion();
+        motion.clip = clip;
+    
+        const animationGraph = createAnimationGraph({
+            layers: [{
+                stateMachine: {
+                    states: { 'motion': { type: 'motion', motion } },
+                    entryTransitions: [{ to: 'motion' }],
+                },
+            }],
+        });
+    
+        const evalMock = new AnimationGraphEvalMock(new Node(), animationGraph);
+    
+        evalMock.goto(0.2);
+        expect(evalMock.controller.getAuxiliaryCurveValue_experimental(fixture.curveName)).toBeCloseTo(fixture.animation.getExpected(0.2));
+    });
+});