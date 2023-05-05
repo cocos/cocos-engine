459e2ca7a56368a6be4d0b6664d532f46591a92a
xubing <58895777@qq.com>
2023-05-05 11:04:15 +0800

    modify pps effect path

--- cocos/rendering/post-process/components/color-grading.ts
+++ cocos/rendering/post-process/components/color-grading.ts
@@ -1,6 +1,6 @@
 import { Texture2D } from '../../../asset/assets';
 import { property } from '../../../core/data/class-decorator';
-import { ccclass, disallowMultiple, executeInEditMode, menu, range, slide } from '../../../core/data/decorators';
+import { ccclass, disallowMultiple, executeInEditMode, menu, range, slide, tooltip } from '../../../core/data/decorators';
 import { PostProcessSetting } from './post-process-setting';
 
 @ccclass('cc.ColorGrading')
@@ -11,7 +11,8 @@ export class ColorGrading extends PostProcessSetting {
     @property
     @slide
     @range([0, 1, 0.01])
-    contribute = 1.0
+    contribute = 0.0
     @property(Texture2D)
+    @tooltip('i18n:color_grading.originalMap')
     colorGradingMap: Texture2D | null = null;
 }
