f0c259b6e18cc06cb4a206e1f6422f11932a1acd
Knox <knox.h@qq.com>
2023-05-10 16:44:51 +0800

    revert animation feature cropping (#15018)

--- exports/animation-runtime-exports.ts
+++
@@ -1,8 +0,0 @@
-import { animation } from './animation';
-import { AnimationController, VariableType, StateMachineComponent } from '../cocos/core/animation/marionette/runtime-exports';
-
-animation['AnimationController'] = AnimationController;
-animation['VariableType'] = VariableType;
-animation['StateMachineComponent'] = StateMachineComponent;
-
-
