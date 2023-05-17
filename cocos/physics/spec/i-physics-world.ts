1032bd9508c7e450755d71527283431173114000
Ling Zhan <lealzhan@126.com>
2023-05-17 18:37:03 +0800

    Sweep Test (#15068)
    * init
    
    # Conflicts:
    #	cocos/physics/physx/physx-adapter.ts
    
    # Conflicts:
    #	cocos/physics/physx/physx-adapter.ts
    
    * init sweepBoxClosest
    
    # Conflicts:
    #	cocos/physics/physx/physx-world.ts
    
    * back
    
    * physx boxCastClosest works now.
    
    * add physx sweepAll
    
    * init bullet sweepBoxClosest. not test yet
    
    * add bullet sweepBox
    
    * bullet add internal functions: sweep/sweepClosest
    
    * init physx c++ sweepBox/sweepBoxClosest
    
    * add sweepSphere, sweepCapsule; add unit test.
    
    * add sweep api doc
    
    * revert physx asm/wasm loading func
    
    * tweaks
    
    * add error log for builtin and cannon.js
    
    * builtin and cannon.js use warnid when calling sweep
    
    * tweak api doc

--- cocos/physics/spec/i-physics-world.ts
+++ cocos/physics/spec/i-physics-world.ts
@@ -22,7 +22,7 @@
  THE SOFTWARE.
 */
 
-import { IVec3Like, RecyclePool, geometry } from '../../core';
+import { IVec3Like, RecyclePool, geometry, IQuatLike } from '../../core';
 import { PhysicsRayResult } from '../framework/physics-ray-result';
 import { PhysicsMaterial } from '../framework';
 
@@ -41,6 +41,18 @@ export interface IPhysicsWorld {
     step (fixedTimeStep: number, timeSinceLastCalled?: number, maxSubSteps?: number): void;
     raycast (worldRay: geometry.Ray, options: IRaycastOptions, pool: RecyclePool<PhysicsRayResult>, results: PhysicsRayResult[]): boolean
     raycastClosest (worldRay: geometry.Ray, options: IRaycastOptions, out: PhysicsRayResult): boolean;
+    sweepBox (worldRay: geometry.Ray, halfExtent: IVec3Like, orientation: IQuatLike,
+        options: IRaycastOptions, pool: RecyclePool<PhysicsRayResult>, results: PhysicsRayResult[]): boolean;
+    sweepBoxClosest (worldRay: geometry.Ray, halfExtent: IVec3Like, orientation: IQuatLike,
+        options: IRaycastOptions, result: PhysicsRayResult): boolean;
+    sweepSphere (worldRay: geometry.Ray, radius: number,
+        options: IRaycastOptions, pool: RecyclePool<PhysicsRayResult>, results: PhysicsRayResult[]): boolean;
+    sweepSphereClosest (worldRay: geometry.Ray, radius: number,
+        options: IRaycastOptions, result: PhysicsRayResult): boolean;
+    sweepCapsule (worldRay: geometry.Ray, radius: number, height: number, orientation: IQuatLike,
+        options: IRaycastOptions, pool: RecyclePool<PhysicsRayResult>, results: PhysicsRayResult[]): boolean;
+    sweepCapsuleClosest (worldRay: geometry.Ray, radius: number, height: number, orientation: IQuatLike,
+        options: IRaycastOptions, result: PhysicsRayResult): boolean;
     emitEvents (): void;
     syncSceneToPhysics (): void;
     syncAfterEvents (): void;
