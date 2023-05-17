e87e4a2bb88a4f17ce2b4e62ca10981ed71d111c
wangzhen <463208406@qq.com>
2023-05-17 18:25:16 +0800

    To add the QNX enum type to the platform.ts file

--- pal/system-info/enum-type/platform.ts
+++ pal/system-info/enum-type/platform.ts
@@ -35,6 +35,7 @@ export enum Platform {
     IOS = 'IOS',
     MACOS = 'MACOS',
     OHOS = 'OHOS',
+	QNX = 'QNX',
     OPENHARMONY = 'OPENHARMONY',
     // Minigame
     WECHAT_GAME = 'WECHAT_GAME',
