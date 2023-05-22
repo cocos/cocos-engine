3d2d945f15f2fa17e71a1c68f3acf81500145084
Leslie Leigh <leslieleigh@outlook.com>
2023-05-22 11:16:51 +0800

    Pose graph copy paste operation

---
+++ editor/src/marionette/visit/visit-pose-node.ts
@@ -0,0 +1,29 @@
+import { Layer, PoseState, StateMachine } from "../../../../cocos/animation/marionette/animation-graph";
+import { PoseNodeStateMachine } from '../../../../cocos/animation/marionette/pose-graph/pose-nodes/state-machine';
+import { PoseGraph, PoseNode } from "../../../exports/new-gen-anim";
+
+export function* visitPoseNodeInLayer(layer: Layer): Generator<PoseNode> {
+    yield* visitPoseNodesInStateMachine(layer.stateMachine);
+    for (const [stashId, stash] of layer.stashes()) {
+        yield* visitPoseNodeInPoseGraph(stash.graph);
+    }
+}
+
+export function* visitPoseNodeInPoseGraph(poseGraph: PoseGraph): Generator<PoseNode> {
+    for (const node of poseGraph.nodes()) {
+        if (node instanceof PoseNode) {
+            yield node;
+        }
+        if (node instanceof PoseNodeStateMachine) {
+            yield* visitPoseNodesInStateMachine(node.stateMachine);
+        }
+    }
+}
+
+export function* visitPoseNodesInStateMachine(stateMachine: StateMachine) {
+    for (const state of stateMachine.states()) {
+        if (state instanceof PoseState) {
+            yield* visitPoseNodeInPoseGraph(state.graph);
+        }
+    }
+}
