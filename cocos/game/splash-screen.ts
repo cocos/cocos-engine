08063379f3c58fb7d9a273a0cf1aeddfffee8d55
bofeng-song <bofeng.song@chukong-inc.com>
2023-05-23 09:41:16 +0800

    support splash for taobao & refine code (#15136)
    * support splash for taobao & refine code

--- cocos/game/splash-screen.ts
+++ cocos/game/splash-screen.ts
@@ -124,8 +124,7 @@ export class SplashScreen {
         };
         this._curTime = 0;
 
-        // TODO: Image can't load with base64 data on Taobao platform.
-        if (EDITOR || TAOBAO || this.settings.base64src === '' || this.settings.totalTime <= 0) {
+        if (EDITOR || this.settings.base64src === '' || this.settings.totalTime <= 0) {
             this.settings.totalTime = 0;
         } else {
             this.device = cclegacy.director.root!.device;
