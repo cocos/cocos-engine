7fdb974567b78d5cc507cb63f49a2b62c4139249
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-05-25 11:48:14 +0800

    add ImageView

--- native/cocos/renderer/pipeline/custom/NativeResourceGraph.cpp
+++ native/cocos/renderer/pipeline/custom/NativeResourceGraph.cpp
@@ -222,6 +222,30 @@ void ResourceGraph::mount(gfx::Device* device, vertex_descriptor vertID) {
         [&](const RenderSwapchain& queue) {
             CC_EXPECTS(queue.swapchain);
             std::ignore = queue;
+        },
+        [&](const FormatView& view) {
+            std::ignore = view;
+            auto parentID = parent(vertID, resg);
+            CC_EXPECTS(parentID != resg.null_vertex());
+            while (resg.isTextureView(parentID)) {
+                parentID = parent(parentID, resg);
+            }
+            CC_EXPECTS(parentID != resg.null_vertex());
+            CC_EXPECTS(resg.isTexture(parentID));
+            CC_ENSURES(!resg.isTextureView(parentID));
+            mount(device, parentID);
+        },
+        [&](const SubresourceView& view) {
+            std::ignore = view;
+            auto parentID = parent(vertID, resg);
+            CC_EXPECTS(parentID != resg.null_vertex());
+            while (resg.isTextureView(parentID)) {
+                parentID = parent(parentID, resg);
+            }
+            CC_EXPECTS(parentID != resg.null_vertex());
+            CC_EXPECTS(resg.isTexture(parentID));
+            CC_ENSURES(!resg.isTextureView(parentID));
+            mount(device, parentID);
         });
 }
 
@@ -267,6 +291,23 @@ bool ResourceGraph::isTexture(vertex_descriptor resID) const noexcept {
         });
 }
 
+bool ResourceGraph::isTextureView(vertex_descriptor resID) const noexcept {
+    return visitObject(
+        resID, *this,
+        [&](const FormatView& view) {
+            std::ignore = view;
+            return true;
+        },
+        [&](const SubresourceView& view) {
+            std::ignore = view;
+            return true;
+        },
+        [&](const auto& res) {
+            std::ignore = res;
+            return false;
+        });
+}
+
 gfx::Buffer* ResourceGraph::getBuffer(vertex_descriptor resID) {
     gfx::Buffer* buffer = nullptr;
     visitObject(
@@ -303,6 +344,16 @@ gfx::Texture* ResourceGraph::getTexture(vertex_descriptor resID) {
         },
         [&](const RenderSwapchain& sc) {
             texture = sc.swapchain->getColorTexture();
+        },
+        [&](const FormatView& view) {
+            // TODO(zhouzhenglong): add ImageView support
+            std::ignore = view;
+            CC_EXPECTS(false);
+        },
+        [&](const SubresourceView& view) {
+            // TODO(zhouzhenglong): add ImageView support
+            std::ignore = view;
+            CC_EXPECTS(false);
         },
         [&](const auto& buffer) {
             std::ignore = buffer;
