a154ea21d02f825b0ba22e62d544d5fc7adbcb83
liqiao <liqiao@cocos.com>
2023-05-29 16:18:10 +0800

    revert to set anchorPoint

--- cocos/spine/skeleton.ts
+++ cocos/spine/skeleton.ts
@@ -737,8 +737,6 @@ export class Skeleton extends UIRenderer {
     public setSkeletonData (skeletonData: spine.SkeletonData) {
         const uiTrans = this.node._uiProps.uiTransformComp!;
         if (skeletonData.width && skeletonData.height) uiTrans.setContentSize(skeletonData.width, skeletonData.height);
-        if (skeletonData.width !== 0) uiTrans.anchorX = Math.abs(skeletonData.x) / skeletonData.width;
-        if (skeletonData.height !== 0) uiTrans.anchorY = Math.abs(skeletonData.y) / skeletonData.height;
 
         if (!EDITOR || legacyCC.GAME_VIEW) {
             if (this._cacheMode === AnimationCacheMode.SHARED_CACHE) {
