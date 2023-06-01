cba7a645cdf8f5f7c08c5392f3d497a8ac568131
Cocos Robot <48829427+cocos-robot@users.noreply.github.com>
2023-06-01 09:47:42 +0800

    [ci skip][AUTO]: Automated code generating update: 7c6ec18fc1bb9cbf2ce561f55e95a74ec67159ae  (#15224) (#15249)
    Co-authored-by: cocos-robot <cocos-robot@cocos.com>

--- native/cocos/cocos-version.h
+++ native/cocos/cocos-version.h
@@ -25,9 +25,9 @@ THE SOFTWARE.
 
 #define COCOS_MAJOR_VERSION     3
 #define COCOS_MINJOR_VERSION    7
-#define COCOS_PATCH_VERSION     3
-#define COCOS_VERSION_STRING    "3.7.3"
+#define COCOS_PATCH_VERSION     4
+#define COCOS_VERSION_STRING    "3.7.4"
 #define COCOS_VERSION_DEFINED   1
-#define COCOS_VERSION           30703
+#define COCOS_VERSION           30704
 
 // #define COCOS_PRE_RELEASE       "release"
