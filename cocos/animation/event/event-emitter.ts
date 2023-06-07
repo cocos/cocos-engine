eee8d96a4842de83e93326e9578e32de2f5e0734
Leslie Leigh <leslieleigh@outlook.com>
2023-06-07 11:42:55 +0800

    Revert

--- cocos/animation/event/event-emitter.ts
+++ cocos/animation/event/event-emitter.ts
@@ -13,9 +13,6 @@ export function invokeComponentMethodsEngagedInAnimationEvent (node: Node, metho
     const nComponents = components.length;
     for (let iComponent = 0; iComponent < nComponents; ++iComponent) {
         const component = components[iComponent];
-        if (!component.enabled) {
-            continue;
-        }
         const fx = component[methodName];
         if (typeof fx === 'function') {
             fx.apply(component, args);
