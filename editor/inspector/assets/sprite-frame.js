0c85cab6d54699bcbf09500e7b2989818a211e35
arsen2010 <arsen2010@126.com>
2023-06-20 20:49:29 +0800

    fix sprite-frame edit button visible bug

--- editor/inspector/assets/sprite-frame.js
+++ editor/inspector/assets/sprite-frame.js
@@ -524,7 +524,7 @@ const Elements = {
             if (panel.assetList.length > 1) {
                 panel.$.editButton.style.display = 'none';
             } else {
-                panel.$.editButton.style.display = undefined; // depends on component itself
+                panel.$.editButton.style.display = 'inline-flex';
             }
 
             if (panel.uuidInSpriteEditor !== panel.meta.uuid) {
