a05ba98377a6d783b13016208bab5d4529f10b0b
Gin <idoon.3@gmail.com>
2023-06-21 14:47:55 +0800

    fix: require path before push module path (#15509)

--- editor/inspector/assets/material.js
+++ editor/inspector/assets/material.js
@@ -1,10 +1,10 @@
 'use strict';
 
-module.paths.push(path.join(Editor.App.path, 'node_modules'));
+const { join, sep, normalize } = require('path');
+module.paths.push(join(Editor.App.path, 'node_modules'));
 
 const { materialTechniquePolyfill } = require('../utils/material');
 const { setDisabled, setReadonly, setHidden, loopSetAssetDumpDataReadonly, injectionStyle } = require('../utils/prop');
-const { join, sep, normalize } = require('path');
 const { escape } = require('lodash');
 
 const effectGroupNameRE = /^db:\/\/(\w+)\//i; // match root DB name
