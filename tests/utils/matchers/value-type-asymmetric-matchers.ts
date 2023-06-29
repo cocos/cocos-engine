a0e3037ae196083b779037458174d8633a7447f8
Leslie Leigh (李的序) <leslieleigh@outlook.com>
2023-06-29 11:05:39 +0800

    Add test utils: vec3, quat equality matcher and pseudo random generator (#15547)
    * Add test utils: vec3 equality matcher and pseudo random generator
    
    * Add Quat jest matcher
    
    * Tweak
    
    * Update

--- tests/utils/matchers/value-type-asymmetric-matchers.ts
+++ tests/utils/matchers/value-type-asymmetric-matchers.ts
@@ -1,3 +1,6 @@
+import { Quat, Vec3 } from "../../../exports/base";
+import matchers from 'expect/build/matchers';
+
 export { };
 
 // https://stackoverflow.com/a/53464807
@@ -18,10 +21,43 @@ expect.extend({
     },
 });
 
+expect.extend({
+    toBeCloseToVec3(actual: unknown, expected: Readonly<Vec3>, numDigits = 5) {
+        return matchers.toStrictEqual.call(
+            this,
+            actual,
+            expect.objectContaining({
+                x: expect.toBeAround(expected.x, numDigits),
+                y: expect.toBeAround(expected.y, numDigits),
+                z: expect.toBeAround(expected.z, numDigits),
+            }),
+        );
+    },
+    toBeCloseToQuat(actual: unknown, expected: Readonly<Quat>, numDigits = 5) {
+        return matchers.toStrictEqual.call(
+            this,
+            actual,
+            expect.objectContaining({
+                x: expect.toBeAround(expected.x, numDigits),
+                y: expect.toBeAround(expected.y, numDigits),
+                z: expect.toBeAround(expected.z, numDigits),
+                w: expect.toBeAround(expected.w, numDigits),
+            }),
+        );
+    },
+});
+
 declare global {
     namespace jest {
         interface Expect {
             toBeAround(expected: number, numDigits?: number): any;
+            toBeCloseToVec3(expected: Readonly<Vec3>, numDigits?: number): any;
+            toBeCloseToQuat(expected: Readonly<Quat>, numDigits?: number): any;
+        }
+
+        interface Matchers<R> {
+            toBeCloseToVec3: (expected: Readonly<Vec3>, numDigits?: number) => CustomMatcherResult;
+            toBeCloseToQuat: (expected: Readonly<Quat>, numDigits?: number) => CustomMatcherResult;
         }
     }
 }
