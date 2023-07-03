ea77a5d8d8bf05b545f854576ac067cf88f32e4b
qiuguohua <qiuguohua111@126.com>
2023-07-03 17:06:00 +0800

    Submit download-dom-image.ts.

---
+++ cocos/asset/asset-manager/download-dom-image.ts
@@ -0,0 +1,35 @@
+/*
+ Copyright (c) 2013-2016 Chukong Technologies Inc.
+ Copyright (c) 2017-2023 Xiamen Yaji Software Co., Ltd.
+ http://www.cocos.com
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+import { ImageData } from 'pal/image';
+import { ImageSource } from '../../../pal/image/types';
+
+export default function downloadDomImage (
+    url: string,
+    options: Record<string, any>,
+    onComplete: ((err: Error | null, data?: ImageSource | ArrayBufferView | null) => void),
+): void {
+    ImageData.loadImage(url).then((imageData) => {
+        onComplete(null, imageData.data);
+    }).catch((err) => {
+        onComplete(err);
+    });
+}
