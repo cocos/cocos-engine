829f84962b2759562355d5846b776796b7c0d95b
Canvas <canvasfull@gmail.com>
2023-07-13 19:05:04 +0800

    rolling conflict codes.

--- cocos/spine/skeleton-cache.ts
+++ cocos/spine/skeleton-cache.ts
@@ -379,14 +379,14 @@ class SkeletonCache {
         return skeletonInfo;
     }
 
-    public getAnimationCache (uuid: string, animationName: string): null | AnimationCache {
+    public getAnimationCache (uuid: string, animationName: string) {
         const skeletonInfo = this._skeletonCache[uuid];
         if (!skeletonInfo) return null;
         const animationsCache = skeletonInfo.animationsCache;
         return animationsCache[animationName];
     }
 
-    public initAnimationCache (uuid: string, data: SkeletonData,  animationName: string): null | AnimationCache {
+    public initAnimationCache (uuid: string, data: SkeletonData,  animationName: string) {
         const spData = data.getRuntimeData();
         if (!spData) return null;
         const skeletonInfo = this._skeletonCache[uuid];
