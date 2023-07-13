fc4894e21e6b3c4b7b500615978971ff328d5b12
Canvas <canvasfull@gmail.com>
2023-07-13 18:17:48 +0800

    remove old code.

--- cocos/spine/skeleton.ts
+++ cocos/spine/skeleton.ts
@@ -728,10 +728,6 @@ export class Skeleton extends UIRenderer {
      * 皮肤等)和动画, 但不保存任何状态。
      */
     public setSkeletonData (skeletonData: spine.SkeletonData) {
-        const uiTrans = this.node._uiProps.uiTransformComp!;
-        if (skeletonData.width && skeletonData.height) uiTrans.setContentSize(skeletonData.width, skeletonData.height);
-        if (skeletonData.width !== 0) uiTrans.anchorX = Math.abs(skeletonData.x) / skeletonData.width;
-        if (skeletonData.height !== 0) uiTrans.anchorY = Math.abs(skeletonData.y) / skeletonData.height;
         if (!EDITOR_NOT_IN_PREVIEW) {
             if (this._cacheMode === AnimationCacheMode.SHARED_CACHE) {
                 this._skeletonCache = SkeletonCache.sharedCache;
