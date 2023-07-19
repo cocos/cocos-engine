6aba5f85d9806dd72a8c22774fe6449e006aa39f
Ling Zhan <lealzhan@126.com>
2023-07-19 18:48:43 +0800

    [fix] physics3d native cct api error (#15779)

--- platforms/native/engine/jsb-physics.js
+++ platforms/native/engine/jsb-physics.js
@@ -44,8 +44,8 @@ const CollisionEventObject = {
 
 const CCTShapeEventObject = {
     type: 'onControllerColliderHit',
-    selfController: null,
-    otherCollider: null,
+    controller: null,
+    collider: null,
     worldPosition: null,
     worldNormal: null,
     motionDirection: null,
@@ -146,8 +146,8 @@ function emitCCTShapeEvent (t, cct, collider, b) {
     CCTShapeEventObject.worldNormal = new cc.Vec3(b[3], b[4], b[5]);
     CCTShapeEventObject.motionDirection = new cc.Vec3(b[6], b[7], b[8]);
     CCTShapeEventObject.motionLength = b[9];
-    CCTShapeEventObject.selfController = cct;
-    CCTShapeEventObject.otherCollider = collider;
+    CCTShapeEventObject.controller = cct;
+    CCTShapeEventObject.collider = collider;
     cct.emit(t, CCTShapeEventObject);
 }
 
@@ -914,7 +914,7 @@ class CharacterController {
         this.setCenter(this._com.center);
         this.setStepOffset(this._com.stepOffset);
         this.setSlopeLimit(this._com.slopeLimit);
-        this.setContactOffset(this._com.contactOffset);
+        this.setContactOffset(this._com.skinWidth);
         this.setDetectCollisions(true);//this._com.detectCollisions);
         this.setOverlapRecovery(true);//this._com.enableOverlapRecovery);
     }
