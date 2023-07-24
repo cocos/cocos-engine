7358280d95ca5311ee15c129a0ca3899d9882f71
troublemaker52025 <troublemaker52025@outlook.com>
2023-07-24 17:50:09 +0800

    update

--- cocos/rendering/post-process/passes/hbao-pass.ts
+++ cocos/rendering/post-process/passes/hbao-pass.ts
@@ -339,7 +339,16 @@ export class HBAOPass extends SettingPass {
         this.material.setProperty('uvDepthToEyePosParams',  this._hbaoParams!.uvDepthToEyePosParams, passIdx);
         this.material.setProperty('radiusParam', this._hbaoParams!.radiusParam, passIdx);
         this.material.setProperty('miscParam', this._hbaoParams!.miscParam, passIdx);
-        this.material.setProperty('randomTexSize', new Vec4(this._hbaoParams!.randomTexture.width, this._hbaoParams!.randomTexture.height, 1.0 / this._hbaoParams!.randomTexture.width, 1.0 / this._hbaoParams!.randomTexture.height), passIdx);
+        this.material.setProperty(
+            'randomTexSize',
+            new Vec4(
+                this._hbaoParams!.randomTexture.width,
+                this._hbaoParams!.randomTexture.height,
+                1.0 / this._hbaoParams!.randomTexture.width,
+                1.0 / this._hbaoParams!.randomTexture.height,
+            ),
+            passIdx,
+        );
         this.material.setProperty('blurParam', this._hbaoParams!.blurParam, passIdx);
 
         passContext.clearFlag = ClearFlagBit.NONE;
