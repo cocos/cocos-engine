205cba28697231e25dd4e3e477c77284cc4ef66c
finscn <finscn@gmail.com>
2023-08-17 04:05:44 +0800

    新增'获取当前触控数量' 的方法( touch-manager.ts)
    需要获取 当前有效的触控数量(按在屏幕上的手指数) 时使用.
    
    使用ccLog 代替 console.log
    
    Update touch-manager.ts
    
    不依赖 ccLog ,直接禁用警告
    
    Update touch-manager.ts
    
    重新用回 ccLog
    
    Update input.ts
    
    Add some proxy methods of touchManager for getting touche infomation
    
    Update cocos/input/input.ts
    
    Co-authored-by: PP <chinese1236@qq.com>
    
    Update pal/input/touch-manager.ts
    
    Co-authored-by: PP <chinese1236@qq.com>
    
    Update cocos/input/input.ts
    
    Co-authored-by: PP <chinese1236@qq.com>
    
    Update cocos/input/input.ts
    
    Co-authored-by: PP <chinese1236@qq.com>
    
    Update cocos/input/input.ts
    
    Co-authored-by: PP <chinese1236@qq.com>
    
    Update input.ts
    
    直接通过 touchManager._touchMap 获取数据
    
    Update touch-manager.ts
    
    将 _touchMap 暴露出去. 并且移除之前添加的 getTouchCount()方法
    
    Update touch-manager.ts
    
    直接使用 log()
    
    Update input.ts
    
    Update input.ts
    
    获得 touch对象时, 不克隆.
    
    Update touch-manager.ts
    
    新增是否 克隆 touch对象的 参数.  不指定值时, 默认克隆
    
    Update input.ts
    
    微调注释, 避免歧义
    
    Update input.ts
    
    格式化
    
    Update touch-manager.ts
    
    移除多余的赋值操作
    
    Update touch-manager.ts
    
    移除无用的clone
    
    Update touch-manager.ts
    
    将 `_cloneTouch()` 设置为public, 暴露给 input 使用.
    
    Update input.ts
    
    在获取 touch 对象时, 可通过参数决定是否clone.
    
    Update input.ts
    
    更新注释
    
    Update input.ts
    
    优化格式和类型定义.
    
    Update input.ts
    
    优化格式
    
    Update input.ts
    
    格式化
    
    Update touch-manager.ts
    
    优化格式
    
    Update touch-manager.ts
    
    # Conflicts:
    #	cocos/input/input.ts

--- pal/input/touch-manager.ts
+++ pal/input/touch-manager.ts
@@ -22,9 +22,10 @@
  THE SOFTWARE.
 */
 
+import { Vec2 } from '../../cocos/core/math/vec2';
+import { log } from '../../cocos/core/platform/debug';
 import { macro } from '../../cocos/core/platform/macro';
 import { Touch } from '../../cocos/input/types';
-import { Vec2 } from '../../cocos/core/math/vec2';
 
 const tempVec2 = new Vec2();
 
@@ -32,7 +33,7 @@ class TouchManager {
     /**
      * A map from touch ID to touch object.
      */
-    private _touchMap: Map<number, Touch>;
+    public _touchMap: Map<number, Touch>;
     private readonly _maxTouches = 8;
 
     constructor () {
@@ -44,7 +45,7 @@ class TouchManager {
      * @param touch
      * @returns
      */
-    private _cloneTouch (touch: Touch): Touch {
+    public _cloneTouch (touch: Touch): Touch {
         const touchID = touch.getID();
         touch.getStartLocation(tempVec2);
         const clonedTouch = new Touch(tempVec2.x, tempVec2.y, touchID);
@@ -68,18 +69,18 @@ class TouchManager {
      */
     private _createTouch (touchID: number, x: number, y: number): Touch | undefined {
         if (this._touchMap.has(touchID)) {
-            console.log('Cannot create the same touch object.');
+            log('Cannot create the same touch object.');
             return undefined;
         }
         const checkResult = this._checkTouchMapSizeMoreThanMax(touchID);
         if (checkResult) {
-            console.log('The touches is more than MAX_TOUCHES.');  // TODO: logID 2300
+            log('The touches is more than MAX_TOUCHES.');  // TODO: logID 2300
             return undefined;
         }
         const touch = new Touch(x, y, touchID);
         this._touchMap.set(touchID, touch);
         this._updateTouch(touch, x, y);
-        return this._cloneTouch(touch);
+        return touch;
     }
 
     /**
@@ -99,26 +100,25 @@ class TouchManager {
      * @param touchID
      * @returns
      */
-    public getTouch (touchID: number, x: number, y: number): Touch | undefined {
+    public getTouch (touchID: number, x: number, y: number, clone?: boolean): Touch | undefined {
         let touch = this._touchMap.get(touchID);
         if (!touch) {
             touch = this._createTouch(touchID, x, y);
         } else {
             this._updateTouch(touch, x, y);
         }
-        return touch ? this._cloneTouch(touch) : undefined;
+        return touch ? (clone === false ? touch : this._cloneTouch(touch)) : undefined;
     }
 
     /**
      * Get all the current touches objects.
      * @returns
      */
-    public getAllTouches (): Touch[] {
+    public getAllTouches (clone?: boolean): Touch[] {
         const touches: Touch[] = [];
         this._touchMap.forEach((touch) => {
             if (touch) {
-                const clonedTouch = this._cloneTouch(touch);
-                touches.push(clonedTouch);
+                touches.push(clone === false ? touch : this._cloneTouch(touch));
             }
         });
         return touches;
@@ -148,7 +148,7 @@ class TouchManager {
         const now = performance.now();
         this._touchMap.forEach((touch) => {
             if (now - touch.lastModified > macro.TOUCH_TIMEOUT) {
-                console.log(`The touches is more than MAX_TOUCHES, release touch id ${touch.getID()}.`);
+                log(`The touches is more than MAX_TOUCHES, release touch id ${touch.getID()}.`);
                 // TODO: need to handle touch cancel event when exceed the max number of touches ?
                 this.releaseTouch(touch.getID());
             }
