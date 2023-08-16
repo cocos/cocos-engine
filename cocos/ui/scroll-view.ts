29269989400f868afbf6b19e52b4dd6eddfa1d13
ChiaNing <sun.cloud.mo@gmail.com>
2023-08-16 18:12:08 +0800

    as string

--- cocos/ui/scroll-view.ts
+++ cocos/ui/scroll-view.ts
@@ -1411,12 +1411,12 @@ export class ScrollView extends ViewGroup {
     }
 
     protected _dispatchEvent (event: string): void {
-        if (event === EventType.SCROLL_ENDED.toString()) {
+        if (event === EventType.SCROLL_ENDED as string) {
             this._scrollEventEmitMask = 0;
-        } else if (event === EventType.SCROLL_TO_TOP.toString()
-            || event === EventType.SCROLL_TO_BOTTOM.toString()
-            || event === EventType.SCROLL_TO_LEFT.toString()
-            || event === EventType.SCROLL_TO_RIGHT.toString()) {
+        } else if (event === EventType.SCROLL_TO_TOP as string
+            || event === EventType.SCROLL_TO_BOTTOM as string
+            || event === EventType.SCROLL_TO_LEFT as string
+            || event === EventType.SCROLL_TO_RIGHT as string) {
             const flag = (1 << eventMap[event]);
             if (this._scrollEventEmitMask & flag) {
                 return;
