1256656efd31a85a6770665be81cfac61609158b
troublemaker52025 <troublemaker52025@outlook.com>
2023-08-16 15:16:43 +0800

    deferred tone-mapping and fog with float output in custom pipeline (#15968)

--- native/cocos/scene/Pass.cpp
+++ native/cocos/scene/Pass.cpp
@@ -498,6 +498,10 @@ gfx::Shader *Pass::getShaderVariant(const ccstd::vector<IMacroPatch> &patches) {
         _defines[patch.name] = patch.value;
     }
 
+    if (isBlend()) {
+        _defines["CC_IS_TRANSPARENCY_PASS"] = MacroValue(true);
+    }
+
     gfx::Shader *shader = nullptr;
     auto *programLib = render::getProgramLibrary();
     if (programLib) {
@@ -516,6 +520,17 @@ gfx::Shader *Pass::getShaderVariant(const ccstd::vector<IMacroPatch> &patches) {
         }
     }
     return shader;
+}
+
+bool Pass::isBlend() {
+    bool isBlend = false;
+    for (const auto target : _blendState.targets) {
+        if (target.blend) {
+            isBlend = true;
+        }
+    }
+
+    return isBlend;
 }
 
 IPassInfoFull Pass::getPassInfoFull() const {
