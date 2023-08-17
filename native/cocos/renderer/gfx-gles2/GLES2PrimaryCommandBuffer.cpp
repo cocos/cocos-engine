b5ea4e4c30949db9b48fdb1d754399c4915ce956
minggo <nudtzxm@gmail.com>
2023-08-17 16:11:53 +0800

    fix merge error

--- native/cocos/renderer/gfx-gles2/GLES2PrimaryCommandBuffer.cpp
+++ native/cocos/renderer/gfx-gles2/GLES2PrimaryCommandBuffer.cpp
@@ -80,8 +80,19 @@ void GLES2PrimaryCommandBuffer::nextSubpass() {
     cmdFuncGLES2BeginRenderPass(GLES2Device::getInstance(), ++_curSubpassIdx);
 }
 
-void GLES2PrimaryCommandBuffer::drawIndirect(Buffer *buffer, uint32_t offset, uint32_t count, uint32_t stride) {
+void GLES2PrimaryCommandBuffer::insertMarker(const MarkerInfo &marker) {
     cmdFuncGLES2InsertMarker(GLES2Device::getInstance(), marker.name.size(), marker.name.data());
+}
+
+void GLES2PrimaryCommandBuffer::beginMarker(const MarkerInfo &marker) {
+    cmdFuncGLES2PushGroupMarker(GLES2Device::getInstance(), marker.name.size(), marker.name.data());
+}
+
+void GLES2PrimaryCommandBuffer::endMarker() {
+    cmdFuncGLES2PopGroupMarker(GLES2Device::getInstance());
+}
+
+void GLES2PrimaryCommandBuffer::drawIndirect(Buffer *buffer, uint32_t offset, uint32_t count, uint32_t stride) {
     // not support
     std::ignore = buffer;
     std::ignore = offset;
@@ -90,7 +101,6 @@ void GLES2PrimaryCommandBuffer::drawIndirect(Buffer *buffer, uint32_t offset, ui
 }
 
 void GLES2PrimaryCommandBuffer::drawIndexedIndirect(Buffer *buffer, uint32_t offset, uint32_t count, uint32_t stride) {
-    cmdFuncGLES2PushGroupMarker(GLES2Device::getInstance(), marker.name.size(), marker.name.data());
     // not support
     std::ignore = buffer;
     std::ignore = offset;
