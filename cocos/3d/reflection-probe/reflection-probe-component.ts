406ea1452228ae42513eb265c63dbdce69a8fdf1
xubing <58895777@qq.com>
2023-08-18 18:41:30 +0800

    Optimizing model filtering methods for planar reflections

--- cocos/3d/reflection-probe/reflection-probe-component.ts
+++ cocos/3d/reflection-probe/reflection-probe-component.ts
@@ -192,6 +192,7 @@ export class ReflectionProbe extends Component {
      * @zh 相机的缓冲清除标志位，指定帧缓冲的哪部分要每帧清除。
      */
     @type(Enum(ProbeClearFlag))
+    @visible(function (this: ReflectionProbe) { return this.probeType === ProbeType.CUBE; })
     set clearFlag (value: number) {
         this._clearFlag = value;
         this.probe.clearFlag = this._clearFlag;
@@ -204,7 +205,7 @@ export class ReflectionProbe extends Component {
      * @en Clearing color of the camera.
      * @zh 相机的颜色缓冲默认值。
      */
-    @visible(function (this: ReflectionProbe) { return this._clearFlag === ProbeClearFlag.SOLID_COLOR; })
+    @visible(function (this: ReflectionProbe) { return this._clearFlag === ProbeClearFlag.SOLID_COLOR && this.probeType === ProbeType.CUBE; })
     @type(Color)
     set backgroundColor (val: Color) {
         this._backgroundColor = val;
@@ -220,6 +221,7 @@ export class ReflectionProbe extends Component {
      */
     @type(Layers.BitMask)
     @tooltip('i18n:camera.visibility')
+    @visible(function (this: ReflectionProbe) { return this.probeType === ProbeType.CUBE; })
     get visibility (): number {
         return this._visibility;
     }
