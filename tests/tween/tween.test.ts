63a3fe2ca23bfb2ec7322ba5fdffcfe2f6960437
zxx43 <zxx43@hotmail.com>
2023-08-28 17:05:31 +0800

    add unit test

--- tests/tween/tween.test.ts
+++ tests/tween/tween.test.ts
@@ -1,8 +1,11 @@
-import { Vec3, System } from "../../cocos/core";
-import { tween, Tween, TweenSystem } from "../../cocos/tween";
+import { Vec3, System, Color, Quat, Rect, Size, Vec2, Vec4 } from "../../cocos/core";
+import { tween, Tween, TweenAction, TweenSystem } from "../../cocos/tween";
 import { Node, Scene } from "../../cocos/scene-graph";
 import { Component } from "../../cocos/scene-graph/component";
 import { game, director } from "../../cocos/game";
+import { CSMShadowLayer } from "../../cocos/rendering/shadow/csm-layers";
+import { Shadows } from "../../cocos/render-scene/scene";
+import { CameraComponent } from "../../cocos/misc";
 
 test('remove actions by tag', function () {
     const scene = new Scene('test-tags');
@@ -38,4 +41,102 @@ test('destroySelf', function () {
     game.step();
     expect(onDestroy).toBeCalledTimes(1);
     director.unregisterSystem(sys);
+});
+
+test('type color', function () {
+    const shadow = new Shadows();
+    const tweenact = tween(shadow).to(1, {shadowColor: Color.TRANSPARENT});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Color).toBeTruthy();
+    }
+});
+
+test('type quat', function () {
+    const node = new Node();
+    const tweenact = tween(node).to(1, {rotation: new Quat(1, 1, 1, 1)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Quat).toBeTruthy();
+    }
+});
+
+test('type rect', function () {
+    const camera = new CameraComponent();
+    const tweenact = tween(camera).to(1, {rect: new Rect(1, 1, 10, 20)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Rect).toBeTruthy();
+    }   
+});
+
+test('type size', function () {
+    const rect = new Rect();
+    const tweenact = tween(rect).to(1, {size: new Size(800, 600)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Size).toBeTruthy();
+    }   
+});
+
+test('type vec2', function () {
+    const rect = new Rect();
+    const tweenact = tween(rect).to(1, {origin: new Vec2(20, 10)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Vec2).toBeTruthy();
+    }   
+});
+
+test('type vec3', function () {
+    const node = new Node();
+    const tweenact = tween(node).to(1, {position: new Vec3(10, 20, 30)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Vec3).toBeTruthy();
+    }   
+});
+
+test('type vec4', function () {
+    const shadowLayer = new CSMShadowLayer(4);
+    const tweenact = tween(shadowLayer).to(1, {csmAtlas: new Vec4(10, 20, 30, 40)});
+    tweenact.start();
+    // @ts-expect-error access private property 
+    const action = tweenact._actions[0] as TweenAction;
+    // @ts-expect-error access private property 
+    const props = action._props;
+    for (const property in props) {
+        const prop = props[property];
+        expect(prop.current instanceof Vec4).toBeTruthy();
+    }   
 });
