091557dcecc78959a0fc1d3ccbacd5d38dd62b19
袁炜海 <741484865@qq.com>
2023-08-31 11:00:34 +0800

    feat: use css var

--- editor/inspector/components/block-input-events.js
+++ editor/inspector/components/block-input-events.js
@@ -1,17 +1,16 @@
 const { $, update, close } = require('./base');
 
 exports.style = /* css */`
-    .block-input-events-tip {
-        background-color: rgb(51, 51, 51);
-        border: 1px solid rgb(102, 102, 102);
-        border-radius: 3px;
-        margin: 10px;
-        padding: 10px;
+    .block-input-events {
+        border: 1px solid var(--color-default-border-weaker);
+        border-radius: 4px;
+        margin: 8px;
+        padding: 8px;
     }
 `;
 
 exports.template =/* html */ `
-<div class="block-input-events-tip">
+<div class="block-input-events">
     <ui-label value="i18n:ENGINE.components.blockInputEventsTip"></ui-label>
 </div>`;
 exports.$ = $;
