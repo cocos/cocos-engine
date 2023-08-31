bcf72ba307eefd3cc2086e09e682d1476ad763f3
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-08-31 16:07:25 +0800

    Revert "Replace old pipeline (#168)"
    This reverts commit b5c272aa98d9207e4affda59f2fb61b91b63371a.
    
    # Conflicts:
    #	editor/engine-features/render-config.json

--- exports/custom-pipeline.ts
+++ exports/custom-pipeline.ts
@@ -1,11 +1,8 @@
 import { legacyCC } from '../cocos/core/global-exports';
-import { macro } from '../cocos/core/platform';
 import * as rendering from '../cocos/rendering/custom';
 import * as postProcess from '../cocos/rendering/post-process';
 
 export { rendering };
 export { postProcess };
-
-macro.CUSTOM_PIPELINE_NAME = 'Forward';
 
 legacyCC.rendering = rendering;
