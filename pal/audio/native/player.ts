8df60081d34e5a28399431e5d96c91c11c768ff1
PP_Pro <chinese1236@qq.com>
2023-09-13 15:24:13 +0800

    fix compile native ts

--- pal/audio/native/player.ts
+++ pal/audio/native/player.ts
@@ -25,7 +25,8 @@
 import { systemInfo } from 'pal/system-info';
 import { AudioType, AudioState, AudioEvent, AudioPCMDataView, AudioBufferView, AudioLoadOptions } from '../type';
 import { EventTarget } from '../../../cocos/core/event';
-import { cclegacy, clamp01 } from '@base/global';
+import { clamp01 } from '../../../cocos/core';
+import { cclegacy } from '@base/global';
 import { enqueueOperation, OperationInfo, OperationQueueable } from '../operation-queue';
 import { Platform } from '../../system-info/enum-type';
 import { Game, game } from '../../../cocos/game';
