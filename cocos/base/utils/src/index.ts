e1ba067819c33f00f9a831a4f3b56fb045b90300
PP_Pro <chinese1236@qq.com>
2023-09-25 17:18:11 +0800

    add x-deprecated in @base/utils module

--- cocos/base/utils/src/index.ts
+++ cocos/base/utils/src/index.ts
@@ -3,5 +3,6 @@ import * as js from './js';
 
 export { js };
 export { memop };
+export * from './x-deprecated';
 // NOTE: This is deprecated, should be removed in the future
 export * from './memop';
