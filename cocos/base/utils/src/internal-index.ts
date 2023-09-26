f2de85761a1e181513b38b0586abc95de9674103
PP_Pro <chinese1236@qq.com>
2023-09-26 16:40:23 +0800

    fix: deprecate `isPlainEmptyObj_DEV`

--- cocos/base/utils/src/internal-index.ts
+++ cocos/base/utils/src/internal-index.ts
@@ -1,1 +1,10 @@
+import { js } from './js';
+
 export { ScalableContainer, scalableContainerManager } from './memop/scalable-container';
+
+export function isPlainEmptyObj (obj): boolean {
+    if (!obj || obj.constructor !== Object) {
+        return false;
+    }
+    return js.isEmptyObject(obj);
+}
