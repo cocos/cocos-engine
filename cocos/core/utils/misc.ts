c0ba1de24475ed2144ec2a50c8b9e92daf3e3a87
PP_Pro <chinese1236@qq.com>
2023-10-08 17:57:59 +0800

    deprecate `misc.BASE64_VALUES`

--- cocos/core/utils/misc.ts
+++ cocos/core/utils/misc.ts
@@ -32,18 +32,16 @@ import { warnID } from '@base/debug';
 import { js } from '@base/utils';
 import { isPlainEmptyObj } from '@base/utils/internal';
 import { toRadian, toDegree } from '../math';
+import values from '../../../external/compression/base64-values';
 
 /**
  * @deprecated since v3.9.0
  */
 export const BUILTIN_CLASSID_RE = /^(?:cc|dragonBones|sp|ccsg)\..+/;
 
-const BASE64_KEYS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-const values: number[] = new Array(123); // max char code in base64Keys
-for (let i = 0; i < 123; ++i) { values[i] = 64; } // fill with placeholder('=') index
-for (let i = 0; i < 64; ++i) { values[BASE64_KEYS.charCodeAt(i)] = i; }
-
-// decoded value indexed by base64 char code
+/**
+ * @deprecated since v3.9.0
+ */
 export const BASE64_VALUES = values;
 
 /**
