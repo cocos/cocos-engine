24842354da0d1898089bc8bd0c59ef30fd817ab6
nam <thanhnam96.uit@gmail.com>
2023-11-07 13:17:50 +0700

    + fix display bug when mask has opacity = 0

--- cocos/2d/renderer/batcher-2d.ts
+++ cocos/2d/renderer/batcher-2d.ts
@@ -840,7 +840,8 @@ export class Batcher2D implements IBatcher {
         // ATTENTION: Will also reset colorDirty inside postUpdateAssembler
         if (render && render.enabledInHierarchy) {
             render.postUpdateAssembler(this);
-            if ((render.stencilStage === Stage.ENTER_LEVEL || render.stencilStage === Stage.ENTER_LEVEL_INVERTED)
+            if (!approx(opacity, 0, EPSILON) 
+            && (render.stencilStage === Stage.ENTER_LEVEL || render.stencilStage === Stage.ENTER_LEVEL_INVERTED)
             && (StencilManager.sharedManager!.getMaskStackSize() > 0)) {
                 this.autoMergeBatches(this._currComponent!);
                 this.resetRenderStates();
