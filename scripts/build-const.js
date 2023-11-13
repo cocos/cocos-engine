c41807c4f69a7f39dedefdd137eb0a400c23bf68
SijieWang <devhacker520@hotmail.com>
2023-11-13 09:31:40 +0800

    [Optimize] Simplify workflow (#16503)
    * [Optimize] Simplify workflow

--- scripts/build-const.js
+++ scripts/build-const.js
@@ -1,21 +1,23 @@
 const { StatsQuery } = require('@cocos/build-engine');
-const ps = require('path');
-const fsExt = require('fs-extra');
-const chalk = require('chalk').default;
+const { join } = require('path');
+const { outputFileSync } = require('fs-extra');
+const { magenta, green } = require('chalk');
 
-const engineRoot = ps.join(__dirname, '../');
-const targetConstFile = ps.join(engineRoot, `./@types/consts.d.ts`);
+const engineRoot = join(__dirname, '../');
+const targetConstFile = join(engineRoot, `./@types/consts.d.ts`);
 
-console.log(chalk.green(`\nGenerate consts.d.ts: ${targetConstFile}\n`));
+const prefix = ''.padStart(20, '=');
+console.log(magenta(`${prefix} Build const ${prefix}`));
 
-StatsQuery.create(engineRoot).then(statsQuery => {
+StatsQuery.create(engineRoot).then((statsQuery) => {
     const ic = statsQuery.constantManager.genInternalConstants();
-    const source =
-          `/**\n` +
-          ` * This is an auto-generated declaration file for constants internally used in engine.\n` +
-          ` * You may regenerate it by running cli command \`npm run build-const\`.\n` +
-          ` */\n` +
-          `\n` +
-          ic.replace(/\t/g, '    ');
-    fsExt.outputFileSync(targetConstFile, source, 'utf8');
+    const source =          `/**\n`
+          + ` * This is an auto-generated declaration file for constants internally used in engine.\n`
+          + ` * You may regenerate it by running cli command \`npm run build-const\`.\n`
+          + ` */\n`
+          + `\n${
+              ic.replace(/\t/g, '    ')}`;
+    outputFileSync(targetConstFile, source, 'utf8');
+
+    console.log(green(`\nGenerate consts.d.ts: ${targetConstFile}\n`));
 });
