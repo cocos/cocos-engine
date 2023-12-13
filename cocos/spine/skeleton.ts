3095b1e924e3ee8d444a2fc1d95106762b27184c
bofeng <bofeng.song@chukong-inc.com>
2023-12-13 16:39:30 +0800

    Fix setAnimation can not work normally at cache mode while animation's name different with default.

--- cocos/spine/skeleton.ts
+++ cocos/spine/skeleton.ts
@@ -891,6 +891,7 @@ export class Skeleton extends UIRenderer {
             let cache = this._skeletonCache.getAnimationCache(this._skeletonData!.uuid, name);
             if (!cache) {
                 cache = this._skeletonCache.initAnimationCache(this.skeletonData!.uuid, this._skeletonData!, name);
+                cache?.setSkin(this._skinName);
             }
             if (cache) {
                 this._animationName = name;
@@ -990,6 +991,7 @@ export class Skeleton extends UIRenderer {
                 this._animCache.setSkin(name);
             }
         }
+        this._skinName = name;
         this.invalidAnimationCache();
     }
 
