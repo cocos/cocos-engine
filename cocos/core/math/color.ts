314f90ea2881be506dbdf1625db8a5a13f8b4629
finscn <finscn@gmail.com>
2023-12-20 16:57:02 +0800

    支持 3位和4位的 color hex string

--- cocos/core/math/color.ts
+++ cocos/core/math/color.ts
@@ -140,6 +140,10 @@ export class Color extends ValueType {
             hex = hex[0] === '#' ? hex.substring(1) : hex;
             if (hex.length === 6) {
                 hex += 'FF';
+            } else if (hex.length === 3) {
+                hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2] + 'FF';
+            } else if (hex.length === 4) {
+                hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
             }
             hexNumber = Number('0x' + hex);
         } else {
