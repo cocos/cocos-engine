dbd251c7ffbd8173432ed6f36aa0311ea065f86a
媛媛 <yuanyuan1497@foxmail.com>
2024-01-12 16:06:05 +0800

    [3.8.3] add isNativeModule/hidden/readonly in engine feature render config (#16662)

--- editor/engine-features/types.ts
+++ editor/engine-features/types.ts
@@ -1,40 +1,5 @@
 // tslint:disable:interface-name
 
-export type EngineFeature =
-    | 'gfx-webgl'
-    | 'gfx-webgl2'
-    | 'gfx-webgpu'
-    | 'base'
-    | 'graphcis'
-    | '3d'
-    | '2d'
-    | 'xr'
-    | 'ui'
-    | 'particle'
-    | 'physics'
-    | 'physics-cannon'
-    | 'physics-ammo'
-    | 'physics-builtin'
-    | 'physics-physx'
-    | 'particle-2d'
-    | 'webview'
-    | 'tween'
-    | 'physics-2d'
-    | 'physics-2d-box2d'
-    | 'physics-2d-builtin'
-    | 'physics-2d-box2d-wasm'
-    | 'intersection-2d'
-    | 'audio'
-    | 'video'
-    | 'terrain'
-    | 'tiled-map'
-    | 'spine'
-    | 'dragon-bones'
-    | 'primitive'
-    | 'profiler'
-    | 'marionette'
-    | 'animation'
-    | 'skeletal-animation';
 export interface ModuleRenderConfig {
     $schema?: string;
 
@@ -47,74 +12,96 @@ export interface ModuleRenderConfig {
      * The categories info
      */
     categories: { [category: string]: CategoryInfo };
+
+    version: string;
+
+    /**
+     * The script to migrate, this script should export a const migrations: Migration[]`.
+     */
+    migrationScript?: string;
 }
 
-export type Features = Record<EngineFeature, Item>;
-
-export interface FlagBaseItem {
-    /**
-     * Display text.
-     */
-    label: string;
-
-    /**
-     * Description.
-     */
-    description?: string;
-
-    native?: string;
-
-    wechatPlugin?: boolean;
-
-    /**
-     * Different with wechat plugin, taobao plugin can't read buffer from local wasm.
-     * So we need another config item for taobao minigame plugin.
-     */
-    taobaoMinigamePlugin?: boolean;
-
-    default?: string[];
+export interface Migration {
+    version: string;
+    migrate(moduleCache: Record<string, boolean>): Record<string, boolean>;
 }
+
+export type Features = Record<string, IFeatureItem>;
 
 export interface BaseItem {
     /**
-     * Display text.
+     * @zh 在项目设置上显示的模块名称，支持 i18n 格式
+     * @en the module name displayed on the project settings, can be configured in i18n format.
      */
-    label: string;
+    label?: string;
 
     /**
-     * Description.
+     * @zh 模块详细描述，将会显示在模块鼠标上移后的提示，支持 `i18n` 的格式
+     * @en the detailed description of the module, which will be displayed in the tooltip when the mouse is moved over the module, can be configured in i18n format.
      */
     description?: string;
 
-    required?: boolean;
-
-    native?: string;
-
-    wechatPlugin?: boolean;
+    /**
+     * @zh 是否默认包含该模块
+     * @en whether the module is included by default.
+     */
+    default: boolean;
 
     /**
-     * Different with wechat plugin, taobao plugin can't read buffer from local wasm.
-     * So we need another config item for taobao minigame plugin.
+     * @zh 标识是否为只读模块，设为 `true` 后用户无法开启或关闭该模块。
+     * @en whether the module is read-only. If set to true, users cannot modify this module.
      */
-    taobaoMinigamePlugin?: boolean;
+    readonly?: boolean;
 
-    default?: string[];
+    /**
+     * @zh 在原生引擎的模块宏配置，如果在原生端有原生实现，在此处补充对应字段，后续根据项目设置的配置情况，会将选择值设置到 `cmake` 配置内。
+     * @en The macro configuration of the native module in the native engine. If there is a native implementation in the native engine, please fill in the corresponding fields here. The value will be set to `cmake` configuration according to the project settings.
+     */
+    cmakeConfig?: string;
 
+    /**
+     * @zh 是否为原生模块，这部分模块的编译模式可能是 wasm 也可能是共存或只有 asmjs，为 true 的模块，如果模块勾选构建面板上才会显示原生代码打包模式的配置。
+     * @en Whether it is a native module. This part of the module may be compiled as wasm or asmjs, and the module with this attribute will be displayed in the packaging mode configuration in the build panel if it is selected.
+     */
+    isNativeModule?: boolean;
+
+    /**
+     * @zh 是否在项目设置上隐藏该模块，设为 `true` 后将不会显示在项目设置中。
+     * @en Whether to hide the module in the project settings. If set to true, it will not be displayed in the project settings.
+     */
+    hidden?: boolean;
+
+    /**
+     * @zh 默认的模块分组归属，对应 `features` 字段同级的 `categories` 中配置的目录。
+     * @en The default module group belongs to, corresponding to the directory configured under the `categories` field at the same level of the `features` field.
+     */
     category?: string;
 
-    flags?: Record<string, FlagBaseItem>;
+    /**
+     * @zh 该模块依赖的其他模块，如果依赖了其他模块，则此模块勾选后，依赖模块也会被自动勾选。反过来，依赖的模块被移除勾选，此模块也会被一并移除。
+     * @en The other modules that this module depends on. If the module depends on other modules, the dependent modules will be automatically selected. In addition, if the dependent module is removed, this module will also be removed.
+     */
+    dependencies?: string[];
+
+    /**
+     * @zh 是否默认以及允许包含在上传的各个小游戏引擎插件内，目前由于部分引擎模块包体较大，默认不会打包在官方的微信引擎分离插件内。（临时方案）
+     * @en Whether it is included in the upload of the various engine plugins by default. Currently, because some engine modules are packaged in a large package, the official WeChat engine separation plugin does not package them by default. (Temporary solution)
+     */
+    enginePlugin?: boolean;
+
+    /**
+     * @zh 是否为必选模块，新增模块为必选模块时，旧版本升级后会强制选择此模块，否则不会选择。
+     * @en Whether it is a required module. When adding a new module, the old version will be forced to select this module after the upgrade, otherwise it will not be selected.
+     */
+    required?: boolean;
 }
 
-export interface Item extends BaseItem {
-    /**
-     * Whether if the feature of options allow multiple selection.
-     */
-    multi?: boolean;
-
-    options?: Record<EngineFeature, BaseItem>;
+export interface IFeatureItem extends BaseItem {
+    options: Record<string, BaseItem>
 }
 
 export interface CategoryInfo {
     label?: string;
     description?: string;
+    checkable?: boolean;
 }
