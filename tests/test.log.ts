17374c391a7c7695c5506f844738f274c36e9ea3
苏珊虹 <shanhong.su@cocos.com>
2024-04-07 09:56:20 +0800

    测试pr流程

--- tests/test.log.ts
+++ tests/test.log.ts
@@ -2,6 +2,7 @@ const logEnabled = false;
 
 export function log(message?: any, ...optionalParams: any[]): void {
     if (logEnabled) {
+        console.log('111')
         console.log(message, ...optionalParams);
     }
 }
