cc2712ef48ae901c40dcfc074c6269b12e26311e
finscn <finscn@gmail.com>
2024-05-10 03:04:06 +0800

    Update skeleton.ts
    此处应统一使用 `_color`

--- cocos/spine/skeleton.ts
+++ cocos/spine/skeleton.ts
@@ -1662,7 +1662,7 @@ export class Skeleton extends UIRenderer {
     public _updateColor (): void {
         const a = this.node._uiProps.opacity;
         // eslint-disable-next-line max-len
-        if (this._tempColor.r === this._color.r && this._tempColor.g === this.color.g && this._tempColor.b === this.color.b && this._tempColor.a === a) {
+        if (this._tempColor.r === this._color.r && this._tempColor.g === this._color.g && this._tempColor.b === this._color.b && this._tempColor.a === a) {
             return;
         }
         this.node._uiProps.colorDirty = true;
