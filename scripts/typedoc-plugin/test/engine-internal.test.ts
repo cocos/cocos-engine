94f95777cec707aa89f03c302251ba35427c0e85
James Chen <dumganhar@gmail.com>
2024-05-28 17:36:47 +0800

    Remove typedoc-plugin

--- scripts/typedoc-plugin/test/engine-internal.test.ts
+++
@@ -1,6 +0,0 @@
-import ps from 'path';
-import { runTest } from './test-util';
-
-test(`fix type arguments`, async () => {
-    await runTest(ps.join(__dirname, './inputs/link/'), 'engine-internal.ts');
-});
--- scripts/typedoc-plugin/test/inputs/link/fix-type-arguments.ts
+++
@@ -1,5 +0,0 @@
-export class A {}
-
-export function test<T extends A>(t: A): Record<string, T> {
-    throw 'not impl';
-}
