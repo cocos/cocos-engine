4199d3be6d8a78ad15c7c80d19d5ea2dbb3ab091
bofeng-song <bofeng.song@chukong-inc.com>
2024-05-31 15:30:55 +0800

    Fix b2 will reset to empty at taobao platform (#17075)
    * fix b2 will reset to empty at taobao platform

--- cocos2d/core/physics/box2d-adapter.js
+++ cocos2d/core/physics/box2d-adapter.js
@@ -1,6 +1,12 @@
 let box2d = require('../../../external/box2d/box2d');
 
-window.b2 = {};
+let sys = cc.sys;
+if (sys.platform === sys.TAOBAO_MINIGAME) {
+    //b2 exist at TAOBAO_MINIGAME platform.
+    window.b2 = b2 ? b2 : {};
+} else {
+    window.b2 = {};
+}
 
 for (var key in box2d) {
     if (key.indexOf('b2_') !== -1) {
