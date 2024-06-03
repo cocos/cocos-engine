7b2ffc9edadefe6e94c7f1e1d9355b55e01733f2
minggo <nudtzxm@gmail.com>
2024-06-03 09:51:35 +0800

    fix logic error of set world rotation/position/scale (#17079)
    * fix logic error of set world rotation/position/scale

--- cocos/scene-graph/node.ts
+++ cocos/scene-graph/node.ts
@@ -2365,20 +2365,29 @@ export class Node extends CCObject implements ISchedulable, CustomSerializable {
     public setWorldPosition (val: Vec3 | number, y?: number, z?: number): void {
         const worldPosition = this._pos;
 
+        // Force update may happen in the situation:
+        // - node is created, and added to a scene
+        // - set node's world position to default value(Vec3.ZERO), which equals to node._pos
+        // Then need to update local position, or local position will be wrong.
+        const forceUpdateLocalPosition = this._parent
+                                         && (this._transformFlags & TransformBit.POSITION) !== TransformBit.NONE;
+
         if (y === undefined) {
-            if (worldPosition.equals(val as Vec3)) {
+            if (!forceUpdateLocalPosition && worldPosition.equals(val as Vec3)) {
                 return;
             }
 
             Vec3.copy(worldPosition, val as Vec3);
         } else {
             Vec3.set(v3_a, val as number, y, z!);
-            if (worldPosition.equals(v3_a)) {
+
+            if (!forceUpdateLocalPosition && worldPosition.equals(v3_a)) {
                 return;
             }
 
             Vec3.copy(worldPosition, v3_a);
         }
+
         const parent = this._parent;
         const local = this._lpos;
         if (parent) {
@@ -2432,20 +2441,29 @@ export class Node extends CCObject implements ISchedulable, CustomSerializable {
 
     public setWorldRotation (val: Quat | number, y?: number, z?: number, w?: number): void {
         const worldRotation = this._rot;
+
+        // Force update may happen in the situation:
+        // - node is created, and added to a scene
+        // - set node's world rotation to default value(Vec3.ZERO), which equals to node._rot
+        // Then need to update local rotation.
+        const forceUpdateLocalRotation = this._parent
+                                        && (this._transformFlags & TransformBit.ROTATION) !== TransformBit.NONE;
+
         if (y === undefined) {
-            if (worldRotation.equals(val as Quat)) {
+            if (!forceUpdateLocalRotation && worldRotation.equals(val as Quat)) {
                 return;
             }
 
             Quat.copy(worldRotation, val as Quat);
         } else {
             Quat.set(q_a, val as number, y, z!, w!);
-            if (worldRotation.equals(q_a)) {
+            if (!forceUpdateLocalRotation && worldRotation.equals(q_a)) {
                 return;
             }
 
             Quat.copy(worldRotation, q_a);
         }
+
         if (this._parent) {
             this._parent.updateWorldTransform();
             Quat.multiply(this._lrot, Quat.conjugate(this._lrot, this._parent._rot), worldRotation);
@@ -2470,8 +2488,15 @@ export class Node extends CCObject implements ISchedulable, CustomSerializable {
     public setWorldRotationFromEuler (x: number, y: number, z: number): void {
         const worldRotation = this._rot;
 
+        // Force update may happen in the situation:
+        // - node is created, and added to a scene
+        // - set node's world rotation to default value(Vec3.ZERO), which equals to node._rot
+        // Then need to update local rotation.
+        const forceUpdateLocalRotation = this._parent
+                                        && (this._transformFlags & TransformBit.ROTATION) !== TransformBit.NONE;
+
         Quat.fromEuler(q_a, x, y, z);
-        if (worldRotation.equals(q_a)) {
+        if (!forceUpdateLocalRotation && worldRotation.equals(q_a)) {
             return;
         }
 
@@ -2523,19 +2548,25 @@ export class Node extends CCObject implements ISchedulable, CustomSerializable {
     public setWorldScale (val: Vec3 | number, y?: number, z?: number): void {
         const worldScale = this._scale;
 
+        // Force update may happen in the situation:
+        // - node is created, and added to a scene
+        // - set node's world scale to default value(Vec3(1,1,1)), which equals to node._scale
+        // Then need to update local scale.
+        const forceUpdateLocalScale = this._parent
+                                    && (this._transformFlags & TransformBit.SCALE) !== TransformBit.NONE;
         const parent = this._parent;
         if (parent) {
             this.updateWorldTransform();
         }
         if (y === undefined) {
-            if (worldScale.equals(val as Vec3)) {
+            if (!forceUpdateLocalScale && worldScale.equals(val as Vec3)) {
                 return;
             }
 
             Vec3.copy(worldScale, val as Vec3);
         } else {
             Vec3.set(v3_a, val as number, y, z!);
-            if (worldScale.equals(v3_a)) {
+            if (!forceUpdateLocalScale && worldScale.equals(v3_a)) {
                 return;
             }
 
