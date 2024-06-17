fe069da3ef60e4f617f394c6243e1a29c9877434
James Chen <dumganhar@gmail.com>
2024-06-17 13:37:21 +0800

    fixed #17162: Memory leak in WebSocket-okhttp.cpp (#17164)
    * fixed #17162: Memory leak in WebSocket-okhttp.cpp
    
    * Use JNI_ABORT

--- native/cocos/network/WebSocket-okhttp.cpp
+++ native/cocos/network/WebSocket-okhttp.cpp
@@ -389,6 +389,7 @@ JNI_PATH(nativeOnBinaryMessage)(JNIEnv *env,
         jboolean isCopy = JNI_FALSE;
         jbyte *array = env->GetByteArrayElements(static_cast<jbyteArray>(strongRef), &isCopy);
         wsOkHttp3->onBinaryMessage(reinterpret_cast<uint8_t *>(array), len);
+        env->ReleaseByteArrayElements(static_cast<jbyteArray>(strongRef), array, JNI_ABORT);
         env->DeleteGlobalRef(strongRef);
     } while (false));
 }
