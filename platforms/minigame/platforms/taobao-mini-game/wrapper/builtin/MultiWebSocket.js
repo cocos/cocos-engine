3255c6d53077c7182ced45a6d68917ca7c4d799e
bofeng <bofeng.song@chukong-inc.com>
2024-07-18 16:09:38 +0800

    Due to the single-instance version of the WebSocket, the onMessage response res contains only one layer of data. For compatibility with the online game, Taobao removed the extra layer of data

--- platforms/minigame/platforms/taobao-mini-game/wrapper/builtin/MultiWebSocket.js
+++ platforms/minigame/platforms/taobao-mini-game/wrapper/builtin/MultiWebSocket.js
@@ -42,8 +42,8 @@ export default class MultiWebSocket {
 
     socketTask.onMessage((res) => {
       if (typeof this.onmessage === 'function') {
-        if (res && res.data && res.data.data && res.data.isBuffer) {
-          res.data = _utils.base64ToArrayBuffer(res.data.data)
+        if (res && res.data && res.isBuffer) {
+          res.data = _utils.base64ToArrayBuffer(res.data)
         }
         this.onmessage(res)
       }
