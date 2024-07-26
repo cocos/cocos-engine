06e9950b394da0d176d95c5993de70b027e60658
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-07-26 15:05:34 +0800

    add missing createDefaultPipeline

--- cocos/rendering/legacy/index.jsb.ts
+++ cocos/rendering/legacy/index.jsb.ts
@@ -45,9 +45,11 @@ import * as decors from '../../native-binding/decorators';
 import { ccclass, serializable, editable, type } from '../../core/data/class-decorator';
 import { RenderTexture } from '../../asset/assets/render-texture';
 
-export {
-    createDefaultPipeline
-} from './index';
+export function createDefaultPipeline (): ForwardPipeline {
+    const rppl = new ForwardPipeline();
+    rppl.initialize({ flows: [] });
+    return rppl;
+}
 export const ForwardPipeline: typeof NrForwardPipeline = nr.ForwardPipeline;
 export type ForwardPipeline = NrForwardPipeline;
 export const ForwardFlow: typeof NrForwardFlow = nr.ForwardFlow;
