d37077b3e7dd40b0707b1a659ea00e8f29f974d8
Jerry <81011681+Jerry457@users.noreply.github.com>
2024-08-02 19:09:39 +0800

    fix: property retrieval to only include direct children

--- cocos/tiledmap/tmx-xml-parser.ts
+++ cocos/tiledmap/tmx-xml-parser.ts
@@ -89,7 +89,7 @@ function strToColor (value: string): Color {
 
 function getPropertyList (node: Element, map?: PropertiesInfo): PropertiesInfo {
     const res: any[] = [];
-    const properties = node.getElementsByTagName('properties');
+    const properties = node.querySelectorAll(':scope > properties');
     for (let i = 0; i < properties.length; ++i) {
         const property = properties[i].getElementsByTagName('property');
         for (let j = 0; j < property.length; ++j) {
