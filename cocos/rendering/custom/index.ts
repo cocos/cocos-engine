8115f76e42aac511f9f892edc78aa41cf6ea5480
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-08-20 15:11:42 +0800

    wrap arrayBuffer with Uint8Array

--- cocos/rendering/custom/index.ts
+++ cocos/rendering/custom/index.ts
@@ -75,18 +75,19 @@ export function getCustomPipeline (name: string): PipelineBuilder {
     return builder;
 }
 
-export function init (device: Device, arrayBuffer: ArrayBuffer | null): void {
-    if (arrayBuffer && arrayBuffer.byteLength >= LAYOUT_HEADER_SIZE) {
-        const header = new DataView(arrayBuffer, 0, LAYOUT_HEADER_SIZE);
+export function init (device: Device, arrayBufferLike: ArrayBuffer | null): void {
+    if (arrayBufferLike && arrayBufferLike.byteLength >= LAYOUT_HEADER_SIZE) {
+        const uint8Array = new Uint8Array(arrayBufferLike);
+        const header = new DataView(uint8Array.buffer, 0, LAYOUT_HEADER_SIZE);
         if (header.getUint32(0) === INVALID_ID) {
             // Data is compressed
-            const inflator = new zlib.Inflate(new Uint8Array(arrayBuffer, LAYOUT_HEADER_SIZE));
+            const inflator = new zlib.Inflate(new Uint8Array(uint8Array, LAYOUT_HEADER_SIZE));
             const decompressed = inflator.decompress() as Uint8Array;
             const readBinaryData = new BinaryInputArchive(decompressed.buffer);
             loadLayoutGraphData(readBinaryData, defaultLayoutGraph);
         } else {
             // Data is not compressed
-            const readBinaryData = new BinaryInputArchive(arrayBuffer);
+            const readBinaryData = new BinaryInputArchive(uint8Array.buffer);
             loadLayoutGraphData(readBinaryData, defaultLayoutGraph);
         }
     }
