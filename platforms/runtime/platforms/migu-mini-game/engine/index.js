a72d870229df86a5437e96b2a5c4b310870e42e8
lanbinqiang <39689887+W-MTZing@users.noreply.github.com>
2024-10-17 14:48:40 +0800

    [bug] fix migu platforms subpackage load fail (#17733)
    Co-authored-by: lbq <563294062@qq.com>

--- platforms/runtime/platforms/migu-mini-game/engine/index.js
+++ platforms/runtime/platforms/migu-mini-game/engine/index.js
@@ -1,1 +1,5 @@
-require('../../../common/engine/index');
+require('../../../common/engine/fs-utils');
+require('../../../common/engine/canvas');
+require('./asset-manager.js');
+require('../../../common/engine/EditBox.js');
+require('../../../common/engine/game.js');
