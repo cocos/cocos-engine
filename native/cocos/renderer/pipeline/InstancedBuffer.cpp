9261cec6648c16913646b71c3384c7935b318178
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-02-07 19:23:14 +0800

    fix lint

--- native/cocos/renderer/pipeline/InstancedBuffer.cpp
+++ native/cocos/renderer/pipeline/InstancedBuffer.cpp
@@ -69,7 +69,7 @@ void InstancedBuffer::merge(scene::SubModel *subModel, uint32_t passIdx, gfx::Sh
     gfx::Texture *reflectionProbeBlendCubemap = ENABLE_PROBE_BLEND
         ? descriptorSet->getTexture(REFLECTIONPROBEBLENDCUBEMAP::BINDING)
         : nullptr;
-    uint32_t reflectionProbeType = subModel->getReflectionProbeType();
+    const uint32_t reflectionProbeType = subModel->getReflectionProbeType();
     auto *shader = shaderImplant;
     if (!shader) {
         shader = subModel->getShader(passIdx);
@@ -104,6 +104,7 @@ void InstancedBuffer::merge(scene::SubModel *subModel, uint32_t passIdx, gfx::Sh
         if (instance.drawInfo.instanceCount >= instance.capacity) { // resize buffers
             instance.capacity <<= 1;
             const auto newSize = instance.stride * instance.capacity;
+            // NOLINTNEXTLINE(bugprone-suspicious-realloc-usage)
             instance.data = static_cast<uint8_t *>(CC_REALLOC(instance.data, newSize));
             instance.vb->resize(newSize);
         }
@@ -113,7 +114,7 @@ void InstancedBuffer::merge(scene::SubModel *subModel, uint32_t passIdx, gfx::Sh
         if (instance.descriptorSet != descriptorSet) {
             instance.descriptorSet = descriptorSet;
         }
-        memcpy(instance.data + instance.stride * instance.drawInfo.instanceCount++, attrs.buffer.buffer()->getData(), stride);
+        memcpy(instance.data + static_cast<size_t>(instance.stride) * instance.drawInfo.instanceCount++, attrs.buffer.buffer()->getData(), stride);
         _hasPendingModels = true;
         return;
     }
