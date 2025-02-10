ebae9b9a49edbab0770476c8164b6c745964a39e
James Chen <dumganhar@gmail.com>
2025-02-10 15:17:02 +0800

    Fix sprite inspector i18n configurations

--- editor/i18n/en/modules/ui.js
+++ editor/i18n/en/modules/ui.js
@@ -209,7 +209,7 @@ module.exports = {
                         displayName: 'Sprite Atlas',
                         tooltip: 'Atlas that the image belongs to.',
                     },
-                    'sprite_frame': {
+                    'spriteFrame': {
                         displayName: 'Sprite Frame',
                         tooltip: 'Sprite Frame image to use.',
                     },
@@ -221,7 +221,7 @@ module.exports = {
                             '- Tiled : When changing the size, the original size image will continue to be tiled. <br>' +
                             '- Filled : set a certain starting position and direction of filling, and the picture can be cropped and displayed at a certain ratio.',
                     },
-                    'size_mode': {
+                    'sizeMode': {
                         displayName: 'Size Mode',
                         tooltip: 'Set the size of the node on which the Sprite component is on. ' +
                             '<br>CUSTOM for setting width and height manually;' +
