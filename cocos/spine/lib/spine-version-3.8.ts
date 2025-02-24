3abf8b4239cc57fd9745058391d94a5b6989658a
bofeng <bofeng.song@chukong-inc.com>
2025-02-24 16:02:29 +0800

    refine

--- cocos/spine/lib/spine-version-3.8.ts
+++ cocos/spine/lib/spine-version-3.8.ts
@@ -21,16 +21,12 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
 */
-// for merge SPINE_VERSION to namespace 'spine'
 import spine from './spine-core';
 import { ensureWasmModuleReady } from 'pal/wasm';
 import { error } from '../../core';
 import { shouldUseWasmModule, initWasm, initAsmJS } from './spine-wasm-utils';
 
-
 (spine as any).SPINE_VERSION = '3.8';
-
-
 
 export function waitForSpineWasmInstantiation (): Promise<void> {
     const errorReport = (msg: any): void => { error(msg); };
