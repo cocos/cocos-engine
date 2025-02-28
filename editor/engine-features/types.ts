ec35f033904b6af350f0058c608b6801c3e0ff91
yanOO1497 <yuanyuan1497@foxmail.com>
2025-02-28 13:48:59 +0800

    update engine feature config

--- editor/engine-features/types.ts
+++ editor/engine-features/types.ts
@@ -27,7 +27,7 @@ export interface Migration {
 }
 
 export interface Features {
-    [feature: string]: IFeatureItem;
+    [feature: string]: IFeatureItem | IFeatureGroup;
 };
 
 export interface BaseItem {
@@ -56,18 +56,6 @@ export interface BaseItem {
     readonly?: boolean;
 
     /**
-     * @zh 在原生引擎的模块宏配置，如果在原生端有原生实现，在此处补充对应字段，后续根据项目设置的配置情况，会将选择值设置到 `cmake` 配置内。
-     * @en The macro configuration of the native module in the native engine. If there is a native implementation in the native engine, please fill in the corresponding fields here. The value will be set to `cmake` configuration according to the project settings.
-     */
-    cmakeConfig?: string;
-
-    /**
-     * @zh 是否为原生模块，这部分模块的编译模式可能是 wasm 也可能是共存或只有 asmjs，为 true 的模块，如果模块勾选构建面板上才会显示原生代码打包模式的配置。
-     * @en Whether it is a native module. This part of the module may be compiled as wasm or asmjs, and the module with this attribute will be displayed in the packaging mode configuration in the build panel if it is selected.
-     */
-    isNativeModule?: boolean;
-
-    /**
      * @zh 是否在项目设置上隐藏该模块，设为 `true` 后将不会显示在项目设置中。
      * @en Whether to hide the module in the project settings. If set to true, it will not be displayed in the project settings.
      */
@@ -79,6 +67,14 @@ export interface BaseItem {
      */
     category?: string;
 
+    /**
+     * @zh 是否为必选模块，新增模块为必选模块时，旧版本升级后会强制选择此模块，否则不会选择。
+     * @en Whether it is a required module. When adding a new module, the old version will be forced to select this module after the upgrade, otherwise it will not be selected.
+     */
+    required?: boolean;
+}
+
+export interface IFeatureItem extends BaseItem {
     /**
      * @zh 该模块依赖的其他模块，如果依赖了其他模块，则此模块勾选后，依赖模块也会被自动勾选。反过来，依赖的模块被移除勾选，此模块也会被一并移除。
      * @en The other modules that this module depends on. If the module depends on other modules, the dependent modules will be automatically selected. In addition, if the dependent module is removed, this module will also be removed.
@@ -104,20 +100,26 @@ export interface BaseItem {
     fallback?: string;
 
     /**
-     * @zh 是否为必选模块，新增模块为必选模块时，旧版本升级后会强制选择此模块，否则不会选择。
-     * @en Whether it is a required module. When adding a new module, the old version will be forced to select this module after the upgrade, otherwise it will not be selected.
-     */
-    required?: boolean;
-
-    /**
      * @zh 当选择了某个模块时，可以做些附加的配置
      * @en When a module is selected, additional configurations can be made
      */
     flags: {[k: string]: Pick<BaseItem, 'default' | 'label' | 'description' > & {'ui-type': 'checkbox' | 'select'}}
+
+    /**
+     * @zh 是否为原生模块，这部分模块的编译模式可能是 wasm 也可能是共存或只有 asmjs，为 true 的模块，如果模块勾选构建面板上才会显示原生代码打包模式的配置。
+     * @en Whether it is a native module. This part of the module may be compiled as wasm or asmjs, and the module with this attribute will be displayed in the packaging mode configuration in the build panel if it is selected.
+     */
+    isNativeModule?: boolean;
+
+    /**
+     * @zh 在原生引擎的模块宏配置，如果在原生端有原生实现，在此处补充对应字段，后续根据项目设置的配置情况，会将选择值设置到 `cmake` 配置内。
+     * @en The macro configuration of the native module in the native engine. If there is a native implementation in the native engine, please fill in the corresponding fields here. The value will be set to `cmake` configuration according to the project settings.
+     */
+    cmakeConfig?: string;
 }
 
-export interface IFeatureItem extends BaseItem {
-    options: {[feature: string]: BaseItem};
+export interface IFeatureGroup extends BaseItem {
+    options: {[feature: string]: IFeatureItem};
 }
 
 export interface CategoryInfo {
