b26d282a8ca9af36d7af0bdbf333bdb05703f630
James Chen <dumganhar@gmail.com>
2025-03-07 15:26:06 +0800

    Update version to 3.8.7

--- native/cocos/cocos-version.h
+++ native/cocos/cocos-version.h
@@ -25,9 +25,9 @@ THE SOFTWARE.
 
 #define COCOS_MAJOR_VERSION     3
 #define COCOS_MINJOR_VERSION    8
-#define COCOS_PATCH_VERSION     6
-#define COCOS_VERSION_STRING    "3.8.6"
+#define COCOS_PATCH_VERSION     7
+#define COCOS_VERSION_STRING    "3.8.7"
 #define COCOS_VERSION_DEFINED   1
-#define COCOS_VERSION           30806
+#define COCOS_VERSION           30807
 
 // #define COCOS_PRE_RELEASE       "release"
