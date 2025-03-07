83c258be42afcc8ad0cff1bd3f3704a86b0cf928
gt3mb <68908903+gt3mb@users.noreply.github.com>
2025-03-07 10:02:18 +0000

    Override destroy an remove touch listner from joint

--- cocos/physics-2d/box2d-wasm/joints/mouse-joint.ts
+++ cocos/physics-2d/box2d-wasm/joints/mouse-joint.ts
@@ -119,6 +119,12 @@ export class B2MouseJoint extends B2Joint implements IMouseJoint {
     }
 
     onTouchEnd (event: Touch): void {
+        
+        this.destroy();
+        this._isTouched = false;
+    }
+    destroy (): void {
+        super.destroy();
         const canvas = find('Canvas');
         if (canvas) {
             canvas.off(NodeEventType.TOUCH_START, this.onTouchBegan, this);
@@ -126,10 +132,7 @@ export class B2MouseJoint extends B2Joint implements IMouseJoint {
             canvas.off(NodeEventType.TOUCH_END, this.onTouchEnd, this);
             canvas.off(NodeEventType.TOUCH_CANCEL, this.onTouchEnd, this);
         }
-        this.destroy();
-        this._isTouched = false;
     }
-
     update (): void {
         if (!this._isTouched || !this.isValid()) {
             return;
