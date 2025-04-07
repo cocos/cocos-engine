54dfdc4f3321e44eb301882fc73ef8ae919c2a3c
James Chen <dumganhar@gmail.com>
2025-04-07 15:12:39 +0800

    Update test case

--- tests/tween/tween.test.ts
+++ tests/tween/tween.test.ts
@@ -5005,7 +5005,7 @@ test('parallel with set action', function () {
     director.unregisterSystem(sys);
 });
 
-test('associateNodeState(true) default test', function () {
+test('bindNodeTarget(true) default test', function () {
     const sys = new TweenSystem();
     (TweenSystem.instance as any) = sys;
     director.registerSystem(TweenSystem.ID, sys, System.Priority.MEDIUM);
@@ -5036,7 +5036,7 @@ test('associateNodeState(true) default test', function () {
     director.unregisterSystem(sys);
 });
 
-test('associateNodeState(true) test', function () {
+test('bindNodeTarget(true) test', function () {
     const sys = new TweenSystem();
     (TweenSystem.instance as any) = sys;
     director.registerSystem(TweenSystem.ID, sys, System.Priority.MEDIUM);
@@ -5049,7 +5049,7 @@ test('associateNodeState(true) test', function () {
     node.active = false;
 
     tween(node)
-        .bindNode(true)
+        .bindNodeTarget(true)
         .by(1, { position: v3(1, 1, 1) })
         .start();
 
@@ -5068,7 +5068,7 @@ test('associateNodeState(true) test', function () {
     director.unregisterSystem(sys);
 });
 
-test('associateNodeState(false) test', function () {
+test('bindNodeTarget(false) test', function () {
     const sys = new TweenSystem();
     (TweenSystem.instance as any) = sys;
     director.registerSystem(TweenSystem.ID, sys, System.Priority.MEDIUM);
@@ -5081,7 +5081,7 @@ test('associateNodeState(false) test', function () {
     node.active = false;
 
     tween(node)
-        .bindNode(false)
+        .bindNodeTarget(false)
         .by(2, { position: v3(2, 2, 2) })
         .start();
 
