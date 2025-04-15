a6096fd967a31ccaa6cd2e9e282f2f9c04cb7919
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-04-15 10:55:57 +0800

    add comment

--- native/cocos/renderer/gfx-metal/MTLUtils.mm
+++ native/cocos/renderer/gfx-metal/MTLUtils.mm
@@ -1059,6 +1059,7 @@ NSUInteger mu::highestSupportedFeatureSet(id<MTLDevice> device) {
     NSUInteger maxKnownFeatureSet;
 #if CC_PLATFORM == CC_PLATFORM_IOS
     if (@available(iOS 12.0, *)) {
+        // Upgrade to MTLFeatureSet_iOS_GPUFamily5_v1 might cause bug with ASTC.
         maxKnownFeatureSet = MTLFeatureSet_iOS_GPUFamily4_v2;
     } else if (@available(iOS 11.0, *)) {
         maxKnownFeatureSet = MTLFeatureSet_iOS_GPUFamily4_v1;
