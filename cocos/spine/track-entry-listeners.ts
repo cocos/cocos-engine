3dfa2fba458bf59c1a88f57d3b94c2b3ffc3fdfe
finscn <finscn@gmail.com>
2025-04-22 14:41:09 +0800

    Update track-entry-listeners.ts
    event 不存在时, 不执行 emitListener() 和  emitTrackEntryListener() 中的逻辑.

--- cocos/spine/track-entry-listeners.ts
+++ cocos/spine/track-entry-listeners.ts
@@ -52,7 +52,7 @@ export class TrackEntryListeners {
 
     static emitListener (id: number, entry: spine.TrackEntry, event: spine.Event, eventType: spine.EventType): void {
         const listener = TrackEntryListeners._listenerSet.get(id);
-        if (!listener) return;
+        if (!listener || !event) return;
         switch (eventType) {
         case spine.EventType.event:
             if (listener.event) {
@@ -92,7 +92,7 @@ export class TrackEntryListeners {
 
     static emitTrackEntryListener (id: number, entry: spine.TrackEntry, event: spine.Event, eventType: spine.EventType): void {
         const curTrack = this._trackSet.get(id);
-        if (!curTrack) return;
+        if (!curTrack || !event) return;
         switch (eventType) {
         case spine.EventType.start:
             if (curTrack.listener.start) {
