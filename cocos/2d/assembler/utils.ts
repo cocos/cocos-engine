630bb6528caf9b434a720869144379db4b11f505
qiuguohua <qiuguohua111@126.com>
2025-05-13 13:53:56 +0800

    Fix the problem of continuous error reporting due to chunk being null

--- cocos/2d/assembler/utils.ts
+++ cocos/2d/assembler/utils.ts
@@ -79,6 +79,10 @@ export function fillMeshVertices3D (node: Node, renderer: IBatcher, renderData: 
 }
 
 export function updateOpacity (renderData: RenderData, opacity: number): void {
+    if(!renderData.chunk) {
+        // When the allocation of StaticVBAccessor fails (when the allocated buffer is too large), chunk will be null.
+        return;
+    }
     const vfmt = renderData.vertexFormat;
     const vb = renderData.chunk.vb;
     let vbUint32View: Uint32Array | undefined;
