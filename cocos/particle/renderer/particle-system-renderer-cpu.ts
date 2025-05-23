4da4808d4f3dc1c3f30b8ad0a34175f70ae0f64e
tangkai <1944876319@qq.com>
2025-05-23 15:34:56 +0800

    update

--- cocos/particle/renderer/particle-system-renderer-cpu.ts
+++ cocos/particle/renderer/particle-system-renderer-cpu.ts
@@ -426,7 +426,7 @@ export default class ParticleSystemRendererCPU extends ParticleSystemRendererBas
             }
         }
 
-        for (let i = this._particles!.length - 1; i > 0; i--) {
+        for (let i = this._particles!.length - 1; i >= 0; i--) {
             const p = this._particles!.data[i];
             p.remainingLifetime -= dt;
             Vec3.set(p.animatedVelocity, 0, 0, 0);
