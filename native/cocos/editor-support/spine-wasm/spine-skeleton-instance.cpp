9e1b5ab04703eb1dee0d15bbff091c0d7205fa10
bofeng <bofeng.song@chukong-inc.com>
2025-06-25 14:34:31 +0800

    Fix the issue where setSlotTexture does not work properly when attachments come from different skeletonData objects.

--- native/cocos/editor-support/spine-wasm/spine-skeleton-instance.cpp
+++ native/cocos/editor-support/spine-wasm/spine-skeleton-instance.cpp
@@ -16,6 +16,20 @@ using namespace spine;
 extern HashMap<SkeletonData *, HashMap<Attachment *, AttachmentVertices *>*> spineAttachmentVerticesMap;
 extern HashMap<SkeletonData *, HashMap<spine::String, spine::String>*> spineTexturesMap;
 
+/**
+ * The slot-associated attachment may exist on different skeletonData, so setSlotTexture needs to traverse all skeletonData.
+ */
+AttachmentVertices *getAttachmentVertices(Attachment *attachment) {
+    auto entries = spineAttachmentVerticesMap.getEntries();
+    while (entries.hasNext()) {
+        auto entry = entries.next();
+        auto *skeletonData = entry.key;
+        auto *attachmentVerticesMap = entry.value;
+        if (attachmentVerticesMap->containsKey(attachment)) {
+            return (*attachmentVerticesMap)[attachment];
+        }
+    }
+}
 
 template<typename VertexType, typename UVArrayType>
 void loopUVCoords(VertexType* tmp, const UVArrayType& uvs, int count) {
@@ -621,11 +635,6 @@ void SpineSkeletonInstance::onAnimationStateEvent(TrackEntry *entry, EventType t
 
 void SpineSkeletonInstance::resizeSlotRegion(const spine::String &slotName, uint32_t width, uint32_t height, bool createNew) {
     if (!_skeleton) return;
-    HashMap<Attachment *, AttachmentVertices *> *attachmentVerticesMap = nullptr;
-    if (spineAttachmentVerticesMap.containsKey(_skeletonData)) {
-        attachmentVerticesMap = spineAttachmentVerticesMap[_skeletonData];
-    }
-    if (!attachmentVerticesMap) return;
     auto* slot = _skeleton->findSlot(slotName);
     if (!slot) return;
     auto *attachment = slot->getAttachment();
@@ -674,10 +683,7 @@ void SpineSkeletonInstance::resizeSlotRegion(const spine::String &slotName, uint
         region->updateRegion();
 #endif
 
-        AttachmentVertices *attachmentVertices = nullptr;
-        if (attachmentVerticesMap->containsKey(attachment)) {
-            attachmentVertices = (*attachmentVerticesMap)[attachment];
-        }
+        auto *attachmentVertices = getAttachmentVertices(slot->getAttachment());
         if (!attachmentVertices) return;
         if (createNew) {
             attachmentVertices = attachmentVertices->copy();
@@ -723,11 +729,7 @@ void SpineSkeletonInstance::resizeSlotRegion(const spine::String &slotName, uint
         mesh->updateRegion();
 #endif
 
-        auto *oldAttachment = slot->getAttachment();
-        AttachmentVertices *attachmentVertices = nullptr;
-        if (attachmentVerticesMap->containsKey(oldAttachment)) {
-            attachmentVertices = (*attachmentVerticesMap)[oldAttachment];
-        }
+        auto *attachmentVertices = getAttachmentVertices(slot->getAttachment());
         if (!attachmentVertices) return;
         if (createNew) {
             attachmentVertices = attachmentVertices->copy();
