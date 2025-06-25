e8a22d3d03c0d7fcd70a008faef90afdf00597bf
Zhou Zhenglong <zlzhou.sh@outlook.com>
2025-06-25 16:31:40 +0800

    update vulkan version

--- native/cocos/renderer/pipeline/custom/NativeExecutor.cpp
+++ native/cocos/renderer/pipeline/custom/NativeExecutor.cpp
@@ -564,7 +564,6 @@ struct RenderGraphVisitor : boost::dfs_visitor<> {
             copyInfo.srcSubres.layerCount = copy.numSlices;
 
             copyInfo.dstSubres.mipLevel = copy.targetMostDetailedMip + i;
-
             copyInfo.dstSubres.baseArrayLayer = copy.targetFirstSlice;
             copyInfo.dstSubres.layerCount = copy.numSlices;
 
