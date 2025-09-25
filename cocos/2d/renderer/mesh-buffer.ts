ac0d98f320a0b1bb7def9cb6b2688aad59d3412b
zhefeng.zhang <35944775+zhefengzhang@users.noreply.github.com>
2025-09-25 10:19:43 +0800

    Update mesh-buffer.ts

--- cocos/2d/renderer/mesh-buffer.ts
+++ cocos/2d/renderer/mesh-buffer.ts
@@ -25,7 +25,7 @@
 import { JSB } from 'internal:constants';
 import { Device, BufferUsageBit, MemoryUsageBit, Attribute, Buffer, BufferInfo, InputAssembler, InputAssemblerInfo, Feature, API } from '../../gfx';
 import { getAttributeStride } from './vertex-format';
-import { cclegacy, sys, getError, warnID, assertIsTrue } from '../../core';
+import { sys, getError, warnID, assertIsTrue } from '../../core';
 import { NativeUIMeshBuffer } from './native-2d';
 
 interface IIARef {
