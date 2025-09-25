942c9d62d1bcccaeb8f546e9ae974338bcf4057a
zhefeng.zhang <35944775+zhefengzhang@users.noreply.github.com>
2025-09-25 10:19:08 +0800

    Update mesh-buffer.ts

--- cocos/2d/renderer/mesh-buffer.ts
+++ cocos/2d/renderer/mesh-buffer.ts
@@ -252,10 +252,9 @@ export class MeshBuffer {
         this.floatsPerVertex = getAttributeStride(attrs) >> 2;
 
         var vDataCountLimit = 65536; // 2^16 - 1
-        const root = cclegacy.director.root;
-        if (root) {
-            const glApi = root.device.gfxAPI;
-            if (glApi === API.WEBGPU || glApi === API.WEBGL2 || glApi === API.WEBGL && root.device.hasFeature(Feature.ELEMENT_INDEX_UINT)) {
+        if (device) {
+            const glApi = device.gfxAPI;
+            if (glApi === API.WEBGPU || glApi === API.WEBGL2 || glApi === API.WEBGL && device.hasFeature(Feature.ELEMENT_INDEX_UINT)) {
                     vDataCountLimit = 4294967295; // 2^32 - 1
                 }
         }
