34b40c6e08ffda12ea204a545f358c66a4a9007b
tangkai <1944876319@qq.com>
2025-10-24 10:21:43 +0800

    optimize splitLongStringApproximatelyIn2048 codes.

--- cocos/2d/components/rich-text.ts
+++ cocos/2d/components/rich-text.ts
@@ -613,14 +613,8 @@ export class RichText extends Component {
         } else {
             const multilineTexts = text.split('\n');
             for (let i = 0; i < multilineTexts.length; i++) {
-                this._calculateSize(_tempSize, styleIndex, multilineTexts[i]);
-                if (_tempSize.x < 2048) {
-                    partStringArr.push(multilineTexts[i]);
-                } else {
-                    // _updateRichTextWithMaxWidth function will correct the text again.
-                    partStringArr.push(text);
-                    return partStringArr;
-                }
+                // _updateRichTextWithMaxWidth function will correct the text again.
+                partStringArr.push(multilineTexts[i]);
             }
         }
         return partStringArr;
