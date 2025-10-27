9316e67673648d51f4692183e0157e3e8ed51037
zhefeng.zhang <35944775+zhefengzhang@users.noreply.github.com>
2025-10-27 16:49:09 +0800

    Update particle-system.ts

--- cocos/particle/particle-system.ts
+++ cocos/particle/particle-system.ts
@@ -1443,7 +1443,8 @@ export class ParticleSystem extends ModelRenderer {
         // emit particles.
         const startDelay = self.startDelay.evaluate(0, 1)!;
         if (self._time > startDelay) {
-            if (self._time - (self.duration + startDelay) > dt) {
+            const timeLeft = self._time - (self.duration + startDelay);
+            if (timeLeft > dt) {
                 // self._time = startDelay; // delay will not be applied from the second loop.(Unity)
                 // self._emitRateTimeCounter = 0.0;
                 // self._emitRateDistanceCounter = 0.0;
@@ -1478,8 +1479,10 @@ export class ParticleSystem extends ModelRenderer {
             }
 
             // bursts
-            for (const burst of self.bursts) {
-                burst.update(self, dt);
+            if (timeLeft <= 0) {
+                for (const burst of self.bursts) {
+                    burst.update(self, dt);
+                }
             }
         }
     }
