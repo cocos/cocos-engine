b2c5c8cd48c28e49bd2c0be1ef1d0c8cb7e785e1
finscn <finscn@gmail.com>
2025-12-17 16:03:05 +0800

    简化 if 条件
    WEBGPU 和 WEBGL2 环境中   device.hasFeature(Feature.ELEMENT_INDEX_UINT) == true ,
    
    所以只用这一个条件判断就可以

--- cocos/2d/renderer/mesh-buffer.ts
+++ cocos/2d/renderer/mesh-buffer.ts
@@ -251,9 +251,8 @@ export class MeshBuffer {
 
         this.floatsPerVertex = getAttributeStride(attrs) >> 2;
 
-        let vDataCountLimit = macro.BATCHER2D_MEM_INCREMENT * 256; // 1024 / 4 = 256
-        const glApi = device.gfxAPI;
-        if (glApi !== API.WEBGPU && glApi !== API.WEBGL2 && !(glApi === API.WEBGL && device.hasFeature(Feature.ELEMENT_INDEX_UINT))) {
+        let vDataCountLimit = macro.BATCHER2D_MEM_INCREMENT * 256; // 256 = 1024 / 4
+        if (!device.hasFeature(Feature.ELEMENT_INDEX_UINT)) {
             vDataCountLimit = Math.min(vDataCountLimit, 65536); // 2^16
         }
         assertIsTrue(this._initVDataCount / this._floatsPerVertex < vDataCountLimit, getError(9005));
