3ce1237fd6349d9cf4728013eeead46799e44dbb
finscn <finscn@gmail.com>
2025-12-17 15:48:52 +0800

    更新计算 vDataCountLimit 的逻辑
    详见 #19104 中的讨论

--- cocos/2d/renderer/mesh-buffer.ts
+++ cocos/2d/renderer/mesh-buffer.ts
@@ -251,10 +251,10 @@ export class MeshBuffer {
 
         this.floatsPerVertex = getAttributeStride(attrs) >> 2;
 
-        var vDataCountLimit = 65536; // 2^16 - 1
+        let vDataCountLimit = macro.BATCHER2D_MEM_INCREMENT * 256;  // 1024 / 4 = 256
         const glApi = device.gfxAPI;
-        if (glApi === API.WEBGPU || glApi === API.WEBGL2 || glApi === API.WEBGL && device.hasFeature(Feature.ELEMENT_INDEX_UINT)) {
-            vDataCountLimit = 4294967295; // 2^32 - 1
+        if (glApi !== API.WEBGPU && glApi !== API.WEBGL2 && !(glApi === API.WEBGL && device.hasFeature(Feature.ELEMENT_INDEX_UINT))) {
+            vDataCountLimit = Math.min(vDataCountLimit, 65536);
         }
         assertIsTrue(this._initVDataCount / this._floatsPerVertex < vDataCountLimit, getError(9005));
 
