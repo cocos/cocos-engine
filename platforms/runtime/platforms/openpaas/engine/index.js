4137b55248850aa2a33c6e81b3ac947b46be6dfa
qiuguohua <qiuguohua111@126.com>
2026-09-10 15:31:03 +0800

    Remove the platform adaptation for openpass.

--- platforms/runtime/platforms/openpaas/engine/index.js
+++
@@ -1,5 +0,0 @@
-require('../../../common/engine/fs-utils');
-require('../../../common/engine/canvas');
-require('./asset-manager.js');
-require('../../../common/engine/EditBox.js');
-require('../../../common/engine/game.js');
