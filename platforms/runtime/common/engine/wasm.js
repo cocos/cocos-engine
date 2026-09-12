9908900209ce936ffa0a2758bfcddf65ad976074
qiuguohua <qiuguohua111@126.com>
2026-09-12 10:58:39 +0800

    chore: privatize pal and platforms

--- platforms/runtime/common/engine/wasm.js
+++
@@ -1,4 +0,0 @@
-if(typeof ral.WebAssembly === 'object' && typeof ral.WebAssembly.instantiateFromFile === 'function'){
-    ral.WebAssembly.instantiate = ral.WebAssembly.instantiateFromFile;
-    window.CCWebAssembly = ral.WebAssembly;
-}
