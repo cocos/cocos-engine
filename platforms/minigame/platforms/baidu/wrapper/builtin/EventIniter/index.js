876cecc2de236adca7f9bcfff6863ba1306868ee
qiuguohua <qiuguohua111@126.com>
2026-09-24 13:44:14 +0800

    feat(openpaas): configure engine overrides and npm patches for v3.7.4+patch.1

--- platforms/minigame/platforms/baidu/wrapper/builtin/EventIniter/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-export { default as TouchEvent } from './TouchEvent'
-export { default as PointerEvent } from './PointerEvent'
-export { default as MouseEvent } from './MouseEvent'
